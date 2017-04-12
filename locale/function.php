1f1eed530e71c5169f2c41f15078c3849c37ea32
daniel <daniel@daniel-DC1A-S>
2017-04-11 22:53:45 -0300

    Fix Language problem

--- locale/function.php
+++ locale/function.php
@@ -1,9 +1,12 @@
 <?php
+
+require_once $global['systemRootPath'] . 'objects/configuration.php';
+$config = new Configuration();
 if(empty($_SESSION['language'])){
-    $_SESSION['language'] = $global['language'];
+    $_SESSION['language'] = $config->getLanguage();
 }
 if(!empty($_GET['lang'])){
-    $_SESSION['language'] = $global['language'] = $_GET['lang'];
+    $_SESSION['language'] = $config->getLanguage() = $_GET['lang'];
 }
 @include_once "{$global['systemRootPath']}locale/{$_SESSION['language']}.php";
 function __($str){
