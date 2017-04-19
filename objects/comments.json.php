83b67c7d6613476caf003e66c721919892868984
daniel <daniel@daniel-DC1A-S>
2017-04-19 15:48:20 -0300

    Better comments

--- objects/comments.json.php
+++ objects/comments.json.php
@@ -8,7 +8,7 @@ $total = Comment::getTotalComments($_GET['video_id']);
 foreach ($categories as $key => $value) {
     $name = empty($value['name'])?substr($value['user'], 0,5)."...":$value['name'];
     //$categories[$key]['comment'] = " <div class=\"commenterName\"><strong>{$name}</strong><div class=\"date sub-text\">{$value['created']}</div></div><div class=\"commentText\">". nl2br($value['comment'])."</div>";
-    $categories[$key]['comment'] = '<div class="pull-left"><img src="'.User::getPhoto($value['id']).'" alt="" class="img img-responsive img-circle" style="max-width: 50px;"/></div><div class="commentDetails"><div class="commenterName"><strong>'.$name.'</strong> <small>'.humanTiming(strtotime($value['created'])).'</small></div>'. nl2br($value['comment']).'</div>';
+    $categories[$key]['comment'] = '<div class="pull-left"><img src="'.User::getPhoto($value['users_id']).'" alt="" class="img img-responsive img-circle" style="max-width: 50px;"/></div><div class="commentDetails"><div class="commenterName"><strong>'.$name.'</strong> <small>'.humanTiming(strtotime($value['created'])).'</small></div>'. nl2br($value['comment']).'</div>';
 }
 
 
