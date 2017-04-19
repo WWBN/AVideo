4a08b20c2773d0a6cfde8da638480509910bbbf2
daniel <daniel@daniel-DC1A-S>
2017-04-18 23:15:26 -0300

    Download progress

--- objects/downloadVideoProgress.php
+++ objects/downloadVideoProgress.php
@@ -1,10 +1,12 @@
 <?php
+header('Content-Type: application/json');
 require_once '../videos/configuration.php';
 require_once $global['systemRootPath'] . 'objects/user.php';
 $progressFilename = "{$global['systemRootPath']}videos/downloaded/{$_GET['filename']}_downloadProgress.txt";
 $content = file_get_contents($progressFilename);
 preg_match_all('/\[download\] +([0-9.]+)% of/', $content, $matches, PREG_SET_ORDER);
-echo "<pre>";
-print_r($matches);
-print_r(end($matches));
+$m = end($matches);
+$obj = new stdClass();
+$obj->progress = empty($m[1])?0:intval($m[1]);
+echo json_encode($obj);
 exit;
