15ac9fe3a965523257420812a6d510ff7ba705d6
daniel <daniel@daniel-DC1A-S>
2017-04-22 16:10:08 -0300

    Donload external videos better look on mobile

--- view/downloadExternalVideo.php
+++ view/downloadExternalVideo.php
@@ -46,8 +46,8 @@ function isYoutubeDl() {
             }else{
             ?>
             <div class="row">
-                <div class="col-xs-6 col-sm-4 col-lg-3"></div>
-                <div class="col-xs-6 col-sm-4 col-lg-6">
+                <div class="col-xs-1 col-sm-1 col-lg-2"></div>
+                <div class="col-xs-10 col-sm-10 col-lg-8">
                     <form class="form-compact well form-horizontal"  id="updateUserForm" onsubmit="">
                         <fieldset>
                             <legend><?php echo __("Download Video"); ?></legend>
@@ -85,7 +85,7 @@ function isYoutubeDl() {
                         <div id="downloadProgress" class="progress-bar" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0px"></div>
                     </div>
                 </div>
-                <div class="col-xs-6 col-sm-4 col-lg-3"></div>
+                <div class="col-xs-1 col-sm-1 col-lg-2"></div>
             </div>
             <script>
                 $(document).ready(function () {
