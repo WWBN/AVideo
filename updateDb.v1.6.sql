e4412e7349647d08c637980a13b9efe78aefca9a
daniel <daniel@daniel-DC1A-S>
2017-04-24 11:16:42 -0300

    New Version Video partial error on encode

--- updateDb.v1.6.sql
+++ updateDb.v1.6.sql
@@ -9,16 +9,16 @@ SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
-ALTER TABLE `youPHPTube`.`videos` 
+ALTER TABLE `videos` 
 CHANGE COLUMN `status` `status` ENUM('a', 'i', 'e', 'x', 'd', 'xmp4', 'xwebm', 'xmp3', 'xogg', 'ximg') NOT NULL DEFAULT 'e' COMMENT 'a = active\ni = inactive\ne = encoding\nx = encoding error\nd = downloading\nxmp4 = encoding mp4 error \nxwebm = encoding webm error \nxmp3 = encoding mp3 error \nxogg = encoding ogg error \nximg = get image error' ,
 ADD COLUMN `videoDownloadedLink` VARCHAR(255) NULL DEFAULT NULL AFTER `type`;
 
-ALTER TABLE `youPHPTube`.`categories` 
+ALTER TABLE `categories` 
 CHANGE COLUMN `created` `created` DATETIME NOT NULL ,
 CHANGE COLUMN `modified` `modified` DATETIME NOT NULL ,
 ADD COLUMN `iconClass` VARCHAR(45) NOT NULL DEFAULT 'fa fa-folder' AFTER `modified`;
 
-ALTER TABLE `youPHPTube`.`configurations` 
+ALTER TABLE `configurations` 
 CHANGE COLUMN `modified` `modified` DATETIME NOT NULL ,
 CHANGE COLUMN `created` `created` DATETIME NOT NULL ;
 
