1d3441da48ad513d87c63352dad9a2b4e2ffa587
daniel <daniel@daniel-DC1A-S>
2017-04-24 10:16:30 -0300

    Better audio support

--- view/include/audio.php
+++ view/include/audio.php
@@ -1,24 +1,12 @@
 <div class="row main-video" style="padding: 10px;">
     <div class="col-xs-12 col-sm-12 col-lg-2"></div>
     <div class="col-xs-12 col-sm-12 col-lg-8 ">
-        <audio controls class="center-block video-js"  id="mainAudio"   data-setup="{}">
+        <audio controls class="center-block video-js"  id="mainAudio" autoplay   data-setup="{}"
+               poster="<?php echo $global['webSiteRootURL']; ?>img/recorder.gif">
             <source src="<?php echo $global['webSiteRootURL']; ?>videos/<?php echo $video['filename']; ?>.ogg" type="audio/ogg" />
             <source src="<?php echo $global['webSiteRootURL']; ?>videos/<?php echo $video['filename']; ?>.mp3" type="audio/mpeg" />
             <a href="<?php echo $global['webSiteRootURL']; ?>videos/<?php echo $video['filename']; ?>.mp3">horse</a>
         </audio>
-        <script>
-            var playCount = 0;
-            $('#mainAudio').bind('play', function (e) {
-                playCount++;
-                if (playCount == 1) {
-                    $.ajax({
-                        url: '<?php echo $global['webSiteRootURL']; ?>addViewCountVideo',
-                        method: 'post',
-                        data: {'id': "<?php echo $video['id']; ?>"}
-                    });
-                }
-            });
-        </script>
     </div> 
 
     <div class="col-xs-12 col-sm-12 col-lg-2"></div>
