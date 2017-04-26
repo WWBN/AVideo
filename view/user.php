3248780de521b5da59b9eef31c82c4eccca8a18c
daniel <daniel@daniel-DC1A-S>
2017-04-26 20:08:18 -0300

    Fix user logged layout in mobile

--- view/user.php
+++ view/user.php
@@ -130,8 +130,8 @@ $config = new Configuration();
             } else {
                 ?>
                 <div class="row">
-                    <div class="col-xs-1 col-sm-1 col-lg-2"></div>
-                    <div class="col-xs-10 col-sm-10 col-lg-8">
+                    <div class="col-xs-1 col-sm-2 col-lg-4"></div>
+                    <div class="col-xs-10 col-sm-8 col-lg-4">
                         <form class="form-compact well form-horizontal"  id="loginForm">
                             <fieldset>
                                 <legend><?php echo __("Please sign in"); ?></legend>
@@ -195,7 +195,7 @@ $config = new Configuration();
                         </form>
 
                     </div>
-                    <div class="col-xs-1 col-sm-1 col-lg-2"></div>
+                    <div class="col-xs-1 col-sm-2 col-lg-4"></div>
                 </div>
                 <script>
                     $(document).ready(function () {
