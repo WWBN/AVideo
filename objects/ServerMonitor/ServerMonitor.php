b6fffb37143a1ee5957325f7b6576c3d4f0d2f12
daniel <daniel@daniel-DC1A-S>
2017-04-26 15:27:31 -0300

    Fix the memory

--- objects/ServerMonitor/ServerMonitor.php
+++ objects/ServerMonitor/ServerMonitor.php
@@ -29,7 +29,7 @@ class ServerMonitor {
 
     static function getMemory() {
         $obj = new stdClass();
-        $cmd = "cat /proc/meminfo";
+        $cmd = "free";
         exec($cmd . "  2>&1", $output, $return_val);
         if ($return_val !== 0) {
             $obj->error = "Get Memmory ERROR** " . print_r($output, true);
@@ -39,32 +39,19 @@ class ServerMonitor {
             $obj->success = 1;
             $obj->output = $output;
             $obj->command = $cmd;
-            $obj->memTotal = 0;
-            $obj->memFree = 0;
-            $obj->memAvailable = 0;
-            foreach ($output as $value) {
-                if (preg_match("/MemTotal: *([0-9]+) *([a-z]+) */i", $value, $match)) {
-                    $obj->memTotalBytes = $match[1] * 1024;
-                    $obj->memTotal = self::humanFileSize($obj->memTotalBytes);
-                    continue;
-                }
-                if (preg_match("/MemFree: *([0-9]+) *([a-z]+) */i", $value, $match)) {
-                    $obj->memFreeBytes = $match[1] * 1024;
-                    $obj->memFree = self::humanFileSize($obj->memFreeBytes);
-                    continue;
-                }
-                if (preg_match("/MemAvailable: *([0-9]+) *([a-z]+) */i", $value, $match)) {
-                    $obj->memAvailableBytes = $match[1] * 1024;
-                    $obj->memAvailable = self::humanFileSize($obj->memAvailableBytes);
-                    continue;
-                }
-                if (!empty($obj->memTotal) && !empty($obj->memFree) && !empty($obj->memAvailable)) {
-                    $onePc = $obj->memTotalBytes / 100;
-                    $obj->usedBytes = $obj->memTotalBytes - ($obj->memFreeBytes + $obj->memAvailableBytes);
-                    $obj->used = self::humanFileSize($obj->usedBytes);
-                    $obj->percent = $obj->usedBytes / $onePc;
-                    $obj->title = "Total: {$obj->memTotal} | Free: {$obj->memFree} | Available: {$obj->memAvailable} | Used: {$obj->used}";
-                }
+            $obj->memTotalBytes = 0;
+            $obj->memUsedBytes = 0;
+            $obj->memFreeBytes = 0;
+            if (preg_match("/Mem: *([0-9]+) *([0-9]+) *([0-9]+) */i", $output[1], $match)) {
+                $obj->memTotalBytes = $match[1];
+                $obj->memUsedBytes = $match[2];
+                $obj->memFreeBytes = $match[3];
+                $onePc = $obj->memTotalBytes / 100;
+                $obj->memTotal = self::humanFileSize($obj->memTotalBytes);
+                $obj->memUsed = self::humanFileSize($obj->memUsedBytes);
+                $obj->memFree = self::humanFileSize($obj->memFreeBytes);
+                $obj->percent = $obj->memUsedBytes / $onePc;
+                $obj->title = "Total: {$obj->memTotal} | Free: {$obj->memFree} | Used: {$obj->memUsed}";
             }
         }
         return $obj;
