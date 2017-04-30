7d66669269a9f9bdb72f4a9a77c3bc9b7df6314c
daniel <daniel@daniel-DC1A-S>
2017-04-29 23:19:00 -0300

    Do not stop of you can not get video title

--- objects/downloadVideo.php
+++ objects/downloadVideo.php
@@ -19,23 +19,24 @@ $cmd = "youtube-dl -e {$_POST['videoURL']}";
 exec($cmd . "  2>&1", $output, $return_val);
 if ($return_val !== 0) {
     $obj->error = "youtube-dl get title ERROR** ". print_r($output, true);
-    $obj->type = "error";
+    $obj->type = "warning";
     $obj->title = __("Sorry!");
-    $obj->text = sprintf(__("We could not get your video (%s)"), $output[0]);
+    $obj->text = sprintf(__("We could not get the title of your video (%s)"), $output[0]);
     $obj->command = $cmd;
-    die(json_encode($obj));
-} else {
-    $title = end($output);
-    $filename = preg_replace("/[^A-Za-z0-9]+/", "_", $title);
-    $filename = uniqid("{$filename}_", true).".mp4";
-    $cmd = "/usr/bin/php -f youtubeDl.php {$filename} {$_POST['videoURL']} {$userId} > /dev/null 2>/dev/null &";
-    exec($cmd);
+    $output[] = "Update Youtube-dl";
+    //die(json_encode($obj));
+}else{
     $obj->type = "success";
     $obj->title = __("Congratulations!");
     $obj->text = sprintf(__("Your video (%s) is downloading"), $title);
-    $obj->command = $cmd;
-    $obj->filename = $filename;
-    $obj->response = print_r($output, true);
-    die(json_encode($obj));
 }
+$title = end($output);
+$filename = preg_replace("/[^A-Za-z0-9]+/", "_", $title);
+$filename = uniqid("{$filename}_", true).".mp4";
+$cmd = "/usr/bin/php -f youtubeDl.php {$filename} {$_POST['videoURL']} {$userId} > /dev/null 2>/dev/null &";
+exec($cmd);
+$obj->command = $cmd;
+$obj->filename = $filename;
+$obj->response = print_r($output, true);
+die(json_encode($obj));
 
