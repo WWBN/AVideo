8e9318cd5fdac2a53323c1af13c7ba42926db520
daniel <daniel@daniel-DC1A-S>
2017-05-09 09:05:28 -0300

    Footer Fix

--- view/include/footer.php
+++ view/include/footer.php
@@ -4,18 +4,37 @@ $config = new Configuration();
 ?>
 <hr>
 <footer>
-    <p>Powered by <a href="http://www.youphptube.com" class="external btn btn-outline btn-primary" target="_blank">YouPHPTube v<?php echo $config->getVersion(); ?></a> &copy; 2017, All rights reserved</p>
+    <ul class="list-inline">
+        <li>
+            Powered by <a href="http://www.youphptube.com" class="external btn btn-outline btn-primary" target="_blank">YouPHPTube v<?php echo $config->getVersion(); ?></a>
+        </li>
+        <li>
+            <a href="https://www.facebook.com/mediasharingtube/" class="external btn btn-outline btn-primary" target="_blank"><span class="sr-only">Facebook</span><i class="fa fa-fw fa-facebook"></i></a>
+        </li>
+        <li>
+            <a href="https://plus.google.com/u/0/113820501552689289262" class="external btn btn-outline btn-primary" target="_blank"><span class="sr-only">Google Plus</span><i class="fa fa-fw fa-google-plus"></i></a>
+        </li>
+    </ul>
 </footer>
-<script>
-$(function() {
-    <?php 
-    if(!empty($_GET['error'])){
-    ?>
-    swal({title:"Sorry!", text: "<?php echo $_GET['error']; ?>", type:"error",  html:true});
-    <?php
+<script type="application/ld+json">
+    {
+    "@context": "http://schema.org/",
+    "@type": "Product",
+    "name": "YouPHPTube",
+    "image": "http://youphptube.com/img/logo.png",
+    "description": "Free web solution to build your own video sahring site."
     }
+</script>
+<script>
+    $(function () {
+<?php
+if (!empty($_GET['error'])) {
     ?>
-});
+            swal({title: "Sorry!", text: "<?php echo $_GET['error']; ?>", type: "error", html: true});
+    <?php
+}
+?>
+    });
 </script>
 <script src="<?php echo $global['webSiteRootURL']; ?>bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
 <script src="<?php echo $global['webSiteRootURL']; ?>js/seetalert/sweetalert.min.js" type="text/javascript"></script>
