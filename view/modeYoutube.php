3f054d80786c3de83e9a80346edebd2376edeabc
daniel <daniel@daniel-DC1A-S>
2017-05-13 18:05:10 -0300

    Video Ads

--- view/modeYoutube.php
+++ view/modeYoutube.php
@@ -85,8 +85,8 @@ $config = new Configuration();
                     require "{$global['systemRootPath']}view/include/{$video['type']}.php";
                     ?>
                     <div class="row">
-                        <div class="col-xs-12 col-sm-12 col-md-1 col-lg-1"></div>
-                        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 ">
+                        <div class="col-xs-12 col-sm-1 col-md-1 col-lg-1"></div>
+                        <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 ">
                             <div class="row bgWhite">
                                 <div class="row divMainVideo">
                                     <div class="col-xs-4 col-sm-4 col-lg-4">
@@ -465,7 +465,7 @@ $config = new Configuration();
                             </div>
 
                         </div>
-                        <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 bgWhite">
+                        <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4 bgWhite">
                             <div class="col-lg-12 col-sm-12 col-xs-12">
                                 <?php
                                 echo $config->getAdsense();
@@ -541,7 +541,7 @@ $config = new Configuration();
                             </script>
                         </div>
 
-                        <div class="col-xs-12 col-sm-12 col-md-1 col-lg-1"></div>
+                        <div class="col-xs-12 col-sm-1 col-md-1 col-lg-1"></div>
                     </div>
                     <?php
                 } else {
