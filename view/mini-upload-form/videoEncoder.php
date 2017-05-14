4afbc13f480023a0a968136b5aae474358bb245f
daniel <daniel@daniel-DC1A-S>
2017-05-14 17:27:30 -0300

    Spectrum with FFMPEG

--- view/mini-upload-form/videoEncoder.php
+++ view/mini-upload-form/videoEncoder.php
@@ -55,13 +55,14 @@ if ($type == 'audio' || $type == 'mp3' || $type == 'ogg') {
         } else {
             echo "\n {$key} Ok\n";
             if ($type == 'mp3') {
+                echo "Try FFMPEG Spectrum\n";
                 $cmd = "ffmpeg -i {$pathFileName} -filter_complex \"[0:a]showwaves=s=1920x1080:mode=line,format=yuv420p[v]\" -map \"[v]\" -map 0:a -c:v libx264 -c:a copy {$pathFileName}_spectrum.mp4";
                 exec($cmd . "  1> {$global['systemRootPath']}videos/{$filename}_progress_spectrum.txt  2>&1", $output, $return_val);
                 if ($return_val !== 0) {
                     echo "\\n **Spectrum ERROR**\n", print_r($output, true);
                     error_log($cmd . "\n" . print_r($output, true));
                 } else {
-                    
+                    echo "FFMPEG Spectrum Success\n";
                 }
             }
         }
