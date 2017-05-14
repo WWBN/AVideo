654ac0c1963bff67db836083a432283ffdc9202c
daniel <daniel@daniel-DC1A-S>
2017-05-14 11:52:03 -0300

    Remove lines of audio, looks like dont need

--- view/include/audio.php
+++ view/include/audio.php
@@ -4,8 +4,6 @@
     <div class="col-xs-12 col-sm-12 col-lg-2"></div>
     <div class="col-xs-12 col-sm-12 col-lg-8 ">
         <audio controls class="center-block video-js vjs-default-skin "  id="mainAudio" autoplay >
-            <source src="<?php echo $global['webSiteRootURL']; ?>videos/<?php echo $video['filename']; ?>.ogg" type="audio/ogg" />
-            <source src="<?php echo $global['webSiteRootURL']; ?>videos/<?php echo $video['filename']; ?>.mp3" type="audio/mpeg" />
             <a href="<?php echo $global['webSiteRootURL']; ?>videos/<?php echo $video['filename']; ?>.mp3">horse</a>
         </audio>
     </div> 
