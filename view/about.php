6c3005d6daf418254b0ecdc7b8e936e7e9b76de2
sfrancis2016 <sajiv.francis@siffinc.com>
2017-05-30 18:44:19 -0400

    Merge origin/master
    Conflicts:
    	sitemap.xml
    	view/include/footer.php

--- view/about.php
+++ view/about.php
@@ -21,9 +21,9 @@ $config = new Configuration();
             <div class="bgWhite">
                 <div class="jumbotron">
                 <h1><?php echo __("About Sfrancis.ca!"); ?></h1>
-                <?php echo __("<p>This website has both a private and public collection of videos discussing the latest information technology, accounting procedures and tax regulations applicable to businesses.  Authorised users can upload, view, share and comment on videos. We are currently on beta mode and the final version will be released shortly. We hope you enjoy the useful content available on this site and please feel free to give us your input and sugestions. </p>"); ?>
+                <?php echo __("<p>This website has both a private and public collection of videos discussing the latest information technology, project management, accounting procedures and tax regulations applicable to businesses.  Authorised users can upload, view, share and comment on videos. We are currently on beta mode and the final version will be released shortly. We hope you enjoy the useful content available on this site and please feel free to give us your input and sugestions. </p>"); ?>
                 </div>
-                <div class="alert alert-success"><?php printf(__("Business - Information Technology, Accounting & Tax"), $config->getVersion()); ?></div>
+                <div class="alert alert-success"><?php printf(__("Business - Information Technology, Project Management, Accounting & Tax"), $config->getVersion()); ?></div>
             </div>
 
         </div><!--/.container-->
