1c2f1a8ae310250abdf53f2758c737ed60aeebd7
daniel <daniel@daniel-DC1A-S>
2017-06-01 16:13:51 -0300

    Options to enable and disable functions thumbs for ads manager temporary thumbs when it is not done session time out encode mpeg and webm option encode mp3 and spectrum option

--- view/index.php
+++ view/index.php
@@ -1,12 +1,16 @@
 <?php
-if (!file_exists('../videos/configuration.php')) {
+$configFile = '../videos/configuration.php';
+if (!file_exists($configFile)) {
     if (!file_exists('../install/index.php')) {
         die("No Configuration and no Installation");
     }
     header("Location: install/index.php");
 }
-require_once '../videos/configuration.php';
+require_once $configFile;
 if(empty($config)){
+    // update config file for version 2.8
+    $txt = 'require_once $global[\'systemRootPath\'].\'objects/include_config.php\';';
+    $myfile = file_put_contents($configFile, $txt.PHP_EOL , FILE_APPEND | LOCK_EX);
     require_once '../objects/include_config.php';
 }
 $mode = $config->getMode();
