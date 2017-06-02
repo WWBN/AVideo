02e0344f5b205808acc1571f4ca57db7e2c064d0
sfrancis2016 <sajiv.francis@siffinc.com>
2017-06-02 14:21:29 -0400

    Update 06/02/2017

--- view/include/head.php
+++ view/include/head.php
@@ -5,7 +5,7 @@ $config = new Configuration();
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<meta name="description" content="Sfrancis">
+<meta name="description" content="Sfrancis - Professional Tube">
 <meta name="author" content="Daniel Neto">
 <link rel="icon" href="<?php echo $global['webSiteRootURL']; ?>img/favicon.png">
 <link href="<?php echo $global['webSiteRootURL']; ?>bootstrap/css/bootstrap.css" rel="stylesheet" type="text/css"/>
