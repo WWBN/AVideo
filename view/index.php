02e8c8e00f38245b9db35f1c845a00cdb9a48be2
daniel <daniel@daniel-DC1A-S>
2017-06-28 14:57:50 -0300

    There is a player for play list now

--- view/index.php
+++ view/index.php
@@ -1,4 +1,5 @@
 <?php
+
 $configFile = '../videos/configuration.php';
 if (!file_exists($configFile)) {
     if (!file_exists('../install/index.php')) {
@@ -7,17 +8,17 @@ if (!file_exists($configFile)) {
     header("Location: install/index.php");
 }
 require_once $configFile;
-if(empty($config)){
+if (empty($config)) {
     // update config file for version 2.8
     $txt = 'require_once $global[\'systemRootPath\'].\'objects/include_config.php\';';
-    $myfile = file_put_contents($configFile, $txt.PHP_EOL , FILE_APPEND | LOCK_EX);
+    $myfile = file_put_contents($configFile, $txt . PHP_EOL, FILE_APPEND | LOCK_EX);
     require_once '../objects/include_config.php';
 }
 $mode = $config->getMode();
-if(!empty($_GET['videoName'])){
+if (!empty($_GET['videoName']) || !empty($_GET['playlist_id'])) {
     $mode = "Youtube";
 }
-if(!empty($_GET['search'])){
+if (!empty($_GET['search'])) {
     $mode = "Gallery";
 }
-require 'mode'.$mode.".php";
+require 'mode' . $mode . ".php";
