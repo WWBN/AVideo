1c4e60bf022d2abbcbf6f9cb7ff8eeb6cbe0bc59
daniel <daniel@daniel-DC1A-S>
2017-09-26 13:14:03 -0300

    Google ad on live view

--- plugin/Live/view/modeYoutubeLive.php
+++ plugin/Live/view/modeYoutubeLive.php
@@ -35,7 +35,13 @@ $video['creator'] = '<div class="pull-left"><img src="' . User::getPhoto($user_i
         $lt = new LiveTransmition($t['id']);
         if($lt->userCanSeeTransmition()){
         ?>
+        
         <div class="container-fluid principalContainer " itemscope itemtype="http://schema.org/VideoObject">
+            <div class="col-md-12">
+                    <?php
+                    echo $config->getAdsense();
+                    ?>
+            </div>
             <div class="col-md-12">
                 <?php
                 require "{$global['systemRootPath']}plugin/Live/view/liveVideo.php";
