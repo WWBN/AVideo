96f9235ee8fc7b15fa19b851249ca716139b55c8
daniel <daniel@daniel-DC1A-S>
2017-09-26 13:09:05 -0300

    Google ad on live view

--- plugin/Live/view/modeYoutubeLive.php
+++ plugin/Live/view/modeYoutubeLive.php
@@ -42,14 +42,19 @@ $video['creator'] = '<div class="pull-left"><img src="' . User::getPhoto($user_i
                 ?>
             </div>  
         </div>
-        <div class="container ">
-            <div class="col-md-8 col-md-offset-2 list-group-item">
+        <div class="container-fluid ">
+            <div class="col-md-6 col-md-offset-2 list-group-item">
                 <h1 itemprop="name">
                     <i class="fa fa-video-camera"></i> <?php echo $t['title']; ?>
                 </h1>
                 <p><?php echo nl2br(textToLink($t['description'])); ?></p>
                 <div class="col-xs-12 col-sm-12 col-lg-12"><?php echo $video['creator']; ?></div>
             </div> 
+            <div class="col-md-2">
+                    <?php
+                    echo $config->getAdsense();
+                    ?>
+            </div>
         </div>
         <?php
         }else{
