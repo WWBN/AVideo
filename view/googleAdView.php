625ffaf43ba930d2040784cf5b195d72b1aadc7f
daniel <daniel@daniel-DC1A-S>
2017-09-27 11:43:59 -0300

    Google Ad

--- view/googleAdView.php
+++ view/googleAdView.php
@@ -1,5 +1,17 @@
-<?php
-require_once '../videos/configuration.php';
-echo $config->getAdsense();
-?>
-
+<html>
+    <head>
+        <style>
+            html, body{
+                height: 100%;
+                margin: 0;
+                padding: 0;
+            }
+        </style>
+    </head>
+    <body>
+        <?php
+        require_once '../videos/configuration.php';
+        echo $config->getAdsense();
+        ?>
+    </body>
+</html>
