5bcf95516a632e631a657e79c81174b43c345b4f
daniel <daniel@daniel-DC1A-S>
2017-10-14 15:45:08 -0300

    Hide Live Stream URL, making it valid for 10 seconds

--- plugin/Live/playURL.php
+++ plugin/Live/playURL.php
@@ -3,7 +3,15 @@ require_once '../../videos/configuration.php';
 require_once $global['systemRootPath'] . 'plugin/Live/Objects/LiveTransmition.php';
 $t = LiveTransmition::getValidPlayURL($_GET['playURL']);
 if ($t){
-   header('location: '.$t);
+    header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
+    header("Cache-Control: no-store, no-cache,must-revalidate");
+    header("Cache-Control: post-check=0, pre-check=0",false);
+    header("Pragma: no-cache");
+    header('Content-type:application/force-download');
+    header('Content-Disposition: attachment; filename=YouPHPTube_Stream');
+    header('Content-Length: ' . filesize($t));
+    header('Content-Type: '.mime_content_type($t));
+   echo file_get_contents($t);
 }else{
     die("Not allow to play this stream on {$_SERVER['HTTP_HOST']}");
 }
