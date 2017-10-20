1e7eb03fbec79ca557584446564f1deed27bb5cc
daniel <daniel@daniel-DC1A-S>
2017-10-19 22:36:53 -0300

    Add tooltip

--- plugin/Live/view/onlineUsers.php
+++ plugin/Live/view/onlineUsers.php
@@ -6,5 +6,5 @@ if(!YouPHPTubePlugin::isEnabled("cf145581-7d5e-4bb6-8c12-48fc37c0630d")){
     return false;
 }
 ?>
-<span class="label label-primary"><i class="fa fa-user"></i> <b class="liveUsersOnline_<?php echo $streamName; ?>">0</b></span>
-<span class="label label-default"><i class="fa fa-eye"></i> <b class="liveUsersViews_<?php echo $streamName; ?>">0</b></span>
+<span class="label label-primary"   data-toggle="tooltip" title="<?php echo __("Watching Now"); ?>" data-placement="bottom" ><i class="fa fa-user"></i> <b class="liveUsersOnline_<?php echo $streamName; ?>">0</b></span>
+<span class="label label-default"   data-toggle="tooltip" title="<?php echo __("Total Views"); ?>" data-placement="bottom" ><i class="fa fa-eye"></i> <b class="liveUsersViews_<?php echo $streamName; ?>">0</b></span>
