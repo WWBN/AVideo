1be741c840a9c42a8bada95571cce709b3a6d590
daniel <daniel@daniel-DC1A-S>
2017-10-19 22:34:37 -0300

    Add tooltip

--- plugin/Live/view/menuRight.php
+++ plugin/Live/view/menuRight.php
@@ -62,8 +62,8 @@ if (User::canStream()) {
             // the live users plugin
             if (YouPHPTubePlugin::isEnabled("cf145581-7d5e-4bb6-8c12-48fc37c0630d")) {
                 ?>
-            <span class="label label-primary"><i class="fa fa-user"></i> <b class="liveUsersOnline">0</b></span>
-                <span class="label label-default"><i class="fa fa-eye"></i> <b class="liveUsersViews">0</b></span>
+                <span class="label label-primary"  data-toggle="tooltip" title="<?php echo __("Watching Now"); ?>" data-placement="bottom" ><i class="fa fa-user"></i> <b class="liveUsersOnline">0</b></span>
+                <span class="label label-default"  data-toggle="tooltip" title="<?php echo __("Total Views"); ?>" data-placement="bottom" ><i class="fa fa-eye"></i> <b class="liveUsersViews">0</b></span>
                 <?php
             }
             ?>
