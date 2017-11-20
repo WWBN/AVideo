3e3b9feeb18bcf130fe5c4769f06b51d32817192
daniel <daniel@daniel-DC1A-S>
2017-11-20 11:41:05 -0300

    Select All videos from videos manager, Bulk Upload YouTube and Bulk Delete

--- objects/videoDelete.json.php
+++ objects/videoDelete.json.php
@@ -9,5 +9,17 @@ if (!User::canUpload() || empty($_POST['id'])) {
     die('{"error":"'.__("Permission denied").'"}');
 }
 require_once 'video.php';
-$obj = new Video("", "", $_POST['id']);
-echo '{"status":"'.$obj->delete().'"}';
+if (!is_array($_POST['id'])) {
+    $_POST['id'] = array($_POST['id']);
+}
+$id = 0;
+foreach ($_POST['id'] as $value) {    
+    $obj = new Video("", "", $value);
+    if (!$obj->userCanManageVideo()) {
+        $obj->msg = __("You can not Manage This Video");
+        die(json_encode($obj));
+    }
+    $id = $obj->delete();
+}
+
+echo '{"status":"'.$id.'"}';
