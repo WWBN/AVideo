61ddf89b80495b6a771bab06eb237a52709dc573
daniel <daniel@daniel-DC1A-S>
2017-11-22 16:39:55 -0300

    Making Playlist sortable

---
+++ objects/playlistSort.php
@@ -0,0 +1,21 @@
+<?php
+header('Content-Type: application/json');
+if (empty($global['systemRootPath'])) {
+    $global['systemRootPath'] = '../';
+}
+require_once $global['systemRootPath'] . 'videos/configuration.php';
+require_once $global['systemRootPath'] . 'objects/user.php';
+require_once $global['systemRootPath'] . 'objects/playlist.php';
+if (!User::isLogged()) {
+    die('{"error":"'.__("Permission denied").'"}');
+}
+$obj = new PlayList($_POST['playlist_id']);
+if(User::getId() != $obj->getUsers_id()){
+    die('{"error":"'.__("Permission denied").'"}');
+}
+
+foreach ($_POST['list'] as $key => $value) {
+    $result = $obj->addVideo($value, true, $key);
+}
+
+echo '{"status":"'.$result.'"}';
