8c2559d1cb387a94525493a7687d40d675c9d303
daniel <daniel@daniel-DC1A-S>
2017-11-28 14:08:10 -0300

    Securing Directory

---
+++ view/xsendfile.php
@@ -0,0 +1,25 @@
+<?php
+
+require_once dirname(__FILE__) . '/../videos/configuration.php';
+require_once $global['systemRootPath'] . 'plugin/YouPHPTubePlugin.php';
+
+if (empty($_GET['file'])) {
+    die('GET file not found');
+}
+
+$p = YouPHPTubePlugin::loadPluginIfEnabled("SecureVideosDirectory");
+
+
+
+$path_parts = pathinfo($_GET['file']);
+$file = $path_parts['basename'];
+$path = "{$global['systemRootPath']}videos/{$file}";
+
+if ($p) {
+    $p->secure();
+}
+
+header("X-Sendfile: {$path}");
+header("Content-type: " . mime_content_type($path));
+header('Content-Length: ' . filesize($path));
+die();
