d40e7f6bbaeb3c5cce02125fbc99e425e359041c
daniel <daniel@daniel-DC1A-S>
2017-12-22 18:17:16 -0300

    Fix conflict between jQuery UI and Bootstrap

--- plugin/LiveChat/view/panel.php
+++ plugin/LiveChat/view/panel.php
@@ -3,14 +3,6 @@ $p = YouPHPTubePlugin::loadPlugin("LiveChat");
 ?>
 <link href="<?php echo $global['webSiteRootURL']; ?>plugin/LiveChat/view/style.css" rel="stylesheet" type="text/css"/>
 <script src="<?php echo $global['webSiteRootURL']; ?>plugin/LiveChat/view/script.js" type="text/javascript"></script>
-<link href="<?php echo $global['webSiteRootURL']; ?>js/jquery-ui/jquery-ui.min.css" rel="stylesheet" type="text/css"/>
-<script src="<?php echo $global['webSiteRootURL']; ?>js/jquery-ui/jquery-ui.min.js" type="text/javascript"></script>
-<script>
-    /*** Handle jQuery plugin naming conflict between jQuery UI and Bootstrap ***/
-    $.widget.bridge('uibutton', $.ui.button);
-    $.widget.bridge('uitooltip', $.ui.tooltip);
-</script>
-<script src="<?php echo $global['webSiteRootURL']; ?>bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
 <link href="<?php echo $global['webSiteRootURL']; ?>css/font-awesome-animation.min.css" rel="stylesheet" type="text/css"/>
 <div class="alert alert-warning" id="chatOffline">
     Trying to establish a chat server connection
