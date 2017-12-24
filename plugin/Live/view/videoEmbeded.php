3ca7d2a93ef7fede19aa9d90225d514b54721869
daniel <daniel@daniel-DC1A-S>
2017-12-24 10:35:58 -0300

    Making it ready for VAST/VPAID ad sense

--- plugin/Live/view/videoEmbeded.php
+++ plugin/Live/view/videoEmbeded.php
@@ -44,6 +44,7 @@ $p = YouPHPTubePlugin::loadPlugin("Live");
         <script src="<?php echo $global['webSiteRootURL']; ?>js/video.js/video.js" type="text/javascript"></script>
         <script src="<?php echo $global['webSiteRootURL']; ?>js/videojs-contrib-ads/videojs.ads.min.js" type="text/javascript"></script>
         <script src="<?php echo $global['webSiteRootURL']; ?>plugin/Live/view/videojs-contrib-hls.min.js" type="text/javascript"></script>
+        <script src="<?php echo $global['webSiteRootURL']; ?>js/videojs-persistvolume/videojs.persistvolume.js" type="text/javascript"></script>
         <?php
         require_once $global['systemRootPath'].'plugin/YouPHPTubePlugin.php';
         echo YouPHPTubePlugin::getFooterCode();
