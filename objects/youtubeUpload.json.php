2e4e33ffd44c4e0187e28efa79344e5749c56689
daniel <daniel@daniel-DC1A-S>
2018-01-01 15:49:40 -0300

    Fix google Youtube Upload

--- objects/youtubeUpload.json.php
+++ objects/youtubeUpload.json.php
@@ -7,8 +7,10 @@ $obj->success = false;
 require_once $global['systemRootPath'] . 'objects/functions.php';
 require_once $global['systemRootPath'] . 'google/autoload.php';
 header('Content-Type: application/json');
-$OAUTH2_CLIENT_ID = $config->getAuthGoogle_id();
-$OAUTH2_CLIENT_SECRET = $config->getAuthGoogle_key();
+
+$obj = YouPHPTubePlugin::getObjectData("LoginGoogle");
+$OAUTH2_CLIENT_ID = $obj->id;
+$OAUTH2_CLIENT_SECRET = $obj->key;
 /*
  * You can acquire an OAuth 2.0 client ID and client secret from the
  * {{ Google Cloud Console }} <{{ https://cloud.google.com/console }}>
