92ca10f9228fa3bd6e76cec50735b3a666b6671e
Vinzenz Hersche <hersche@openmailbox.org>
2018-01-02 21:26:16 +0100

    make hotkeys as a plugin and revert commit "add hotkey-plugin for videojs" out of this move

---
+++ plugin/Hotkeys/Hotkeys.php
@@ -0,0 +1,52 @@
+<?php
+
+require_once $global['systemRootPath'] . 'plugin/Plugin.abstract.php';
+
+class Hotkeys extends PluginAbstract {
+
+    public function getDescription() {
+        global $global;
+        return "Enable hotkeys for videos (experimental)";
+    }
+
+    public function getName() {
+        return "Hotkeys";
+    }
+
+    public function getUUID() {
+        return "11355314-1b30-ff15-afb-67516fcccff7";
+    }
+        
+    public function getEmptyDataObject() {
+        global $global;
+        $obj = new stdClass();
+        $obj->id = "";
+        $obj->key = "";
+        return $obj;
+    }
+    
+    public function getTags() {
+        return array('free', 'videos', 'hotkeys');
+    }
+    
+
+    public function getFooterCode() {
+        global $global;
+        $obj = $this->getDataObject();
+        echo $_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+        echo $global['webSiteRootURL'];
+        if(("https://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']!=$global['webSiteRootURL'])&&("http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']!=$global['webSiteRootURL'])&&("http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']!=$global['webSiteRootURL']."cat/")&&("https://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']!=$global['webSiteRootURL']."cat/")&&("http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']!=$global['webSiteRootURL']."login/")&&("http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']!=$global['webSiteRootURL']."login/")&&("http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']!=$global['webSiteRootURL']."mvideos")&&("https://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']!=$global['webSiteRootURL']."mvideos")){
+        return "<script src=\"{$global['webSiteRootURL']}plugin/Hotkeys/videojs.hotkeys.min.js\"> </script>
+                <script>
+                   videojs('mainVideo').ready(function() {
+                   this.hotkeys({
+                      volumeStep: 0.1,
+                      seekStep: 5,
+                      enableModifiersForNumbers: false
+                      });  
+                   });  </script>";
+        }
+        return "";
+    }
+    
+}
