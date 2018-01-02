c9de51d3d30b4bc14071d6f22dfb0d0d7b090578
Vinzenz Hersche <hersche@openmailbox.org>
2018-01-02 22:16:38 +0100

    add first options, fullscreen works, volume-option does not work yet

--- plugin/Hotkeys/Hotkeys.php
+++ plugin/Hotkeys/Hotkeys.php
@@ -20,8 +20,8 @@ class Hotkeys extends PluginAbstract {
     public function getEmptyDataObject() {
         global $global;
         $obj = new stdClass();
-        $obj->id = "";
-        $obj->key = "";
+        $obj->Volume = True;
+        $obj->Fullscreen = True;
         return $obj;
     }
     
@@ -34,15 +34,28 @@ class Hotkeys extends PluginAbstract {
         global $global;
         $obj = $this->getDataObject();
         if(("https://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']!=$global['webSiteRootURL'])&&("http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']!=$global['webSiteRootURL'])&&("http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']!=$global['webSiteRootURL']."cat/")&&("https://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']!=$global['webSiteRootURL']."cat/")&&("http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']!=$global['webSiteRootURL']."login/")&&("http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']!=$global['webSiteRootURL']."login/")&&("http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']!=$global['webSiteRootURL']."mvideos")&&("https://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']!=$global['webSiteRootURL']."mvideos")){
-        return "<script src=\"{$global['webSiteRootURL']}plugin/Hotkeys/videojs.hotkeys.min.js\"> </script>
+        $tmp = "<script src=\"{$global['webSiteRootURL']}plugin/Hotkeys/videojs.hotkeys.min.js\"> </script>
                 <script>
                    videojs('mainVideo').ready(function() {
                    this.hotkeys({
-                      volumeStep: 0.1,
-                      seekStep: 5,
-                      enableModifiersForNumbers: false
+                      seekStep: 5,";
+               if($obj->Volume==1){
+                    $tmp .= "enableVolumeScroll: true,";
+               } else {
+                    // Could not use Up/Down-Keys as excepted. What's the right option?
+                    $tmp .= "enableVolumeScroll: false,";
+               }
+               
+               if($obj->Fullscreen==1){
+                    $tmp .= "enableFullscreen: true,";
+               } else {
+                    $tmp .= "enableFullscreen: false,";
+               }
+        
+        $tmp .= "enableModifiersForNumbers: false
                       });  
                    });  </script>";
+                   return $tmp;
         }
         return "";
     }
