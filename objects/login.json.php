bd64c6de634ceb1beeff0e99a5410fb29cf33958
daniel <daniel@daniel-DC1A-S>
2018-01-03 22:34:33 -0300

    Ajust for mobile login

--- objects/login.json.php
+++ objects/login.json.php
@@ -1,5 +1,6 @@
 <?php
 header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: Content-Type");
 header('Content-Type: application/json');
 if (empty($global['systemRootPath'])) {
     $global['systemRootPath'] = '../';
