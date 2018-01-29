b1e0745b0e146a198f8903f97c3450c2df05d2a2
daniel <daniel@daniel-DC1A-S>
2018-01-29 20:50:40 -0300

    Fix Paypal

--- plugin/PayPalYPT/PayPalYPT.php
+++ plugin/PayPalYPT/PayPalYPT.php
@@ -31,9 +31,6 @@ class PayPalYPT extends PluginAbstract {
         $obj = new stdClass();
         $obj->ClientID = "ASUkHFpWX0T8sr8EiGdLZ05m-RAb8l-hdRxoq-OXWmua2i7EUfqFkMZvSoGgH2LhK7zAqt29IiS2oRTn";
         $obj->ClientSecret = "ECxtMBsLr0cFwSCgI0uaDiVzEUbVlV3r_o_qaU-SOsQqCEOKPq4uGlr1C0mhdDmEyO30mw7-PF0bOnfo";
-        $obj->APIUsername = "";
-        $obj->APIPassword = "";
-        $obj->Signature = "";
         $obj->currencyID = "USD";
 
         return $obj;
