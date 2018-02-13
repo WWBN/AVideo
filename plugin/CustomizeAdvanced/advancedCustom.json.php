f4848137d6309e41aa25b7c05ba7ab82556ea738
daniel <daniel@daniel-DC1A-S>
2018-02-13 18:06:05 -0300

    Add cache on gallery

--- plugin/CustomizeAdvanced/advancedCustom.json.php
+++ plugin/CustomizeAdvanced/advancedCustom.json.php
@@ -3,3 +3,4 @@ require_once '../../videos/configuration.php';
 header('Content-Type: application/json');
 $obj = YouPHPTubePlugin::getObjectDataIfEnabled("CustomizeAdvanced");
 echo json_encode($obj);
+include $global['systemRootPath'].'objects/include_end.php';
