886f163c3d544c8244ef85bcda92e7f8f5a97281
daniel <daniel@daniel-DC1A-S>
2018-02-24 14:53:12 -0300

    Theater mode button is a plugin now

---
+++ plugin/TheaterButton/addButton.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+// Extend default
+    if (typeof player == 'undefined') {
+        player = videojs('mainVideo');
+    }
+// Extend default
+    var Button = videojs.getComponent('Button');
+    var Theater = videojs.extend(Button, {
+        //constructor: function(player, options) {
+        constructor: function () {
+            Button.apply(this, arguments);
+            //this.addClass('vjs-chapters-button');
+            this.addClass('fa');
+            this.addClass('fa-compress');
+            this.addClass('vjs-button-fa-size');
+            this.controlText("Theater");
+            if (Cookies.get('compress') === "true") {
+                toogleEC(this);
+            }
+        },
+        handleClick: function () {
+            toogleEC(this);
+        }
+    });
+
+// Register the new component
+    videojs.registerComponent('Theater', Theater);
+    player.getChild('controlBar').addChild('Theater', {}, getPlayerButtonIndex('RemainingTimeDisplay') + 1);
+
+});
