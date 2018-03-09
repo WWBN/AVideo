0a0725506bd968254d99f33ca308da09bfe91b14
Vinzenz Hersche <hersche@openmailbox.org>
2018-03-09 02:29:31 +0100

    - fix loading of hotkey-plugin in cat-view via regex (proper) - add options for individual shortcuts for play and fullscreen

--- plugin/Hotkeys/Hotkeys.php
+++ plugin/Hotkeys/Hotkeys.php
@@ -23,6 +23,8 @@ class Hotkeys extends PluginAbstract {
         $obj->Volume = true;
         $obj->ReplaceVolumeWithPlusMinus = true;
         $obj->Fullscreen = true;
+        $obj->FullscreenKey = "F";
+        $obj->PlayPauseKey = " ";
         $obj->AlwaysCaptureHotkeys = false;
         return $obj;
     }
@@ -42,7 +44,9 @@ class Hotkeys extends PluginAbstract {
         if(strpos($global['webSiteRootURL'],"https://")!==false){
            $httpSpacer = 8;    
         }
-        if(strpos($url,substr($global['webSiteRootURL'],$httpSpacer)."video/")!==false){
+        $catUrlResult;
+        preg_match("/cat\/(.*)\/video\/(.*)/", $url, $catUrlResult);
+        if((strpos($url,substr($global['webSiteRootURL'],$httpSpacer)."video/")!==false)||(sizeof($catUrlResult)>0)){
             
             $tmp = "<script src=\"{$global['webSiteRootURL']}plugin/Hotkeys/videojs.hotkeys.min.js\"> </script>
                     <script>
@@ -65,6 +69,12 @@ class Hotkeys extends PluginAbstract {
                 $tmp .= "enableFullscreen: true,";
             } else {
                 $tmp .= "enableFullscreen: false,";
+            }
+            if(($obj->FullscreenKey!=="F")||($obj->FullscreenKey!=="")){
+                $tmp .= "fullscreenKey: function(event, player) { return (event.which ===".ord($obj->FullscreenKey)."); },";
+            }
+            if(($obj->PlayPauseKey!==" ")||($obj->PlayPauseKey!=="")){
+                $tmp .= "playPauseKey: function(event, player) { return (event.which ===".ord($obj->PlayPauseKey)."); },";
             }
             if($obj->ReplaceVolumeWithPlusMinus){
                 $tmp .= "volumeUpKey: function(event, player) { return (event.which === 107); },
