bd23697e54aa77bb0481e381cbfb8df73e728f92
daniel <daniel@daniel-DC1A-S>
2018-03-13 20:50:54 -0300

    Do not stop queue in case of error

--- view/xsendfile.php
+++ view/xsendfile.php
@@ -9,6 +9,47 @@ if (empty($_GET['file'])) {
     die('GET file not found');
 }
 
+function send_video($path){
+    if (file_exists($path)){
+        $size=filesize($path);
+        $fm=@fopen($path,'rb');
+        if(!$fm) {
+            // You can also redirect here
+            header ("HTTP/1.0 404 Not Found");
+            die;
+        }
+        $begin=0;
+        $end=$size;
+        if(isset($_SERVER['HTTP_RANGE'])) {
+            if(preg_match('/bytes=\h*(\d+)-(\d*)[\D.*]?/i',   
+            $_SERVER['HTTP_RANGE'],$matches)){
+                $begin=intval($matches[0]);
+                if(!empty($matches[1])) {
+                    $end=intval($matches[1]);
+                }
+            }
+        }
+        if($begin>0||$end<$size)
+            header('HTTP/1.0 206 Partial Content');
+        else
+            header('HTTP/1.0 200 OK');
+        header('Accept-Ranges: bytes');
+        header('Content-Length:'.($end-$begin));
+        header("Content-Disposition: inline;");
+        header("Content-Range: bytes $begin-$end/$size");
+        header("Content-Transfer-Encoding: binary\n");
+        header('Connection: close');
+        $cur=$begin;
+        fseek($fm,$begin,0);
+        while(!feof($fm)&&$cur<$end&&(connection_status()==0)){
+            echo fread($fm,min(1024*16,$end-$cur));
+            $cur+=1024*16;
+            usleep(1000);
+        }
+        die;
+    }
+}
+
 $path_parts = pathinfo($_GET['file']);
 $file = $path_parts['basename'];
 $path = "{$global['systemRootPath']}videos/{$file}";
@@ -34,6 +75,10 @@ if(empty($_GET['download'])){
 }
 header('Content-Length: ' . filesize($path));
 if(!empty($advancedCustom->doNotUseXsendFile)){
-    echo file_get_contents($path);
+    if(strtolower($path_parts['extension'])==="mp4" || strtolower($path_parts['extension'])==="webm"){
+        send_video($path);
+    }else{
+        echo file_get_contents($path);
+    }
 }
 die();
