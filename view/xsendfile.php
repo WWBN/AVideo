d50007ba2ef71f4c805a40b632b62973bb472b87
daniel <daniel@daniel-DC1A-S>
2018-03-13 21:05:40 -0300

    Xsendfile alternative

--- view/xsendfile.php
+++ view/xsendfile.php
@@ -76,7 +76,9 @@ if(empty($_GET['download'])){
 header('Content-Length: ' . filesize($path));
 if(!empty($advancedCustom->doNotUseXsendFile)){
     if(strtolower($path_parts['extension'])==="mp4" || strtolower($path_parts['extension'])==="webm"){
-        send_video($path);
+        // Not working yet
+        //send_video($path);
+        echo file_get_contents($path);
     }else{
         echo file_get_contents($path);
     }
