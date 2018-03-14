cf6d4ed9ae2c6d8ec8e870d605f93f14308870ce
daniel <daniel@daniel-DC1A-S>
2018-03-14 15:18:31 -0300

    Fix Image in playlist

--- view/channel.php
+++ view/channel.php
@@ -135,7 +135,7 @@ $playlists = PlayList::getAllFromUser($user_id, $publicOnly);
                                         $poster = $images->thumbsJpg;
                                         ?>
                                         <li class="col-lg-2 col-md-4 col-sm-4 col-xs-6 galleryVideo " id="<?php echo $value['id']; ?>">
-                                            <a class="aspectRatio16_9" href="<?php echo $global['webSiteRootURL']; ?>video/<?php echo $value['clean_title']; ?>" title="<?php echo $value['title']; ?>" style="padding: 0; margin: 0;" >
+                                            <a class="aspectRatio16_9" href="<?php echo $global['webSiteRootURL']; ?>video/<?php echo $value['clean_title']; ?>" title="<?php echo $value['title']; ?>" style="margin: 0;" >
                                                 <img src="<?php echo $poster; ?>" alt="<?php echo $value['title']; ?>" class="img img-responsive <?php echo $img_portrait; ?>  rotate<?php echo $value['rotation']; ?>" />
                                                 <span class="duration"><?php echo Video::getCleanDuration($value['duration']); ?></span>
                                             </a>
