061fa04265cc021cd117ebb44f02cd1c48af64a9
daniel <daniel@daniel-DC1A-S>
2018-03-22 11:03:49 -0300

    Fix twitch embed https://github.com/DanielnetoDotCom/YouPHPTube/issues/505

--- objects/functions.php
+++ objects/functions.php
@@ -455,6 +455,14 @@ function parseVideos($videoString = null) {
 
         $id = $matches[2];
         return '//player.twitch.tv/?channel=' . $id . '#';
+    }  else if (strpos($link, 'twitch.tv/videos') !== FALSE) {
+        //extract the ID
+        preg_match(
+                '/\/\/(www\.)?twitch.tv\/videos\/([a-zA-Z0-9_-]+)$/', $link, $matches
+        );
+
+        $id = $matches[2];
+        return '//player.twitch.tv/?video=' . $id . '#';
     } else if (strpos($link, '/video/') !== FALSE) {
         //extract the ID
         preg_match(
