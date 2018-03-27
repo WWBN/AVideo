a9d79681498c70569bb25b2fac3f584de0dae648
Vinzenz Hersche <hersche@openmailbox.org>
2018-03-27 05:10:17 +0200

    rename mode-nr's to names in mcategorys

--- view/managerCategories.php
+++ view/managerCategories.php
@@ -40,7 +40,7 @@ require_once $global['systemRootPath'] . 'objects/category.php';
                         <th data-column-id="name" data-order="desc"><?php echo __("Name"); ?></th>
                         <th data-column-id="clean_name"><?php echo __("Clean Name"); ?></th>
                         <th data-column-id="description"><?php echo __("Description"); ?></th>
-                        <th data-column-id="nextVideoOrder"><?php echo __("Next video order"); ?></th>
+                        <th data-column-id="nextVideoOrder" data-formatter="nextVideoOrder"><?php echo __("Next video order"); ?></th>
                         <th data-column-id="commands" data-formatter="commands" data-sortable="false"></th>
                     </tr>
                 </thead>
@@ -60,9 +60,9 @@ require_once $global['systemRootPath'] . 'objects/category.php';
                                 <input type="text" id="inputName" class="form-control first" placeholder="<?php echo __("Name"); ?>" required autofocus>
                                 <label for="inputCleanName" class="sr-only"><?php echo __("Clean Name"); ?></label>
                                 <input type="text" id="inputCleanName" class="form-control last" placeholder="<?php echo __("Clean Name"); ?>" required>
-                                    <label class="sr-only" for="description"><?php echo __("Description"); ?></label>
-                                    <textarea class="form-control" rows="5" id="description" placeholder="<?php echo __("Description"); ?>"></textarea>
-                                  <select class="form-control" id="nextVideoOrder">
+                                    <label class="sr-only" for="inputDescription"><?php echo __("Description"); ?></label>
+                                    <textarea class="form-control" rows="5" id="inputDescription" placeholder="<?php echo __("Description"); ?>"></textarea>
+                                  <select class="form-control" id="inputNextVideoOrder">
                                         <option value="0"><?php echo __("Random"); ?></option>
                                         <option value="1"><?php echo __("By name"); ?></option>
                                   </select>
@@ -105,6 +105,13 @@ require_once $global['systemRootPath'] . 'objects/category.php';
                     ajax: true,
                     url: "<?php echo $global['webSiteRootURL'] . "categories.json"; ?>",
                     formatters: {
+                        "nextVideoOrder": function(column, row){
+                            if(row.nextVideoOrder==0){
+                                return "<?php echo __("Random"); ?>";
+                            } else {
+                                return "<?php echo __("By name"); ?>";
+                            }
+                        },
                         "commands": function (column, row)
                         {
                             var editBtn = '<button type="button" class="btn btn-xs btn-default command-edit" data-row-id="' + row.id + '" data-toggle="tooltip" data-placement="left" title="Edit"><span class="glyphicon glyphicon-edit" aria-hidden="true"></span></button>'
@@ -122,8 +129,8 @@ require_once $global['systemRootPath'] . 'objects/category.php';
                         $('#inputCategoryId').val(row.id);
                         $('#inputName').val(row.name);
                         $('#inputCleanName').val(row.clean_name);
-                        $('#description').val(row.description);
-                        $('#nextVideoOrder').val(row.nextVideoOrder);
+                        $('#inputDescription').val(row.description);
+                        $('#inputNextVideoOrder').val(row.nextVideoOrder);
                         $(".iconCat i").attr("class", row.iconClass);
 
                         $('#categoryFormModal').modal();
@@ -175,8 +182,8 @@ require_once $global['systemRootPath'] . 'objects/category.php';
                     $('#inputCategoryId').val('');
                     $('#inputName').val('');
                     $('#inputCleanName').val('');
-                    $('#description').val('');
-                    $('#nextVideoOrder').val('');
+                    $('#inputDescription').val('');
+                    $('#inputNextVideoOrder').val('');
 
                     $('#categoryFormModal').modal();
                 });
@@ -190,7 +197,7 @@ require_once $global['systemRootPath'] . 'objects/category.php';
                     modal.showPleaseWait();
                     $.ajax({
                         url: 'addNewCategory',
-                        data: {"id": $('#inputCategoryId').val(), "name": $('#inputName').val(), "clean_name": $('#inputCleanName').val(),"description": $('#description').val(),"nextVideoOrder": $('#nextVideoOrder').val(), "iconClass": $(".iconCat i").attr("class")},
+                        data: {"id": $('#inputCategoryId').val(), "name": $('#inputName').val(), "clean_name": $('#inputCleanName').val(),"description": $('#inputDescription').val(),"nextVideoOrder": $('#inputNextVideoOrder').val(), "iconClass": $(".iconCat i").attr("class")},
                         type: 'post',
                         success: function (response) {
                             if (response.status === "1") {
