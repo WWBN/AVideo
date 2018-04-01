710e7e3887eb94fc616a2ce50733d61143991367
Vinzenz Hersche <hersche@openmailbox.org>
2018-04-01 23:03:16 +0200

    first changes that make audio working again (also via streamer-upload)

--- view/include/audio.php
+++ view/include/audio.php
@@ -3,9 +3,13 @@
     <div class="col-xs-12 col-sm-12 col-lg-8 ">
         <audio controls class="center-block video-js vjs-default-skin "  id="mainAudio" autoplay data-setup='{}'
                poster="<?php echo $global['webSiteRootURL']; ?>img/recorder.gif">
+            <?php if(file_exists($global['systemRootPath']."videos/".$video['filename'].".ogg")){ ?>
             <source src="<?php echo $global['webSiteRootURL']; ?>videos/<?php echo $video['filename']; ?>.ogg" type="audio/ogg" />
-            <source src="<?php echo $global['webSiteRootURL']; ?>videos/<?php echo $video['filename']; ?>.mp3" type="audio/mpeg" />
+            <a href="<?php echo $global['webSiteRootURL']; ?>videos/<?php echo $video['filename']; ?>.ogg">horse</a>
+            <?php } else { ?>
+            <source src="<?php echo $global['webSiteRootURL']; ?>videos/<?php echo $video['filename']; ?>.mp3" type="audio/mpeg" /> 
             <a href="<?php echo $global['webSiteRootURL']; ?>videos/<?php echo $video['filename']; ?>.mp3">horse</a>
+            <?php } ?>
         </audio>
     </div>
 
