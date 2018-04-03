f7e193dc3209c4226e122914b6ac6f1435263b1b
Vinzenz Hersche <hersche@openmailbox.org>
2018-04-03 03:58:18 +0200

    add download-button for audio (when download is allowed)

--- view/include/audio.php
+++ view/include/audio.php
@@ -3,14 +3,23 @@
     <div class="col-xs-12 col-sm-12 col-lg-8 ">
         <audio controls class="center-block video-js vjs-default-skin "  id="mainAudio" autoplay data-setup='{}'
                poster="<?php echo $global['webSiteRootURL']; ?>img/recorder.gif">
-            <?php if(file_exists($global['systemRootPath']."videos/".$video['filename'].".ogg")){ ?>
+            <?php
+            $ext = "";
+            if(file_exists($global['systemRootPath']."videos/".$video['filename'].".ogg")){ ?>
             <source src="<?php echo $global['webSiteRootURL']; ?>videos/<?php echo $video['filename']; ?>.ogg" type="audio/ogg" />
             <a href="<?php echo $global['webSiteRootURL']; ?>videos/<?php echo $video['filename']; ?>.ogg">horse</a>
-            <?php } else { ?>
+            <?php
+            $ext = ".ogg";
+            } else { ?>
             <source src="<?php echo $global['webSiteRootURL']; ?>videos/<?php echo $video['filename']; ?>.mp3" type="audio/mpeg" /> 
             <a href="<?php echo $global['webSiteRootURL']; ?>videos/<?php echo $video['filename']; ?>.mp3">horse</a>
-            <?php } ?>
+            <?php
+            $ext = ".mp3";
+                   } ?>
         </audio>
+        <?php if ($config->getAllow_download()) { ?>
+            <a class="btn btn-xs btn-default " role="button" href="<?php echo $global['webSiteRootURL'] . "videos/" . $video['filename'].$ext; ?>" download="<?php echo $video['title'] . $ext; ?>"><?php echo __("Download audio"); ?></a>
+            <?php } ?>
     </div>
     <script>
         $(document).ready(function () {
