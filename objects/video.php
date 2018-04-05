1b6ebdd8eccc8f2ab058f8aabea9cae046e33854
daniel <daniel@daniel-DC1A-S>
2018-04-05 15:04:47 -0300

    Add option for S3 Direct Link

--- objects/video.php
+++ objects/video.php
@@ -1247,13 +1247,18 @@ class Video {
         }
          * 
          */
+        $aws_s3 = YouPHPTubePlugin::loadPluginIfEnabled('AWS_S3');
+        $aws_s3_obj = $aws_s3->getDataObject();
+        if(!empty($aws_s3_obj->useS3DirectLink)){
+            $includeS3 = true;
+        }
+        
         $source = array();
         $source['path'] = "{$global['systemRootPath']}videos/{$filename}{$type}";
         $source['url'] = "{$global['webSiteRootURL']}videos/{$filename}{$type}";
         /* need it because getDurationFromFile*/
         if($includeS3 && ($type==".mp4" || $type==".webm")){
             if (!file_exists($source['path']) || filesize($source['path']) < 1024) {
-                $aws_s3 = YouPHPTubePlugin::loadPluginIfEnabled('AWS_S3');
                 if (!empty($aws_s3)) {
                     $source = $aws_s3->getAddress("{$filename}{$type}");
                 }
