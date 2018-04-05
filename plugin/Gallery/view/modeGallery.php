e640da593164b4e4e4f305ac998165e14265d574
Vinzenz Hersche <hersche@openmailbox.org>
2018-04-06 01:06:25 +0200

    clean-up subcat-part in modeGallery

--- plugin/Gallery/view/modeGallery.php
+++ plugin/Gallery/view/modeGallery.php
@@ -196,63 +196,36 @@ echo $config->getWebSiteTitle();
                                 
                                 $videos = Video::getAllVideos();
                                 
-                                //
-                                
+                                // make a row each 6 cols
+                                if ($countCols % 6 === 0) {
+                                    echo '</div><div class="row aligned-row ">';
+                                }
+                                $countCols ++;       
                                 unset($_GET['catName']);
-                                if (! empty($videos)) {
-                                    foreach ($videos as $value) {
-                                        $name = User::getNameIdentificationById($value['users_id']);
-                                        
-                                        // make a row each 6 cols
-                                        if ($countCols % 6 === 0) {
-                                            echo '</div><div class="row aligned-row ">';
-                                        }
-                                        $countCols ++;
-                                        ?>
-                        <div class="col-lg-2 col-md-4 col-sm-4 col-xs-6 galleryVideo thumbsImage fixPadding">
-							<a href="<?php echo $global['webSiteRootURL']; ?>cat/<?php echo $cat['clean_name']; ?>" title="<?php $cat['name']; ?>">
-                            <?php
-                                $images = Video::getImageFromFilename($value['filename'], $value['type']);
-                                $poster = $images->thumbsJpg;
-                            ?>
-                                <div class="aspectRatio16_9">
-									<img src="<?php echo $poster; ?>" alt="" data-toggle="tooltip" title="<?php echo $description; ?>" class="thumbsJPG img img-responsive <?php echo $img_portrait; ?>  rotate<?php echo $value['rotation']; ?>" id="thumbsJPG<?php echo $value['id']; ?>" />
-                            <?php if ((! empty($imgGif)) && (! $o->LiteGalleryNoGifs)) { ?>
-                                    <img src="<?php echo $imgGif; ?>" style="position: absolute; top: 0; display: none;" alt="" data-toggle="tooltip" title="<?php echo $description; ?>" id="thumbsGIF<?php echo $value['id']; ?>" class="thumbsGIF img-responsive <?php echo $img_portrait; ?>  rotate<?php echo $value['rotation']; ?>" height="130" />
-                            <?php }
-                                $videoCount = $global['mysqli']->query("SELECT COUNT(title) FROM videos WHERE categories_id = " . $value['categories_id'] . ";"); ?>
-                                </div>
-								<div class="videoInfo">
-                            <?php if ($videoCount) { ?>
-                                    <span class="label label-default" style="top: 10px !important; position: absolute;">
-                                        <i class="glyphicon glyphicon-cd"></i><?php echo $videoCount->fetch_array()[0]; ?>
-                                    </span>
-                            <?php } ?>
-                                </div>
-								<div data-toggle="tooltip" title="<?php echo $description; ?>" class="tile__title" style="border-radius: 10px; background-color: black; color: white; position: absolute; margin-left: 10%; width: 80% !important; bottom: 40% !important; opacity: 0.8 !important; text-align: center;">
-                                    <?php echo $cat['name']; ?>
-                                </div>
-							</a>
-						</div>        
-                    <?php
-                        break;
-                        }
-                    } else { 
-                                        if ($countCols % 6 === 0) {
-                                            echo '</div><div class="row aligned-row ">';
-                                        }
-                                        $countCols ++;
-                                        ?>
-                        <div class="col-lg-2 col-md-4 col-sm-4 col-xs-6 galleryVideo thumbsImage fixPadding">
-							<a href="<?php echo $global['webSiteRootURL']; ?>cat/<?php echo $cat['clean_name']; ?>" title="<?php $cat['name']; ?>"><?php
-                                    $poster = $global['webSiteRootURL'] . "view/img/notfound.jpg";
-                                    $description = $cat['description'];
-                                    ?>
-                                <div class="aspectRatio16_9">
-									<img src="<?php echo $poster; ?>" alt="" data-toggle="tooltip" title="<?php echo $description; ?>" class="thumbsJPG img img-responsive" id="thumbsJPG<?php echo $cat['id']; ?>" />
-                                <?php
-                                    $videoCount = $global['mysqli']->query("SELECT COUNT(title) FROM videos WHERE categories_id = " . $cat['id'] . ";");
                                 ?>
+                            <div class="col-lg-2 col-md-4 col-sm-4 col-xs-6 galleryVideo thumbsImage fixPadding">
+				                <a href="<?php echo $global['webSiteRootURL']; ?>cat/<?php echo $cat['clean_name']; ?>" title="<?php $cat['name']; ?>">
+                                <div class="aspectRatio16_9">
+                                <?php
+                            if (!empty($videos)) {
+                                foreach ($videos as $value) {
+                                        $name = User::getNameIdentificationById($value['users_id']);
+                                        $images = Video::getImageFromFilename($value['filename'], $value['type']);
+                                        $poster = $images->thumbsJpg;
+                                ?>
+									<img src="<?php echo $poster; ?>" alt="" data-toggle="tooltip" title="<?php echo $description; ?>" class="thumbsJPG img img-responsive <?php echo $img_portrait; ?>  rotate<?php echo $value['rotation']; ?>" id="thumbsJPG<?php echo $value['id']; ?>" />
+                                <?php if ((! empty($imgGif)) && (! $o->LiteGalleryNoGifs)) { ?>
+                                    <img src="<?php echo $imgGif; ?>" style="position: absolute; top: 0; display: none;" alt="" data-toggle="tooltip" title="<?php echo $description; ?>" id="thumbsGIF<?php echo $value['id']; ?>" class="thumbsGIF img-responsive <?php echo $img_portrait; ?>  rotate<?php echo $value['rotation']; ?>" height="130" />
+                                <?php }
+                                $videoCount = $global['mysqli']->query("SELECT COUNT(title) FROM videos WHERE categories_id = " . $value['categories_id'] . ";"); 
+                                break;
+                        }
+                    } else {
+                                $poster = $global['webSiteRootURL'] . "view/img/notfound.jpg"; ?>
+								<img src="<?php echo $poster; ?>" alt="" data-toggle="tooltip" title="<?php echo $description; ?>" class="thumbsJPG img img-responsive" id="thumbsJPG<?php echo $cat['id']; ?>" />
+                                <?php
+                                $videoCount = $global['mysqli']->query("SELECT COUNT(title) FROM videos WHERE categories_id = " . $cat['id'] . ";");
+                    } ?>
                                 </div>
 								<div class="videoInfo">
                                 <?php if ($videoCount) { ?>
@@ -266,10 +239,9 @@ echo $config->getWebSiteTitle();
                                         <?php echo $cat['name']; ?>
                                 </div>
 							</a>
-						</div>    
-                    <?php
-                        }
-                    }        
+						</div>          
+                        
+                    <?php } // end of foreach-cat       
                     unset($_POST['sort']);
                     $_GET['catName'] = $originalCat;
                     ?>
