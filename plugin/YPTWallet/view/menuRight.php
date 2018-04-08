e7eba84d4458a3c225f3a3f0c968e5a69b6ffd26
daniel <daniel@daniel-DC1A-S>
2018-04-08 15:03:29 -0300

    Making ready Wallet plugin

---
+++ plugin/YPTWallet/view/menuRight.php
@@ -0,0 +1,49 @@
+<?php
+if (!User::isLogged()) {
+    return;
+}
+$plugin = YouPHPTubePlugin::loadPluginIfEnabled("YPTWallet");
+$balance = $plugin->getBalance(User::getId());
+?>
+<style>
+</style>
+<li>
+    <div class="btn-group">
+        <button type="button" class="btn btn-default  dropdown-toggle navbar-btn pull-left"  data-toggle="dropdown">
+            <span class="fa fa-usd"></span> <span class=""> <?php echo $obj->wallet_button_title; ?> <span class="badge"><?php echo $obj->currency_symbol; ?> <span class="walletBalance"><?php echo number_format($balance, $obj->decimalPrecision); ?></span> <?php echo $obj->currency; ?></span></span> <span class="caret"></span>
+        </button>
+        <ul class="dropdown-menu dropdown-menu-right" role="menu"> 
+            <li class="dropdown-submenu">
+                <a tabindex="-1" href="<?php echo $global['webSiteRootURL']; ?>plugin/YPTWallet/view/addFunds.php">
+                    <i class="fa fa-plus" aria-hidden="true"></i>
+                    <?php echo __("Add Funds"); ?>
+                </a>
+            </li> 
+            <li class="dropdown-submenu">
+                <a tabindex="-1" href="<?php echo $global['webSiteRootURL']; ?>plugin/YPTWallet/view/history.php">
+                    <i class="fa fa-history" aria-hidden="true"></i>
+                    <?php echo __("History"); ?>
+                </a>
+            </li> 
+            <?php
+            if (User::isAdmin()) {
+                ?>
+                <li class="dropdown-header">Admin Menu</li>
+                <li class="dropdown-submenu">
+                    <a tabindex="-1" href="<?php echo $global['webSiteRootURL']; ?>plugin/YPTWallet/view/adminManageWallets.php">
+                        <i class="fa fa-users" aria-hidden="true"></i>
+                        <?php echo __("Manage Wallets"); ?>
+                    </a>
+                </li> 
+                <?php
+            }
+            ?>
+        </ul>
+    </div>
+
+</li>
+
+<script>
+    $(document).ready(function () {
+    });
+</script>
