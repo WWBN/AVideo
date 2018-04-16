7a5b6ae237262f8da02b7f76909ae558f4290c6d
daniel <daniel@daniel-DC1A-S>
2018-04-15 22:20:14 -0300

    Put an option to dont allow unauthenticated users use the LiveChat Thanks Bryan

--- plugin/LiveChat/view/panel.php
+++ plugin/LiveChat/view/panel.php
@@ -1,9 +1,11 @@
 <?php
 $p = YouPHPTubePlugin::loadPlugin("LiveChat");
+$canSendMessage = $p->canSendMessage();
 ?>
 <link href="<?php echo $global['webSiteRootURL']; ?>plugin/LiveChat/view/style.css" rel="stylesheet" type="text/css"/>
 <script src="<?php echo $global['webSiteRootURL']; ?>plugin/LiveChat/view/script.js" type="text/javascript"></script>
 <link href="<?php echo $global['webSiteRootURL']; ?>css/font-awesome-animation.min.css" rel="stylesheet" type="text/css"/>
+<link href="<?php echo $global['webSiteRootURL']; ?>js/jquery-ui/jquery-ui.min.css" rel="stylesheet" type="text/css"/>
 <div class="alert alert-warning" id="chatOffline">
     <?php echo __("Trying to establish a chat server connection"); ?>
 </div>
@@ -14,13 +16,24 @@ $p = YouPHPTubePlugin::loadPlugin("LiveChat");
             <div class="panel-body">  
                 <ul class="messages"></ul>
             </div>
+
             <div class="panel-footer">
-                <div class="input-group">
-                    <input type="text" class="form-control message_input" placeholder="<?php echo __("Type your message..."); ?>">
-                    <span class="input-group-btn">
-                        <button class="btn btn-secondary send_message" type="button"><i class="fa fa-send"></i> <?php echo __("Send"); ?></button>
-                    </span>
-                </div>
+                <?php
+                if ($canSendMessage) {
+                    ?>
+                    <div class="input-group">
+                        <input type="text" class="form-control message_input" placeholder="<?php echo __("Type your message..."); ?>">
+                        <span class="input-group-btn">
+                            <button class="btn btn-secondary send_message" type="button"><i class="fa fa-send"></i> <?php echo __("Send"); ?></button>
+                        </span>
+                    </div>
+                    <?php
+                }else{
+                ?>
+                <a href="<?php echo $global['webSiteRootURL']; ?>user" class="btn btn-default"> <?php echo __('Login'); ?></a>
+                <?php
+                }
+                ?>
             </div>
         </div>
     </div>
@@ -35,6 +48,7 @@ $p = YouPHPTubePlugin::loadPlugin("LiveChat");
         </li>
     </div>
 </div>
+<script src="<?php echo $global['webSiteRootURL']; ?>js/jquery-ui/jquery-ui.min.js" type="text/javascript"></script>
 <script>
     var attempChatConnections = 3;
     var conn;
@@ -103,9 +117,7 @@ $p = YouPHPTubePlugin::loadPlugin("LiveChat");
     }
     function makeDrag() {
         //$("#chatOnline").draggable('destroy');
-        $("#chatOnline").draggable({
-            handle: ".panel-heading"
-        });
+        $("#chatOnline").draggable({handle: ".panel-heading"});
     }
     $(function () {
         $('#collapseBtn').click(function (e) {
@@ -113,15 +125,23 @@ $p = YouPHPTubePlugin::loadPlugin("LiveChat");
                 //makeDrag();
             });
         });
-        $('.send_message').click(function (e) {
-            sendJsonMessage(JSON.stringify(getJsonDataObject()), 'right');
-        });
-        $('.message_input').keyup(function (e) {
-            if (e.which === 13) {
+
+<?php
+if ($canSendMessage) {
+    ?>
+            $('.send_message').click(function (e) {
                 sendJsonMessage(JSON.stringify(getJsonDataObject()), 'right');
-                $('.message_input').val('');
-            }
-        });
+            });
+            $('.message_input').keyup(function (e) {
+                if (e.which === 13) {
+                    sendJsonMessage(JSON.stringify(getJsonDataObject()), 'right');
+                    $('.message_input').val('');
+                }
+            });
+
+    <?php
+}
+?>
         connect();
         makeDrag();
     });
