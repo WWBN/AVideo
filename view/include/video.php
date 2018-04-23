3cf94d046f7588375686fe5489d0104837172e8c
Vinzenz Hersche <hersche@openmailbox.org>
2018-04-23 20:54:56 +0200

    safe media-id for php in isMediaPlaySite

--- view/include/video.php
+++ view/include/video.php
@@ -71,6 +71,7 @@ if (YouPHPTubePlugin::isEnabled("0e225f8e-15e2-43d4-8ff7-0cb07c2a2b3b")) {
 </div>
 <!--/row-->
 <script>
+    <?php $_GET['isMediaPlaySite'] = $playNowVideo['id']; ?>
     var mediaId = <?php echo $playNowVideo['id']; ?>;
     var player;
     $(document).ready(function () {
