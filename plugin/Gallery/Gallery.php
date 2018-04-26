0710b53eff2ca1afbb324d51725aabf1d3d89dac
Vinzenz Hersche <hersche@openmailbox.org>
2018-04-26 18:12:45 +0200

    respecting youphpflix-hybrid

--- plugin/Gallery/Gallery.php
+++ plugin/Gallery/Gallery.php
@@ -46,7 +46,9 @@ class Gallery extends PluginAbstract {
     
     public function getFirstPage(){
         global $global;
-        return $global['systemRootPath'].'plugin/Gallery/view/modeGallery.php';
+        if(!YouPHPTubePlugin::isEnabled("d3sa2k4l3-23rds421-re323-4ae-423")){
+            return $global['systemRootPath'].'plugin/Gallery/view/modeGallery.php';
+        }
     }   
     
     public function getTags() {
