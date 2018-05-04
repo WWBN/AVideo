991a8a63f48ed3e9780b3897ca75a584e05bf1e0
daniel <daniel@daniel-DC1A-S>
2018-05-03 22:33:11 -0300

    Fix for mobile

--- objects/login.json.php
+++ objects/login.json.php
@@ -1,5 +1,5 @@
 <?php
-//header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Headers: Content-Type");
 header('Content-Type: application/json');
 if (empty($global['systemRootPath'])) {
