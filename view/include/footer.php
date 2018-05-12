178d886f8e4f51f84289b2a8621b8ca3cb439682
Vinzenz Hersche <hersche@openmailbox.org>
2018-05-12 15:32:53 +0200

    log to the original file, add the requested URI to the logged line

--- view/include/footer.php
+++ view/include/footer.php
@@ -31,10 +31,10 @@
             url="embed in html";
         }
         $.ajax({
-            url: webSiteRootURL+"objects/ajaxErrorCatcher.php?error="+encodeURI("JS-Err: "+errorMsg+" @ line "+lineNumber+" in file "+url),
+            url: webSiteRootURL+"objects/ajaxErrorCatcher.php?error="+encodeURI("JS-Err: "+errorMsg+" @ line "+lineNumber+" in file "+url+" at visit on <?php echo $_SERVER['REQUEST_URI'] ?>"),
             context: document.body
         }).done(function() {
-            console.log("<?php echo 'A Javascript-error happend. Please tell your admin to clear the folder videos/cache. \r\n If this doesn\'t help, attach these infos to a github-pull-request:'; ?> \r\n Msg:" + errorMsg+" \r\n Url: "+url+ ", line: "+lineNumber);
+            console.log("<?php echo 'A Javascript-error happend. Please tell your admin to clear the folder videos/cache. \r\n If this doesn\'t help, attach these infos to a github-pull-request:'; ?> \r\n Msg:" + errorMsg+" \r\n Url: "+url+ ", line: "+lineNumber+" Address: <?php echo $_SERVER['REQUEST_URI'] ?>");
         });
     return false;
     }
