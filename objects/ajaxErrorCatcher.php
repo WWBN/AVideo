e751c5e5623f2c638adb578556a74a725457a677
Vinzenz Hersche <hersche@openmailbox.org>
2018-05-12 23:36:25 +0200

    log js to a seperate logfile again, minify JS when the right global is set

--- objects/ajaxErrorCatcher.php
+++ objects/ajaxErrorCatcher.php
@@ -1,5 +1,5 @@
 <?php 
-ini_set('error_log', '../videos/youphptube.log');
+ini_set('error_log', '../videos/youphptube.js.log');
 if(!empty($_GET['error'])){
     error_log("[aEC] ".$_GET['error']);
 }
