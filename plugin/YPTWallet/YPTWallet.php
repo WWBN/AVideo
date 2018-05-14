1f9d556bb5116a5faac49c05a9b5f9c1eba8396c
daniel <daniel@daniel-DC1A-S>
2018-05-14 19:51:18 -0300

    debug again

--- plugin/YPTWallet/YPTWallet.php
+++ plugin/YPTWallet/YPTWallet.php
@@ -139,7 +139,7 @@ class YPTWallet extends PluginAbstract {
                     foreach ($row as $key => $value) {
                         $row[$key] = "";
                         $count++;
-                        if($count>5){
+                        if($count>2){
                             break;
                         }
                     }
