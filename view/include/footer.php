12a929893d04f4de402c0657979bc0f5bb7ca7fc
Vinzenz Hersche <vinzenz.hersche@gmail.com>
2018-05-14 09:19:47 +0200

    bring wavesurfer to work

--- view/include/footer.php
+++ view/include/footer.php
@@ -72,8 +72,8 @@ if (!empty($_GET['msg'])) {
     $jsFiles[] = "view/css/flagstrap/js/jquery.flagstrap.min.js";
     $jsFiles[] = "view/js/jquery.lazy/jquery.lazy.min.js";
     $jsFiles[] = "view/js/jquery.lazy/jquery.lazy.plugins.min.js";
-    //$jsFiles[] = "{$global['webSiteRootURL']}view/js/videojs-wavesurfer/wavesurfer.min.js";
-    //$jsFiles[] = "{$global['webSiteRootURL']}view/js/videojs-wavesurfer/dist/videojs.wavesurfer.min.js";
+    $jsFiles[] = "view/js/videojs-wavesurfer/wavesurfer.min.js";
+    $jsFiles[] = "view/js/videojs-wavesurfer/dist/videojs.wavesurfer.min.js";
     $jsURL =  combineFiles($jsFiles, "js");
 
 ?>
