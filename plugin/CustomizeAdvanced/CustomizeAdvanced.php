663abdc3c472c4085f2d0c3e8170de2a41c2ac45
Vinzenz Hersche <hersche@openmailbox.org>
2018-05-16 08:40:26 +0200

    begining help of customizedadvanced

--- plugin/CustomizeAdvanced/CustomizeAdvanced.php
+++ plugin/CustomizeAdvanced/CustomizeAdvanced.php
@@ -56,6 +56,13 @@ class CustomizeAdvanced extends PluginAbstract {
         return $obj;
     }
     
+    public function getHelp(){
+        if(User::isAdmin()){
+            return "<h2 id='CustomizeAdvanced help'>CustomizeAdvanced (admin)</h2><ul><li>EnableWavesurfer = Enables the visualisation for audio. This will always download full audio first, so with big audio-files, you might better disable it.</li><li>commentsMaxLength = Maximum lenght for comments in videos</li><li>disableYoutubePlayerIntegration = Disables the integrating of youtube-videos and just embed them.</li><li>EnableMinifyJS = Minify your JS. Clear videos/cache after changing this option.</li></ul>";   
+        }
+        return "";
+    }
+    
     public function getTags() {
         return array('free', 'customization', 'buttons', 'resolutions');
     }
