c5043d83cce6751c8e838c41ccf0f35e74abd139
Vinzenz Hersche <hersche@openmailbox.org>
2018-05-16 06:08:12 +0200

    reformat

--- view/include/embed.php
+++ view/include/embed.php
@@ -42,59 +42,99 @@
                 }
                 ?>"></iframe>
 
-                <?php } else { 
-                $_GET['isEmbedded'] = "y";
-                ?>
+                <?php } else {     
+                    // youtube!
+                    $_GET['isEmbedded'] = "y";
+                ?>      
+                    <div id="main-video" class="embed-responsive embed-responsive-16by9">
+                        <video id="mainVideo" class="embed-responsive-item video-js vjs-default-skin <?php echo $vjsClass; ?> vjs-big-play-centered" controls <?php if ($config->getAutoplay()) { echo " autoplay "; } ?> data-setup='{"aspectRatio": "16:9", "techOrder": ["youtube"], "sources": [{ "type": "video/youtube", "src": "<?php echo $video['videoLink']; ?>"}] }' ></video>
+                        <script>
+                            var player;
+                            var mediaId = <?php echo $video['id']; ?>;
+                            <?php if (!$config->getAllow_download()) { ?>
+                                // Prevent HTML5 video from being downloaded (right-click saved)?
+                                $('#mainVideo').bind('contextmenu', function () {
+                                    return false;
+                                });
+                            <?php } ?>
                 
-                <div id="main-video" class="embed-responsive embed-responsive-16by9">
-                <video
-                    id="mainVideo"
-                    class="embed-responsive-item video-js vjs-default-skin <?php echo $vjsClass; ?> vjs-big-play-centered"
-                       controls
-                       <?php
-                if ($config->getAutoplay()) {
-                    echo " autoplay ";
-                }
-                ?>
-                       data-setup='{  "aspectRatio": "16:9", "techOrder": ["youtube"], "sources": [{ "type": "video/youtube", "src": "<?php echo $video['videoLink']; ?>"}] }' >
-            </video>
-                <script>
-                var player;
-                var mediaId = <?php echo $video['id']; ?>;
-                
-                    <?php if (!$config->getAllow_download()) { ?>
-                    // Prevent HTML5 video from being downloaded (right-click saved)?
-                    $('#mainVideo').bind('contextmenu', function () {
-                        return false;
-                    });
-                    <?php } ?>
-                
-                $(document).ready(function () {
-                    player = videojs('mainVideo');
-                            player.ready(function () {
-                            <?php if ($config->getAutoplay()) {
-	                           echo "setTimeout(function () { if(typeof player === 'undefined'){ player = videojs('mainVideo');} player.play(); }, 50);";
-                            } else { ?>
-                if (Cookies.get('autoplay') && Cookies.get('autoplay') !== 'false') {
-                    setTimeout(function () { if(typeof player === 'undefined'){ player = videojs('mainVideo');} player.play();}, 50);                    
-                }
-                <?php } ?>
-                            num = $('#videosList').find('.pagination').find('li.active').attr('data-lp');
-                            loadPage(num);                 
-                            });
-                    
+                            $(document).ready(function () {
+                                player = videojs('mainVideo');
+                                player.ready(function () {
+                                    <?php if ($config->getAutoplay()) {
+	                                   echo "setTimeout(function () { if(typeof player === 'undefined'){ player = videojs('mainVideo');} player.play(); }, 150);";
+                                    } else { ?>
+                                        if (Cookies.get('autoplay') && Cookies.get('autoplay') !== 'false') {
+                                            setTimeout(function () { if(typeof player === 'undefined'){ player = videojs('mainVideo');} player.play();}, 150);                    
+                                        }
+                                <?php } ?>
+                                        num = $('#videosList').find('.pagination').find('li.active').attr('data-lp');
+                                        loadPage(num);                 
+                                    });
+                                //$(".vjs-big-play-button").hide();
+                                $(".vjs-control-bar").css("opacity: 1; visibility: visible;");
+                                player.on('play', function () {
+                                    addView(<?php echo $video['id']; ?>);
+                                });
+                                <?php if (!empty($logId)) { ?>
+                                    isPlayingAd = true;
+                                    this.on('ended', function () {
+                                    console.log("Finish Video");
+                                    if (isPlayingAd) {
+                                        isPlayingAd = false;
+                                        $('#adButton').trigger("click");
+                                    }
+                                    <?php
+	                                   // if autoplay play next video
+	                                   if (!empty($autoPlayVideo)) { ?>
+                                            else if (Cookies.get('autoplay') && Cookies.get('autoplay') !== 'false') {
+                                                document.location = '<?php echo $autoPlayVideo['url']; ?>';
+                                            }
+                                    <?php } ?>
+                                    });
+                                    this.on('timeupdate', function () {
+                                        var durationLeft = fullDuration - this.currentTime();
+                                        $("#adUrl .time").text(secondsToStr(durationLeft + 1, 2));
+                                    <?php if (!empty($ad['skip_after_seconds'])) { ?>
+                                        if (isPlayingAd && this.currentTime() ><?php
+		                                      echo intval($ad['skip_after_seconds']); ?>) {
+                                                $('#adButton').fadeIn();
+                                                }
+                                    <?php } ?>
+                                    });
+                                <?php } else { ?>
+                                    this.on('ended', function () {
+                                        console.log("Finish Video");
+                                <?php if (!empty($autoPlayVideo)) { ?>
+                                            if (Cookies.get('autoplay') && Cookies.get('autoplay') !== 'false') {
+                                                document.location = '<?php echo $autoPlayVideo['url']; ?>';
+                                            }
+                                <?php } ?>
 
-                    //$(".vjs-big-play-button").hide();
-                    $(".vjs-control-bar").css("opacity: 1; visibility: visible;");
-                    
-                    player.on('play', function () {
-                        addView(<?php echo $video['id']; ?>);
-                    });
-                });
-                </script>
+                                    });
+                                <?php } ?>
+                                });
+                                player.ready(function () {
+                                    player.persistvolume({
+                                        namespace: "YouPHPTube"
+                                    });
+                                });
+                                <?php if (!empty($logId)){
+	                               $sources = getSources($video['filename'], true); ?>
+                                    $('#adButton').click(function () {
+                                        isPlayingAd = false;
+                                        console.log("Change Video");
+                                        fullDuration = strToSeconds('<?php echo $video['duration']; ?>');
+                                        changeVideoSrc(player, <?php echo json_encode($sources); ?>);
+                                        addView(<?php echo $video['id']; ?>);
+                                        $(".ad").removeClass("ad");
+                                        return false;
+                                    });
+                                <?php } ?>
+                        </script>
                 
                 <?php
-                }
+                } // youtube! end
                 require_once $global['systemRootPath'] . 'plugin/YouPHPTubePlugin.php';
                 // the live users plugin
                 if (YouPHPTubePlugin::isEnabled("0e225f8e-15e2-43d4-8ff7-0cb07c2a2b3b")) {
