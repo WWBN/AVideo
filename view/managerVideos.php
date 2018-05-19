ad66ee4cc10ad0aa9a10b544da7b1af2a3285acb
Vinzenz Hersche <hersche@openmailbox.org>
2018-05-19 02:28:46 +0200

    translate bootgrid to german and make it possible at all.. #685

--- view/managerVideos.php
+++ view/managerVideos.php
@@ -754,6 +754,14 @@ if (!empty($row)) {
                                             $('#inputNextVideo-id').val("");
                                         });
                                         var grid = $("#grid").bootgrid({
+                                            labels: {
+                                                noResults: "<?php echo __("No results found!"); ?>",
+                                                all: "<?php echo __("All"); ?>",
+                                                infos: "<?php echo __("Showing {{ctx.start}} to {{ctx.end}} of {{ctx.total}} entries"); ?>",
+                                                loading: "<?php echo __("Loading..."); ?>",
+                                                refresh: "<?php echo __("Refresh"); ?>",
+                                                search: "<?php echo __("Search"); ?>",
+                                            },
                                             ajax: true,
                                             url: "<?php echo $global['webSiteRootURL'] . "videos.json"; ?>",
                                             formatters: {
