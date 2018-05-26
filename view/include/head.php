73313597797bdc269c74e0d7eaa39b83abc3aacc
daniel <daniel@daniel-DC1A-S>
2018-05-26 11:09:09 -0300

    Remove duplicate fontawesome from header

--- view/include/head.php
+++ view/include/head.php
@@ -15,7 +15,6 @@ $theme = $config->getTheme();
 <meta name="description" content="<?php echo $custom; ?>">
 <link rel="icon" href="<?php echo $global['webSiteRootURL']; ?>img/favicon.png">
 <link href="<?php echo $global['webSiteRootURL']; ?>bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
-<link href="<?php echo $global['webSiteRootURL']; ?>css/font-awesome-5.0.10/web-fonts-with-css/css/fontawesome.min.css" rel="stylesheet" type="text/css"/>
 <link href="<?php echo $global['webSiteRootURL']; ?>css/font-awesome-5.0.10/web-fonts-with-css/css/fontawesome-all.min.css" rel="stylesheet" type="text/css"/>
 <link href="<?php echo $global['webSiteRootURL']; ?>css/flagstrap/css/flags.css" rel="stylesheet" type="text/css"/>
 <?php
