abd4e3ecab059a6d220a34a996260fdfcecf2de7
daniel <daniel@daniel-DC1A-S>
2018-06-03 09:49:00 -0300

    Avoiding  Error in writeSql : (1366) Incorrect integer value: '' for column 'type' at row 1, SQL-CMD:UPDATE `category_type_cache` SET `type` = '' WHERE `category_type_cache`.`categoryId` = ?;

--- objects/video.php
+++ objects/video.php
@@ -236,6 +236,8 @@ if (!class_exists('Video')) {
                         $sql .= "1";
                     } else if ($videoFound) {
                         $sql .= "2";
+                    }else{
+                        $sql .= "0";
                     }
                     $sql .= "' WHERE `category_type_cache`.`categoryId` = ?;";
                     sqlDAL::writeSql($sql,"i",array($catId));
