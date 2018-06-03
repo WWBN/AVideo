91246f1c0439a8898839060d4210cd75fdf3ee5e
Vinzenz Hersche <hersche@openmailbox.org>
2018-06-03 05:32:09 +0200

    split to linkVideo and linkAudio instead of link only

--- update/updateDb.v5.5.sql
+++ update/updateDb.v5.5.sql
@@ -1,1 +1,1 @@
-ALTER TABLE `videos` CHANGE `type` `type` ENUM('audio','video','embed','link') CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL DEFAULT 'video';
+ALTER TABLE `videos` CHANGE `type` `type` ENUM('audio','video','embed','linkVideo','linkAudio') CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL DEFAULT 'video';
