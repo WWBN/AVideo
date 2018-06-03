95908e491162a561913d2afff17d6f1f9eda3b1c
daniel <daniel@daniel-DC1A-S>
2018-06-03 19:01:30 -0300

    Temporary disable LiveLink Images

--- plugin/LiveLinks/getImage.php
+++ plugin/LiveLinks/getImage.php
@@ -2,6 +2,12 @@
 
 require_once '../../videos/configuration.php';
 session_write_close();
+
+header('Content-Type: image/x-png');
+$filename = $global['systemRootPath'] . 'plugin/Live/view/OnAir.jpg';
+echo file_get_contents($filename);
+exit;
+
 require_once $global['systemRootPath'] . 'objects/user.php';
 require_once $global['systemRootPath'] . 'objects/subscribe.php';
 require_once $global['systemRootPath'] . 'objects/functions.php';
@@ -11,10 +17,8 @@ $liveLink = new LiveLinksTable($_GET['id']);
 if (empty($_GET['format'])) {
     $_GET['format'] = "png";
 }
-header('Content-Type: image/x-png');
 $video = $liveLink->getLink();
 
-$filename = $global['systemRootPath'] . 'plugin/Live/view/OnAir.jpg';
     
 if (preg_match("/\b(?:(?:https?):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i", $video)) {
     $url = $config->getEncoderURL() . "getImage/" . base64_encode($video) . "/{$_GET['format']}";
