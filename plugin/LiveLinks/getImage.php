9becba18b2904c074caaf547d9937808b47d0ffd
daniel <daniel@daniel-DC1A-S>
2018-06-03 21:36:36 -0300

    Better debug

--- plugin/LiveLinks/getImage.php
+++ plugin/LiveLinks/getImage.php
@@ -22,21 +22,21 @@ if (preg_match("/\b(?:(?:https?):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+
     if (empty($_SESSION[$url]['expire']) || $_SESSION[$url]['expire'] < time()) {
         $content = url_get_contents($url);
         session_start();
-        error_log($url . " Image Expired in ".  date("d/m/Y H:i:s", @$_SESSION[$url]['expire'])." NOW is ".  date("d/m/Y H:i:s"));
+        error_log(" Image Expired in ".  date("d/m/Y H:i:s", @$_SESSION[$url]['expire'])." NOW is ".  date("d/m/Y H:i:s"));
         $_SESSION[$url] = array('content' => $content, 'expire' => strtotime("+2 min"));
-        error_log($url . " New Image will Expired in ".  date("d/m/Y H:i:s", $_SESSION[$url]['expire'])." NOW is ".  date("d/m/Y H:i:s"));
+        error_log(" New Image will Expired in ".  date("d/m/Y H:i:s", $_SESSION[$url]['expire'])." NOW is ".  date("d/m/Y H:i:s"));
     }
     if(!empty($_SESSION[$url]['content'])){
         echo $_SESSION[$url]['content'];
-        error_log($url . " Cached Good until ".  date("d/m/Y H:i:s", $_SESSION[$url]['expire'])." NOW is ".  date("d/m/Y H:i:s"));
+        error_log(" Cached Good until ".  date("d/m/Y H:i:s", $_SESSION[$url]['expire'])." NOW is ".  date("d/m/Y H:i:s"));
     }else{
         echo file_get_contents($filename);
-        error_log($url . " Get default image ");
+        error_log(" Get default image ");
     }
     
 } else {
     echo file_get_contents($filename);
-    error_log($url . " Invalid URL ");
+    error_log(" Invalid URL ");
 }
 $p = YouPHPTubePlugin::loadPluginIfEnabled("Cache");
 if(!empty($p)){
