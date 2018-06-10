a57c50b9fde5767a322a8208f6f2c077f2386eb6
Andres Tello Abrego <criptos@mobil.aullox.com>
2018-06-10 01:16:31 -0400

    Fix overwriting of object user, because is used with no scope

--- view/include/head.php
+++ view/include/head.php
@@ -61,6 +61,7 @@ if (!$config->getDisable_analytics()) {
 echo $config->getHead();
 echo $head;
 
-        $user = new User($video['users_id']);
-        echo $user->getAnalytics();
+$userAnalytics = new User($video['users_id']);
+echo $userAnalytics->getAnalytics();
+unset($userAnalytics);
 ?>
