0c486a7e94c6b5ccfc6c1b11dec4566f90fe0d93
daniel <daniel@daniel-DC1A-S>
2018-06-23 11:02:35 -0300

    Add videos ads frequency

--- plugin/AD_Server/AD_Server.php
+++ plugin/AD_Server/AD_Server.php
@@ -14,7 +14,7 @@ require_once $global['systemRootPath'] . 'plugin/AD_Server/Objects/VastCampaigns
 class AD_Server extends PluginAbstract {
 
     public function getDescription() {
-        return "VAST Ad Server";
+        return "VAST Ad Server<br><small>If you want to show the ads on a specific position set showAdsOnRandomPositions to 0</small>";
     }
 
     public function getName() {
@@ -25,105 +25,184 @@ class AD_Server extends PluginAbstract {
         return "3f2a707f-3c06-4b78-90f9-a22f2fda92ef";
     }
 
+    public function getEmptyDataObject() {
+        $obj = new stdClass();
+        $obj->start = true;
+        $obj->mid25Percent = true;
+        $obj->mid50Percent = true;
+        $obj->mid75Percent = true;
+        $obj->end = true;
+        $obj->skipoffset = "10%";
+        $obj->showMarkers = true;
+        $obj->showAdsOnEachVideoView = 1;
+        $obj->showAdsOnRandomPositions = 2;
+        return $obj;
+    }
+
+    public function canLoadAds() {
+        if (empty($_GET['videoName'])) {
+            return false;
+        }
+        session_write_close();
+        session_start();
+        // count it each 2 seconds
+        if (empty($_SESSION['lastAdShowed']) || $_SESSION['lastAdShowed'] + 2 <= time()) {
+            $_SESSION['lastAdShowed'] = time();
+
+            if (!isset($_SESSION['showAdsCount'])) {
+                error_log("Show Ads Count started");
+                $_SESSION['showAdsCount'] = 1;
+            } else {
+                $_SESSION['showAdsCount'] ++;
+            }
+        }
+        error_log("Show Ads Count {$_SESSION['showAdsCount']}");
+        $obj = $this->getDataObject();
+        if ($_SESSION['showAdsCount'] % $obj->showAdsOnEachVideoView === 0) {
+            return true;
+        }
+        session_write_close();
+        return false;
+    }
+
     public function getHeadCode() {
         $obj = $this->getDataObject();
-        if (empty($_GET['videoName'])) {
+        if (!$this->canLoadAds()) {
             return "";
         }
         global $global;
-        $_GET['vmap_id'] = uniqid();
-        
-        $js   = '<script src="//imasdk.googleapis.com/js/sdkloader/ima3.js"></script>';
-        $css  = '<link href="' . $global['webSiteRootURL'] . 'plugin/AD_Server/videojs-ima/videojs.ima.css" rel="stylesheet" type="text/css"/>';
-        
-        if(!empty($obj->showMarkers)){
-            $css .= '<link href="' . $global['webSiteRootURL'] . 'plugin/AD_Server/videojs-markers/videojs.markers.css" rel="stylesheet" type="text/css"/>';            
+        $_GET['vmap_id'] = session_id();
+
+        $js = '<script src="//imasdk.googleapis.com/js/sdkloader/ima3.js"></script>';
+        $css = '<link href="' . $global['webSiteRootURL'] . 'plugin/AD_Server/videojs-ima/videojs.ima.css" rel="stylesheet" type="text/css"/>';
+
+        if (!empty($obj->showMarkers)) {
+            $css .= '<link href="' . $global['webSiteRootURL'] . 'plugin/AD_Server/videojs-markers/videojs.markers.css" rel="stylesheet" type="text/css"/>';
         }
         $css .= '<style>.ima-ad-container{z-index:1000 !important;}</style>';
         return $js . $css;
     }
 
-    public function getEmptyDataObject() {
-        $obj = new stdClass();
-        $obj->start = true;
-        $obj->mid25Percent = false;
-        $obj->mid50Percent = false;
-        $obj->mid75Percent = false;
-        $obj->end = true;
-        $obj->skipoffset = "10%";
-        $obj->showMarkers = true;
-        $obj->showAdsOnEachVideoView = 1;
-        return $obj;
-    }
-
     public function getFooterCode() {
         $obj = $this->getDataObject();
-        if (empty($_GET['videoName'])) {
+        if (!$this->canLoadAds()) {
             return "";
         }
         global $global;
 
         $video = Video::getVideoFromCleanTitle($_GET['videoName']);
         $video_length = parseDurationToSeconds($video['duration']);
-        
-        $vmap_id = @$_GET['vmap_id'];        
-        
-        if(!empty($_GET['vmap_id']) && !empty($_SESSION['vmap'][$_GET['vmap_id']])){
+
+        $vmap_id = @$_GET['vmap_id'];
+
+        if (!empty($_GET['vmap_id']) && !empty($_SESSION['vmap'][$_GET['vmap_id']])) {
             $vmaps = unserialize($_SESSION['vmap'][$_GET['vmap_id']]);
-        }else{
+        } else {
             $vmaps = $this->getVMAPs($video_length);
             $_SESSION['vmap'][$_GET['vmap_id']] = serialize($vmaps);
         }
-        
-        if($this->VMAPsHasVideos()){
+
+        if ($this->VMAPsHasVideos()) {
             include $global['systemRootPath'] . 'plugin/AD_Server/footer.php';
-        }else{
+        } else {
             echo "<!-- NO Videos found for VAST ads -->";
         }
+    }
+    
+    private function getRandomPositions(){        
+        
+        $obj = $this->getDataObject();
+        session_write_close();
+        session_id($_GET['vmap_id']);
+        session_start();
+        $options = array();
+        
+        if (!empty($obj->start)) {
+            $options[] = 1;
+        }
+        if (!empty($obj->mid25Percent)) {
+            $options[] = 2;
+        }
+        if (!empty($obj->mid50Percent) ) {
+            $options[] = 3;
+        }
+        if (!empty($obj->mid75Percent)) {
+            $options[] = 4;
+        }
+        if (!empty($obj->end)) {
+            $options[] = 5;
+        }
+        
+        $selectedOptions = array();
+        // count it each 2 seconds
+        error_log('sessions_id');
+        error_log(session_id());
+        error_log($_SESSION['lastAdRandomPositions']);
+        if (empty($_SESSION['lastAdRandomPositions']) || $_SESSION['lastAdRandomPositions'] + 20 <= time()) {
+            $_SESSION['lastAdRandomPositions'] = time();
+
+            
+            if (empty($obj->showAdsOnRandomPositions)) {
+                $selectedOptions = $options;
+            } else {
+                for ($i = 0; $i < $obj->showAdsOnRandomPositions; $i++) {
+                    shuffle($options);
+                    $selectedOptions[] = array_pop($options);
+                }
+            }
+            $_SESSION['adRandomPositions'] = $selectedOptions;
+        }
+        session_write_close();
+        
+        
+        error_log("VMAP select those options: ".print_r($_SESSION['adRandomPositions'], true));
+        return $_SESSION['adRandomPositions'];
     }
 
     public function getVMAPs($video_length) {
         $vmaps = array();
-        
+
         $obj = $this->getDataObject();
-        if (!empty($obj->start)) {
+
+        $selectedOptions = $this->getRandomPositions();
+
+        if (!empty($obj->start) && in_array(1, $selectedOptions)) {
             $vmaps[] = new VMAP("start", new VAST(1));
         }
-        if (!empty($obj->mid25Percent)) {
+        if (!empty($obj->mid25Percent) && in_array(2, $selectedOptions)) {
             $val = $video_length * (25 / 100);
             $vmaps[] = new VMAP($val, new VAST(2));
         }
-        if (!empty($obj->mid50Percent)) {
+        if (!empty($obj->mid50Percent) && in_array(3, $selectedOptions)) {
             $val = $video_length * (50 / 100);
             $vmaps[] = new VMAP($val, new VAST(3));
         }
-        if (!empty($obj->mid75Percent)) {
+        if (!empty($obj->mid75Percent) && in_array(4, $selectedOptions)) {
             $val = $video_length * (75 / 100);
             $vmaps[] = new VMAP($val, new VAST(4));
         }
-        if (!empty($obj->end)) {
+        if (!empty($obj->end) && in_array(5, $selectedOptions)) {
             $vmaps[] = new VMAP("end", new VAST(5), $video_length);
         }
-        
+
         return $vmaps;
     }
-    
+
     public function VMAPsHasVideos() {
         $vmaps = $this->getVMAPs(100);
         //var_dump($vmaps);exit;
-        foreach($vmaps as $value){
-            if(empty($value->VAST->campaing)){
+        foreach ($vmaps as $value) {
+            if (empty($value->VAST->campaing)) {
                 return false;
             }
         }
         return true;
     }
-    
-    public function showAdsNow(){
-        if(!$this->VMAPsHasVideos()){
+
+    public function showAdsNow() {
+        if (!$this->VMAPsHasVideos()) {
             return false;
         }
-        
     }
 
     static public function getVideos() {
