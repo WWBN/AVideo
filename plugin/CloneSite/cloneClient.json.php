87b43af09b8698e16e99949c9ff9fc7ab10a0b14
daniel <daniel@daniel-DC1A-S>
2018-06-30 11:54:30 -0300

    Last fixes

--- plugin/CloneSite/cloneClient.json.php
+++ plugin/CloneSite/cloneClient.json.php
@@ -3,6 +3,7 @@ require_once '../../videos/configuration.php';
 set_time_limit(0);
 require_once $global['systemRootPath'] . 'objects/plugin.php';
 require_once $global['systemRootPath'] . 'plugin/CloneSite/CloneSite.php';
+require_once $global['systemRootPath'] . 'plugin/CloneSite/CloneLog.php';
 require_once $global['systemRootPath'] . 'plugin/CloneSite/functions.php';
 session_write_close();
 header('Content-Type: application/json');
@@ -11,18 +12,20 @@ $resp = new stdClass();
 $resp->error = true;
 $resp->msg = "";
 
-error_log("Clone Start");
+$log = new CloneLog();
+
+$log->add("Clone Start");
 
 if(!User::isAdmin()){
     $resp->msg = "You cant do this";
-    error_log("Clone: {$resp->msg}");
+    $log->add("Clone: {$resp->msg}");
     die(json_encode($resp));
 }
 
 $obj = YouPHPTubePlugin::getObjectDataIfEnabled("CloneSite");
 if(empty($obj->cloneSiteURL)){
     $resp->msg = "Your Clone Site URL is empty, please click on the Edit parameters buttons and place an YouPHPTube URL";
-    error_log("Clone: {$resp->msg}");
+    $log->add("Clone: {$resp->msg}");
     die(json_encode($resp));
 }
 
@@ -41,17 +44,23 @@ if (!file_exists($photosDir)) {
 
 $url = $obj->cloneSiteURL."plugin/CloneSite/cloneServer.json.php?url=".urlencode($global['webSiteRootURL'])."&key={$obj->myKey}";
 // check if it respond
-error_log("Clone: check URL {$url}");
-//$content = url_get_contents($url);
-var_dump($content);exit;
+$log->add("Clone: check URL {$url}");
+$content = url_get_contents($url);
+//var_dump($url, $content);exit;
 $json = json_decode($content);
+
+if(!empty($json->error)){
+    $resp->msg = "Clone Server message: {$json->msg}";
+    $log->add("Clone: {$resp->msg}");
+    die(json_encode($resp));
+}
 
 // get dump file
 $cmd = "wget -O {$clonesDir}{$json->sqlFile} {$obj->cloneSiteURL}videos/cache/clones/{$json->sqlFile}";
-error_log("Clone: Get Dump {$cmd}");
+$log->add("Clone: Get Dump {$cmd}");
 exec($cmd." 2>&1", $output, $return_val);
 if ($return_val !== 0) {
-    error_log("Clone Error: ". print_r($output, true));
+    $log->add("Clone Error: ". print_r($output, true));
 }
 
 // remove the first warning line
@@ -62,10 +71,10 @@ file_put_contents($file, implode("\r\n", $contents));
 
 // restore dump
 $cmd = "mysql -u {$mysqlUser} -p{$mysqlPass} --host {$mysqlHost} {$mysqlDatabase} < {$clonesDir}{$json->sqlFile}";
-error_log("Clone: restore dump {$cmd}");
+$log->add("Clone: restore dump {$cmd}");
 exec($cmd." 2>&1", $output, $return_val);
 if ($return_val !== 0) {
-    error_log("Clone Error: ". print_r($output, true));
+    $log->add("Clone Error: ". print_r($output, true));
 }
 
 $videoFiles = getCloneFilesInfo($videosDir);
@@ -73,15 +82,21 @@ $newVideoFiles = detectNewFiles($json->videoFiles, $videoFiles);
 $photoFiles = getCloneFilesInfo($photosDir, "userPhoto/");
 $newPhotoFiles = detectNewFiles($json->photoFiles, $photoFiles);
 
+$total = count($newVideoFiles);
+$count = 0;
 // copy videos
 foreach ($newVideoFiles as $value) {
-    error_log("Clone: Copying {$value->url}");
+    $count++;
+    $log->add("Clone: Copying Videos {$count} of {$total} {$value->url}");
     file_put_contents("{$videosDir}{$value->filename}", fopen("$value->url", 'r'));
 }
 
+$total = count($newPhotoFiles);
+$count = 0;
 // copy Photos
 foreach ($newPhotoFiles as $value) {
-    error_log("Clone: Copying {$value->url}");
+    $count++;
+    $log->add("Clone: Copying Photos {$count} of {$total} {$value->url}");
     file_put_contents("{$photosDir}{$value->filename}", fopen("$value->url", 'r'));
 }
 
