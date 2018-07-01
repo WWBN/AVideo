9df032ec13cd30f4dc9a7472d4d08a977317ffd7
daniel <daniel@daniel-DC1A-S>
2018-07-01 12:30:47 -0300

    Fix the Clone

--- plugin/CloneSite/cloneClient.json.php
+++ plugin/CloneSite/cloneClient.json.php
@@ -1,4 +1,5 @@
 <?php
+
 require_once '../../videos/configuration.php';
 set_time_limit(0);
 require_once $global['systemRootPath'] . 'objects/plugin.php';
@@ -18,14 +19,14 @@ $log = new CloneLog();
 
 $log->add("Clone: Clone Start");
 
-if(!User::isAdmin()){
+if (!User::isAdmin()) {
     $resp->msg = "You cant do this";
     $log->add("Clone: {$resp->msg}");
     die(json_encode($resp));
 }
 
 $obj = YouPHPTubePlugin::getObjectDataIfEnabled("CloneSite");
-if(empty($obj->cloneSiteURL)){
+if (empty($obj->cloneSiteURL)) {
     $resp->msg = "Your Clone Site URL is empty, please click on the Edit parameters buttons and place an YouPHPTube URL";
     $log->add("Clone: {$resp->msg}");
     die(json_encode($resp));
@@ -38,26 +39,26 @@ $photosDir = "{$videosDir}userPhoto/";
 $photosSite = "{$videosSite}userPhoto/";
 if (!file_exists($clonesDir)) {
     mkdir($clonesDir, 0777, true);
-    file_put_contents($clonesDir."index.html", '');
+    file_put_contents($clonesDir . "index.html", '');
 }
 if (!file_exists($photosDir)) {
     mkdir($photosDir, 0777, true);
 }
 
-$url = $obj->cloneSiteURL."plugin/CloneSite/cloneServer.json.php?url=".urlencode($global['webSiteRootURL'])."&key={$obj->myKey}";
+$url = $obj->cloneSiteURL . "plugin/CloneSite/cloneServer.json.php?url=" . urlencode($global['webSiteRootURL']) . "&key={$obj->myKey}";
 // check if it respond
 $log->add("Clone (1 of {$totalSteps}): Asking the Server the database and the files");
 $content = url_get_contents($url);
 //var_dump($url, $content);exit;
 $json = json_decode($content);
 
-if(empty($json)){
+if (empty($json)) {
     $resp->msg = "Clone Server Unknow ERROR";
     $log->add("Clone: Server Unknow ERROR");
     die(json_encode($resp));
 }
 
-if(!empty($json->error)){
+if (!empty($json->error)) {
     $resp->msg = "Clone Server message: {$json->msg}";
     $log->add("Clone: {$resp->msg}");
     die(json_encode($resp));
@@ -68,9 +69,9 @@ $log->add("Clone: Good start! the server has answered");
 // get dump file
 $cmd = "wget -O {$clonesDir}{$json->sqlFile} {$obj->cloneSiteURL}videos/cache/clones/{$json->sqlFile}";
 $log->add("Clone (2 of {$totalSteps}): Geting MySQL Dump file");
-exec($cmd." 2>&1", $output, $return_val);
+exec($cmd . " 2>&1", $output, $return_val);
 if ($return_val !== 0) {
-    $log->add("Clone Error: ". print_r($output, true));
+    $log->add("Clone Error: " . print_r($output, true));
 }
 $log->add("Clone: Nice! we got the MySQL Dump file");
 
@@ -83,9 +84,9 @@ file_put_contents($file, implode("\r\n", $contents));
 $log->add("Clone (3 of {$totalSteps}): Overwriting our database with the server database");
 // restore dump
 $cmd = "mysql -u {$mysqlUser} -p{$mysqlPass} --host {$mysqlHost} {$mysqlDatabase} < {$clonesDir}{$json->sqlFile}";
-exec($cmd." 2>&1", $output, $return_val);
+exec($cmd . " 2>&1", $output, $return_val);
 if ($return_val !== 0) {
-    $log->add("Clone Error: ". print_r($output, true));
+    $log->add("Clone Error: " . print_r($output, true));
 }
 $log->add("Clone: Great! we overwrite it with success.");
 
@@ -97,37 +98,45 @@ $newPhotoFiles = detectNewFiles($json->photoFiles, $photoFiles);
 $total = count($newVideoFiles);
 $count = 0;
 
-$log->add("Clone (4 of {$totalSteps}): Now we will copy {$total} new video files, usually this takes a while.");
-// copy videos
-foreach ($newVideoFiles as $value) {
-    $count++;
-    $log->add("Clone: Copying Videos {$count} of {$total} {$value->url}");
-    file_put_contents("{$videosDir}{$value->filename}", fopen("$value->url", 'r'));
+if (!empty($total)) {
+    $log->add("Clone (4 of {$totalSteps}): Now we will copy {$total} new video files, usually this takes a while.");
+    // copy videos
+    foreach ($newVideoFiles as $value) {
+        $count++;
+        $log->add("Clone: Copying Videos {$count} of {$total} {$value->url}");
+        file_put_contents("{$videosDir}{$value->filename}", fopen("$value->url", 'r'));
+    }
+    $log->add("Clone: Copying video files done.");
+} else {
+    $log->add("Clone (4 of {$totalSteps}): There is no new video file to copy.");
 }
-$log->add("Clone: Copying video files done.");
 
 $total2 = count($newPhotoFiles);
 $count2 = 0;
 
-$log->add("Clone (5 of {$totalSteps}): Now we will copy {$total2} new user photo files.");
+if (!empty($total2)) {
+    $log->add("Clone (5 of {$totalSteps}): Now we will copy {$total2} new user photo files.");
 // copy Photos
-foreach ($newPhotoFiles as $value) {
-    $count2++;
-    $log->add("Clone: Copying Photos {$count2} of {$total2} {$value->url}");
-    file_put_contents("{$photosDir}{$value->filename}", fopen("$value->url", 'r'));
+    foreach ($newPhotoFiles as $value) {
+        $count2++;
+        $log->add("Clone: Copying Photos {$count2} of {$total2} {$value->url}");
+        file_put_contents("{$photosDir}{$value->filename}", fopen("$value->url", 'r'));
+    }
+    $log->add("Clone: Copying user photo files done.");
+} else {
+    $log->add("Clone (5 of {$totalSteps}): There is no new video file to copy.");
 }
-$log->add("Clone: Copying user photo files done.");
 
 // notify to delete dump
-$url = $url."&deleteDump={$json->sqlFile}";
+$url = $url . "&deleteDump={$json->sqlFile}";
 // check if it respond
 $log->add("Clone (6 of {$totalSteps}): Notify Server to Delete Dump");
 $content2 = url_get_contents($url);
 //var_dump($url, $content);exit;
 $json2 = json_decode($content);
-if(!empty($json2->error)){
+if (!empty($json2->error)) {
     $log->add("Clone: Dump NOT deleted");
-}else{
+} else {
     $log->add("Clone: Dump DELETED");
 }
 
@@ -137,7 +146,7 @@ $log->add("Clone (7 of {$totalSteps}): Resotre the Clone Configuration");
 $plugin = new CloneSite();
 $p = new Plugin(0);
 $p->loadFromUUID($plugin->getUUID());
-$p->setObject_data(json_encode($obj, JSON_UNESCAPED_UNICODE ));
+$p->setObject_data(json_encode($obj, JSON_UNESCAPED_UNICODE));
 $p->save();
 
 echo json_encode($json);
