298a77ceb0c5793306210f81b2d55be5d77b396a
daniel <daniel@daniel-DC1A-S>
2018-07-03 11:24:56 -0300

    Fix left menu

--- view/include/navbar.php
+++ view/include/navbar.php
@@ -352,7 +352,7 @@ if (empty($advancedCustom->userMustBeLoggedIn) || User::isLogged()) {
                         <li>
                             <div>
                                 <a href="<?php echo $global['webSiteRootURL']; ?>logoff" class="btn btn-default btn-block" >
-                                    <i class="fas fa-sign-out-alt"></i> <?php echo __("Logoff"); ?>
+                                    <i class="fas fa-sign-out-alt"></i> <?php echo __("Sign out"); ?>
                                 </a>
                             </div>
                         </li>
