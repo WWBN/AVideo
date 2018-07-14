d62e7163815d1f13e8f86c91865f0e2b555ebc61
daniel <daniel@daniel-DC1A-S>
2018-07-14 10:45:21 -0300

    Click on esc only once to leave fullscreeen

--- plugin/Gallery/fullscreen.js
+++ plugin/Gallery/fullscreen.js
@@ -2,24 +2,13 @@ $(document).ready(function () {
     $('a.galleryLink').click(function (event) {
         event.preventDefault();
         var id = $(this).attr('videos_id');
-        $('body').addClass('fullScreen');
-        fullscreenOn()
-        var div = $('<div id="divIframeFull" style="background-color:black;"><div id="divTopBar" style="position: fixed; top: 0; left: 0; height: 50px; width: 100vw; z-index: 99999; padding:10px; "><span id="closeBtnFull" class="btn pull-right" onclick="closeIframe();" style="opacity: 0.5; filter: alpha(opacity=50);"><i class="fa fa-times"></i></span></div></div>').append('<iframe src="' + webSiteRootURL + 'v/' + id + '" style="background-color:black; position: fixed; top: 0; left: 0; height: 100vh; width: 100vw; z-index: 9999; overflow: hidden;" id="iframeFull">');
-        $('body').append(div);
-        $("#divIframeFull").fadeIn();
-        Cookies.set("autoplay", true, {path: '/', expires: 365});
-    });
-    
+        startFullScreen(webSiteRootURL + 'v/' + id);
+    });    
     
     $('a.hrefLink').click(function (event) {
         event.preventDefault();
         var link = $(this).attr('href');
-        $('body').addClass('fullScreen');
-        fullscreenOn()
-        var div = $('<div id="divIframeFull" style="background-color:black;"><div id="divTopBar" style="position: fixed; top: 0; left: 0; height: 50px; width: 100vw; z-index: 99999; padding:10px; "><span id="closeBtnFull" class="btn pull-right" onclick="closeIframe();" style="opacity: 0.5; filter: alpha(opacity=50);"><i class="fa fa-times"></i></span></div></div>').append('<iframe src="' + link + '" style="background-color:black; position: fixed; top: 0; left: 0; height: 100vh; width: 100vw; z-index: 9999; overflow: hidden;" id="iframeFull">');
-        $('body').append(div);
-        $("#divIframeFull").fadeIn();
-        Cookies.set("autoplay", true, {path: '/', expires: 365});
+        startFullScreen(link);
     });
 
     $(document).on('keyup', function (evt) {
@@ -27,8 +16,24 @@ $(document).ready(function () {
             closeIframe();
         }
     });
+    
+    
+    setInterval(function(){
+        if (!(!window.screenTop && !window.screenY)) {
+            closeIframe();
+        }
+    }, 1000);
 
 });
+
+function startFullScreen(link){
+    $('body').addClass('fullScreen');
+    fullscreenOn();
+    var div = $('<div id="divIframeFull" style="background-color:black;"><div id="divTopBar" style="position: fixed; top: 0; left: 0; height: 50px; width: 100vw; z-index: 99999; padding:10px; "><span id="closeBtnFull" class="btn pull-right" onclick="closeIframe();" style="opacity: 0.5; filter: alpha(opacity=50);"><i class="fa fa-times"></i></span></div></div>').append('<iframe src="' + link + '" style="background-color:black; position: fixed; top: 0; left: 0; height: 100vh; width: 100vw; z-index: 9999; overflow: hidden;" id="iframeFull">');
+    $('body').append(div);
+    $("#divIframeFull").fadeIn();
+    Cookies.set("autoplay", true, {path: '/', expires: 365});
+}
 
 function fullscreenOn(){
         var docElm = document.documentElement;
@@ -45,20 +50,21 @@ function fullscreenOn(){
 }
 
 function closeIframe() {
-    $("#divIframeFull").fadeOut("slow", function () {
-        $('body').removeClass('fullScreen');
-        $('#divIframeFull').remove();
-        if (document.exitFullscreen) {
-            document.exitFullscreen();
-        } else if (document.msExitFullscreen) {
-            document.msExitFullscreen();
-        } else if (document.mozCancelFullScreen) {
-            document.mozCancelFullScreen();
-        } else if (document.webkitCancelFullScreen) {
-            document.webkitCancelFullScreen();
-        }
-    });
-
+    if($('#divIframeFull').length){
+        $("#divIframeFull").fadeOut("slow", function () {
+            $('body').removeClass('fullScreen');
+            $('#divIframeFull').remove();
+            if (document.exitFullscreen) {
+                document.exitFullscreen();
+            } else if (document.msExitFullscreen) {
+                document.msExitFullscreen();
+            } else if (document.mozCancelFullScreen) {
+                document.mozCancelFullScreen();
+            } else if (document.webkitCancelFullScreen) {
+                document.webkitCancelFullScreen();
+            }
+        });
+    }
 
 }
 
