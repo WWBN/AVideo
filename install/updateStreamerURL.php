8b84a8607271ddaeec72b3f3f3391093120a5b77
daniel <daniel@daniel-DC1A-S>
2018-07-16 14:16:23 -0300

    Commandline tool to update an changed URL or domain

---
+++ install/updateStreamerURL.php
@@ -0,0 +1,35 @@
+<?php
+//streamer config
+require_once '../videos/configuration.php';
+
+if(!isCommandLineInterface()){
+    return die('Command Line only');
+}
+$streamerURL = trim(readline("Enter the new Streamer URL or press enter to skip: "));
+
+if(!empty($streamerURL)){
+    if (substr($streamerURL, -1) !== '/') {
+       $streamerURL.="/";
+    }
+    $global['webSiteRootURL'] = $streamerURL;
+    echo "Rewrite Streamer Config File\n";
+    // change the streamer config file
+    Configuration::rewriteConfigFile();
+}
+
+$encoderConfigFile = "{$global['systemRootPath']}Encoder/videos/configuration.php";
+
+if(file_exists($encoderConfigFile)){
+    require_once $encoderConfigFile;
+    // change the encoder database for admin user
+    echo "Encoder Update configurations set allowedStreamersURL\n";
+    $sql = "update configurations set allowedStreamersURL = '{$streamerURL}';";
+    $global['mysqli']->query($sql);
+    echo "Encoder Update streamers set siteURL\n";
+    $sql = "update streamers set siteURL = '{$streamerURL}';";
+    $global['mysqli']->query($sql);
+    // change the encoder config file
+}
+
+
+
