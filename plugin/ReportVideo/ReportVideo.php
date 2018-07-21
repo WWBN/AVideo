31e31a9dd9f1e3d2aa1c63bf628836a825fff303
daniel <daniel@daniel-DC1A-S>
2018-07-21 11:36:18 -0300

    Add a report button, this is necessary for iOS approval

--- plugin/ReportVideo/ReportVideo.php
+++ plugin/ReportVideo/ReportVideo.php
@@ -43,7 +43,7 @@ class ReportVideo extends PluginAbstract {
         //Set who the message is to be sent from
         $mail->setFrom($config->getContactEmail(), $config->getWebSiteTitle());
         //Set who the message is to be sent to
-        $mail->addCC($email);
+        $mail->addAddress($email);
         //Set the subject line
         $mail->Subject = $subject;
         $mail->msgHTML($body);
