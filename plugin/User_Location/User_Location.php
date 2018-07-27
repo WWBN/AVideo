001c21fa062f3b4a0de95ad0770383cd9eaaa97a
daniel <daniel@daniel-DC1A-S>
2018-07-27 15:34:26 -0300

    Add user to location plugin feature

--- plugin/User_Location/User_Location.php
+++ plugin/User_Location/User_Location.php
@@ -8,6 +8,7 @@ class User_Location extends PluginAbstract {
     public function getDescription() {
         $ret = "Detects user location for various purposes";
         $ret .= "<br>This site or product includes IP2Location LITE data available from http://www.ip2location.com.";
+        $ret .= "<br><strong>Before use this plugin unzip the install.zip file and install the IPs tables<strong>";
         return $ret;
     }
 
