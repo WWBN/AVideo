925c2d6be30a8937942031247dab68f196391eb5
daniel <daniel@daniel-DC1A-S>
2018-09-24 09:14:27 -0300

    Fix markers

--- plugin/AD_Server/footer.php
+++ plugin/AD_Server/footer.php
@@ -38,43 +38,40 @@ if (!empty($obj->showMarkers)) {
 }
 ?>
         player = videojs('mainVideo');
-        var options = {
-            id: 'mainVideo',
-            adTagUrl: '<?php echo $global['webSiteRootURL'] ?>plugin/AD_Server/VMAP.php?video_length=<?php echo $video_length ?>&vmap_id=<?php echo $vmap_id ?>'
-                    };
-                    player.ima(options);
-                    // Remove controls from the player on iPad to stop native controls from stealing
-                    // our click
-                    var contentPlayer = document.getElementById('content_video_html5_api');
-                    if ((navigator.userAgent.match(/iPad/i) ||
-                            navigator.userAgent.match(/Android/i)) &&
-                            contentPlayer.hasAttribute('controls')) {
-                        contentPlayer.removeAttribute('controls');
-                    }
-
-                    // Initialize the ad container when the video player is clicked, but only the
-                    // first time it's clicked.
-                    var startEvent = 'click';
-                    if (navigator.userAgent.match(/iPhone/i) ||
-                            navigator.userAgent.match(/iPad/i) ||
-                            navigator.userAgent.match(/Android/i)) {
-                        startEvent = 'touchend';
-                    }
-                    player.one(startEvent, function () {
-                        player.ima.initializeAdDisplayContainer();
-                    });
-                    setInterval(function () {
-                        fixAdSize();
-                    }, 100);
-                });
-
-                function fixAdSize() {
-                    ad_container = $('#mainVideo_ima-ad-container');
-                    if (ad_container.length) {
-                        height = ad_container.css('height');
-                        width = ad_container.css('width');
-                        $($('#mainVideo_ima-ad-container div:first-child')[0]).css({'height': height});
-                        $($('#mainVideo_ima-ad-container div:first-child')[0]).css({'width': width});
-                    }
+        var options = {id: 'mainVideo', adTagUrl: '<?php echo $global['webSiteRootURL'] ?>plugin/AD_Server/VMAP.php?video_length=<?php echo $video_length ?>&vmap_id=<?php echo $vmap_id ?>'};
+                player.ima(options);
+                // Remove controls from the player on iPad to stop native controls from stealing
+                // our click
+                var contentPlayer = document.getElementById('content_video_html5_api');
+                if ((navigator.userAgent.match(/iPad/i) ||
+                        navigator.userAgent.match(/Android/i)) &&
+                        contentPlayer.hasAttribute('controls')) {
+                    contentPlayer.removeAttribute('controls');
                 }
+
+                // Initialize the ad container when the video player is clicked, but only the
+                // first time it's clicked.
+                var startEvent = 'click';
+                if (navigator.userAgent.match(/iPhone/i) ||
+                        navigator.userAgent.match(/iPad/i) ||
+                        navigator.userAgent.match(/Android/i)) {
+                    startEvent = 'touchend';
+                }
+                player.one(startEvent, function () {
+                    player.ima.initializeAdDisplayContainer();
+                });
+                setInterval(function () {
+                    fixAdSize();
+                }, 100);
+            });
+
+            function fixAdSize() {
+                ad_container = $('#mainVideo_ima-ad-container');
+                if (ad_container.length) {
+                    height = ad_container.css('height');
+                    width = ad_container.css('width');
+                    $($('#mainVideo_ima-ad-container div:first-child')[0]).css({'height': height});
+                    $($('#mainVideo_ima-ad-container div:first-child')[0]).css({'width': width});
+                }
+            }
 </script>
