1741a8cbf6ea866ce2c0cf7252c094bf00e3cbcc
daniel <daniel@daniel-DC1A-S>
2018-10-06 12:06:00 -0300

    add a function to read tail

--- objects/functions.php
+++ objects/functions.php
@@ -1,33 +1,35 @@
 <?php
-function forbiddenWords($text){
+
+function forbiddenWords($text) {
     global $global;
-    if(empty($global['forbiddenWords'])){
+    if (empty($global['forbiddenWords'])) {
         return false;
     }
     foreach ($global['forbiddenWords'] as $value) {
-        if(preg_match("/{$value}/i", $text)){
+        if (preg_match("/{$value}/i", $text)) {
             return true;
         }
     }
     return false;
 }
 
-function xss_esc($text){
-  return htmlspecialchars($text, ENT_QUOTES, 'UTF-8');
+function xss_esc($text) {
+    return htmlspecialchars($text, ENT_QUOTES, 'UTF-8');
 }
 
-function xss_esc_back($text){
+function xss_esc_back($text) {
     $text = htmlspecialchars_decode($text, ENT_QUOTES);
-    $text = str_replace(array('&amp;','&#039;', "#039;"), array(" ", "`", "`"), $text);
+    $text = str_replace(array('&amp;', '&#039;', "#039;"), array(" ", "`", "`"), $text);
     return $text;
 }
 
 // make sure SecureVideosDirectory will be the first
 function cmpPlugin($a, $b) {
-    if ($a['name']=='SecureVideosDirectory')
+    if ($a['name'] == 'SecureVideosDirectory')
         return -1;
     return 1;
 }
+
 // Returns a file size limit in bytes based on the PHP upload_max_filesize
 // and post_max_size
 function file_upload_max_size() {
@@ -249,9 +251,9 @@ function getRealIpAddr() {
         $ip = $_SERVER['HTTP_CLIENT_IP'];
     } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {   //to check ip is pass from proxy
         $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
-    } else if(!empty ($_SERVER['REMOTE_ADDR'])) {
+    } else if (!empty($_SERVER['REMOTE_ADDR'])) {
         $ip = $_SERVER['REMOTE_ADDR'];
-    }else{
+    } else {
         $ip = "127.0.0.1";
     }
     return $ip;
@@ -338,7 +340,7 @@ function getSecondsTotalVideosLength() {
         $values = array($id);
     }
 
-    $res = sqlDAL::readSql($sql,$formats,$values);
+    $res = sqlDAL::readSql($sql, $formats, $values);
     $fullData = sqlDAL::fetchAllAssoc($res);
     sqlDAL::close($res);
     $seconds = 0;
@@ -759,7 +761,7 @@ function im_resize($file_src, $file_dest, $wd, $hd, $q = 50) {
 function im_resizeV2($file_src, $file_dest, $wd, $hd, $q = 50) {
 
     $newImage = im_resize($file_src, $file_dest, $wd, $hd);
-    if(!$newImage){
+    if (!$newImage) {
         return false;
     }
     $src = imagecreatefromjpeg($file_dest);
@@ -767,14 +769,14 @@ function im_resizeV2($file_src, $file_dest, $wd, $hd, $q = 50) {
     $hs = imagesy($src);
 
     if ($ws < $wd) {
-        $dst_x = ($wd-$ws)/2;
-    }else{
+        $dst_x = ($wd - $ws) / 2;
+    } else {
         $dst_x = 0;
     }
 
-    if($hs < $hd){
-        $dst_y = ($hd-$hs)/2;
-    }else{
+    if ($hs < $hd) {
+        $dst_y = ($hd - $hs) / 2;
+    } else {
         $dst_y = 0;
     }
 
@@ -802,13 +804,12 @@ function decideMoveUploadedToVideos($tmp_name, $filename) {
         $bb_b2->move_uploaded_file($tmp_name, $filename);
     } else {
         if (!move_uploaded_file($tmp_name, "{$global['systemRootPath']}videos/{$filename}")) {
-            if(!rename($tmp_name, "{$global['systemRootPath']}videos/{$filename}")){
-                if(!copy($tmp_name, "{$global['systemRootPath']}videos/{$filename}")){
+            if (!rename($tmp_name, "{$global['systemRootPath']}videos/{$filename}")) {
+                if (!copy($tmp_name, "{$global['systemRootPath']}videos/{$filename}")) {
                     $obj->msg = "Error on decideMoveUploadedToVideos({$tmp_name}, {$global['systemRootPath']}videos/{$filename})";
                     die(json_encode($obj));
                 }
             }
-
         }
     }
 }
@@ -908,21 +909,19 @@ function combineFiles($filesArray, $extension = "js") {
     foreach ($filesArray as $value) {
         $fileName .= $value;
     }
-    if($advancedCustom!=false){
+    if ($advancedCustom != false) {
         $minifyEnabled = $advancedCustom->EnableMinifyJS;
     } else {
-      $minifyEnabled = false;
+        $minifyEnabled = false;
     }
     $md5FileName = md5($fileName) . ".{$extension}";
     if (!file_exists($cacheDir . $md5FileName)) {
         foreach ($filesArray as $value) {
-            if(file_exists($global['systemRootPath'].$value)){
-                $str .= "\n/*{$value} created local with systemRootPath */\n" . local_get_contents($global['systemRootPath'].$value);
-            }
-            else if(file_exists($value)){
+            if (file_exists($global['systemRootPath'] . $value)) {
+                $str .= "\n/*{$value} created local with systemRootPath */\n" . local_get_contents($global['systemRootPath'] . $value);
+            } else if (file_exists($value)) {
                 $str .= "\n/*{$value} created local with full-path given */\n" . local_get_contents($value);
-            }
-            else {
+            } else {
                 $allowed = "";
                 if (ini_get('allow_url_fopen')) {
                     $allowed .= "allow_url_fopen is on and ";
@@ -930,40 +929,37 @@ function combineFiles($filesArray, $extension = "js") {
                 if (function_exists('curl_init')) {
                     $allowed .= "curl is on";
                 } else {
-                   $allowed .= "curl is off";
+                    $allowed .= "curl is off";
                 }
 
-                    $content = url_get_contents($value);
-                    if(empty($content)){
-                        $allowed .= " - web-fallback 1 (add webSiteRootURL)";
-                        $content = url_get_contents($global['webSiteRootURL'] . $value);
-                    }
-                    $str .= "\n/*{$value} created via web with own url ({$allowed}) */\n" . $content;
-
+                $content = url_get_contents($value);
+                if (empty($content)) {
+                    $allowed .= " - web-fallback 1 (add webSiteRootURL)";
+                    $content = url_get_contents($global['webSiteRootURL'] . $value);
+                }
+                $str .= "\n/*{$value} created via web with own url ({$allowed}) */\n" . $content;
             }
         }
-        if(($extension=="js")&&($minifyEnabled)){
-                require_once $global['systemRootPath'] . 'objects/jshrink.php';
-                $str = \JShrink\Minifier::minify($str, array('flaggedComments' => false));
+        if (($extension == "js") && ($minifyEnabled)) {
+            require_once $global['systemRootPath'] . 'objects/jshrink.php';
+            $str = \JShrink\Minifier::minify($str, array('flaggedComments' => false));
         }
         file_put_contents($cacheDir . $md5FileName, $str);
     }
     return $global['webSiteRootURL'] . 'videos/cache/' . $md5FileName;
 }
 
-
-function local_get_contents($path){
-    if (function_exists('fopen')){
+function local_get_contents($path) {
+    if (function_exists('fopen')) {
         $myfile = fopen($path, "r") or die("Unable to open file!");
-        $text = fread($myfile,filesize($path));
+        $text = fread($myfile, filesize($path));
         fclose($myfile);
         return $text;
     }
 }
 
-
-function url_get_contents($Url, $ctx="") {
-    if(empty($ctx)){
+function url_get_contents($Url, $ctx = "") {
+    if (empty($ctx)) {
         $opts = array(
             "ssl" => array(
                 "verify_peer" => false,
@@ -976,15 +972,15 @@ function url_get_contents($Url, $ctx="") {
         $context = $ctx;
     }
     if (ini_get('allow_url_fopen')) {
-        try{
-            $tmp = @file_get_contents($Url, false,$context);
-            if($tmp!=false){
+        try {
+            $tmp = @file_get_contents($Url, false, $context);
+            if ($tmp != false) {
                 return $tmp;
             }
-        } catch(ErrorException $e){
-
+        } catch (ErrorException $e) {
+            
         }
-    } else  if (function_exists('curl_init')) {
+    } else if (function_exists('curl_init')) {
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $Url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -992,7 +988,7 @@ function url_get_contents($Url, $ctx="") {
         curl_close($ch);
         return $output;
     }
-    return @file_get_contents($Url, false,$context);
+    return @file_get_contents($Url, false, $context);
 }
 
 function getUpdatesFilesArray() {
@@ -1013,24 +1009,76 @@ function getUpdatesFilesArray() {
     return $updateFiles;
 }
 
-function UTF8encode($data){
+function UTF8encode($data) {
     global $advancedCustom, $global;
 
-    if(!empty($advancedCustom->utf8Encode)){
+    if (!empty($advancedCustom->utf8Encode)) {
         return utf8_encode($data);
     }
-    if(!empty($advancedCustom->utf8Decode)){
+    if (!empty($advancedCustom->utf8Decode)) {
         return utf8_decode($data);
     }
     return $data;
 }
 
 //detect search engine bots
-function isBot(){
+function isBot() {
     return false;
     $bot_regex = '/BotLink|bingbot|AhrefsBot|ahoy|AlkalineBOT|anthill|appie|arale|araneo|AraybOt|ariadne|arks|ATN_Worldwide|Atomz|bbot|Bjaaland|Ukonline|borg\-bot\/0\.9|boxseabot|bspider|calif|christcrawler|CMC\/0\.01|combine|confuzzledbot|CoolBot|cosmos|Internet Cruiser Robot|cusco|cyberspyder|cydralspider|desertrealm, desert realm|digger|DIIbot|grabber|downloadexpress|DragonBot|dwcp|ecollector|ebiness|elfinbot|esculapio|esther|fastcrawler|FDSE|FELIX IDE|ESI|fido|H�m�h�kki|KIT\-Fireball|fouineur|Freecrawl|gammaSpider|gazz|gcreep|golem|googlebot|griffon|Gromit|gulliver|gulper|hambot|havIndex|hotwired|htdig|iajabot|INGRID\/0\.1|Informant|InfoSpiders|inspectorwww|irobot|Iron33|JBot|jcrawler|Teoma|Jeeves|jobo|image\.kapsi\.net|KDD\-Explorer|ko_yappo_robot|label\-grabber|larbin|legs|Linkidator|linkwalker|Lockon|logo_gif_crawler|marvin|mattie|mediafox|MerzScope|NEC\-MeshExplorer|MindCrawler|udmsearch|moget|Motor|msnbot|muncher|muninn|MuscatFerret|MwdSearch|sharp\-info\-agent|WebMechanic|NetScoop|newscan\-online|ObjectsSearch|Occam|Orbsearch\/1\.0|packrat|pageboy|ParaSite|patric|pegasus|perlcrawler|phpdig|piltdownman|Pimptrain|pjspider|PlumtreeWebAccessor|PortalBSpider|psbot|Getterrobo\-Plus|Raven|RHCS|RixBot|roadrunner|Robbie|robi|RoboCrawl|robofox|Scooter|Search\-AU|searchprocess|Senrigan|Shagseeker|sift|SimBot|Site Valet|skymob|SLCrawler\/2\.0|slurp|ESI|snooper|solbot|speedy|spider_monkey|SpiderBot\/1\.0|spiderline|nil|suke|http:\/\/www\.sygol\.com|tach_bw|TechBOT|templeton|titin|topiclink|UdmSearch|urlck|Valkyrie libwww\-perl|verticrawl|Victoria|void\-bot|Voyager|VWbot_K|crawlpaper|wapspider|WebBandit\/1\.0|webcatcher|T\-H\-U\-N\-D\-E\-R\-S\-T\-O\-N\-E|WebMoose|webquest|webreaper|webs|webspider|WebWalker|wget|winona|whowhere|wlm|WOLP|WWWC|none|XGET|Nederland\.zoek|AISearchBot|woriobot|NetSeer|Nutch|YandexBot|YandexMobileBot|SemrushBot|FatBot|MJ12bot|DotBot|AddThis|baiduspider|SeznamBot|mod_pagespeed|CCBot|openstat.ru\/Bot|m2e/i';
     $userAgent = empty($_SERVER['HTTP_USER_AGENT']) ? FALSE : $_SERVER['HTTP_USER_AGENT'];
     $isBot = !$userAgent || preg_match($bot_regex, $userAgent);
 
     return $isBot;
+}
+
+/**
+ * A function that could get me the last N lines of a log file.
+ * @param type $filepath
+ * @param type $lines
+ * @param type $adaptive
+ * @return boolean
+ */
+function tail($filepath, $lines = 1, $adaptive = true) {
+    // Open file
+    $f = @fopen($filepath, "rb");
+    if ($f === false)
+        return false;
+    // Sets buffer size, according to the number of lines to retrieve.
+    // This gives a performance boost when reading a few lines from the file.
+    if (!$adaptive)
+        $buffer = 4096;
+    else
+        $buffer = ($lines < 2 ? 64 : ($lines < 10 ? 512 : 4096));
+    // Jump to last character
+    fseek($f, -1, SEEK_END);
+    // Read it and adjust line number if necessary
+    // (Otherwise the result would be wrong if file doesn't end with a blank line)
+    if (fread($f, 1) != "\n")
+        $lines -= 1;
+
+    // Start reading
+    $output = '';
+    $chunk = '';
+    // While we would like more
+    while (ftell($f) > 0 && $lines >= 0) {
+        // Figure out how far back we should jump
+        $seek = min(ftell($f), $buffer);
+        // Do the jump (backwards, relative to where we are)
+        fseek($f, -$seek, SEEK_CUR);
+        // Read a chunk and prepend it to our output
+        $output = ($chunk = fread($f, $seek)) . $output;
+        // Jump back to where we started reading
+        fseek($f, -mb_strlen($chunk, '8bit'), SEEK_CUR);
+        // Decrease our line counter
+        $lines -= substr_count($chunk, "\n");
+    }
+    // While we have too many lines
+    // (Because of buffer size we might have read too many)
+    while ($lines++ < 0) {
+        // Find first newline and remove all text before that
+        $output = substr($output, strpos($output, "\n") + 1);
+    }
+    // Close file and return
+    fclose($f);
+    return trim($output);
 }
