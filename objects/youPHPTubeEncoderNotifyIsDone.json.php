aae87fbd0186f8904d6f0901053d0fa4b559ff30
daniel <daniel@daniel-DC1A-S>
2018-10-10 18:43:27 -0300

    Update status only if status is not unlisted

--- objects/youPHPTubeEncoderNotifyIsDone.json.php
+++ objects/youPHPTubeEncoderNotifyIsDone.json.php
@@ -40,11 +40,15 @@ $video = new Video("", "", $_POST['videos_id']);
 $obj->video_id = $_POST['videos_id'];
 
 if(empty($_POST['fail'])){
-    if(empty($advancedCustom->makeVideosInactiveAfterEncode)){
-        // set active
-        $video->setStatus('a');
-    }else{
-        $video->setStatus('i');
+    $status = $video->getStatus();
+    // if status is not unlisted
+    if($status!=='u'){
+        if(empty($advancedCustom->makeVideosInactiveAfterEncode)){
+            // set active
+            $video->setStatus('a');
+        }else{
+            $video->setStatus('i');
+        }
     }
 }else{
     $video->setStatus('i');
