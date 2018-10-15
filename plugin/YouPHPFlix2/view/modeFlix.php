a0d02d00cda6491db5aacea9d67fa70fbc95794e
daniel <daniel@daniel-DC1A-S>
2018-10-15 13:17:28 -0300

    Add an image placeholder and lazyload

--- plugin/YouPHPFlix2/view/modeFlix.php
+++ plugin/YouPHPFlix2/view/modeFlix.php
@@ -31,7 +31,7 @@ $obj = YouPHPTubePlugin::getObjectData("YouPHPFlix2");
                 $dataFlickirty = new stdClass();
                 $dataFlickirty->wrapAround = true;
                 $dataFlickirty->pageDots = !empty($obj->pageDots);
-                $dataFlickirty->lazyLoad = 7;
+                $dataFlickirty->lazyLoad = 15;
                 $dataFlickirty->setGallerySize = false;
                 $dataFlickirty->cellAlign = 'left';
                 if ($obj->DateAddedAutoPlay) {
@@ -63,7 +63,7 @@ $obj = YouPHPTubePlugin::getObjectData("YouPHPFlix2");
                     $dataFlickirty = new stdClass();
                     $dataFlickirty->wrapAround = true;
                     $dataFlickirty->pageDots = !empty($obj->pageDots);
-                    $dataFlickirty->lazyLoad = 7;
+                    $dataFlickirty->lazyLoad = 15;
                     $dataFlickirty->setGallerySize = false;
                     $dataFlickirty->cellAlign = 'left';
                     if ($obj->MostWatchedAutoPlay) {
@@ -93,7 +93,7 @@ $obj = YouPHPTubePlugin::getObjectData("YouPHPFlix2");
                     $dataFlickirty = new stdClass();
                     $dataFlickirty->wrapAround = true;
                     $dataFlickirty->pageDots = !empty($obj->pageDots);
-                    $dataFlickirty->lazyLoad = 7;
+                    $dataFlickirty->lazyLoad = 15;
                     $dataFlickirty->setGallerySize = false;
                     $dataFlickirty->cellAlign = 'left';
                     if ($obj->MostPopularAutoPlay) {
