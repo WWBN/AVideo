ab203eabb856e21e253f4caa9ac6ad0e70bbbe7a
daniel <daniel@daniel-DC1A-S>
2018-10-27 10:24:13 -0300

    add nl2br JS function

--- view/js/script.js
+++ view/js/script.js
@@ -373,3 +373,11 @@ function copyToClipboard(text) {
     $('#elementToCopy').select();
     document.execCommand('copy');
 }
+
+function nl2br (str, is_xhtml) {
+    if (typeof str === 'undefined' || str === null) {
+        return '';
+    }
+    var breakTag = (is_xhtml || typeof is_xhtml === 'undefined') ? '<br />' : '<br>';
+    return (str + '').replace(/([^>\r\n]?)(\r\n|\n\r|\r|\n)/g, '$1' + breakTag + '$2');
+}
