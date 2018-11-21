73bf3729a76292e8679ed05e49c6282ae7e53c60
daniel <daniel@daniel-DC1A-S>
2018-11-21 11:07:06 -0300

    Fix switch

--- plugin/VR360/view/menuRight.php
+++ plugin/VR360/view/menuRight.php
@@ -2,7 +2,7 @@
     <div class="material-switch navbar-text" style="width: 120px;">
         <i class="fa fa-cube"></i> VR360 &nbsp;&nbsp;
         <input id="vr360" type="checkbox" value="0" <?php if($is_enabled){ ?> checked="checked" <?php } ?>/>
-        <label for="vr360" class="badge-success"></label>
+        <label for="vr360" class="label-primary"></label>
     </div>
 </li>
 <script>
