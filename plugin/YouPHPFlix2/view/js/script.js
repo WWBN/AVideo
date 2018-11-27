b8d3410dc544339836dfaf9e730a79d65e0165b2
daniel <daniel@daniel-DC1A-S>
2018-11-27 10:43:14 -0300

    Fix fullscreen actions

--- plugin/YouPHPFlix2/view/js/script.js
+++ plugin/YouPHPFlix2/view/js/script.js
@@ -3,23 +3,6 @@ var cat;
 var videos_id;
 var $carousel;
 
-function flixFullScreen(link){
-    $('body').addClass('fullScreen');
-    var div = $('<div id="divIframeFull" style="background-color:black;height: 100vh; width: 100vw; text-align: center; position: fixed; top: 0;left: 0; z-index: 9999;"><div id="divTopBar" style="position: fixed; top: 0; left: 0; height: 50px; width: 100vw; z-index: 99999; padding:10px; "><span id="closeBtnFull" class="btn pull-right" onclick="closeFlixFullScreen();" style="opacity: 0.5; filter: alpha(opacity=50);"><i class="fa fa-times"></i></span></div></div>').append('<iframe src="' + link + '" style="background-color:black; position: fixed; top: 0; left: 0; height: 100vh; width: 100vw; z-index: 9999; overflow: hidden;" id="iframeFull" allowfullscreen webkitallowfullscreen mozallowfullscreen oallowfullscreen msallowfullscreen>');
-    $('body').append(div);
-    $("#divIframeFull").fadeIn();
-}
-
-function closeFlixFullScreen() {
-    if($('#divIframeFull').length){
-        $("#divIframeFull").fadeOut("slow", function () {
-            $('body').removeClass('fullScreen');
-            $('#divIframeFull').remove();
-        });
-    }
-
-}
-
 function isFlickityEnabled(selector) {
     var isEnabled = $(selector).hasClass('flickity-enabled');
     if (isEnabled) {
@@ -35,11 +18,6 @@ function isFlickityEnabled(selector) {
 }
 
 $(function () {
-    $(document).on('keyup', function (evt) {
-        if (evt.keyCode == 27) {
-            closeFlixFullScreen();
-        }
-    });
     $(".thumbsImage").on("mouseenter", function () {
         //$(this).find(".thumbsGIF").height($(this).find(".thumbsJPG").height());
         //$(this).find(".thumbsGIF").width($(this).find(".thumbsJPG").width());
