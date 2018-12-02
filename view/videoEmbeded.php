2b979b16c44af816335a67f1845e36b678ba4c94
daniel <daniel@daniel-DC1A-S>
2018-12-02 16:24:00 -0300

    https://github.com/DanielnetoDotCom/YouPHPTube/issues/1260#issuecomment-443422527 Fix js error on embed

--- view/videoEmbeded.php
+++ view/videoEmbeded.php
@@ -59,12 +59,11 @@ if (($video['type'] !== "audio")&&($video['type'] !== "linkAudio")) {
         <link href="<?php echo $global['webSiteRootURL']; ?>view/js/video.js/video-js.min.css" rel="stylesheet" type="text/css"/>
         <link href="<?php echo $global['webSiteRootURL']; ?>view/css/player.css" rel="stylesheet" type="text/css"/>
         <link href="<?php echo $global['webSiteRootURL']; ?>view/css/social.css" rel="stylesheet" type="text/css"/>
-        <link href="<?php echo $global['webSiteRootURL']; ?>view/css/font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
+        <link href="<?php echo $global['webSiteRootURL']; ?>view/css/fontawesome-free-5.5.0-web/css/all.min.css" rel="stylesheet" type="text/css"/>
         <script src="<?php echo $global['webSiteRootURL']; ?>view/js/jquery-3.3.1.min.js" type="text/javascript"></script>
         <script src="<?php echo $global['webSiteRootURL']; ?>view/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
         <script src="<?php echo $global['webSiteRootURL']; ?>view/js/video.js/video.js" type="text/javascript"></script>
         <script src="<?php echo $global['webSiteRootURL']; ?>view/js/videojs-rotatezoom/videojs.zoomrotate.js" type="text/javascript"></script>
-        <script src="<?php echo $global['webSiteRootURL']; ?>view/js/script.js" type="text/javascript"></script>
         <style>
             body {
                 padding: 0 !important;
@@ -79,20 +78,17 @@ if (($video['type'] !== "audio")&&($video['type'] !== "linkAudio")) {
         </style>
 
         <?php
+        
     $jsFiles = array();
-    //$jsFiles[] = "{$global['webSiteRootURL']}bootstrap/js/bootstrap.min.js";
     $jsFiles[] = "view/js/seetalert/sweetalert.min.js";
     $jsFiles[] = "view/js/bootpag/jquery.bootpag.min.js";
     $jsFiles[] = "view/js/bootgrid/jquery.bootgrid.js";
     $jsFiles[] = "view/bootstrap/bootstrapSelectPicker/js/bootstrap-select.min.js";
     $jsFiles[] = "view/js/script.js";
-    //$jsFiles[] = "view/js/bootstrap-toggle/bootstrap-toggle.min.js";
     $jsFiles[] = "view/js/js-cookie/js.cookie.js";
     $jsFiles[] = "view/css/flagstrap/js/jquery.flagstrap.min.js";
     $jsFiles[] = "view/js/jquery.lazy/jquery.lazy.min.js";
     $jsFiles[] = "view/js/jquery.lazy/jquery.lazy.plugins.min.js";
-    //$jsFiles[] = "{$global['webSiteRootURL']}view/js/videojs-wavesurfer/wavesurfer.min.js";
-    //$jsFiles[] = "{$global['webSiteRootURL']}view/js/videojs-wavesurfer/dist/videojs.wavesurfer.min.js";
     $jsURL =  combineFiles($jsFiles, "js");
 
 ?>
@@ -107,6 +103,7 @@ if (($video['type'] !== "audio")&&($video['type'] !== "linkAudio")) {
             <?php
             if ($video['type'] == "embed") {
                 ?>
+                <video id="mainVideo" style="display: none; height: 0;width: 0;" ></video>
                 <iframe class="embed-responsive-item" src="<?php
                 echo parseVideos($video['videoLink']);
                 if ($config->getAutoplay()) {
@@ -116,7 +113,7 @@ if (($video['type'] !== "audio")&&($video['type'] !== "linkAudio")) {
 
                 <script>
             $(document).ready(function () {
-                addView(<?php echo $video['id']; ?>,this.currentTime());
+                addView(<?php echo $video['id']; ?>,0);
             });
                 </script>
                 <?php
