2e945226d2fea87656cdf281dfa6909f17e1fbc4
Puyodead1 <23562356riley@gmail.com>
2018-12-17 16:23:26 -0500

    DiscordNotify Plugin Update

--- plugin/DiscordNotify/DiscordNotify.php
+++ plugin/DiscordNotify/DiscordNotify.php
@@ -1,6 +1,9 @@
 <?php
 
-global $global;
+global $global, $config;
+if (!isset($global['systemRootPath'])) {
+    require_once '../videos/configuration.php';
+}
 require_once $global['systemRootPath'] . 'plugin/Plugin.abstract.php';
 
 class DiscordNotify extends PluginAbstract
@@ -40,11 +43,11 @@ class DiscordNotify extends PluginAbstract
 		$obj->favicon_url = "";
 		$obj->botuser = "";
 		$obj->botlogo = "";
-		$obj->baseurl = "";
         return $obj;
     }
     public function afterNewVideo($videos_id)
     {
+		global $global;
         $o = $this->getDataObject();
 		$users_id = Video::getOwner($videos_id);
 		$user = new User($users_id);
@@ -60,59 +63,21 @@ class DiscordNotify extends PluginAbstract
 		$url = $o->webhook_url;
 		$favicon_url = $o->favicon_url;
 		$botuser = $o->botuser;
-		$baseurl = $o->baseurl;
 		$botlogo = $o->botlogo;
 
 $hookObject = json_encode([
-    /*
-     * The general "message" shown above your embeds
-     */
     "content" => "",
-    /*
-     * The username shown in the message
-     */
     "username" => $botuser,
-    /*
-     * The image location for the senders image
-     */
     "avatar_url" => $favicon_url,
-    /*
-     * Whether or not to read the message in Text-to-speech
-     */
     "tts" => false,
-    /*
-     * File contents to send to upload a file
-     */
-    // "file" => "",
-    /*
-     * An array of Embeds
-     */
     "embeds" => [
-        /*
-         * Our first embed
-         */
         [
-            // Set the title for your embed
             "title" => $username . " just uploaded a video",
-
-            // The type of your embed, will ALWAYS be "rich"
             "type" => "rich",
-
-            // A description for your embed
             "description" => "",
-
-            // The URL of where your title will be a link to
-            "url" => $baseurl . "/" . $channelName,
-
-            /* A timestamp to be displayed below the embed, IE for when an an article was posted
-             * This must be formatted as ISO8601
-             */
+            "url" => $global['webSiteRootURL'] . $channelName,
             "timestamp" => "",
-
-            // The integer color to be used on the left side of the embed
             "color" => hexdec( "FF0000" ),
-
-            // Footer object
             "footer" => [
                 "text" => $botuser,
                 "icon_url" => $botlogo
@@ -126,21 +91,17 @@ $hookObject = json_encode([
              //   "url" => $userThumbnail
             //],
 
-            // Field array of objects
             "fields" => [
-                // Field 1
                 [
                     "name" => "Video Name",
                     "value" => $videoName,
                     "inline" => true
                 ],
-                // Field 2
                 [
                     "name" => "Video Link",
                     "value" => $videoLink,
                     "inline" => true
                 ],
-                // Field 3
                 [
                     "name" => "Video Duration",
                     "value" => $videoDuration,
@@ -172,4 +133,3 @@ curl_setopt_array( $ch, [
 return curl_exec( $ch );
     }
 }
-
