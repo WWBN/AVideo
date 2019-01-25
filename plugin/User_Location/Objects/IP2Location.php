2a3c0b50626eb1cc383a57a1108caf921cf0dbb1
daniel <daniel@daniel-DC1A-S>
2019-01-25 14:16:05 -0300

    https://github.com/DanielnetoDotCom/YouPHPTube/issues/1427#issuecomment-457507665

--- plugin/User_Location/Objects/IP2Location.php
+++ plugin/User_Location/Objects/IP2Location.php
@@ -60,7 +60,7 @@ class IP2Location extends ObjectYPT {
                 $content = json_encode($rows);
                 file_put_contents($cachefile, $content);
             } else {
-                die($sql . '\nError : (' . $global['mysqli']->errno . ') ' . $global['mysqli']->error);
+                return array();
             }
         }else{
             $content = file_get_contents($cachefile);
