91ac8d58a08c2a82dc8fdd652991e6271836d70c
daniel <daniel@daniel-DC1A-S>
2019-02-04 19:18:14 -0300

    ver 6.5 Add RRAting option on the videos

--- view/img/rrating/rating-pg-13.php
+++ view/img/rrating/rating-pg-13.php
@@ -1,6 +1,6 @@
 <button class="btn btn-default btn-outline btn-xs btn-sm" onclick="$(this).find('table').slideToggle();" style="position: relative;">
     PG-13
-    <table class="table-bordered" style="border: 2px solid #555; border-radius: 4px; margin: 2px;border-collapse: separate !important; display: none; position: absolute; left: 0;">
+    <table class="table-bordered" style="z-index: 1;border: 2px solid #555; border-radius: 4px; margin: 2px;border-collapse: separate !important; display: none; position: absolute; left: 0;">
         <tr>
             <td style="font-size: 1.5em; text-align: center; font-weight: bold; padding: 1px 2px;">PG-13</td>
             <td style="font-size: 0.8em; text-align: center; font-weight: bold; padding: 1px 2px;"><?php echo strtoupper(__("Parental Guidance Suggested")); ?></td>
--- view/img/rrating/rating-pg.php
+++ view/img/rrating/rating-pg.php
@@ -1,6 +1,6 @@
 <button class="btn btn-default btn-outline btn-xs btn-sm" onclick="$(this).find('table').slideToggle();" style="position: relative;">
     PG
-    <table class="table-bordered" style="border: 2px solid #555; border-radius: 4px; margin: 2px;border-collapse: separate !important; display: none; position: absolute; left: 0;">
+    <table class="table-bordered" style="z-index: 1;border: 2px solid #555; border-radius: 4px; margin: 2px;border-collapse: separate !important; display: none; position: absolute; left: 0;">
         <tr>
             <td style="font-size: 1.5em; text-align: center; font-weight: bold; padding: 1px 2px;">PG</td>
             <td style="font-size: 0.8em; text-align: center; font-weight: bold; padding: 1px 2px;"><?php echo strtoupper(__("Parental Guidance Suggested")); ?></td>
