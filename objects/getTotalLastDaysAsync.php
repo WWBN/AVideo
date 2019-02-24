fea0977657f8ce3b8ca84d4bec5f499b1b0c8a9e
daniel <daniel@daniel-DC1A-S>
2019-02-24 14:56:13 -0300

    Correct Subscribers tables adding video statics index for faster results

---
+++ objects/getTotalLastDaysAsync.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once '../videos/configuration.php';
+require_once $global['systemRootPath'] . 'objects/video_statistics.php';
+//getTotalTodayAsync($video_id) 
+$video_id = $argv[1];
+$numberOfDays = $argv[2];
+$cacheFileName = $argv[3];
+$lockFile = $cacheFileName.".lock";
+if(file_exists($lockFile)){
+    return false;
+}
+file_put_contents($lockFile, 1);
+$total = VideoStatistic::getTotalLastDaysAsync($video_id, $numberOfDays);
+file_put_contents($cacheFileName, json_encode($total));
+unlink($lockFile);
