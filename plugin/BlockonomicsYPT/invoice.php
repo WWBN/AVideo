27739d6c57e1810f2541490f148145c23268b335
daniel <daniel@daniel-DC1A-S>
2019-03-04 14:01:53 -0300

    Adding Blockonomics payment method

---
+++ plugin/BlockonomicsYPT/invoice.php
@@ -0,0 +1,121 @@
+<?php
+require_once '../../videos/configuration.php';
+require_once $global['systemRootPath'] . 'objects/user.php';
+if (!User::isLogged()) {
+    header("Location: {$global['webSiteRootURL']}?error=" . __("You can not do this"));
+    exit;
+}
+
+$plugin = YouPHPTubePlugin::loadPluginIfEnabled("BlockonomicsYPT");
+$obj = YouPHPTubePlugin::getObjectData("BlockonomicsYPT");
+$order_id = $plugin->setUpPayment($_GET['value']);
+$order = new BlockonomicsOrder($order_id);
+?>
+<!DOCTYPE html>
+<html lang="<?php echo $_SESSION['language']; ?>">
+    <head>
+        <title><?php echo $config->getWebSiteTitle(); ?>  :: Audit</title>
+        <?php
+        include $global['systemRootPath'] . 'view/include/head.php';
+        ?>
+        <style>
+        </style>
+    </head>
+    <body class="<?php echo $global['bodyClass']; ?>">
+        <?php
+        include $global['systemRootPath'] . 'view/include/navbar.php';
+        ?>
+
+
+        <div class="container">
+
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h1>
+                        Order#: <?php echo sprintf('%08d', $order->getId()); ?>
+                    </h1>
+                </div>
+                <div class="panel-body">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <h2>Bitcoin Address</h2>
+                            <a href="bitcoin:<?php echo $order->getAddr(); ?>?amount=<?php echo $order->getFormatedBits(); ?>">
+                                <div id="qrcode" class="text-center"></div>
+                            </a>
+                            <br/>
+                            <div class="field">
+                                <div class="control">
+                                    <input type="text" class="input form-control" value="<?php echo $order->getAddr(); ?>" readonly="readonly">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <h2>To pay, send exact amount of BTC to the given address</h2>
+                            <h4 class="subtitle has-text-danger" ng-show="invoice.status == -3"> Payment Expired</h4>
+                            <h4 class="subtitle has-text-danger" ng-show="invoice.status == -2"> Payment Error</h4>
+                            <h4 class="subtitle" ng-show="invoice.status == 0"> Unconfirmed</h4>
+                            <h4 class="subtitle" ng-show="invoice.status == 1"> Partially Confirmed</h4>
+                            <h4 class="subtitle has-text-success" ng-show="invoice.status >= 2" >Confirmed</h4>
+                            <h3>Amount</h3>
+                            <p><strong><?php echo $order->getFormatedBits(); ?></strong> BTC ⇌ <strong><?php echo $order->getTotal_value(); ?></strong> <?php echo $order->getCurrency(); ?></p>
+
+                            <br/>
+
+                            <h2 ng-hide="invoice.status == -1">Payment Details: </h2>
+                            <div ng-hide="invoice.status == -1" class="value ng-binding">
+                                Received : <strong><?php echo $order->getFormatedBits_payed(); ?></strong>
+                                <small>BTC</small> 
+                            </div>
+                            <div ng-show="invoice.status != -1" class="value ng-binding" style="margin-bottom:10px;" >
+                                Transaction : <a target="_blank" href="http://www.blockonomics.co/api/tx?txid={{invoice.txid}}&addr={{<?php echo $order->getAddr(); ?>}}">{{invoice.txid|limitTo: 20}}</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="progress">
+                            <div class="progress-bar progress-bar-striped active" role="progressbar"
+                                 aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width:100%" id="timeleft">
+                                100%
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <?php
+        include $global['systemRootPath'] . 'view/include/footer.php';
+        ?>
+        <script src="<?php echo $global['webSiteRootURL']; ?>plugin/BlockonomicsYPT/jquery.qrcode.min.js" type="text/javascript"></script>
+        <script>
+                                    var totalSeconds = <?php echo $obj->ExpireInSeconds; ?>;
+                                    var totalSecondsPassed = <?php echo time() - strtotime($order->getCreated()); ?>;
+                                    var totalSecondsleft = totalSeconds - totalSecondsPassed;
+                                    $(document).ready(function () {
+                                        $('#qrcode').qrcode({width: 220, height: 220, text: "bitcoin:<?php echo $order->getAddr(); ?>?amount=<?php echo $order->getFormatedBits(); ?>"});
+
+                                        setInterval(function () {
+                                            totalSecondsleft--;
+                                            if (totalSecondsleft < 1) {
+                                                totalSecondsleft = 0;
+                                            }
+                                            var percent = (totalSecondsleft / totalSeconds) * 100;
+
+                                            $("#timeleft").css('width', percent + "%").html(totalSecondsleft + " <?php echo __("Seconds Left"); ?>");
+                                        }, 1000);
+                                        check();
+                                    });
+
+                                    function check() {
+                                        $.ajax({
+                                            url: '<?php echo $global['webSiteRootURL']; ?>plugin/BlockonomicsYPT/check.php?addr=<?php echo $order->getAddr(); ?>',
+                                            success: function (response) {
+                                                console.log(response);
+                                                setTimeout(function(){check();},3000);
+                                            }
+                                        });
+                                    }
+        </script>
+    </body>
+</html>
---
+++ plugin/YPTWallet/plugins/YPTWalletBlockonomics/requestPayment.json.php
@@ -0,0 +1,31 @@
+<?php
+
+header('Content-Type: application/json');
+
+if (empty($global['systemRootPath'])) {
+    $global['systemRootPath'] = '../../../../';
+}
+require_once $global['systemRootPath'] . 'videos/configuration.php';
+require_once $global['systemRootPath'] . 'objects/user.php';
+
+$plugin = YouPHPTubePlugin::loadPluginIfEnabled("PayPalYPT");
+$pluginS = YouPHPTubePlugin::loadPluginIfEnabled("YPTWallet");
+$objS = $pluginS->getDataObject();
+
+$obj= new stdClass();
+$obj->error = true;
+
+if(empty($_POST['value'])){ 
+    $obj->msg = "Invalid Value";
+    die(json_encode($obj));
+}
+
+$invoiceNumber = uniqid();
+
+$payment = $plugin->setUpPayment($invoiceNumber, $objS->RedirectURL, $objS->CancelURL, $_POST['value'], $objS->currency, $config->getWebSiteTitle()." Payment");
+
+if (!empty($payment)) {
+    $obj->error = false;
+    $obj->approvalLink = $payment->getApprovalLink();
+}
+die(json_encode($obj));
