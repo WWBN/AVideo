a3f49b1f1229c933f20043878058f14649d4e624
daniel <daniel@daniel-DC1A-S>
2019-03-04 14:37:23 -0300

    Adding Blockonomics payment method

--- plugin/YPTWallet/plugins/YPTWalletBlockonomics/cancel_url.php
+++
@@ -1,7 +0,0 @@
-<?php
-if (empty($global['systemRootPath'])) {
-    $global['systemRootPath'] = '../../../../';
-}
-require_once $global['systemRootPath'] . 'videos/configuration.php';
-
-header("Location: {$global['webSiteRootURL']}plugin/YPTWallet/view/addFunds.php?status=fail");
