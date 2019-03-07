42e0f92f012fbb1abefb8ae73b71a752d598b3a5
daniel <daniel@daniel-DC1A-S>
2019-03-07 16:33:27 -0300

    Improve http_reffer

--- objects/include_config.php
+++ objects/include_config.php
@@ -40,7 +40,27 @@ ini_set('session.gc_maxlifetime', $config->getSession_timeout());
 // each client should remember their session id for EXACTLY 1 hour
 session_set_cookie_params($config->getSession_timeout());
 
+
 session_start();
+
+// set the reffer for youPHPTube
+$url1['host'] = "";
+if (!empty($_SERVER["HTTP_REFERER"])) {
+    if((strpos($_SERVER["HTTP_REFERER"], '/video/') !== false || strpos($_SERVER["HTTP_REFERER"], '/v/') !== false) && !empty($_SESSION["LAST_HTTP_REFERER"])){
+        $global["HTTP_REFERER"] = $_SESSION["LAST_HTTP_REFERER"];
+        $url1 = parse_url($global["HTTP_REFERER"]);
+    }else{
+        $global["HTTP_REFERER"] = $_SERVER["HTTP_REFERER"];
+        $url1 = parse_url($global["HTTP_REFERER"]);
+    }
+}
+$url2 = parse_url($global['webSiteRootURL']);
+if ($url1['host'] !== $url2['host']) {
+    $global["HTTP_REFERER"] = $global['webSiteRootURL'];
+}
+$_SESSION["LAST_HTTP_REFERER"] = $global["HTTP_REFERER"];
+//var_dump($global["HTTP_REFERER"], $url1);exit;
+
 $output = ob_get_clean();
 ob_start("ob_gzhandler");
 echo $output;
