7e648fe2052bf9ec4a82047f5d120b0626923436
daniel <daniel@daniel-DC1A-S>
2019-04-08 15:45:47 -0300

    hide date from non logged users

--- objects/playlists.json.php
+++ objects/playlists.json.php
@@ -3,6 +3,9 @@ global $global, $config;
 if(!isset($global['systemRootPath'])){
     require_once '../videos/configuration.php';
 }
+if(!User::isLogged()){
+    die();
+}
 require_once $global['systemRootPath'] . 'objects/playlist.php';
 header('Content-Type: application/json');
 $row = PlayList::getAllFromUser(User::getId(), false);
