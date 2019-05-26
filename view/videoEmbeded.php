bc640d8e44245f53b7f4a5da5e4d6ae657bbef7f
daniel <daniel@daniel-DC1A-S>
2019-05-26 17:12:58 -0300

    try to fix iphone on iframe

--- view/videoEmbeded.php
+++ view/videoEmbeded.php
@@ -217,7 +217,7 @@ if (!empty($_GET['t'])) {
             <?php
         } else {
             ?>
-            <video style="width: 100%; height: 100%; position: absolute; top: 0;" playsinline webkit-playsinline poster="<?php echo $poster; ?>" <?php echo $controls; ?> <?php echo $loop; ?>   <?php echo $mute; ?>
+            <video style="width: 100%; height: 100%; position: fixed; top: 0;" playsinline webkit-playsinline poster="<?php echo $poster; ?>" <?php echo $controls; ?> <?php echo $loop; ?>   <?php echo $mute; ?>
                    class="video-js vjs-default-skin vjs-big-play-centered <?php echo $vjsClass; ?> " id="mainVideo">
                        <?php
                        echo getSources($video['filename']);
