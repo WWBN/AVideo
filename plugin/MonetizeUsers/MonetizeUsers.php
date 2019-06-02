ee3940e9849fc06470b65bf924ebce17529bfd29
daniel <daniel@daniel-DC1A-S>
2019-06-02 09:03:08 -0300

    https://github.com/YouPHPTube/YouPHPTube/issues/1774

--- plugin/MonetizeUsers/MonetizeUsers.php
+++ plugin/MonetizeUsers/MonetizeUsers.php
@@ -6,7 +6,7 @@ class MonetizeUsers extends PluginAbstract {
 
     public function getDescription() {
         $txt = "This plugin will reward your users based on their videos view, each view will affect the user's walled balance";
-        
+
         $txt .= $this->isReadyLabel(array('YPTWallet'));
         return $txt;
     }
@@ -20,7 +20,7 @@ class MonetizeUsers extends PluginAbstract {
     }
 
     public function getPluginVersion() {
-        return "1.0";   
+        return "1.0";
     }
 
     public function getEmptyDataObject() {
@@ -34,17 +34,25 @@ class MonetizeUsers extends PluginAbstract {
     public function getTags() {
         return array('free', 'monetize', 'wallet');
     }
-    
+
     public function addView($videos_id, $total) {
         global $global;
         $obj = $this->getDataObject();
-        if($obj->rewardOnlyLoggedUsersView && !User::isLogged()){
+        if ($obj->rewardOnlyLoggedUsersView && !User::isLogged()) {
             return false;
         }
-        
+
+        // Check ownership to prevent the uploader from farming money from their own video content
+        if (User::isLogged()) {
+            $user_id = User::getId();
+            if (Video::isOwner($videos_id, $user_id)) {
+                return false; // Prevent exploitation of free money; Don't award money if viewer is uploader
+            }
+        }
+
         $wallet = YouPHPTubePlugin::loadPlugin("YPTWallet");
         $video = new Video("", "", $videos_id);
-        return $wallet->transferBalanceFromSiteOwner($video->getUsers_id(),$obj->rewardPerView, "Reward from video <a href='{$global['webSiteRootURL']}v/{$videos_id}'>".$video->getTitle()."</a>", true);
-    }    
+        return $wallet->transferBalanceFromSiteOwner($video->getUsers_id(), $obj->rewardPerView, "Reward from video <a href='{$global['webSiteRootURL']}v/{$videos_id}'>" . $video->getTitle() . "</a>", true);
+    }
 
 }
