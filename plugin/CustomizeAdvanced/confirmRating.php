345e4b6268da4990d4f2a63cdca1c5d21e8a5f58
daniel <daniel@daniel-DC1A-S>
2019-09-03 09:29:28 -0300

    https://github.com/YouPHPTube/YouPHPTube/issues/2002

--- plugin/CustomizeAdvanced/confirmRating.php
+++ plugin/CustomizeAdvanced/confirmRating.php
@@ -6,7 +6,8 @@ $img = $images->poster;
 if (!empty($images->posterPortrait)) {
     $img = $images->posterPortrait;
 }
-
+$imgw = 1280;
+$imgh = 720;
 ?>
 <!DOCTYPE html>
 <html lang="<?php echo $_SESSION['language']; ?>">
@@ -15,6 +16,17 @@ if (!empty($images->posterPortrait)) {
         <?php
         include $global['systemRootPath'] . 'view/include/head.php';
         ?>
+        <link rel="image_src" href="<?php echo $img; ?>" />
+        <meta property="fb:app_id"             content="774958212660408" />
+        <meta property="og:url"                content="<?php echo $global['webSiteRootURL'], "video/", $video['clean_title']; ?>" />
+        <meta property="og:type"               content="video.other" />
+        <meta property="og:title"              content="<?php echo str_replace('"', '', $video['title']); ?> - <?php echo $config->getWebSiteTitle(); ?>" />
+        <meta property="og:description"        content="<?php echo!empty($custom) ? $custom : str_replace('"', '', $video['title']); ?>" />
+        <meta property="og:image"              content="<?php echo $img; ?>" />
+        <meta property="og:image:width"        content="<?php echo $imgw; ?>" />
+        <meta property="og:image:height"       content="<?php echo $imgh; ?>" />
+        <meta property="video:duration" content="<?php echo Video::getItemDurationSeconds($video['duration']); ?>"  />
+        <meta property="duration" content="<?php echo Video::getItemDurationSeconds($video['duration']); ?>"  />
         <style>
             #bg{
                 position: fixed;
