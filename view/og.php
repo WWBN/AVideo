3de1f1296e02a4dcbcf62c40a553347bd0797d65
daniel <daniel@daniel-DC1A-S>
2019-09-20 22:13:52 -0300

    Add opengraph tags for all videos, even the private one

---
+++ view/og.php
@@ -0,0 +1,17 @@
+<?php
+global $global, $config;
+if (!isset($global['systemRootPath'])) {
+    require_once '../videos/configuration.php';
+}
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title><?php echo $config->getWebSiteTitle(); ?></title>
+        <?php 
+        getOpenGraph(@$_GET['videos_id']);
+        ?>
+    </head>
+    <body>
+    </body>
+</html>
