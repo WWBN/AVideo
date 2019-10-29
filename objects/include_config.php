0a598cbaa775b8f06535b352df9e8e7b842ed03e
daniel <daniel@daniel-DC1A-S>
2019-10-29 16:31:18 -0300

    making sure will have the last slash

--- objects/include_config.php
+++ objects/include_config.php
@@ -1,5 +1,8 @@
 <?php
 
+$global['webSiteRootURL'] .= (substr($global['webSiteRootURL'], -1) == '/' ? '' : '/');
+$global['systemRootPath'] .= (substr($global['systemRootPath'], -1) == '/' ? '' : '/');
+
 ini_set('error_log', $global['systemRootPath'] . 'videos/youphptube.log');
 global $global, $config, $advancedCustom, $advancedCustomUser;
 
