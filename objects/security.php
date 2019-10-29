206f76a1a538e08b9753ec62e9760cef6e01f174
daniel <daniel@daniel-DC1A-S>
2019-10-29 12:25:52 -0300

    TALOS-2019-0940

--- objects/security.php
+++ objects/security.php
@@ -1,17 +1,32 @@
 <?php
 
-require_once $global['systemRootPath'].'objects/functions.php';
+require_once $global['systemRootPath'] . 'objects/functions.php';
 // filter some security here
 $securityFilter = array('error', 'catName', 'type', 'channelName', 'captcha', 'showOnly', 'key', 'link');
 $securityFilterInt = array('videos_id', 'video_id', 'categories_id', 'user_id', 'users_id');
+$securityRemoveSingleQuotes = array('search', 'searchPhrase');
 
-if(isset($_GET['search'])){
-    if(is_string($_GET['search'])){
-        $_GET['search'] = trim($_GET['search']);
-    }else if(is_array($_GET['search'])){
-        foreach ($_GET['search'] as $key => $value) {
-            if(is_string($_GET['search'][$key])){
-                $_GET['search'][$key] = trim($_GET['search'][$key]);
+
+foreach ($securityRemoveSingleQuotes as $value) {
+    if (!empty($_POST[$value])) {
+        if (is_string($_POST[$value])) {
+            $_POST[$value] = str_replace("'","",trim($_POST[$value]));
+        } else if (is_array($_POST[$value])) {
+            foreach ($_POST[$value] as $key => $value) {
+                if (is_string($_POST[$value][$key])) {
+                    $_POST[$value][$key] = str_replace("'","",trim($_POST[$value][$key]));
+                }
+            }
+        }
+    }
+    if (!empty($_GET[$value])) {
+        if (is_string($_GET[$value])) {
+            $_GET[$value] = str_replace("'","",trim($_GET[$value]));
+        } else if (is_array($_GET[$value])) {
+            foreach ($_GET[$value] as $key => $value) {
+                if (is_string($_GET[$value][$key])) {
+                    $_GET[$value][$key] = str_replace("'","",trim($_GET[$value][$key]));
+                }
             }
         }
     }
@@ -28,20 +43,20 @@ foreach ($securityFilterInt as $value) {
 
 foreach ($securityFilter as $value) {
     if (!empty($_POST[$value])) {
-        $_POST[$value] = str_replace(array("'",'"',"&quot;","&#039;"), array('','','',''), xss_esc($_POST[$value]));
+        $_POST[$value] = str_replace(array("'", '"', "&quot;", "&#039;"), array('', '', '', ''), xss_esc($_POST[$value]));
     }
     if (!empty($_GET[$value])) {
-        $_GET[$value] = str_replace(array("'",'"',"&quot;","&#039;"), array('','','',''), xss_esc($_GET[$value]));
+        $_GET[$value] = str_replace(array("'", '"', "&quot;", "&#039;"), array('', '', '', ''), xss_esc($_GET[$value]));
     }
 }
 
-if(!empty($_GET['sort']) && is_array($_GET['sort'])){
+if (!empty($_GET['sort']) && is_array($_GET['sort'])) {
     foreach ($_GET['sort'] as $key => $value) {
-        $_GET['sort'][xss_esc($key)] = strcasecmp($value, "ASC")===0?"ASC":"DESC";
+        $_GET['sort'][xss_esc($key)] = strcasecmp($value, "ASC") === 0 ? "ASC" : "DESC";
     }
 }
-if(!empty($_POST['sort']) && is_array($_POST['sort'])){
+if (!empty($_POST['sort']) && is_array($_POST['sort'])) {
     foreach ($_POST['sort'] as $key => $value) {
-        $_POST['sort'][xss_esc($key)] = strcasecmp($value, "ASC")===0?"ASC":"DESC";
+        $_POST['sort'][xss_esc($key)] = strcasecmp($value, "ASC") === 0 ? "ASC" : "DESC";
     }
 }
