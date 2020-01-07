772d10274f813bb40887bf8b2242347d5d3736e3
daniel <daniel@daniel-DC1A-S>
2020-01-07 10:21:32 -0300

    Fix

--- objects/functions.php
+++ objects/functions.php
@@ -2534,7 +2534,7 @@ function _session_start(Array $options = array()) {
 
 function _mysql_connect() {
     global $global, $mysqlHost, $mysqlUser, $mysqlPass, $mysqlDatabase, $mysqlPort;
-    if (!$global['mysqli']->ping()) {
+    if ($global['mysqli']->ping()===false) {
         try {
             $global['mysqli'] = new mysqli($mysqlHost, $mysqlUser, $mysqlPass, $mysqlDatabase, @$mysqlPort);
             if (!empty($global['mysqli_charset'])) {
