a9eb0c25b9eca6a35b9d801be1d1c9431e742f79
daniel <daniel@daniel-DC1A-S>
2020-01-10 08:58:50 -0300

    try to avoid cache on ads

--- plugin/AD_Server/footer.php
+++ plugin/AD_Server/footer.php
@@ -4,7 +4,7 @@
     if (typeof player === 'undefined') {
         player = videojs('mainVideo'<?php echo PlayerSkins::getDataSetup(); ?>);
     }
-    var options = {id: 'mainVideo', adTagUrl: '<?php echo $global['webSiteRootURL'] ?>plugin/AD_Server/VMAP.php?video_length=<?php echo $video_length ?>&vmap_id=<?php echo $vmap_id ?>'};
+    var options = {id: 'mainVideo', adTagUrl: '<?php echo $global['webSiteRootURL'] ?>plugin/AD_Server/VMAP.php?video_length=<?php echo $video_length ?>&vmap_id=<?php echo $vmap_id ?>&random=<?php echo uniqid(); ?>'};
         player.ima(options);
         $(document).ready(function () {
 <?php
