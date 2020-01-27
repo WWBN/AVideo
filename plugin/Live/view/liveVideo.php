7a5b151e2eb2cdab1ba291102829dd2b78bf6aa4
daniel <daniel@daniel-DC1A-S>
2020-01-27 11:10:27 -0300

    Add the inline option for livestream

--- plugin/Live/view/liveVideo.php
+++ plugin/Live/view/liveVideo.php
@@ -12,7 +12,7 @@
                 </button>
             </div>
             <div id="main-video" class="embed-responsive embed-responsive-16by9">
-                <video poster="<?php echo $global['webSiteRootURL']; ?>plugin/Live/view/OnAir.jpg" controls 
+                <video poster="<?php echo $global['webSiteRootURL']; ?>plugin/Live/view/OnAir.jpg" controls playsinline webkit-playsinline="webkit-playsinline" 
                        class="embed-responsive-item video-js vjs-default-skin vjs-big-play-centered liveVideo vjs-16-9" 
                        id="mainVideo">
                     <source src="<?php echo $p->getM3U8File($uuid); ?>" type='application/x-mpegURL'>
