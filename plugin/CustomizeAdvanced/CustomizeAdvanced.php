66d104ec2d23d712e98b488b852a27f1c2896531
daniel <daniel@daniel-DC1A-S>
2020-02-11 14:39:37 -0300

    Custom bulk email list size

--- plugin/CustomizeAdvanced/CustomizeAdvanced.php
+++ plugin/CustomizeAdvanced/CustomizeAdvanced.php
@@ -60,7 +60,7 @@ class CustomizeAdvanced extends PluginAbstract {
         $obj->EnableMinifyJS = false;
         $obj->disableShareAndPlaylist = false;
         $obj->disableEmailSharing = false;
-        $obj->splitBulkEmailSend = 90;
+        $obj->splitBulkEmailSend = 50;
         $obj->disableComments = false;
         $obj->commentsMaxLength = 200;
         $obj->commentsNoIndex = false;
