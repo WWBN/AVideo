12270fd62e799445b303057be0b083d006a30879
daniel <daniel@daniel-DC1A-S>
2020-03-13 11:14:57 -0300

    add a proxy to handle http/https content

--- plugin/LiveLinks/proxy.php
+++ plugin/LiveLinks/proxy.php
@@ -14,5 +14,7 @@ $url = parse_url($_GET['livelink']);
 if ($url['scheme'] == 'https') {
     header("Location: {$_GET['livelink']}");
 } else {
+    header ("Content-Type: video/vnd.mpegurl");
+    header ("Content-Disposition: attachment;filename=playlist.m3u");
     echo url_get_contents($_GET['livelink']);
 }
