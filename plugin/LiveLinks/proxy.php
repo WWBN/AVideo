386662fb0fe2fb31b0ce0b98ade0a32129045c87
daniel <daniel@daniel-DC1A-S>
2020-03-13 12:11:16 -0300

    add a proxy to handle http/https content

--- plugin/LiveLinks/proxy.php
+++ plugin/LiveLinks/proxy.php
@@ -1,7 +1,14 @@
 <?php
+
 require_once '../../videos/configuration.php';
 session_write_close();
-$global['mysqli']->close();
+try {
+    $global['mysqli']->close();
+} catch (Exception $exc) {
+    //echo $exc->getTraceAsString();
+}
+
+
 /*
  * this file is to handle HTTP URLs into HTTPS
  */
@@ -9,12 +16,19 @@ if (!filter_var($_GET['livelink'], FILTER_VALIDATE_URL)) {
     echo "Invalid Link";
     exit;
 }
-$url = parse_url($_GET['livelink']);
-
-if ($url['scheme'] == 'https') {
-    header("Location: {$_GET['livelink']}");
-} else {
-    header ("Content-Type: video/vnd.mpegurl");
-    header ("Content-Disposition: attachment;filename=playlist.m3u");
-    echo url_get_contents($_GET['livelink']);
-}
+header("Content-Type: video/vnd.mpegurl");
+header("Content-Disposition: attachment;filename=playlist.m3u");
+$content = url_get_contents($_GET['livelink']);
+foreach (preg_split("/((\r?\n)|(\r\n?))/", $content) as $line) {
+    $line = trim($line);
+    if (!empty($line) && $line[0] !== "#") {
+        if (!filter_var($line, FILTER_VALIDATE_URL)) {
+            $pathinfo = pathinfo($_GET['livelink']);
+            if(!empty($pathinfo["extension"])){
+                $_GET['livelink'] = str_replace($pathinfo["basename"], "", $_GET['livelink']);
+            }
+            $line = $_GET['livelink'].$line;
+        }
+    }
+    echo $line.PHP_EOL;
+} 
