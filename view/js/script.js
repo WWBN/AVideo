458fc4b7354cac53b023c27022dd94daed482404
daniel <daniel@daniel-DC1A-S>
2020-03-30 08:59:12 -0300

    https://github.com/WWBN/AVideo/issues/2780

--- view/js/script.js
+++ view/js/script.js
@@ -22,8 +22,8 @@ $(document).mousemove(function (e) {
 });
 
 String.prototype.stripAccents = function () {
-    var translate_re = /[àáâãäçèéêëìíîïñòóôõöùúûüýÿÀÁÂÃÄÇÈÉÊËÌÍÎÏÑÒÓÔÕÖÙÚÛÜÝ]/g;
-    var translate = 'aaaaaceeeeiiiinooooouuuuyyAAAAACEEEEIIIINOOOOOUUUUY';
+    var translate_re = /[àáâãäçèéêëìíîïñòóôõöùúûüýÿÀÁÂÃÄÇÈÉÊËÌÍÎÏÑÒÓÔÕÖÙÚÛÜÝЄІЃі№єѓАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюя—«»…]/g;
+    var translate = 'aaaaaceeeeiiiinooooouuuuyyAAAAACEEEEIIIINOOOOOUUUUYYEIGi#yegABVGDEYOZHZIJKLMNOPRSTUFHCCHSHSHH-YEYUYAabvgdeyozhzijklmnoprstufhcchshshhyeyuya-';
     return (this.replace(translate_re, function (match) {
         return translate.substr(translate_re.source.indexOf(match) - 1, 1);
     })
@@ -552,11 +552,12 @@ function playerPlay(currentTime) {
     }
 }
 
-function formatBytes(bytes,decimals) {
-   if(bytes == 0) return '0 Bytes';
-   var k = 1024,
-       dm = decimals <= 0 ? 0 : decimals || 2,
-       sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'],
-       i = Math.floor(Math.log(bytes) / Math.log(k));
-   return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+function formatBytes(bytes, decimals) {
+    if (bytes == 0)
+        return '0 Bytes';
+    var k = 1024,
+            dm = decimals <= 0 ? 0 : decimals || 2,
+            sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'],
+            i = Math.floor(Math.log(bytes) / Math.log(k));
+    return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
 }
