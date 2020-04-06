599a8723484450eef141e30ee0cd02d61e12b7dd
daniel <daniel@daniel-DC1A-S>
2020-04-06 14:49:45 -0300

    https://github.com/WWBN/AVideo/issues/2823

--- plugin/LiveLinks/getImage.php
+++ plugin/LiveLinks/getImage.php
@@ -1,7 +1,7 @@
 <?php
+
 require_once '../../videos/configuration.php';
 session_write_close();
-header('Content-Type: image/x-png');
 $filename = $global['systemRootPath'] . 'plugin/Live/view/OnAir.jpg';
 //echo file_get_contents($filename);exit;
 
@@ -13,35 +13,45 @@ require_once $global['systemRootPath'] . 'plugin/LiveLinks/Objects/LiveLinksTabl
 $liveLink = new LiveLinksTable($_GET['id']);
 if (empty($_GET['format'])) {
     $_GET['format'] = "png";
+    header('Content-Type: image/x-png');
+} else if ($_GET['format'] === 'jpg') {
+    header('Content-Type: image/jpg');
+    $destination .= "." . $_GET['format'];
+} else if ($_GET['format'] === 'gif') {
+    header('Content-Type: image/gif');
+} else if ($_GET['format'] === 'webp') {
+    header('Content-Type: image/webp');
+} else {
+    $_GET['format'] = "png";
+    header('Content-Type: image/x-png');
 }
 $video = $liveLink->getLink();
 
-    
+
 if (preg_match("/\b(?:(?:https?):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i", $video)) {
     $url = $config->getEncoderURL() . "getImage/" . base64_encode($video) . "/{$_GET['format']}";
     if (empty($_SESSION[$url]['expire']) || $_SESSION[$url]['expire'] < time()) {
-        _error_log("LiveLink: getImage.php: ".$url);
+        _error_log("LiveLink: getImage.php: " . $url);
         $content = url_get_contents($url);
         if (session_status() == PHP_SESSION_NONE) {
             session_start();
         }
-        _error_log(" Image Expired in ".  date("d/m/Y H:i:s", @$_SESSION[$url]['expire'])." NOW is ".  date("d/m/Y H:i:s"));
+        _error_log(" Image Expired in " . date("d/m/Y H:i:s", @$_SESSION[$url]['expire']) . " NOW is " . date("d/m/Y H:i:s"));
         $_SESSION[$url] = array('content' => $content, 'expire' => strtotime("+2 min"));
-        _error_log(" New Image will Expired in ".  date("d/m/Y H:i:s", $_SESSION[$url]['expire'])." NOW is ".  date("d/m/Y H:i:s"));
+        _error_log(" New Image will Expired in " . date("d/m/Y H:i:s", $_SESSION[$url]['expire']) . " NOW is " . date("d/m/Y H:i:s"));
     }
-    if(!empty($_SESSION[$url]['content'])){
+    if (!empty($_SESSION[$url]['content'])) {
         echo $_SESSION[$url]['content'];
-        _error_log(" Cached Good until ".  date("d/m/Y H:i:s", $_SESSION[$url]['expire'])." NOW is ".  date("d/m/Y H:i:s"));
-    }else{
+        _error_log(" Cached Good until " . date("d/m/Y H:i:s", $_SESSION[$url]['expire']) . " NOW is " . date("d/m/Y H:i:s"));
+    } else {
         echo url_get_contents($filename);
         _error_log(" Get default image ");
     }
-    
 } else {
     echo local_get_contents($filename);
     _error_log(" Invalid URL ");
 }
 $p = AVideoPlugin::loadPluginIfEnabled("Cache");
-if(!empty($p)){
+if (!empty($p)) {
     $p->getEnd();
 }
