813a564f05f03d5eb72b3cf9336333008e8f3b69
daniel <daniel@daniel-DC1A-S>
2020-04-23 08:13:20 -0300

    Add custom colors feature

--- admin/index.php
+++ admin/index.php
@@ -63,6 +63,7 @@ $menu = new MenuAdmin(__("Design"), "fas fa-pen-fancy");
 $menu->addItem(new MenuAdmin(__("First Page Style"), "fas fa-columns", "design_first_page"));
 $menu->addItem(new MenuAdmin(__("Player Skin"), "fas fa-play-circle", "design_player"));
 $menu->addItem(new MenuAdmin(__("Themes"), "fas fa-palette", "design_themes"));
+$menu->addItem(new MenuAdmin(__("Colors"), "fas fa-palette", "design_colors"));
 //$menu->addItem(new MenuAdmin(__("Custom CSS"), "fab fa-css3-alt", "design_css"));
 $itens[] = $menu;
 
@@ -98,6 +99,9 @@ switch ($_GET['page']) {
         break;
     case "design_themes":
         $includeBody = $global['systemRootPath'] . 'admin/design_themes.php';
+        break;
+    case "design_colors":
+        $includeBody = $global['systemRootPath'] . 'admin/design_colors.php';
         break;
     case "design_player":
         $includeBody = $global['systemRootPath'] . 'admin/design_player.php';
