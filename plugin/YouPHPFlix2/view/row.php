75225250ec47ceff392f942496de42f18abe64cf
daniel <daniel@daniel-DC1A-S>
2020-06-14 12:46:06 -0300

    Fix option

--- plugin/YouPHPFlix2/view/row.php
+++ plugin/YouPHPFlix2/view/row.php
@@ -40,7 +40,7 @@ TimeLogStart($timeLog3);
                             <img style="position: absolute; top: 0; display: none;" src="<?php echo $global['webSiteRootURL']; ?>view/img/placeholder-image.png"  alt="<?php echo $value['title']; ?>" id="tile__img thumbsGIF<?php echo $value['id']; ?>" class="thumbsGIF img-responsive img carousel-cell-image" data-flickity-lazyload="<?php echo $imgGif; ?>" />
                         <?php } ?>
                         <?php
-                        if ($advancedCustom->paidOnlyFreeLabel && $obj->paidOnlyLabelOverPoster) {
+                        if ($advancedCustom->paidOnlyShowLabels && $obj->paidOnlyLabelOverPoster) {
                             foreach ($value['tags'] as $value2) {
                                 if (!empty($value2->label) && $value2->label === __("Paid Content")) {
                                     ?><span class="paidOnlyLabel label label-<?php echo $value2->type; ?>"><?php echo $value2->text; ?></span><?php
@@ -57,7 +57,7 @@ TimeLogStart($timeLog3);
                             <div class="progress-bar progress-bar-danger" role="progressbar" style="width: <?php echo $value['progress']['percent'] ?>%;" aria-valuenow="<?php echo $value['progress']['percent'] ?>" aria-valuemin="0" aria-valuemax="100"></div>
                         </div>
                         <?php
-                        if ($advancedCustom->paidOnlyFreeLabel && !$obj->paidOnlyLabelOverPoster) {
+                        if ($advancedCustom->paidOnlyShowLabels && !$obj->paidOnlyLabelOverPoster) {
                             foreach ($value['tags'] as $value2) {
                                 if (!empty($value2->label) && $value2->label === __("Paid Content")) {
                                     ?><div class="label label-<?php echo $value2->type; ?>" style="margin: 0; margin-top: -2px;  width: 100%; display: block; border-top-left-radius: 0; border-top-right-radius: 0; "><?php echo $value2->text; ?></div><?php
