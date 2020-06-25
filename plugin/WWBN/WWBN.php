e88c6104d449b2769c6834ec286dc5cd732b5d30
daniel <daniel@daniel-DC1A-S>
2020-06-25 10:32:35 -0300

    WWBN connection

---
+++ plugin/WWBN/WWBN.php
@@ -0,0 +1,36 @@
+<?php
+
+require_once $global['systemRootPath'] . 'plugin/Plugin.abstract.php';
+
+class WWBN extends PluginAbstract {
+
+    public function getDescription() {
+        global $global;
+        $desc = "WWBN Network Index (this plugin is under development)";
+        return $desc;
+    }
+
+    public function getName() {
+        return "WWBN";
+    }
+
+    public function getUUID() {
+        return "WWBN-Network-Index";
+    }
+
+    public function getPluginVersion() {
+        return "1.0";
+    }
+
+    public function getEmptyDataObject() {
+        global $global;
+        $obj = new stdClass();
+        return $obj;
+    }
+    
+    public function getPluginMenu() {
+        global $global;
+        return '<a href="'.$global['webSiteRootURL'].'plugin/WWBN/page/wwbn_signup.php" class="btn btn-primary btn-sm btn-xs btn-block"><i class="fa fa-cog"></i> Set up</a>';
+    }
+
+}
---
+++ plugin/WWBN/page/wwbn_signup.php
@@ -0,0 +1,38 @@
+<?php
+require_once '../../../videos/configuration.php';
+require_once $global['systemRootPath'] . 'objects/user.php';
+if (!User::isAdmin()) {
+    header("Location: {$global['webSiteRootURL']}?error=" . __("You can not manager plugin add logo"));
+    exit;
+}
+$obj = AVideoPlugin::getObjectDataIfEnabled("WWBN");
+if(empty($obj)){
+    die("Plugin disabled");
+}
+?>
+<!DOCTYPE html>
+<html lang="<?php echo $_SESSION['language']; ?>">
+    <head>
+        <title><?php echo $config->getWebSiteTitle(); ?>  :: WWBN</title>
+        <?php
+        include $global['systemRootPath'] . 'view/include/head.php';
+        ?>
+
+    </head>
+    <body class="<?php echo $global['bodyClass']; ?>">
+        <?php
+        include $global['systemRootPath'] . 'view/include/navbar.php';
+        ?>
+        <div class="container-fluid"> 
+            <iframe src="https://wwbn.com/" style="width: 100%; height: calc(100vh - 60px);"></iframe>
+        </div>
+        <?php
+        include $global['systemRootPath'] . 'view/include/footer.php';
+        ?>
+        <script>
+            $(document).ready(function () {
+                
+            });
+        </script>
+    </body>
+</html>
