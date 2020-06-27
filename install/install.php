838ba1bacc239777561bcc69b51c6d1bc072ac4b
daniel <daniel@daniel-DC1A-S>
2020-06-27 11:59:00 -0300

    Direct install with command line

--- install/install.php
+++ install/install.php
@@ -9,6 +9,10 @@ if (file_exists("../videos/configuration.php")) {
 }
 
 $webSiteRootURL = @$argv[1];
+$databaseUser = empty($argv[2])?"youphptube":$argv[2];
+$databasePass = empty($argv[3])?"youphptube":$argv[3];
+$systemAdminPass = empty($argv[4])?"123":$argv[4];
+$contactEmail = empty($argv[5])?"undefined@youremail.com":$argv[5];
 while (!filter_var($webSiteRootURL, FILTER_VALIDATE_URL)) {
     if (!empty($webSiteRootURL)) {
         echo "Invalid Site URL\n";
@@ -21,13 +25,13 @@ while (!filter_var($webSiteRootURL, FILTER_VALIDATE_URL)) {
 
 $_POST['systemRootPath'] = getPathToApplication();
 $_POST['databaseHost'] = "localhost";
-$_POST['databaseUser'] = "youphptube";
-$_POST['databasePass'] = "youphptube";
+$_POST['databaseUser'] = $databaseUser;
+$_POST['databasePass'] = $databasePass;
 $_POST['databasePort'] = "3306";
 $_POST['databaseName'] = "AVideoStreamer";
 $_POST['createTables'] = 2;
-$_POST['contactEmail'] = "undefined@youremail.com";
-$_POST['systemAdminPass'] = "123";
+$_POST['contactEmail'] = $contactEmail;
+$_POST['systemAdminPass'] = $systemAdminPass;
 $_POST['mainLanguage'] = "en";
 $_POST['webSiteTitle'] = "AVideo";
 $_POST['webSiteRootURL'] = getURLToApplication();
