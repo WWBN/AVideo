c9eba2413c250959c8689d03526d193846714e85
DanielnetoDotCom <danielneto.com@gmail.com>
2020-07-04 11:19:54 -0300

    Fix notification

--- plugin/Live/view/onlineLabel.php
+++ plugin/Live/view/onlineLabel.php
@@ -106,7 +106,7 @@ $live_servers_id = Live::getCurrentLiveServersId();
                     $('.liveViewCount').text(" " + response.nclients);
                     $('#liveViewStatus<?php echo $live_servers_id; ?>').text(response.msg);
                     $('#onlineApplications').text(response.applications.lenght);
-                    timeout = 10000;
+                    timeout = 15000;
                 }
                 setTimeout(function () {
                     getStats<?php echo $live_servers_id; ?>();
