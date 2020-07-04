b5284174131b0d765770b2d5940671fa56ce6f82
DanielnetoDotCom <danielneto.com@gmail.com>
2020-07-04 10:35:27 -0300

    Fix notification

--- plugin/Live/view/onlineLabel.php
+++ plugin/Live/view/onlineLabel.php
@@ -90,6 +90,7 @@ $live_servers_id = Live::getCurrentLiveServersId();
     }
 
     function getStats<?php echo $live_servers_id; ?>() {
+        var timeout = 5000;
         $.ajax({
             url: '<?php echo $global['webSiteRootURL']; ?>plugin/Live/stats.json.php?live_servers_id=<?php echo $live_servers_id; ?>&Label',
             data: {"name": "<?php echo $streamName; ?>"},
@@ -105,10 +106,11 @@ $live_servers_id = Live::getCurrentLiveServersId();
                     $('.liveViewCount').text(" " + response.nclients);
                     $('#liveViewStatus<?php echo $live_servers_id; ?>').text(response.msg);
                     $('#onlineApplications').text(response.applications.lenght);
+                    timeout = 10000;
                 }
                 setTimeout(function () {
                     getStats<?php echo $live_servers_id; ?>();
-                }, 10000);
+                }, timeout);
             }
         });
     }
