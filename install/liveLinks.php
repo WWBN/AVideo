5e9b3d3303f1cffee8f421502128e0b0e74f400c
DanielnetoDotCom <danielneto.com@gmail.com>
2020-07-31 13:57:50 -0300

    Command line to easily get live stream links

---
+++ install/liveLinks.php
@@ -0,0 +1,30 @@
+<?php
+
+//streamer config
+require_once '../videos/configuration.php';
+
+if (!isCommandLineInterface()) {
+    return die('Command Line only');
+}
+
+AVideoPlugin::loadPlugin("Live");
+
+$channelName = $global['mysqli']->real_escape_string($channelName);
+$sql = "SELECT lt.*, u.* FROM users u LEFT JOIN live_transmitions lt ON users_id = u.id "
+        . " WHERE canStream = 1 AND status = 'a' LIMIT 20";
+$res = sqlDAL::readSql($sql);
+$users = sqlDAL::fetchAllAssoc($res);
+sqlDAL::close($res);
+if ($res != false) {
+    foreach ($users as $row) {
+        echo "-----------------------------------".PHP_EOL;
+        echo "{$row['id']} - {$row['user']} ".PHP_EOL;
+        echo Live::getLinkToLiveFromUsers_id($row['id']).PHP_EOL;        
+        echo "-----------------------------------".PHP_EOL;
+    }
+}
+die();
+
+
+
+
