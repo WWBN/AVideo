52f5a27f8665119aed4cc9fbc9a649be2c3893ed
Luděk Janča <ludek.janca@wedos.com>
2020-09-08 09:50:10 +0200

    l10n - payments settings

--- admin/payments_settings.php
+++ admin/payments_settings.php
@@ -1,16 +1,16 @@
 <div class="row">
     <div class="col-md-12">
         <div class="panel panel-default">
-            <div class="panel-heading"><i class="fas fa-wallet"></i> Wallet <div class="pull-right"><?php echo getPluginSwitch('YPTWallet'); ?></div></div>
+            <div class="panel-heading"><i class="fas fa-wallet"></i> <?php echo __('Wallet'); ?> <div class="pull-right"><?php echo getPluginSwitch('YPTWallet'); ?></div></div>
             <div class="panel-body" style="overflow: hidden;">
                 <?php
                 $filter = array(
-                    'decimalPrecision' => 'Usually for USD we use 2, for cryptocurrencies we use more the 2',
-                    'currency' => 'Australian Dollar = AUD, Brazilian Real = BRL, Canadian Dollar = CAD, Euro = EUR, U.S. Dollar = USD, etc',
-                    'currency_symbol' => '$, R$, etc, the format will be  {currency} {value} {currency_symbol} for example ($ 10.00 USD) or (R$ 10.00 BRL)',
-                    'manualAddFundsTransferFromUserId' => 'When some one buy something on your web site, the wallet balance will be transferred to this user ID',
-                    'enablePlugin_YPTWalletPayPal' => 'You need to enable it to be able to use PayPal to add funds on your wallet',
-                    'enableManualWithdrawFundsPage' => 'Let users request withdraws from his wallet. the withdraw mus be done manually');
+                    'decimalPrecision' => __('Usually for USD we use 2, for cryptocurrencies we use more the 2'),
+                    'currency' => __('Australian Dollar = AUD, Brazilian Real = BRL, Canadian Dollar = CAD, Euro = EUR, U.S. Dollar = USD, etc'),
+                    'currency_symbol' => __('$, R$, etc, the format will be  {currency} {value} {currency_symbol} for example ($ 10.00 USD) or (R$ 10.00 BRL)'),
+                    'manualAddFundsTransferFromUserId' => __('When some one buy something on your web site, the wallet balance will be transferred to this user ID'),
+                    'enablePlugin_YPTWalletPayPal' => __('You need to enable it to be able to use PayPal to add funds on your wallet'),
+                    'enableManualWithdrawFundsPage' => __('Let users request withdraws from his wallet. the withdraw mus be done manually'));
                 createTable("YPTWallet", $filter);
                 ?>
             </div>
@@ -26,5 +26,4 @@
             </div>
         </div>
     </div>
-
 </div>
