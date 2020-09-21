ba1f51090d0a4826d9465f09e603d9786502b638
DanielnetoDotCom <danielneto.com@gmail.com>
2020-09-21 15:20:48 -0300

    Autoplay improvements

--- plugin/Meet/iframe.php
+++ plugin/Meet/iframe.php
@@ -120,6 +120,8 @@ $domain = Meet::getDomainURL();
                 background: #000;
                 position: fixed;
                 top: 0;
+                left: 0;
+                width: 100%;
             }
             
         </style>
