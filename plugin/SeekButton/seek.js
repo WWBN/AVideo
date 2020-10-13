a64e64291cc348e363715a69e87708ba9b17c187
DanielnetoDotCom <danielneto.com@gmail.com>
2020-10-13 15:32:49 -0300

    Make sure the audio will work just like the video https://github.com/WWBN/AVideo/issues/3706 https://github.com/WWBN/AVideo/issues/3808

--- plugin/SeekButton/seek.js
+++ plugin/SeekButton/seek.js
@@ -1,6 +1,9 @@
 player.seekButtons({forward: playerSeekForward, back: playerSeekBack});
+var trackDisplayTimeout;
 this.el_.querySelector(".vjs-text-track-display").style.pointerEvents = "auto"
 document.querySelector(".vjs-text-track-display").addEventListener("dblclick", function (e) {
+    console.log("dbl click happen "+trackDisplayTimeout);
+    clearTimeout(trackDisplayTimeout);
     const playerWidth = document.querySelector("#mainVideo").getBoundingClientRect().width;
     if (0.66 * playerWidth < e.offsetX) {
         $(forwardLayer).prependTo("#mainVideo");
@@ -19,7 +22,6 @@ document.querySelector(".vjs-text-track-display").addEventListener("dblclick", f
                 $('#backLayer').remove();
             });
         }, 100);
-
         player.currentTime((player.currentTime() - playerSeekBack) < 0 ? 0 : (player.currentTime() - playerSeekBack));
     } else {
         if (player.paused()) {
@@ -28,4 +30,19 @@ document.querySelector(".vjs-text-track-display").addEventListener("dblclick", f
             player.pause();
         }
     }
+});
+document.querySelector(".vjs-text-track-display").addEventListener("click", function (e) {
+    console.log("single click happen");
+    clearTimeout(trackDisplayTimeout);
+    trackDisplayTimeout = setTimeout(function (){
+        
+        console.log("single click timeout");
+        if (player.paused()) {
+            player.play();
+        } else {
+            player.pause();
+        }
+    }, 300);
+    
+    console.log("single click register "+trackDisplayTimeout);
 });
