f613a834344f5dcbe2e222d220081026a3e49c2d
DanielnetoDotCom <danielneto.com@gmail.com>
2020-10-26 11:35:17 -0300

    https://github.com/WWBN/AVideo/pull/3840 cannot be bigint

--- updatedb/updateDb.v9.7.sql
+++ updatedb/updateDb.v9.7.sql
@@ -3,10 +3,10 @@
 -- support for encoder 3.3 and dynamic resolutions for MP4 and webm
 
 ALTER TABLE `users` 
-CHANGE COLUMN `email` VARCHAR(254) NULL,
-CHANGE COLUMN `created` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP;
+CHANGE COLUMN `email` `email` VARCHAR(254) NULL,
+CHANGE COLUMN `created` `created` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP;
 
 ALTER TABLE `configurations` 
-CHANGE COLUMN `contactEmail` VARCHAR(254) NOT NULL;
+CHANGE COLUMN `contactEmail` `contactEmail` VARCHAR(254) NOT NULL;
 
 UPDATE configurations SET  version = '9.7', modified = now() WHERE id = 1;
