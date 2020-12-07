1d815b9ac1c691ce7aaa662f20609cf4df9b65cc
DanielnetoDotCom <danielneto.com@gmail.com>
2020-12-07 19:13:40 -0300

    https://github.com/WWBN/AVideo/issues/4128 Add mrss

--- feed/mrss.php
+++ feed/mrss.php
@@ -1,21 +1,13 @@
 <?php
-echo'<rss version="2.0" xmlns:media="http://search.yahoo.com/mrss/"
-  xmlns:georss="http://www.georss.org/georss"
-  xmlns:gml="http://www.opengis.net/gml">'
+echo'<?xml version="1.0" encoding="UTF-8"?>';
 ?>
-<rss version="2.0" xmlns:content="http://purl.org/rss/1.0/modules/content/"
-     xmlns:wfw="http://wellformedweb.org/CommentAPI/"
-     xmlns:dc="http://purl.org/dc/elements/1.1/"
-     xmlns:atom="http://www.w3.org/2005/Atom"
-     xmlns:sy="http://purl.org/rss/1.0/modules/syndication/"
-     xmlns:slash="http://purl.org/rss/1.0/modules/slash/">
+<rss version="2.0" xmlns:media="http://search.yahoo.com/mrss/"
+  xmlns:georss="http://www.georss.org/georss"
+  xmlns:gml="http://www.opengis.net/gml">
     <channel>
-        <atom:link href="<?php echo $global['webSiteRootURL'] . ltrim($_SERVER["REQUEST_URI"], "/"); ?>" rel="self" type="application/rss+xml" />
         <title><?php echo $title; ?></title>
         <description><?php echo $description; ?></description>
         <link><?php echo $link; ?></link>
-        <sy:updatePeriod>hourly</sy:updatePeriod>
-        <sy:updateFrequency>1</sy:updateFrequency>
 
         <image>
         <title><?php echo $title; ?></title>
@@ -32,7 +24,9 @@ echo'<rss version="2.0" xmlns:media="http://search.yahoo.com/mrss/"
             $files = getVideosURL($row['filename']);
             $enclosure = "";
             $videoSource = Video::getHigestResolution($row['filename']);
-            var_dump($row['filename'], $videoSource);
+            if(empty($videoSource["url"])){
+                continue;
+            }
             foreach ($files as $value) {
                 if ($value["type"] === "video" && file_exists($value['path'])) {
                     $path_parts = pathinfo($value['path']);
@@ -53,8 +47,8 @@ echo'<rss version="2.0" xmlns:media="http://search.yahoo.com/mrss/"
                 <pubDate><?php echo date('r', strtotime($row['created'])); ?></pubDate>
                 <guid><?php echo Video::getLinkToVideo($row['id'], $row['clean_title'], false, "permalink"); ?></guid>
 
-                <media:content url="<?php echo $videoSource["url"]; ?>" fileSize="<?php echo $video["filesize"]; ?>" bitrate="128" type="<?php echo mime_content_type_per_filename($video["filesize"]); ?>" expression="full" />
-                <media:embed url="<?php echo Video::getLinkToVideo($row['id'], $row['clean_named'], true); ?>"></media:embed>
+                <media:content url="<?php echo $videoSource["url"]; ?>" fileSize="<?php echo $video["filesize"]; ?>" bitrate="128" type="<?php echo mime_content_type_per_filename($videoSource["path"]); ?>" expression="full" />
+                <media:embed url="<?php echo Video::getLinkToVideo($row['id'], $row['clean_title'], true); ?>"/>
                 <media:status state="active" />
 
             </item>
