a3a7709056504fbe57177d90d093449a96d0d34e
DanielnetoDotCom <danielneto.com@gmail.com>
2020-12-10 13:54:02 -0300

    Improve fullscreen CSS

--- plugin/YouPHPFlix2/view/js/fullscreen.js
+++ plugin/YouPHPFlix2/view/js/fullscreen.js
@@ -13,8 +13,8 @@ function flixFullScreen(link) {
     $('body').addClass('fullScreen');
     var divHTML = '<div id="divIframeFull" style="background-color:black; text-align: center; position: fixed; top: 0;left: 0; z-index: 9999;">';
     divHTML += '<div id="divTopBar" style="position: fixed; top: 0; left: 0; height: 50px; width: 100vw; z-index: 99999; padding:10px; ">';
-    divHTML += '<span id="closeBtnFull" class="btn pull-right" onclick="closeFlixFullScreen();" style="opacity: 0.7; filter: alpha(opacity=70);">';
-    divHTML += '<i class="fa fa-times" style="text-shadow: 1px 1px rgba(255,255,255,0.7);"></i></span></div></div>';
+    divHTML += '<span id="closeBtnFull" class="pull-right" onclick="closeFlixFullScreen();">';
+    divHTML += '<i class="fa fa-times"></i></span></div></div>';
     var div = $(divHTML).append('<iframe src="' + link + '" style="background-color:black; position: fixed; top: 0; left: 0; height: 100vh; width: 100vw; z-index: 9999; overflow: hidden;"  frameBorder="0" id="iframeFull" allow="autoplay" allowfullscreen webkitallowfullscreen mozallowfullscreen oallowfullscreen msallowfullscreen>');
     $('body').append(div);
     $('body').addClass('fullscreen');
