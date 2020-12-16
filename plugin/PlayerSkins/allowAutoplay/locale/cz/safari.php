3b5927aec1b16241dbb9ff064103b428491c3d29
Luděk <ludek.janca@wedos.com>
2020-12-16 12:56:20 +0100

    l10n - Czech localisation of PlayerSkins plugin

---
+++ plugin/PlayerSkins/allowAutoplay/locale/cz/safari.php
@@ -0,0 +1,23 @@
+<style>
+    #allowAutoplay li{
+        text-align: left;
+    }
+</style>
+<small>
+     <ol>
+         <li>
+             <p> V aplikaci Safari <img src="https://help.apple.com/assets/5D7C3E90680CE20102EF0AED/5D7C3EA2680CE20102EF0AF4/en_US/4236b540374dd23e0ba9e00d84f328ac.png" alt="Safari" height ="30"> na počítači Mac zvolte Safari &gt; Nastavení tohoto webu. </p>
+             <p> Můžete také kliknout se stisknutou klávesou Ctrl do pole Inteligentní vyhledávání a zvolit Nastavení pro tento web. </p>
+         </li>
+         <li>
+             <p> Podržte ukazatel napravo od automatického přehrávání, klikněte na rozbalovací nabídku a vyberte požadovanou možnost: </p>
+             <ul>
+                 <li>
+                     <p>
+                         <em> Povolit všechny automatické přehrávání: </em> umožňuje automatické přehrávání videí na tomto webu.
+                     </p>
+                 </li>
+             </ul>
+         </li>
+     </ol>
+</small>
