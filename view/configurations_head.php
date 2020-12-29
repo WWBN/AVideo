9d762685f72fc2ca630f4f708c7c061dcc4d2a42
SiZiOUS <sizious@gmail.com>
2020-12-29 19:49:36 +0100

    Removing jQuery from this file, as it's already added

--- view/configurations_head.php
+++ view/configurations_head.php
@@ -1,4 +1,3 @@
-<script src="<?php echo $global['webSiteRootURL']; ?>view/js/jquery-3.5.1.min.js"></script>
 <link href="<?php echo $global['webSiteRootURL']; ?>view/js/Croppie/croppie.css" rel="stylesheet" type="text/css"/>
 <script src="<?php echo $global['webSiteRootURL']; ?>view/js/Croppie/croppie.min.js" type="text/javascript"></script>
 <style>
