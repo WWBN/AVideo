da8dc27a67f51342cd055fb6c6d49b4194bfd796
DanielnetoDotCom <danielneto.com@gmail.com>
2020-12-30 09:16:23 -0300

    Notice: Constant PAGE_TITLE_SEPARATOR already defined fix

--- objects/configuration.php
+++ objects/configuration.php
@@ -499,10 +499,12 @@ require_once \$global['systemRootPath'].'objects/include_config.php';
     function setEncoderURL($encoderURL) {
         $this->encoderURL = $encoderURL;
     }
-	
-	function getPageTitleSeparator() {
-		define("PAGE_TITLE_SEPARATOR", "&middot;"); // This is ready to be configurable, if needed
-		return " " . PAGE_TITLE_SEPARATOR . " ";
-	}
+
+    function getPageTitleSeparator() {
+        if(!defined(PAGE_TITLE_SEPARATOR)){
+            define("PAGE_TITLE_SEPARATOR", "&middot;"); // This is ready to be configurable, if needed
+        }
+        return " " . PAGE_TITLE_SEPARATOR . " ";
+    }
 
 }
