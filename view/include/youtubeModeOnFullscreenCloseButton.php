ea6a13a211ddc82e55a4cbd5c15c5c012ef0806e
DanielnetoDotCom <danielneto.com@gmail.com>
2021-01-07 09:58:04 -0300

    Fix fullscreen

--- view/include/youtubeModeOnFullscreenCloseButton.php
+++ view/include/youtubeModeOnFullscreenCloseButton.php
@@ -11,8 +11,16 @@ if (empty($backURL)) {
 }
 
 ?>
-<div id="divTopBar" style="position: fixed; top: 0; left: 0; height: 50px; width: 100vw; z-index: 99999; padding:10px; ">
+<div id="divTopBar" style="position: fixed; top: 0; left: 0; height: 50px; width: 100vw; z-index: 99999; padding:10px; display: none;">
     <a href="<?php echo $backURL; ?>" id="closeBtnFull" class="pull-right" >
         <i class="fas fa-times"></i>
     </a>
 </div>
+<script>
+
+            $(document).ready(function () {
+                if(!inIframe()){
+                    $('#divTopBar').fadeIn();
+                }
+            });
+</script>
