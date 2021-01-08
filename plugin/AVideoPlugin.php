a5b120e203332bfe4f9f261c6e5954c646de5845
DanielnetoDotCom <danielneto.com@gmail.com>
2021-01-08 17:45:00 -0300

    https://github.com/WWBN/AVideo/issues/4325

--- plugin/AVideoPlugin.php
+++ plugin/AVideoPlugin.php
@@ -320,17 +320,17 @@ class AVideoPlugin {
                 if (!empty($matches[1])) {
                     _error_log("isPluginTablesInstalled: Found ({$matches[1]})");
                     if (!ObjectYPT::isTableInstalled($matches[1])) {
-                        _error_log("isPluginTablesInstalled: ({$matches[1]}) is installed");
+                        _error_log("isPluginTablesInstalled: ({$matches[1]}) is NOT installed");
                         if ($installIt) {
                             sqlDAL::executeFile($installSQLFile);
                             return self::isPluginTablesInstalled($name);
                         } else {
-                            _error_log("You need to install table {$matches[1]} for the plugin ({$name})", AVideoLog::$ERROR);
+                            _error_log("isPluginTablesInstalled: You need to install table {$matches[1]} for the plugin ({$name})", AVideoLog::$ERROR);
                             $isPluginTablesInstalled[$installSQLFile] = false;
                             return $isPluginTablesInstalled[$installSQLFile];
                         }
                     }else{
-                        _error_log("isPluginTablesInstalled: ({$matches[1]}) is NOT installed");
+                        _error_log("isPluginTablesInstalled: ({$matches[1]}) is installed");
                     }
                 }
             } else if (preg_match($pattern2, $line, $matches)) {
