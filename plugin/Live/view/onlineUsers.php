dd822a9ad1dd1b9fbe1ffb80c2cce8ff52d3387c
DanielnetoDotCom <danielneto.com@gmail.com>
2021-01-13 10:30:26 -0300

    https://github.com/WWBN/AVideo/issues/4346

--- plugin/Live/view/onlineUsers.php
+++ plugin/Live/view/onlineUsers.php
@@ -1,12 +1,20 @@
 
 <?php
-require_once $global['systemRootPath'].'plugin/AVideoPlugin.php';
+require_once $global['systemRootPath'] . 'plugin/AVideoPlugin.php';
 // the live users plugin
 $lu = AVideoPlugin::getObjectDataIfEnabled("LiveUsers");
-if(empty($lu) || $lu->doNotDisplayCounter){
+if (empty($lu) || $lu->doNotDisplayCounter) {
     return false;
 }
 
+if (empty($lu->doNotDisplayLiveCounter)) {
+    ?>
+    <span class="label label-primary"   data-toggle="tooltip" title="<?php echo __("Watching Now"); ?>" data-placement="bottom" ><i class="fa fa-eye"></i> <b class="liveUsersOnline_<?php echo $streamName; ?>">0</b></span>
+    <?php
+}
+if (empty($lu->doNotDisplayTotal)) {
+    ?>
+    <span class="label label-default"   data-toggle="tooltip" title="<?php echo __("Total Views"); ?>" data-placement="bottom" ><i class="fa fa-user"></i> <b class="liveUsersViews_<?php echo $streamName; ?>">0</b></span>
+    <?php
+}
 ?>
-<span class="label label-primary"   data-toggle="tooltip" title="<?php echo __("Watching Now"); ?>" data-placement="bottom" ><i class="fa fa-eye"></i> <b class="liveUsersOnline_<?php echo $streamName; ?>">0</b></span>
-<span class="label label-default"   data-toggle="tooltip" title="<?php echo __("Total Views"); ?>" data-placement="bottom" ><i class="fa fa-user"></i> <b class="liveUsersViews_<?php echo $streamName; ?>">0</b></span>
