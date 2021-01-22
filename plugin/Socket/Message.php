b7253a607b69355f265e674b1c0544096217a9bc
DanielnetoDotCom <danielneto.com@gmail.com>
2021-01-21 22:17:12 -0300

    Debug

--- plugin/Socket/Message.php
+++ plugin/Socket/Message.php
@@ -84,6 +84,7 @@ class Message implements MessageComponentInterface {
         $videos_id = $client['videos_id'];
         $live_key = $client['live_key'];
         if ($this->shouldPropagateInfo($client)) {
+            sleep(1);
             $this->msgToAll($conn, array(), \SocketMessageType::NEW_DISCONNECTION);
             //\AVideoPlugin::onUserSocketDisconnect($users_id, $this->clients[$conn->resourceId]);
             if (!empty($videos_id)) {
