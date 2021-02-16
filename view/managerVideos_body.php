a0306b14ba8222dcb64c93963d8cd650cef8189b
DanielnetoDotCom <danielneto.com@gmail.com>
2021-02-16 09:44:02 -0300

    https://github.com/WWBN/AVideo/issues/4542

--- view/managerVideos_body.php
+++ view/managerVideos_body.php
@@ -1747,14 +1747,15 @@ if (CustomizeUser::canDownloadVideos()) {
                                                                 }
                                                                 var url = row.videosURL[k].url;
                                                                 var downloadURL = addGetParam(url, 'download', 1);
-                                                                downloadURL = addGetParam(downloadURL, 'title', row.clean_title + '_' + k + '.mp4');
                                                                 var pattern = /^m3u8/i;
                                                                 if (pattern.test(k) === true) {
+                                                                    downloadURL = addGetParam(downloadURL, 'title', row.clean_title + '_' + k + '.mp4');
                                                                     download += '<div class="btn-group  btn-group-justified">';
                                                                     download += '<a class="btn btn-default btn-xs" onclick="copyToClipboard(\'' + url + '\');" ><span class="fa fa-copy " aria-hidden="true"></span> ' + k + '</a>';
                                                                     download += '<a href="' + downloadURL + '" class="btn btn-default btn-xs" target="_blank" ><span class="fa fa-download " aria-hidden="true"></span> MP4</a>';
                                                                     download += '</div>';
                                                                 } else {
+                                                                    downloadURL = addGetParam(downloadURL, 'title', row.clean_title + '.mp4');
                                                                     download += '<a href="' + downloadURL + '" class="btn btn-default btn-xs btn-block" target="_blank"  data-placement="left" data-toggle="tooltip" title="<?php echo str_replace("'", "\\'", __("Download File")); ?>" ><span class="fa fa-download " aria-hidden="true"></span> ' + k + '</a>';
                                                                 }
 
