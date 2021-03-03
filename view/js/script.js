0b713156225af17a1b788e948af1d150b070dd2e
DanielnetoDotCom <danielneto.com@gmail.com>
2021-03-03 18:32:21 -0300

    Fix ads on playlists

--- view/js/script.js
+++ view/js/script.js
@@ -263,7 +263,7 @@ function reloadAdsIfIsReady() {
         console.log('reloadAdsIfIsReady change to ', _adTagUrl);
         setTimeout(function () {
             //player.ima.setContentWithAdTag(null, adTagUrl, true);
-            //player.ima.changeAdTag(adTagUrl);
+            player.ima.changeAdTag(adTagUrl);
         }, 500);
         setTimeout(function () {
             //player.ima.initializeAdDisplayContainer();
