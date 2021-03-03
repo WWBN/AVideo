cc69469e4b323c9d069937b7f3c7f53b062534f1
DanielnetoDotCom <danielneto.com@gmail.com>
2021-03-03 11:44:57 -0300

    make sure audio works too

--- view/infoFromURL.php
+++ view/infoFromURL.php
@@ -45,7 +45,7 @@ if (empty($video['filename'])) {
     $obj->msg = "Video Not found";
     die(json_encode($obj));
 }
-if ($video['type'] !== 'video' && video['type'] !== 'audio') {
+if ($video['type'] !== 'video' && $video['type'] !== 'audio') {
     $obj->msg = "Must be a video [{$video['type']}]";
     die(json_encode($obj));
 }
