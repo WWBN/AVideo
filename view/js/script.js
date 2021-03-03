5996156d13bb749b042d9e4ccb5a18c49531735e
DanielnetoDotCom <danielneto.com@gmail.com>
2021-03-03 19:40:12 -0300

    Fix ads on playlists

--- view/js/script.js
+++ view/js/script.js
@@ -247,15 +247,12 @@ function reloadAds() {
                 } else {
                     console.log('reloadAds 2 change to ', _adTagUrl);
                     player.ima.changeAdTag(_adTagUrl);
-                    player.ima.pauseAd();
+                    _reloadAdsTimeout = setTimeout(function () {
+                        player.ima.pauseAd();
+                    }, 1000);
                 }
                 console.log('reloadAds ready state ', player.readyState());
                 player.ima.requestAds();
-                player.on('adsready', function () {
-                    console.log('reloadAds adisready ');
-                    player.ima.resumeAd();
-                    player.pause();
-                });
             }, 1000);
         } catch (e) {
             console.log('reloadAds ERROR', e.message);
