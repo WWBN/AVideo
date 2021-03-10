0804c42bca58c28341f3d0cc1a9469de61cb9438
DanielnetoDotCom <danielneto.com@gmail.com>
2021-03-10 13:47:44 -0300

    Fix layout on network

--- view/theme.css.php
+++ view/theme.css.php
@@ -8,7 +8,11 @@ _mysql_close();
 session_write_close();
 echo "/* theme = {$theme} */".PHP_EOL;
 echo file_get_contents("{$global['systemRootPath']}view/css/custom/{$theme}.css");
+exit;
+/*
 $filename = "{$global['systemRootPath']}videos/cache/custom.css";
 if(file_exists($filename)){
     echo file_get_contents($filename);
 }
+ * 
+ */
