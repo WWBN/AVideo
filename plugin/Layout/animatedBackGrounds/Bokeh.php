bf68529229d2acaaa1000ea038ffa91fe8a81764
DanielnetoDotCom <danielneto.com@gmail.com>
2021-03-11 13:08:17 -0300

    Fix layout

--- plugin/Layout/animatedBackGrounds/Bokeh.php
+++ plugin/Layout/animatedBackGrounds/Bokeh.php
@@ -6,6 +6,9 @@
         position: fixed;
         width: 100vw;
         height: 100vh;
+        top: 0;
+        left: 0;
+        z-index: -1;
     }
     .BokehBackground span {
         width: 20vmin;
