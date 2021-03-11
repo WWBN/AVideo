403e2b44c52d6d951214a5eee1ec19914efa7732
DanielnetoDotCom <danielneto.com@gmail.com>
2021-03-11 08:15:53 -0300

    https://github.com/WWBN/AVideo-Encoder/issues/355

--- install/index.php
+++ install/index.php
@@ -10,12 +10,11 @@ require_once '../locale/function.php';
 <html lang="en">
     <head>
         <title>Install AVideo</title>
+        <script src="../view/js/jquery-3.5.1.min.js" type="text/javascript"></script>
         <link rel="icon" href="../view/img/favicon.png">
         <link href="../view/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
 
         <link href="../view/bootstrap/bootstrapSelectPicker/css/bootstrap-select.min.css" rel="stylesheet" type="text/css"/>
-        <link href="../view/js/seetalert/sweetalert.css" rel="stylesheet" type="text/css"/>
-        <script src="../view/js/jquery-3.5.1.min.js" type="text/javascript"></script>
         <link href="../view/css/fontawesome-free-5.5.0-web/css/all.min.css" rel="stylesheet" type="text/css"/>
         <link href="../view/css/flagstrap/css/flags.css" rel="stylesheet" type="text/css"/>
         <style>
