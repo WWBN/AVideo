e1ded17638de591f65ed60ece99f07b9dd896e44
DanielnetoDotCom <danielneto.com@gmail.com>
2021-03-12 18:43:47 -0300

    Fix cache

--- plugin/Live/on_publish_done.php
+++ plugin/Live/on_publish_done.php
@@ -51,3 +51,4 @@ Live::deleteStatsCache(null);
 $array = setLiveKey($row['key'], $row['live_servers_id']);
 $array['stats'] = LiveTransmitionHistory::getStatsAndRemoveApplication($row['id']);
 $socketObj = sendSocketMessageToAll($array, "socketLiveOFFCallback");
+Live::deleteStatsCache(null);
