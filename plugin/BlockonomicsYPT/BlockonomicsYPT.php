d53efa1d6a934c9b8c6f6dc0c8214d8841a67dac
DanielnetoDotCom <danielneto.com@gmail.com>
2021-03-14 13:02:59 -0300

    Debug

--- plugin/BlockonomicsYPT/BlockonomicsYPT.php
+++ plugin/BlockonomicsYPT/BlockonomicsYPT.php
@@ -5,17 +5,15 @@ require_once $global['systemRootPath'] . 'plugin/Plugin.abstract.php';
 require_once $global['systemRootPath'] . 'plugin/BlockonomicsYPT/Objects/BlockonomicsOrder.php';
 
 class BlockonomicsYPT extends PluginAbstract {
+
     static $BASE_URL = "https://www.blockonomics.co/";
 
-    static function getNEW_ADDRESS_URL(){
-        $obj = AVideoPlugin::getObjectData("BlockonomicsYPT");
-        return self::$BASE_URL.'api/new_address?match_callback='.$obj->Secret;
-    }
-    static function getPRICE_URL(){
+    static function getPRICE_URL() {
         $obj = AVideoPlugin::getObjectData("YPTWallet");
         $currency = $obj->currency;
-        return self::$BASE_URL.'api/price?currency='.strtoupper($currency);
+        return self::$BASE_URL . 'api/price?currency=' . strtoupper($currency);
     }
+
     public function getDescription() {
         global $global;
         $obj = $this->getDataObject();
@@ -40,14 +38,13 @@ class BlockonomicsYPT extends PluginAbstract {
         global $global;
         $obj = new stdClass();
         $obj->APIKey = "NUdcVWq0Juz29qnJH9hebgdvEY0qiSIpWZo5nCWXask";
-        $obj->Secret = md5($global['systemRootPath'].$global['salt']);
+        $obj->Secret = md5($global['systemRootPath'] . $global['salt']);
         $obj->ExpireInSeconds = 600;
         return $obj;
     }
 
-    
     public function setUpPayment($total_cost = '1.00') {
-        global $global; 
+        global $global;
         $total_cost = floatval($total_cost);
         $obj = $this->getDataObject();
         $objWallet = AVideoPlugin::getObjectData("YPTWallet");
@@ -57,27 +54,19 @@ class BlockonomicsYPT extends PluginAbstract {
             echo $json_response = json_encode(array("error" => "Total Is Empty"));
             return;
         }
-        
-        if(!User::isLogged()){
+
+        if (!User::isLogged()) {
             echo $json_response = json_encode(array("error" => "Must be logged in"));
             return;
         }
+
+        //Generate new address for this invoice        
+        $new_address = $this->getNewAddress($obj->APIKey);
         
-        $data = '';
-        $options = array(
-            'http' => array(
-                'header' => 'Authorization: Bearer ' . $obj->APIKey,
-                'method' => 'POST',
-                'content' => $data
-            )
-        );
-        //Generate new address for this invoice
-        $context = stream_context_create($options);
-        $contents = url_get_contents(self::getNEW_ADDRESS_URL(), $context, 0, true);
-        $new_address = json_decode($contents);
-        if(empty($new_address) || !is_object($new_address)){
-            _error_log('Blockonomics ERROR: '.$contents.' '. json_last_error_msg(), AVideoLog::$ERROR);
+        if (empty($new_address)) {
+            _error_log('Blockonomics ERROR 1: ' . $contents . ' ' . json_last_error_msg(), AVideoLog::$ERROR);
         }
+        
         //Getting price
         $options = array('http' => array('method' => 'GET'));
         $context = stream_context_create($options);
@@ -85,7 +74,7 @@ class BlockonomicsYPT extends PluginAbstract {
         $price = json_decode($contents);
         //Total Cart value in bits
         $bits = intval(1.0e8 * $total_cost / $price->price);
-        
+
         // save on database
         $b = new BlockonomicsOrder(0);
         $b->setAddr($new_address->address);
@@ -96,8 +85,40 @@ class BlockonomicsYPT extends PluginAbstract {
         $b->setUsers_id(User::getId());
         $b->setTotal_value($total_cost);
         $b->setCurrency($currency);
-        
+
         return $b->save();
+    }
+
+    public function getNewAddress($api_key) {
+        $url = 'https://www.blockonomics.co/api/new_address';
+
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+
+        $header = "Authorization: Bearer " . $api_key;
+        $headers = array();
+        $headers[] = $header;
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+        $contents = curl_exec($ch);
+        if (curl_errno($ch)) {
+            _error_log("Blockonomics Error 2:" . curl_error($ch));
+            return false;
+        }
+
+        $responseObj = json_decode($contents);
+        $status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        curl_close($ch);
+
+        if ($status == 200) {
+            return $responseObj->address;
+        } else {
+            _error_log("Blockonomics Error 3: [{$status}] {$responseObj->message}");
+        }
+        return false;
     }
 
 }
