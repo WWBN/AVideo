36086ecf5e153360d651e46da717c8ec2fd0bb21
DanielnetoDotCom <danielneto.com@gmail.com>
2021-03-22 14:33:01 -0300

    Fix fullscreen on mobile

--- plugin/YouPHPFlix2/view/js/fullscreen.js
+++ plugin/YouPHPFlix2/view/js/fullscreen.js
@@ -5,7 +5,13 @@ $(document).ready(function () {
             closeFlixFullScreen();
         }
     });
-
+    $("body").addClass("Chat2StaticRight");
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+    window.addEventListener("resize", () => {
+        let vh = window.innerHeight * 0.01;
+        document.documentElement.style.setProperty("--vh", `${vh}px`);
+    });
 });
 
 function transformLinksToEmbed(selector){
