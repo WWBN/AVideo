9ae2f70f6d14ee1ec3f8db6148a66388298e9b4e
DanielnetoDotCom <danielneto.com@gmail.com>
2021-03-24 10:23:33 -0300

    try to fix infinite scroll

--- objects/functiongetPagination.php
+++ objects/functiongetPagination.php
@@ -9,9 +9,9 @@
     $(function () {
         loadInfiniteScrool$uid();
     });
-    
-    function loadInfiniteScrool$uid(retrieve){
-        if(typeof $('$infinityScrollAppendIntoSelector').infiniteScroll !== 'funciton'){
+
+    function loadInfiniteScrool$uid(retrieve) {
+        if (typeof $('$infinityScrollAppendIntoSelector').infiniteScroll !== 'funciton') {
             $container$uid = $('$infinityScrollAppendIntoSelector').infiniteScroll({
                 path: '.pagination__next$uid',
                 append: '$infinityScrollGetFromSelector',
@@ -24,9 +24,38 @@
                 lazyImage();
                 avideoSocket();
             });
+            $container$uid.on('scrollThreshold.infiniteScroll', function (event) {
+                console.log('Scroll at bottom');
+            });
+            $container$uid.on('request.infiniteScroll', function (event, path, fetchPromise) {
+                console.log(`Loading page: ${path}`);
+            });
+
+            let infScroll = $container$uid.data('infiniteScroll');
+            $container$uid.on('load.infiniteScroll', function (event, body, path, response) {
+                console.log(`Loaded: ${path}`,
+                        `Status: ${response.status}`,
+                        `Current page: ${infScroll.pageIndex}`,
+                        `${infScroll.loadCount} pages loaded`
+                        );
+
+                $container$uid.on('append.infiniteScroll', function (event, body, path, items, response) {
+                    console.log(`Appended ${items.length} items on ${path}`);
+                });
+                $container$uid.on('error.infiniteScroll', function (event, error, path, response) {
+                    console.error(`Could not load: ${path}. ${error}`);
+                })
+            });
+
+            $container$uid.on('last.infiniteScroll', function (event, body, path) {
+                console.log(`Last page hit on ${path}`);
+            });
+            $container$uid.on('history.infiniteScroll', function (event, title, path) {
+                console.log(`History changed to: ${path}`);
+            });
         }
     }
-    
+
 </script>
 <center>
     <button class="btn btn-xs btn-default" style="border: none; background: transparent;" onclick="loadInfiniteScrool$uid();"> More </button>
