c963e936231d01ecb50be7c745f2a5db6e084d79
DanielnetoDotCom <danielneto.com@gmail.com>
2021-03-26 08:39:34 -0300

    Fix typo and include

--- plugin/Cache/crontab.php
+++ plugin/Cache/crontab.php
@@ -15,3 +15,12 @@ echo "deleting {$cacheDir}".PHP_EOL;
 // delete caches 3 days old
 $cmd = "find {$cacheDir}* -mtime +3 -type f -name \"*.cache\" -exec rm {} \;";
 exec($cmd);
+
+$cacheDir = getCacheDir();
+if(empty($cacheDir) || !preg_match('/cache/', $cacheDir)){
+    die('Wrong dir: '.$cacheDir);
+}
+echo "deleting {$cacheDir}".PHP_EOL;
+// delete caches 3 days old
+$cmd = "find {$cacheDir}* -mtime +3 -type f -name \"*.cache\" -exec rm {} \;";
+exec($cmd);
