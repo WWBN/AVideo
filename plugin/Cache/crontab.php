52335cb311ed0efaabcc1def11b2825acc3f530a
DanielnetoDotCom <danielneto.com@gmail.com>
2021-03-26 08:36:30 -0300

    Fix typo and include

--- plugin/Cache/crontab.php
+++ plugin/Cache/crontab.php
@@ -11,6 +11,7 @@ $cacheDir = ObjectYPT::getCacheDir();
 if(empty($cacheDir) || !preg_match('/YPTObjectCache/', $cacheDir)){
     die('Wrong dir: '.$cacheDir);
 }
-
+echo "deleting {$cacheDir}".PHP_EOL;
 // delete caches 3 days old
 $cmd = "find {$cacheDir}* -mtime +3 -type f -name \"*.cache\" -exec rm {} \;";
+exec($cmd);
