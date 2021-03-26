334756a5d16d3449576b17fbc313137693be5764
DanielnetoDotCom <danielneto.com@gmail.com>
2021-03-26 08:46:57 -0300

    Fix typo and include

--- plugin/Cache/crontab.php
+++ plugin/Cache/crontab.php
@@ -6,23 +6,26 @@ if (!isCommandLineInterface()) {
     die('Command Line only');
 }
 
-$cacheDir = ObjectYPT::getCacheDir();
+$daysLimit = 3;
 
+// delete object cache
+$cacheDir = ObjectYPT::getCacheDir();
 if(empty($cacheDir) || !preg_match('/YPTObjectCache/', $cacheDir)){
     die('Wrong dir: '.$cacheDir);
 }
 echo "deleting {$cacheDir}".PHP_EOL;
 // delete caches 3 days old
-$cmd = "find {$cacheDir}* -mtime +3 -type f -name \"*.cache\" -exec rm {} \;".PHP_EOL;
+$cmd = "find {$cacheDir}* -mtime +{$daysLimit} -type f -name \"*.cache\" -exec rm {} \;".PHP_EOL;
 echo "Command: {$cmd}";
 exec($cmd);
 
+//delete site cache
 $cacheDir = getCacheDir();
 if(empty($cacheDir) || !preg_match('/cache/', $cacheDir)){
     die('Wrong dir: '.$cacheDir);
 }
 echo "deleting {$cacheDir}".PHP_EOL;
 // delete caches 3 days old
-$cmd = "find {$cacheDir}* -mtime +3 -type f -name \"*.cache\" -exec rm {} \;".PHP_EOL;
+$cmd = "find {$cacheDir}* -mtime +{$daysLimit} -type f -name \"*.cache\" -exec rm {} \;".PHP_EOL;
 echo "Command: {$cmd}";
 exec($cmd);
