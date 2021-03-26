1b80cd28496ffebf538783bfcca9ac5b7103b5f1
DanielnetoDotCom <danielneto.com@gmail.com>
2021-03-26 08:33:26 -0300

    Fix typo and include

--- plugin/Cache/crontab.php
+++ plugin/Cache/crontab.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once '../../videos/configuration.php';
+require_once dirname(__FILE__) . '/../../videos/configuration.php';
 
 if (!isCommandLineInterface()) {
     die('Command Line only');
