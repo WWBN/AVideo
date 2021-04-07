ac6433c2b8135957fac128e7d406afef7b8fff64
DanielnetoDotCom <danielneto.com@gmail.com>
2021-04-07 09:55:33 -0300

    Fix js error

--- plugin/YouPHPFlix2/view/js/fullscreen.js
+++ plugin/YouPHPFlix2/view/js/fullscreen.js
@@ -1,7 +1,7 @@
-let vh = window.innerHeight * 0.01;
+var vh = window.innerHeight * 0.01;
 document.documentElement.style.setProperty("--vh", `${vh}px`);
 window.addEventListener("resize", () => {
-    let vh = window.innerHeight * 0.01;
+    vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty("--vh", `${vh}px`);
 });
 $(document).ready(function () {
