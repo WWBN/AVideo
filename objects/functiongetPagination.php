38e18d5a4a0337581506f6949f37fcd081999c86
DanielnetoDotCom <danielneto.com@gmail.com>
2021-04-09 14:18:45 -0300

    Fix infinity scrool

--- objects/functiongetPagination.php
+++ objects/functiongetPagination.php
@@ -12,7 +12,6 @@
     $(function () {
         loadInfiniteScrool$uid();
     });
-    /*
     document.addEventListener('scroll', function (e) {
         var scrollPosition = window.pageYOffset;
         var windowSize = window.innerHeight;
@@ -23,8 +22,6 @@
             $container$uid.infiniteScroll('loadNextPage');
         }
     });
-     * 
-     */
     var append_infiniteScroll_timout;
     function loadInfiniteScrool$uid() {
         if (typeof $('$infinityScrollAppendIntoSelector').infiniteScroll !== 'funciton') {
