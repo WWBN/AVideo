9b014db0f4129cbf1bc8eb6d451d13e8efa1d6e6
DanielnetoDotCom <danielneto.com@gmail.com>
2021-04-09 15:31:56 -0300

    Fix infinity scrool

--- plugin/Gallery/view/modeGalleryCategory.php
+++ plugin/Gallery/view/modeGalleryCategory.php
@@ -9,57 +9,64 @@ require_once $global['systemRootPath'] . 'objects/category.php';
 $obj = AVideoPlugin::getObjectData("Gallery");
 $liveobj = AVideoPlugin::getObjectData("Live");
 $_REQUEST['rowCount'] = 2;
-if(empty($_GET['current'])){
-    $_REQUEST['current'] = 1;
-}else{
-    $_REQUEST['current'] = intval($_GET['current']);
-}
+$_REQUEST['current'] = getCurrentPage();
 $categories = Category::getAllCategories(false, true);
+$total = Category::getTotalCategories(false, true);
+$totalPages = ceil($total / getRowCount());
+$page = getCurrentPage();
+if ($totalPages < $page) {
+    $page = $totalPages;
+}
+$link = "{$global['webSiteRootURL']}plugin/Gallery/view/modeGalleryCategory.php?tags_id=" . intval(@$_GET['tagsid']) . "&search=" . htmlentities(urlencode(getSearchVar())) . "&current={page}";
 
-if(empty($categories)){
+if (empty($categories)) {
     return false;
 }
 $_REQUEST['current'] = 1;
 $_REQUEST['rowCount'] = $obj->CategoriesRowCount;
 ?>
 <div class="categoriesContainerItem">
-<?php
-foreach ($categories as $_cat) {
-    $_GET['catName'] = $_cat['clean_name'];
-    if (empty($liveobj->doNotShowLiveOnCategoryList)) {
-        $currentCat = $_cat;
-        include $global['systemRootPath'] . 'plugin/Gallery/view/modeGalleryCategoryLive.php';
-    }
-    unset($_POST['sort']);
-    $_POST['sort']['v.created'] = "DESC";
-    $_POST['sort']['likes'] = "DESC";
-    $videos = Video::getAllVideos("viewableNotUnlisted", false, !$obj->hidePrivateVideos);
-    if (empty($videos)) {
-        continue;
-    }
-    if(empty($_cat['clean_name'])){
-        continue;
+    <?php
+    foreach ($categories as $_cat) {
+        $_GET['catName'] = $_cat['clean_name'];
+        if (empty($liveobj->doNotShowLiveOnCategoryList)) {
+            $currentCat = $_cat;
+            include $global['systemRootPath'] . 'plugin/Gallery/view/modeGalleryCategoryLive.php';
+        }
+        unset($_POST['sort']);
+        $_POST['sort']['v.created'] = "DESC";
+        $_POST['sort']['likes'] = "DESC";
+        $videos = Video::getAllVideos("viewableNotUnlisted", false, !$obj->hidePrivateVideos);
+        if (empty($videos)) {
+            continue;
+        }
+        if (empty($_cat['clean_name'])) {
+            continue;
+        }
+        ?>
+        <div class="row clear clearfix">
+            <?php
+            if (canPrintCategoryTitle($_cat['name'])) {
+                ?>
+                <h3 class="galleryTitle">
+                    <a class="btn-default" href="<?php echo $global['webSiteRootURL']; ?>cat/<?php echo $_cat['clean_name']; ?>">
+                        <i class="<?php echo $_cat['iconClass']; ?>"></i> <?php echo $_cat['name']; ?>
+                    </a>
+                </h3>
+                <?php
+            }
+            createGallerySection($videos, "", array(), true);
+            ?>
+        </div>
+
+        <?php
     }
     ?>
-    <div class="row clear clearfix">
-        <?php 
-        if(canPrintCategoryTitle($_cat['name'])){
-        ?>
-        <h3 class="galleryTitle">
-            <a class="btn-default" href="<?php echo $global['webSiteRootURL']; ?>cat/<?php echo $_cat['clean_name']; ?>">
-                <i class="<?php echo $_cat['iconClass']; ?>"></i> <?php echo $_cat['name']; ?>
-            </a>
-        </h3>
-        <?php
-        }
-        createGallerySection($videos, "", array(), true);
-        ?>
-    </div>
-
-    <?php
-}
-?>
 </div>
-<p class="pagination">
-    <a class="pagination__next" href="<?php echo $global['webSiteRootURL']; ?>plugin/Gallery/view/modeGalleryCategory.php?tags_id=<?php echo intval(@$_GET['tagsid']); ?>&search=<?php echo htmlentities(urlencode(getSearchVar())); ?>&current=<?php echo count($categories)?$_REQUEST['current'] + 1:$_REQUEST['current']; ?>"></a>
-</p>
+<!-- modeGalleryCategory -->
+<div class="col-sm-12" style="z-index: 1;">
+    <?php
+    //getPagination($total, $page = 0, $link = "", $maxVisible = 10, $infinityScrollGetFromSelector="", $infinityScrollAppendIntoSelector="")
+    echo getPagination($totalPages, $page, $link, 10, ".categoriesContainerItem", ".categoriesContainerItem");
+    ?>
+</div>
