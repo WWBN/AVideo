a6953559d2e20df490c2ac0ae969eab6d2a85e37
DanielnetoDotCom <danielneto.com@gmail.com>
2021-04-12 17:49:19 -0300

    https://github.com/WWBN/AVideo/issues/4913
    -- Fix and Add Missing column
    -- this update may release a column already exists error in some installations

--- install/checkConfiguration.php
+++ install/checkConfiguration.php
@@ -4,7 +4,7 @@ if (file_exists("../videos/configuration.php")) {
     exit;
 }
 
-$installationVersion = "10.5";
+$installationVersion = "10.7";
 
 error_log("Installation: ".__LINE__." ". json_encode($_POST));
 header('Content-Type: application/json');
---
+++ updatedb/updateDb.v10.7.sql
@@ -0,0 +1,7 @@
+-- Add Missin column
+-- this update may release a column already exists error in some installations
+
+ALTER TABLE `users_extra_info` 
+ADD COLUMN `order` INT NOT NULL DEFAULT 0;
+
+UPDATE configurations SET  version = '10.7', modified = now() WHERE id = 1;
