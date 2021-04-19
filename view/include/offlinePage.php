6f67dc8feff050420e86d8e290ef233e5afebe55
Julian Romero <jromero@swim.com.br>
2021-04-19 12:19:00 -0600

    Update offlinePage.php
    added translations phrases code

--- view/include/offlinePage.php
+++ view/include/offlinePage.php
@@ -6,7 +6,7 @@ error_log("ERROR: your site is offline we could not connect into MySQL");
 error_log("ERROR: used credentials: mysqlHost = $mysqlHost; mysqlUser = $mysqlUser; mysqlPass = $mysqlPass; mysqlDatabase = $mysqlDatabase;");
 ?>
 <!doctype html>
-<title>Site Maintenance</title>
+<title><? echo __("Site Maintenance"); ?></title>
 <style>
   body { text-align: center; padding: 150px; }
   h1 { font-size: 50px; }
@@ -20,9 +20,9 @@ error_log("ERROR: used credentials: mysqlHost = $mysqlHost; mysqlUser = $mysqlUs
     <center>
         <img src="videos/userPhoto/logo.png"/>
     </center>
-    <h1>We&rsquo;ll be back soon!</h1>
+    <h1><? echo __("We&rsquo;ll be back soon!"); ?></h1>
     <div>
-        <p>Sorry for the inconvenience but we&rsquo;re performing some maintenance at the moment.</p>
-        <p>&mdash; The Team</p>
+        <p><? echo __("Sorry for the inconvenience but we&rsquo;re performing some maintenance at the moment."); ?></p>
+        <p><? echo __("&mdash; The Team"); ?></p>
     </div>
 </article>
