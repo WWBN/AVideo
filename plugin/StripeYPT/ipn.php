d92f227c7fd2e2210cb5b8daddf25d1f110a1a75
DanielnetoDotCom <danielneto.com@gmail.com>
2021-04-21 14:41:17 -0300

    Fix Uncaught Error: Cannot use object of type stdClass as array

--- plugin/StripeYPT/ipn.php
+++ plugin/StripeYPT/ipn.php
@@ -25,9 +25,9 @@ $payload = @file_get_contents('php://input');
 $sig_header = $_SERVER['HTTP_STRIPE_SIGNATURE'];
 $event = null;
 
-$payloadObj = _json_decode($payload);
+$payloadObj = json_decode($payload);
 
-$payloadObj = object_to_array($payloadObj);
+//$payloadObj = object_to_array($payloadObj);
 
 //_error_log("StripeIPN: WEBHOOK: ".json_encode($webhook));
 //_error_log("StripeIPN: payload ".json_encode($payloadObj));
