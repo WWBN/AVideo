5c7d34e87cb0d825c732a3296ad611ed1ef7b51d
DanielnetoDotCom <danielneto.com@gmail.com>
2021-04-26 09:04:50 -0300

    https://github.com/WWBN/AVideo/issues/5003

--- view/robots.txt.php
+++ view/robots.txt.php
@@ -9,46 +9,4 @@ header("Content-type: text/plain");
 ?>
 Sitemap: <?php echo $global['webSiteRootURL']; ?>sitemap.xml
 User-Agent: *
-Allow: /plugin/Live/?*
-Allow: /plugin/PlayLists/*.css
-Allow: /plugin/PlayLists/*.js
-Allow: /plugin/TopMenu/*.css
-Allow: /plugin/TopMenu/*.js
-Allow: /plugin/SubtitleSwitcher/*.css
-Allow: /plugin/SubtitleSwitcher/*.js
-Allow: /plugin/Gallery/*.css
-Allow: /plugin/Gallery/*.js
-Allow: /plugin/YouPHPFlix2/*.png
-Allow: /plugin/Live/*.css
-Allow: /plugin/Live/*.js
-Allow: /plugin/*.css
-Allow: /plugin/*.js
-Allow: .js
-Allow: .css
-Disallow: /user
-Disallow: /plugin
-Disallow: /mvideos
-Disallow: /usersGroups
-Disallow: /charts
-Disallow: /upload
-Disallow: /comments
-Disallow: /subscribes
-Disallow: /update
-Disallow: /locale
-Disallow: /objects/*
-Allow: /plugin/Live/?*
-Allow: /plugin/PlayLists/*.css
-Allow: /plugin/PlayLists/*.js
-Allow: /plugin/TopMenu/*.css
-Allow: /plugin/TopMenu/*.js
-Allow: /plugin/SubtitleSwitcher/*.css
-Allow: /plugin/SubtitleSwitcher/*.js
-Allow: /plugin/Gallery/*.css
-Allow: /plugin/Gallery/*.js
-Allow: /plugin/YouPHPFlix2/*.png
-Allow: /plugin/Live/*.css
-Allow: /plugin/Live/*.js
-Allow: /plugin/*.css
-Allow: /plugin/*.js
-Allow: .js
-Allow: .css
+<?php echo $advancedCustom->robotsTXT->value; ?>
