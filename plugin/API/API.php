bfca607842ec81b23b090a34e3f47f8714ed38a4
DanielnetoDotCom <danielneto.com@gmail.com>
2021-05-20 09:27:38 -0300

    Fix fatal error

--- plugin/API/API.php
+++ plugin/API/API.php
@@ -397,7 +397,7 @@ class API extends PluginAbstract {
             //$rows[$key]['wwbnProgram'] = $rows[$key]['pageUrl'];
             //$rows[$key]['wwbnProgramURL'] = $rows[$key]['pageUrl'];
             $rows[$key]['wwbnType'] = $rows[$key]['type'];
-            $rows[$key]['relatedVideos'] = Video::getRelatedMovies($value['id']);
+            $rows[$key]['relatedVideos'] = Video::getRelatedMovies($rows[$key]['id']);
         }
         $obj->totalRows = $totalRows;
         $obj->rows = $rows;
