76d076e5f23917038fa0860e948d48d0d332b02a
DanielnetoDotCom <danielneto.com@gmail.com>
2021-05-21 10:27:19 -0300

    https://github.com/WWBN/AVideo/issues/5074

--- plugin/LiveLinks/view/menuRight.php
+++ plugin/LiveLinks/view/menuRight.php
@@ -1,5 +1,5 @@
 <li>
     <a href="<?php echo $global['webSiteRootURL']; ?>plugin/LiveLinks"  class="btn btn-danger navbar-btn" data-toggle="tooltip" title="<?php echo __("Add an External a Live Streaming"); ?>" data-placement="bottom" >
-        <i class="fas fa-link"></i>  <span class="hidden-md hidden-sm hidden-mdx"><?php echo $obj->buttonTitle; ?></span>
+        <i class="fas fa-link"></i>  <span class="hidden-md hidden-sm hidden-mdx"><?php echo __($obj->buttonTitle); ?></span>
     </a>
 </li>
