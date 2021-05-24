9e2ea1004d37d08f097bdc4e7172e1a6575e1472
DanielnetoDotCom <danielneto.com@gmail.com>
2021-05-24 13:28:38 -0300

    Fiz timezone

--- plugin/Meet/Meet.php
+++ plugin/Meet/Meet.php
@@ -380,7 +380,7 @@ Passcode: {password}
          * Specific User Groups = 0
          * @return type
          */
-        if (empty($meet->getStarts()) || strtotime($meet->getStarts()) <= strtotime('Today 0 hour')) {
+        if (empty($meet->getStarts()) || strtotime($meet->getStarts()) <= strtotime('Yesterday')) {
             // means public
             if ($meet->getPublic() == "2") {
                 $obj->canJoin = true;
