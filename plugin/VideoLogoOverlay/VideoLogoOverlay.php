d2a1693c253fd9f61ee4f34f07ae32723ff12d30
DanielnetoDotCom <danielneto.com@gmail.com>
2021-05-24 09:10:47 -0300

    https://github.com/WWBN/AVideo/issues/5180#issuecomment-846614462 add a max width option

--- plugin/VideoLogoOverlay/VideoLogoOverlay.php
+++ plugin/VideoLogoOverlay/VideoLogoOverlay.php
@@ -32,6 +32,7 @@ class VideoLogoOverlay extends PluginAbstract {
         $obj->position = $o;
         $obj->opacity = 50;
         $obj->useUserChannelImageAsLogo = true;
+        $obj->logoMaxWidthPX = 150;
         return $obj;
     }
     
@@ -105,7 +106,7 @@ class VideoLogoOverlay extends PluginAbstract {
             $logoOverlay = User::getPhoto($video['users_id']);
             $cols = "col-lg-12 col-md-8 col-sm-7 col-xs-6";
         }
-        $html = '<div style="' . $style . '" class="VideoLogoOverlay"><a href="' . $url . '" target="_blank"> <img src="' . $logoOverlay . '" alt="Logo"  class="img img-responsive '.$cols.'" style="max-width:150px;"></a></div>';
+        $html = '<div style="' . $style . '" class="VideoLogoOverlay"><a href="' . $url . '" target="_blank"> <img src="' . $logoOverlay . '" alt="Logo"  class="img img-responsive '.$cols.'" style="max-width:'.$obj->logoMaxWidthPX.'px;"></a></div>';
         $js = "$('{$html}').appendTo('#mainVideo');";
         PlayerSkins::addOnPlayerReady($js);
     }
