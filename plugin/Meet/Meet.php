ece41064354ba206e11365f8bf8c5db3643473e1
DanielnetoDotCom <danielneto.com@gmail.com>
2021-05-25 10:05:05 -0300

    Convert date to timezone

--- plugin/Meet/Meet.php
+++ plugin/Meet/Meet.php
@@ -380,7 +380,7 @@ Passcode: {password}
          * Specific User Groups = 0
          * @return type
          */
-        if (empty($meet->getStarts()) || strtotime($meet->getStarts()) <= strtotime('+1 minutes')) {
+        if (empty($meet->getStarts()) || strtotime($meet->getStarts()) <= strtotime('-1 minutes')) {
             // means public
             if ($meet->getPublic() == "2") {
                 $obj->canJoin = true;
