cb57afe507974a9d2597426ab788809b5116adf0
DanielnetoDotCom <danielneto.com@gmail.com>
2021-05-31 18:39:59 -0300

    Debug

--- plugin/YPTSocket/test.php
+++ plugin/YPTSocket/test.php
@@ -75,11 +75,13 @@ function _test_send($SocketURL, $msg) {
         global $responses;
         preg_match('/(tcp|tls):\/\/([^:]+):([0-9]+)/i', $e->getMessage(), $matches);
         if (empty($matches)) {
-            _log("ERROR on get connect response [" . $e->getMessage() . "]");
+            //_log("ERROR on get connect response [" . $e->getMessage() . "]");
+            $responses[] = $e->getMessage();
+        }else{
+            $c = new AVideoSocketConfiguration($matches[1], $matches[3], $matches[2], false, $e->getMessage());
+            $responses[] = $c;
+            $c->log();
         }
-        $c = new AVideoSocketConfiguration($matches[1], $matches[3], $matches[2], false, $e->getMessage());
-        $responses[] = $c;
-        $c->log();
         printIfComplete();
     });
 
