0423bd2381046a083b1e4dda536de2f70f181618
DanielnetoDotCom <danielneto.com@gmail.com>
2021-06-07 10:59:02 -0300

    Detect embeded youtube video

--- objects/functions.php
+++ objects/functions.php
@@ -5926,7 +5926,7 @@ function get_ffmpeg($ignoreGPU = false) {
 }
 
 function isHTMLPage($url) {
-    if (preg_match('/https?:\/\/(youtu.be|youtube.com)\//i', $type)) {
+    if (preg_match('/https?:\/\/(youtu.be|youtube.com)\//i', $url)) {
         return true;
     } else if ($type = getHeaderContentTypeFromURL($url)) {
         if (preg_match('/text\/html/i', $type)) {
