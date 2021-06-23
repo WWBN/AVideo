eb59cb77fa069b4b80a366a1384b984ee938a4e1
Daniel <developer@youphptube.com>
2021-06-23 10:35:03 -0300

    https://github.com/WWBN/AVideo/issues/5264

--- objects/sendSiteEmailAsync.php
+++ objects/sendSiteEmailAsync.php
@@ -1,22 +1,27 @@
 <?php
 
 //streamer config
-require_once '../videos/configuration.php';
+require_once dirname(__FILE__) . '../videos/configuration.php';
 
+_error_log('sendSiteEmailAsync: Start');
 if (!isCommandLineInterface()) {
+    _error_log('sendSiteEmailAsync: ERROR: command line only');
     return die('Command Line only');
 }
 
 $tmpFile = @$argv[1];
 if(empty($tmpFile)){
+    _error_log('sendSiteEmailAsync: ERROR: empty argument');
     die('sendSiteEmailAsync empty argument');
 }
 if(!file_exists($tmpFile)){
+    _error_log('sendSiteEmailAsync: ERROR: file do not exists '.$tmpFile);
     die('sendSiteEmailAsync file do not exists '.$tmpFile);
 }
 $json = _json_decode(file_get_contents($tmpFile));
 unlink($tmpFile);
 if(empty($json)){
+    _error_log('sendSiteEmailAsync: ERROR JSON invalid');
     die('sendSiteEmailAsync JSON invalid');
 }
 $to = ($json->to);
@@ -24,3 +29,4 @@ $subject = ($json->subject);
 $message = ($json->message);
 
 sendSiteEmail($to, $subject, $message);
+_error_log('sendSiteEmailAsync: Complete');
