818ce1fa6d4684f4017114228633d9fcf19b1cbd
Daniel <danielneto.com@gmail.com>
2021-06-29 20:51:55 -0300

    https://github.com/WWBN/AVideo/issues/5325

--- view/js/sw.js
+++ view/js/sw.js
@@ -1,25 +1,30 @@
-self.addEventListener('install', (e) => {
-    /*
-  e.waitUntil(
-    caches.open('fox-store').then((cache) => cache.addAll([
-      '/pwa-examples/a2hs/',
-      '/pwa-examples/a2hs/index.html',
-      '/pwa-examples/a2hs/index.js',
-      '/pwa-examples/a2hs/style.css',
-      '/pwa-examples/a2hs/images/fox1.jpg',
-      '/pwa-examples/a2hs/images/fox2.jpg',
-      '/pwa-examples/a2hs/images/fox3.jpg',
-      '/pwa-examples/a2hs/images/fox4.jpg',
-    ])),
-  );
-     * 
-     */
-  console.log('sw.js 1',e);
+/*
+ self.addEventListener('install', (e) => {
+ e.waitUntil(
+ caches.open('fox-store').then((cache) => cache.addAll([
+ '/pwa-examples/a2hs/',
+ '/pwa-examples/a2hs/index.html',
+ '/pwa-examples/a2hs/index.js',
+ '/pwa-examples/a2hs/style.css',
+ '/pwa-examples/a2hs/images/fox1.jpg',
+ '/pwa-examples/a2hs/images/fox2.jpg',
+ '/pwa-examples/a2hs/images/fox3.jpg',
+ '/pwa-examples/a2hs/images/fox4.jpg',
+ ])),
+ );
+ console.log('sw.js 1',e);
+ });
+ * 
+ */
+
+
+self.addEventListener('install', event => {
+    event.waitUntil(Promise.all([self.skipWaiting()]));
 });
 
 self.addEventListener('fetch', (e) => {
-  console.log('sw.js 2',e.request.url);
-  e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request)),
-  );
+    console.log('sw.js 2', e.request.url);
+    e.respondWith(
+            caches.match(e.request).then((response) => response || fetch(e.request)),
+            );
 });
