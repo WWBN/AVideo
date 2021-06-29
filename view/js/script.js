e61d7c4b1d6360229ca01e2e613a08b2fadbe2c7
Daniel <danielneto.com@gmail.com>
2021-06-29 20:59:46 -0300

    https://github.com/WWBN/AVideo/issues/5325

--- view/js/script.js
+++ view/js/script.js
@@ -1694,31 +1694,3 @@ if ('serviceWorker' in navigator) {
     .register(webSiteRootURL+'view/js/sw.js')
     .then(() => { console.log('Service Worker Registered'); });
 }
-
-// Code to handle install prompt on desktop
-
-let deferredPrompt;
-window.addEventListener('beforeinstallprompt', (e) => {
-  // Prevent Chrome 67 and earlier from automatically showing the prompt
-  e.preventDefault();
-  // Stash the event so it can be triggered later.
-  deferredPrompt = e;
-  // Update UI to notify the user they can add to home screen
-  addBtn.style.display = 'block';
-
-  addBtn.addEventListener('click', () => {
-    // hide our user interface that shows our A2HS button
-    addBtn.style.display = 'none';
-    // Show the prompt
-    deferredPrompt.prompt();
-    // Wait for the user to respond to the prompt
-    deferredPrompt.userChoice.then((choiceResult) => {
-      if (choiceResult.outcome === 'accepted') {
-        console.log('User accepted the A2HS prompt');
-      } else {
-        console.log('User dismissed the A2HS prompt');
-      }
-      deferredPrompt = null;
-    });
-  });
-});
