6be40ed4b5f10221e815570b2ed858dcc41291cf
Daniel <danielneto.com@gmail.com>
2021-06-30 12:27:27 -0300

    Fix padding

--- plugin/Gallery/view/mainAreaChannels.php
+++ plugin/Gallery/view/mainAreaChannels.php
@@ -1,15 +1,15 @@
 <?php
 global $global;
 
-if(empty($global['systemRootPath'])){
-    $global['systemRootPath'] = dirname(__FILE__).'/../../../';
+if (empty($global['systemRootPath'])) {
+    $global['systemRootPath'] = dirname(__FILE__) . '/../../../';
 }
 
-require_once $global['systemRootPath'].'videos/configuration.php';
-require_once $global['systemRootPath'].'objects/subscribe.php';
-require_once $global['systemRootPath'].'plugin/Gallery/functions.php';
+require_once $global['systemRootPath'] . 'videos/configuration.php';
+require_once $global['systemRootPath'] . 'objects/subscribe.php';
+require_once $global['systemRootPath'] . 'plugin/Gallery/functions.php';
 
-if(empty($obj)){
+if (empty($obj)) {
     $obj = AVideoPlugin::getDataObject('Gallery');
 }
 
@@ -17,7 +17,7 @@ $itemsPerPage = 4;
 $total = Subscribe::getTotalSubscribedChannels(User::getId());
 $page = getCurrentPage();
 $channels = Subscribe::getSubscribedChannels(User::getId(), $itemsPerPage, $page);
-if(empty($channels)){
+if (empty($channels)) {
     return '';
 }
 $totalPages = ceil($total / $itemsPerPage);
@@ -26,18 +26,20 @@ if ($totalPages < $page) {
 }
 ?>
 <!-- mainAreaChannel start -->
-<div class="mainAreaChannels">  
-<?php
-foreach ($channels as $value) {
-    $_POST['disableAddTo'] = 0;
-    createChannelItem($value['users_id'], $value['photoURL'], $value['identification'], $obj->SubscribedChannelsRowCount);
-}
-?>
-</div>
-<div class="col-sm-12" style="z-index: 1;">
-<?php
+<div class="row">
+    <div class="mainAreaChannels">  
+        <?php
+        foreach ($channels as $value) {
+            $_POST['disableAddTo'] = 0;
+            createChannelItem($value['users_id'], $value['photoURL'], $value['identification'], $obj->SubscribedChannelsRowCount);
+        }
+        ?>
+    </div>
+    <div class="col-sm-12" style="z-index: 1;">
+        <?php
 //getPagination($total, $page = 0, $link = "", $maxVisible = 10, $infinityScrollGetFromSelector="", $infinityScrollAppendIntoSelector="")
-echo getPagination($totalPages, $page, "{$global['webSiteRootURL']}plugin/Gallery/view/mainAreaChannels.php", 10, ".mainAreaChannels", ".mainAreaChannels");
-?>
+        echo getPagination($totalPages, $page, "{$global['webSiteRootURL']}plugin/Gallery/view/mainAreaChannels.php", 10, ".mainAreaChannels", ".mainAreaChannels");
+        ?>
+    </div>
 </div>
 <!-- mainAreaChannel end -->
