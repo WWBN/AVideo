275b2d7d78b9492f134a63bdde8372875cc175f8
Daniel <danielneto.com@gmail.com>
2021-06-29 21:03:09 -0300

    https://github.com/WWBN/AVideo/issues/5325

--- view/js/script.js
+++ view/js/script.js
@@ -1690,7 +1690,31 @@ function avideoAjax(url, data) {
 
 // Register service worker to control making site work offline
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker
-    .register(webSiteRootURL+'view/js/sw.js')
-    .then(() => { console.log('Service Worker Registered'); });
+    navigator.serviceWorker
+            .register(webSiteRootURL + 'view/js/sw.js')
+            .then(() => {
+                console.log('Service Worker Registered');
+            });
 }
+
+// Code to handle install prompt on desktop
+
+let deferredPrompt;
+window.addEventListener('beforeinstallprompt', (e) => {
+    // Prevent Chrome 67 and earlier from automatically showing the prompt
+    e.preventDefault();
+    // Stash the event so it can be triggered later.
+    deferredPrompt = e;
+
+    // Show the prompt
+    deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    deferredPrompt.userChoice.then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+            console.log('User accepted the A2HS prompt');
+        } else {
+            console.log('User dismissed the A2HS prompt');
+        }
+        deferredPrompt = null;
+    });
+});
