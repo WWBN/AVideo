3e30ceedf5d961777e7fb0924f65e0058867f6f4
Daniel <danielneto.com@gmail.com>
2021-06-30 17:36:08 -0300

    Avoid error

--- sw.js
+++ sw.js
@@ -1,23 +1,3 @@
-/*
- self.addEventListener('install', (e) => {
- e.waitUntil(
- caches.open('fox-store').then((cache) => cache.addAll([
- '/pwa-examples/a2hs/',
- '/pwa-examples/a2hs/index.html',
- '/pwa-examples/a2hs/index.js',
- '/pwa-examples/a2hs/style.css',
- '/pwa-examples/a2hs/images/fox1.jpg',
- '/pwa-examples/a2hs/images/fox2.jpg',
- '/pwa-examples/a2hs/images/fox3.jpg',
- '/pwa-examples/a2hs/images/fox4.jpg',
- ])),
- );
- console.log('sw.js 1',e);
- });
- * 
- */
-
-
 self.addEventListener('install', event => {
     //console.log('sw.js 1', event);
     event.waitUntil(Promise.all([self.skipWaiting()]));
