d79491408b11b08e4bf3170b5631701294961914
Daniel <danielneto.com@gmail.com>
2021-07-06 12:46:13 -0300

    Fix ipn

--- plugin/PayPalYPT/PayPalYPT.php
+++ plugin/PayPalYPT/PayPalYPT.php
@@ -651,7 +651,7 @@ class PayPalYPT extends PluginAbstract {
         
         $ch = curl_init($ipnURL);
         
-        curl_setopt($curl, CURLOPT_USERAGENT, 'PHP-IPN-VerificationScript');
+        curl_setopt($ch, CURLOPT_USERAGENT, 'PHP-IPN-VerificationScript');
         curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
         curl_setopt($ch, CURLOPT_POST, 1);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
