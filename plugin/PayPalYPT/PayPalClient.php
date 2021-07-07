941fe6b1d38e88f4718ee0f0416a34d384f0f4e6
Daniel <danielneto.com@gmail.com>
2021-07-07 16:52:59 -0300

    Remove errors on screen

--- plugin/PayPalYPT/PayPalClient.php
+++ plugin/PayPalYPT/PayPalClient.php
@@ -1,9 +1,9 @@
 <?php
 use PaypalPayoutsSDK\Core\PayPalHttpClient;
 use PaypalPayoutsSDK\Core\SandboxEnvironment;
-ini_set('error_reporting', E_ALL); // or error_reporting(E_ALL);
-ini_set('display_errors', '1');
-ini_set('display_startup_errors', '1');
+//ini_set('error_reporting', E_ALL); // or error_reporting(E_ALL);
+//ini_set('display_errors', '1');
+//ini_set('display_startup_errors', '1');
 class PayPalClient
 {
     /**
