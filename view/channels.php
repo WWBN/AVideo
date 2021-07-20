3dafed4181a5f3728943072b910c9bc53652ccbd
Daniel <danielneto.com@gmail.com>
2021-07-20 11:52:04 -0300

    Remove AsyncJobs

--- view/channels.php
+++ view/channels.php
@@ -132,7 +132,7 @@ $metaDescription = __("Channels");
                                     $_POST['current'] = 1;
                                     $_REQUEST['rowCount'] = 6;
                                     $_POST['sort']['created'] = "DESC";
-                                    $uploadedVideos = Video::getAllVideosAsync("viewable", $value['id']);
+                                    $uploadedVideos = Video::getAllVideos("viewable", $value['id']);
                                     
                                     createGallerySection($uploadedVideos, dechex(crc32($value['channelName'])));
                                     ?>
