9eb11d940559f539875ba35118280fb035aa4a51
Daniel <danielneto.com@gmail.com>
2021-07-27 15:31:32 -0300

    https://github.com/WWBN/AVideo/issues/5488

--- plugin/Live/Live.php
+++ plugin/Live/Live.php
@@ -2022,7 +2022,7 @@ class Live extends PluginAbstract {
     public function getUploadMenuButton() {
         global $global;
         $obj = $this->getDataObject();
-        if (!empty($obj->doNotShowGoLiveButton) || !User::canStream()) {
+        if (!empty(!User::canStream()) {
             return '';
         }
         $buttonTitle = $this->getButtonTitle();
