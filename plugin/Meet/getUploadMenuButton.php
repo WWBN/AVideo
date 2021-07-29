0665b11b7b34f7891af57a5aa7f96419677175f3
Daniel <danielneto.com@gmail.com>
2021-07-29 14:37:24 -0300

    Allow you customize meet title

--- plugin/Meet/getUploadMenuButton.php
+++ plugin/Meet/getUploadMenuButton.php
@@ -1,5 +1,5 @@
 <li>
-    <a href="<?php echo $global['webSiteRootURL']; ?>plugin/Meet"  data-toggle="tooltip" title="<?php echo  __('Meet'); ?>" data-placement="left" >
-        <i class="fas fa-comments"></i>  <?php echo __('Meet'); ?>
+    <a href="<?php echo $global['webSiteRootURL']; ?>plugin/Meet"  data-toggle="tooltip" title="<?php echo  __($buttonTitle); ?>" data-placement="left" >
+        <i class="fas fa-comments"></i>  <?php echo __($buttonTitle); ?>
     </a>
 </li>
