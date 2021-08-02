ca317af2bce93d62ffaca54e77d92e8de4c74717
Daniel <danielneto.com@gmail.com>
2021-08-02 16:25:22 -0300

    Make sure unload works

--- view/js/script.js
+++ view/js/script.js
@@ -457,7 +457,7 @@ function addViewBeacon() {
         return false;
     }
     if (typeof mediaId !== 'undefined') {
-        console.log('addViewBeacon will be sent');
+        console.log('addViewBeacon will be sent', mediaId, playerCurrentTime, seconds_watching_video);
         var url = webSiteRootURL + 'objects/videoAddViewCount.json.php?PHPSESSID=' + PHPSESSID;
         url = addGetParam(url, 'id', mediaId);
         url = addGetParam(url, 'currentTime', playerCurrentTime);
