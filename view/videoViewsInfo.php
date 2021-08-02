c11e109db8415bc263e50bd9fbe65613ef84a855
Daniel <danielneto.com@gmail.com>
2021-08-02 12:57:30 -0300

    Improve layout

--- view/videoViewsInfo.php
+++ view/videoViewsInfo.php
@@ -16,10 +16,10 @@ if (!Video::canEdit($videos_id)) {
 
 $_REQUEST['rowCount'] = 20;
 $_POST['sort']['created'] = 'DESC';
-        
+
 $statistics = VideoStatistic::getAllFromVideos_id($videos_id);
 $total = VideoStatistic::getTotalFromVideos_id($videos_id);
-$totalPages = ceil($total/$_REQUEST['rowCount']);
+$totalPages = ceil($total / $_REQUEST['rowCount']);
 
 $v = new Video('', '', $videos_id);
 
@@ -39,66 +39,73 @@ $v = new Video('', '', $videos_id);
         include $global['systemRootPath'] . 'view/include/navbar.php';
         ?>
         <div class="container-fluid">
-            <h1>
-                <?php
-                echo $v->getTitle();
-                ?>
-            </h1>
-            <h3>
-                <?php
-                echo number_format_short($v->getViews_count()); 
-                ?>
-                Views and watched 
-                <?php
-                echo seconds2human($v->getTotal_seconds_watching()); 
-                ?>
-            </h3>
-            <?php
-            $pag = getPagination($totalPages);
-            echo $pag;
-            ?>
-            <table class="table table-hover">
-                <thead>
-                    <tr>
+            <div class="panel panel-default">
+                <div class="panel-heading">
+
+                    <h1>
+                        <?php
+                        echo $v->getTitle();
+                        ?>
+                    </h1>
+                </div>
+                <div class="panel-body">
+                    <h3>
+                        <?php
+                        echo number_format_short($v->getViews_count());
+                        ?>
+                        Views and watched 
+                        <?php
+                        echo seconds2human($v->getTotal_seconds_watching());
+                        ?>
+                    </h3>
+                    <?php
+                    $pag = getPagination($totalPages);
+                    echo $pag;
+                    ?>
+                    <table class="table table-hover">
+                        <thead>
+                            <tr>
+                                <th>User</th>
+                                <th>When</th>
+                                <th>Time</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                            foreach ($statistics as $value) {
+                                ?>
+                                <tr>
+                                    <td>
+                                        <?php
+                                        echo User::getNameIdentificationById($value['users_id']);
+                                        ?>
+                                    </td>
+                                    <td>
+                                        <?php
+                                        echo humanTimingAgo($value['when']);
+                                        ?>
+                                    </td>
+                                    <td>
+                                        <?php
+                                        echo seconds2human($value['seconds_watching_video']);
+                                        ?>
+                                    </td>
+                                </tr>
+                                <?php
+                            }
+                            ?>
+                        </tbody>
+                        <tfoot>
                         <th>User</th>
                         <th>When</th>
                         <th>Time</th>
-                    </tr>
-                </thead>
-                <tbody>
+                        </tfoot>
+                    </table>
                     <?php
-                    foreach ($statistics as $value) {
-                        ?>
-                        <tr>
-                            <td>
-                                <?php
-                                echo User::getNameIdentificationById($value['users_id']);
-                                ?>
-                            </td>
-                            <td>
-                                <?php
-                                echo humanTimingAgo($value['when']);
-                                ?>
-                            </td>
-                            <td>
-                                <?php
-                                echo seconds2human($value['seconds_watching_video']);
-                                ?>
-                            </td>
-                        </tr>
-                        <?php
-                    }
+                    echo $pag;
                     ?>
-                </tbody>
-                <tfoot>
-                <th>User</th>
-                <th>When</th>
-                <th>Time</th>
-                </tfoot>
-            </table>
-            <?php
-            echo $pag;
-            ?>
+                </div>
+            </div>
         </div>
         <?php
         include $global['systemRootPath'] . 'view/include/footer.php';
