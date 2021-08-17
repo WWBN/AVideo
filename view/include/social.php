5ca39da8b08f3972932e6864ad40125cc31ee129
Daniel <danielneto.com@gmail.com>
2021-08-17 09:38:35 -0300

    https://github.com/WWBN/AVideo/issues/5559#issuecomment-899811271

--- view/include/social.php
+++ view/include/social.php
@@ -23,6 +23,8 @@ $reddit = "http://www.reddit.com/submit?url=$urlSocial&title=$titleSocial";
 $linkedin = "http://www.linkedin.com/shareArticle?mini=true&url=$urlSocial&title=$titleSocial&summary=&source=$urlSocial";
 $wordpress = "http://wordpress.com/press-this.php?u=$urlSocial&quote=$titleSocial&s=";
 $pinboard = "https://pinboard.in/popup_login/?url=$urlSocial&title=$titleSocial&description=";
+$gabURL = "https://gab.com/compose?url={$urlSocial}&text={$titleSocial}";
+
 if (empty($socialAdded)) { // do not add the CSS more then once
     ?>     
     <link href="<?php echo getCDN(); ?>view/css/social.css" rel="stylesheet" type="text/css"/>
@@ -39,6 +41,13 @@ $socialAdded = 1;
     <li><a href="<?php echo $linkedin; ?>" target="_blank"  class="icoLinkedin" title="LinkedIn" data-toggle="tooltip" ><i class="fab fa-linkedin-in"></i></a></li>
     <li><a href="<?php echo $wordpress; ?>" target="_blank"  class="icoWordpress" title="Wordpress" data-toggle="tooltip" ><i class="fab fa-wordpress-simple"></i></a></li>
     <li><a href="<?php echo $pinboard; ?>" target="_blank"  class="icoPinboard" title="Pinboard" data-toggle="tooltip" ><i class="fas fa-thumbtack"></i></a></li>
+    <li>
+        <a href="<?php echo $gabURL; ?>" style="" target="_blank" class="icoGab" title="Gab" data-toggle="tooltip" >
+            <i class="fas">
+                <img src="<?php echo getCDN(); ?>view/img/gab.png" title="gab" style="height: 30px;"/>
+            </i>
+        </a>
+    </li>
     <li>
         <a href="#" class="icoCopy" title="<?php echo __('Copy to Clipboard'); ?>" data-toggle="tooltip" onclick="copyToClipboard('<?php echo urldecode($urlSocial); ?>');$(this).closest('.modal').modal('hide');" >
             <i class="far fa-copy"></i>
