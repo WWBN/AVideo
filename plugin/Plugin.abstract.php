829d5d1d385f80538453c316d8545e04524df692
Daniel <danielneto.com@gmail.com>
2021-08-18 10:08:01 -0300

    Fix parameters

--- plugin/Plugin.abstract.php
+++ plugin/Plugin.abstract.php
@@ -149,8 +149,10 @@ abstract class PluginAbstract {
                 $to = gettype($o->$key);
                 if($teo !== $to){ // this will make sure the type is the same
                     if(!is_numeric($value) || !is_numeric($o->$key)){
-                        _error_log("getDataObject - type is different $teo !== $to uuid = $uuid");
-                        $o->$key = $value;
+                        if(!(is_numeric($value) && is_bool($o->$key)) || !(is_bool($value) && is_numeric($o->$key))){
+                            _error_log("getDataObject - type is different $teo !== $to uuid = $uuid");
+                            $o->$key = $value;
+                        }
                     }
                 }
                 if(isset($value->type) && is_array($value->type) && isset($o->$key) && isset($o->$key->type)){
