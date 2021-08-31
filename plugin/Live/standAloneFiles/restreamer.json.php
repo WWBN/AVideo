b366eca9fcfa109ca45f40a59fbfe38a3ae8ebe1
Daniel <danielneto.com@gmail.com>
2021-08-31 17:47:10 -0300

    Allow choose if wants to push or not

--- plugin/Live/standAloneFiles/restreamer.json.php
+++ plugin/Live/standAloneFiles/restreamer.json.php
@@ -158,6 +158,7 @@ function startRestream($m3u8, $restreamsDestinations, $logFile, $tries = 1) {
         sleep($tries);
         return startRestream($m3u8, $restreamsDestinations, $logFile, $tries + 1);
     }
+    sleep(3);
     /*
       $command = "ffmpeg -i {$m3u8} ";
       foreach ($restreamsDestinations as $value) {
