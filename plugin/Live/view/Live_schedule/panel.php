ee58918e8979b12e10fb223e53c0cda12f4e97c8
Daniel <danielneto.com@gmail.com>
2021-08-31 14:21:45 -0300

    Add server URL + key option

--- plugin/Live/view/Live_schedule/panel.php
+++ plugin/Live/view/Live_schedule/panel.php
@@ -86,14 +86,14 @@ global $Schedulecount;
              text-overflow: ellipsis;"></div>
              <br>
         <div class="btn-group pull-left">
-            <button class="btn btn-default btn-xs" onclick="copyToClipboard($(this).attr('serverURL'));" data-toggle="tooltip" title="<?php echo __('Server URL'); ?>" >
-                <i class="far fa-clipboard"></i> <i class="fa fa-server"></i> <span class="hidden-sm hidden-xs"><?php echo __('Server URL'); ?></span>
+            <button class="btn btn-default btn-xs" onclick="copyToClipboard($(this).attr('serverURL')); avideoToastInfo($(this).attr('serverURL'));" data-toggle="tooltip" title="<?php echo __('Server URL'); ?>" >
+                <i class="fa fa-server"></i> <span class="hidden-sm hidden-xs"><?php echo __('Server URL'); ?></span>
             </button>
-            <button class="btn btn-default btn-xs" onclick="copyToClipboard($(this).attr('key'));" data-toggle="tooltip" title="<?php echo __('Key'); ?>" >
-                <i class="far fa-clipboard"></i> <i class="fa fa-key"></i> <span class="hidden-sm hidden-xs"><?php echo __('Key'); ?></span>
+            <button class="btn btn-default btn-xs" onclick="copyToClipboard($(this).attr('key'));avideoToastInfo($(this).attr('key'));" data-toggle="tooltip" title="<?php echo __('Key'); ?>" >
+                <i class="fa fa-key"></i> <span class="hidden-sm hidden-xs"><?php echo __('Key'); ?></span>
             </button>
-            <button class="btn btn-default btn-xs" onclick="copyToClipboard($(this).attr('serverURL')+$(this).attr('key'));" data-toggle="tooltip" title="<?php echo __('Server URL'); ?> + <?php echo __('Key'); ?>" >
-                <i class="far fa-clipboard"></i> <i class="fa fa-server"></i> + <i class="fa fa-key"></i> <span class="hidden-sm hidden-xs"><?php echo __('Server URL'); ?> + <?php echo __('Key'); ?></span>
+            <button class="btn btn-default btn-xs" onclick="copyToClipboard($(this).attr('serverURL')+$(this).attr('key'));avideoToastInfo($(this).attr('serverURL')+$(this).attr('key'));" data-toggle="tooltip" title="<?php echo __('Server URL'); ?> + <?php echo __('Key'); ?>" >
+                <i class="fa fa-server"></i> + <i class="fa fa-key"></i> <span class="hidden-sm hidden-xs"><?php echo __('Server URL'); ?> + <?php echo __('Key'); ?></span>
             </button>
         </div>
         <div class="btn-group pull-right">
