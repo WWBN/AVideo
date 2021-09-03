3dd341e59904f68f1c905322d3816a656d29ae99
Daniel <danielneto.com@gmail.com>
2021-09-03 10:21:46 -0300

    Do not use chat on fullscreen if it is a custom embed chat

--- plugin/Live/standAloneFiles/WebRTCServer/player/index.php
+++ plugin/Live/standAloneFiles/WebRTCServer/player/index.php
@@ -47,7 +47,7 @@ if(empty($webRTCServerCDNURL)){
         <link href="<?php echo $httpsWebSiteRootURL; ?>view/css/custom/netflix.css" rel="stylesheet" type="text/css"/>
         <link href="<?php echo $httpsWebSiteRootURL; ?>view/css/fontawesome-free-5.5.0-web/css/all.min.css" rel="stylesheet" type="text/css"/>
         <script src="<?php echo $httpsWebSiteRootURL; ?>view/js/jquery-3.5.1.min.js" type="text/javascript"></script>
-        <link href="<?php echo $webRTCServerCDNURL; ?>player/playerButtons.css?<?php echo uniqid(); ?>" rel="stylesheet" type="text/css"/>
+        <link href="<?php echo $webRTCServerCDNURL; ?>player/playerButtons.css?cache=<?php echo filemtime('player/playerButtons.css'); ?>" rel="stylesheet" type="text/css"/>
         <script>
             var webSiteRootURL = '<?php echo $httpsWebSiteRootURL; ?>';
             var WebRTCserver = '<?php echo $global['WebRTCserver']; ?>';
@@ -182,10 +182,10 @@ if(empty($webRTCServerCDNURL)){
     <script src="<?php echo $httpsWebSiteRootURL; ?>view/js/js-cookie/js.cookie.js" type="text/javascript"></script>
     <script src="<?php echo $httpsWebSiteRootURL; ?>view/js/jquery-toast/jquery.toast.min.js" type="text/javascript"></script>
     <script src="<?php echo $httpsWebSiteRootURL; ?>view/js/seetalert/sweetalert.min.js" type="text/javascript"></script>
-    <script src="<?php echo $webRTCServerCDNURL; ?>player/adapter.js"></script>
-    <script src="<?php echo $webRTCServerCDNURL; ?>player/underscore-min.js"></script>
-    <script src="<?php echo $webRTCServerCDNURL; ?>player/OvenWebRTCInput.js"></script>
-    <script src="<?php echo $webRTCServerCDNURL; ?>player/playerButtons.js?<?php echo uniqid(); ?>"></script>
+    <script src="<?php echo $webRTCServerCDNURL; ?>player/adapter.js?cache=<?php echo filemtime('player/adapter.js'); ?>"></script>
+    <script src="<?php echo $webRTCServerCDNURL; ?>player/underscore-min.js?cache=<?php echo filemtime('player/underscore-min.js'); ?>"></script>
+    <script src="<?php echo $webRTCServerCDNURL; ?>player/OvenWebRTCInput.js?cache=<?php echo filemtime('player/OvenWebRTCInput.js'); ?>"></script>
+    <script src="<?php echo $webRTCServerCDNURL; ?>player/playerButtons.js?cache=<?php echo filemtime('player/playerButtons.js'); ?>"></script>
     <script>
 
             window.addEventListener('message', event => {
