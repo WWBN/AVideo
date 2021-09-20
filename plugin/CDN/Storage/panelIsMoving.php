0dc5e16ebbf07fa78fcad637cc9f89cabf42fdde
Daniel <danielneto.com@gmail.com>
2021-09-20 11:51:15 -0300

    Fixes

--- plugin/CDN/Storage/panelIsMoving.php
+++ plugin/CDN/Storage/panelIsMoving.php
@@ -6,7 +6,13 @@
         <i class="fas fa-sync fa-spin"></i>
         <?php
         echo humanTimingAgo($isMoving['modified']);
-        echo CDNStorage::getLogFile($videos_id);
+        ?>
+    </div>
+    
+    <div class="panel-footer">
+        <?php
+        $file = CDNStorage::getLogFile($videos_id);
+        echo nl2br(file_get_contents($file));
         ?>
     </div>
 </div>
