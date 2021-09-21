aece4f8b67c4c65c9ed69e9bbf84dd158f865693
Daniel <danielneto.com@gmail.com>
2021-09-21 10:10:40 -0300

    Fix share link

--- plugin/Live/view/modeYoutubeLive.php
+++ plugin/Live/view/modeYoutubeLive.php
@@ -188,6 +188,9 @@ if (empty($sideAd) && !AVideoPlugin::loadPluginIfEnabled("Chat2")) {
                             </div>
                             <?php
                             $link = Live::getLinkToLiveFromUsers_id($user_id);
+                            if(!empty($_REQUEST['live_schedule'])){
+                                $link = addQueryStringParameter($link, 'live_schedule', $_REQUEST['live_schedule']);
+                            }
                             if (empty($advancedCustom->disableShareAndPlaylist) && empty($advancedCustom->disableShareOnly)) {
                                 getShareMenu($liveTitle, $link, $link, addQueryStringParameter($link, 'embed', 1), $img,"row bgWhite list-group-item menusDiv");
                             }
