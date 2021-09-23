04756dfc554b671c690f19591fe0da665288014a
Daniel <danielneto.com@gmail.com>
2021-09-23 15:15:23 -0300

    Show error

--- plugin/CDN/Storage/CDNStorage.php
+++ plugin/CDN/Storage/CDNStorage.php
@@ -22,8 +22,8 @@ class CDNStorage {
             $CDNstorage->login($obj->storage_username, $obj->storage_password);
             $CDNstorage->pasv(true);
         } catch (Exception $exc) {
-            _error_log("FTP:getClient fail ($obj->storage_username), ($obj->storage_password)" . $exc->getMessage());
-            die('Error');
+            _error_log("FTP:getClient fail ($obj->storage_hostname) ($obj->storage_username), ($obj->storage_password) " . $exc->getMessage());
+            die('CDNStorage FTP Error '.$exc->getMessage());
         }
         _error_log("FTP:getClient finish");
         return $CDNstorage;
