eab41201a0bea0b83690e9033f5edd6921079e0a
Daniel <danielneto.com@gmail.com>
2021-09-28 12:48:43 -0300

    Show errors on server

--- plugin/YPTSocket/server.php
+++ plugin/YPTSocket/server.php
@@ -6,6 +6,11 @@ use Socket\Message;
 //use React\Socket\Server as Reactor;
 
 require_once dirname(__FILE__) . '/../../videos/configuration.php';
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 require_once $global['systemRootPath'] . 'plugin/YPTSocket/Message.php';
 require_once $global['systemRootPath'] . 'objects/autoload.php';
 
