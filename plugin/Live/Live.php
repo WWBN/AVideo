b8c3a30b7e410a280b31bdf758921ddfea003195
Daniel <danielneto.com@gmail.com>
2021-10-07 21:12:38 -0300

    Log deleting cache

--- plugin/Live/Live.php
+++ plugin/Live/Live.php
@@ -2194,7 +2194,7 @@ class Live extends PluginAbstract {
             $pattern = "/.getStats.{$live_servers_id}.*/i";
             ObjectYPT::deleteCachePattern($pattern);
         }
-        //_error_log("deleteStatsCache: {$cacheDir} ".json_encode(debug_print_backtrace()));
+        _error_log("deleteStatsCache: {$cacheDir} ".json_encode(debug_print_backtrace()));
         rrmdir($cacheDir);
         if ($clearFirstPage) {
             clearCache(true);
