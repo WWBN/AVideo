b245a2550bf908c0ff2d95efa45469f84e9655ee
Daniel <danielneto.com@gmail.com>
2021-10-14 09:37:15 -0300

    Allow force autoplay

--- plugin/PlayerSkins/PlayerSkins.php
+++ plugin/PlayerSkins/PlayerSkins.php
@@ -48,6 +48,7 @@ class PlayerSkins extends PluginAbstract {
         $obj->showLogo = false;
         $obj->showShareSocial = true;
         $obj->showShareAutoplay = true;
+        $obj->forceAlwaysAutoplay = false;
         $obj->showLogoOnEmbed = false;
         $obj->showLogoAdjustScale = "0.4";
         $obj->showLogoAdjustLeft = "-74px";
@@ -212,7 +213,7 @@ class PlayerSkins extends PluginAbstract {
             $js .= "<script>var isLive = true;</script>";
         }
         if (isVideo() || !empty($_GET['videoName']) || !empty($_GET['u']) || !empty($_GET['evideo']) || !empty($_GET['playlists_id'])) {
-            if (!empty($_REQUEST['autoplay'])) {
+            if (!empty($_REQUEST['autoplay']) || !empty($obj->forceAlwaysAutoplay)) {
                 $js .= "<script>var autoplay = true;var forceautoplay = true;</script>";
             } else if (self::isAutoplayEnabled()) {
                 $js .= "<script>var autoplay = true;</script>";
