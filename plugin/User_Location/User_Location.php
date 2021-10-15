eb4ebe60375782e40b17e3f235f22cfc33f041fa
Daniel <danielneto.com@gmail.com>
2021-10-15 09:22:19 -0300

    https://github.com/WWBN/AVideo/issues/5828

--- plugin/User_Location/User_Location.php
+++ plugin/User_Location/User_Location.php
@@ -1,4 +1,5 @@
 <?php
+
 global $global;
 require_once $global['systemRootPath'] . 'plugin/Plugin.abstract.php';
 require_once $global['systemRootPath'] . 'plugin/User_Location/Objects/IP2Location.php';
@@ -10,18 +11,19 @@ class User_Location extends PluginAbstract {
             PluginTags::$FREE,
         );
     }
+
     public function getDescription() {
         global $global, $mysqlDatabase;
         $ret = "Detects user location for various purposes";
         $ret .= "<br>This site or product includes IP2Location LITE data available from http://www.ip2location.com.";
         $ret .= "<br><strong>Before use this plugin unzip the install.zip file and install the IPs tables<strong>";
         $ret .= "<br><pre>cd {$global['systemRootPath']}plugin/User_Location/install && unzip install.zip</pre>";
-        
-        if(!ObjectYPT::isTableInstalled("ip2location_db1_ipv6")){
+
+        if (!ObjectYPT::isTableInstalled("ip2location_db1_ipv6")) {
             $ret .= "<br><strong>For IPV6 support unzip the ip2location_db1_ipv6.zip file and install the IPs tables<strong>";
             $ret .= "<br><pre>cd {$global['systemRootPath']}plugin/User_Location/install && unzip ip2location_db1_ipv6.zip && mysql -u root -p {$mysqlDatabase} <  {$global['systemRootPath']}plugin/User_Location/install/ip2location_db1_ipv6.sql </pre>";
         }
-        
+
         return $ret;
     }
 
@@ -34,22 +36,22 @@ class User_Location extends PluginAbstract {
     }
 
     public function getPluginVersion() {
-        return "1.0";   
+        return "1.0";
     }
-    
+
     public function getEmptyDataObject() {
         $obj = new stdClass();
         $obj->autoChangeLanguage = true;
         $o = new stdClass();
-        $o->type = array('browser'=>__("Detect language from Browser"), 'ip'=>__("Detect language from IP"));
+        $o->type = array('browser' => __("Detect language from Browser"), 'ip' => __("Detect language from IP"));
         $o->value = 'browser';
         $obj->useLanguageFrom = $o;
         return $obj;
-    }    
-    
-    static function getSessionLocation(){
+    }
+
+    static function getSessionLocation() {
         $ip = getRealIpAddr();
-        if(!empty($_SESSION['User_Location'][$ip]['country_name'])){
+        if (!empty($_SESSION['User_Location'][$ip]['country_name'])) {
             if ($_SESSION['User_Location'][$ip]['country_name'] == "United States of America") {
                 $_SESSION['User_Location'][$ip]['country_name'] = "United States";
             }
@@ -57,45 +59,53 @@ class User_Location extends PluginAbstract {
         }
         return false;
     }
-    
-    static function setSessionLocation($value){
+
+    static function setSessionLocation($value) {
         $ip = getRealIpAddr();
         $_SESSION['User_Location'][$ip] = $value;
         //_error_log("User_Location: $ip ". json_encode($_SESSION['User_Location'][$ip]));
     }
-    
+
     static function getThisUserLocation() {
         $location = self::getSessionLocation();
-        if(!empty($location['country_code'])){
+        if (!empty($location['country_code'])) {
             return $location;
         }
         return self::getLocationFromIP(getRealIpAddr());
     }
-    
+
     static function getLocationFromIP($ip) {
         return IP2Location::getLocation($ip);
-    } 
-    
+    }
+
     static function getLanguageFromBrowser() {
-        if(empty($_SERVER['HTTP_ACCEPT_LANGUAGE'])){
+        if (empty($_SERVER['HTTP_ACCEPT_LANGUAGE'])) {
             return false;
         }
         $parts = explode(',', $_SERVER['HTTP_ACCEPT_LANGUAGE']);
         return str_replace('-', '_', $parts[0]);
     }
-    
-    static function setLanguage($lang) {        
+
+    static function setLanguage($lang) {
         global $global;
+        if (!empty($_SESSION['language'])) {
+            $file = "{$global['systemRootPath']}locale/{$_SESSION['language']}.php";
+            if (file_exists($file)) {
+                include_once $file;
+                return true;
+            }
+        }
+
         $file = "{$global['systemRootPath']}locale/{$lang}.php";
         _session_start();
-        if(file_exists($file)){
+        if (file_exists($file)) {
             $_SESSION['language'] = $lang;
             include_once $file;
             return true;
-        }else{
+        } else {
             $lang = strtolower($lang);
             $file = "{$global['systemRootPath']}locale/{$lang}.php";
-            if(file_exists($file)){
+            if (file_exists($file)) {
                 $_SESSION['language'] = $lang;
                 include_once $file;
                 return true;
@@ -103,33 +113,33 @@ class User_Location extends PluginAbstract {
         }
         return false;
     }
-    
+
     static function setLanguageFromBrowser() {
         return self::setLanguage(self::getLanguageFromBrowser());
     }
-    
+
     static function setLanguageFromIP() {
         $User_Location = self::getThisUserLocation();
         return self::setLanguage($User_Location['country_code']);
     }
-    
+
     public function getStart() {
         global $global, $config;
         $obj = $this->getDataObject();
         $User_Location = self::getThisUserLocation();
-        if($obj->autoChangeLanguage){
-            if($obj->useLanguageFrom->value=='browser'){
+        if ($obj->autoChangeLanguage) {
+            if ($obj->useLanguageFrom->value == 'browser') {
                 $changed = self::setLanguageFromBrowser();
-                if(!$changed){
+                if (!$changed) {
                     $changed = self::setLanguageFromIP();
                 }
-            }else{
+            } else {
                 $changed = self::setLanguageFromIP();
-                if(!$changed){
+                if (!$changed) {
                     $changed = self::setLanguageFromBrowser();
                 }
             }
-            if(!$changed){
+            if (!$changed) {
                 $_SESSION['language'] = $config->getLanguage();
             }
         }
@@ -138,7 +148,6 @@ class User_Location extends PluginAbstract {
         return false;
     }
 
-    
     public function getPluginMenu() {
         global $global;
         $filename = $global['systemRootPath'] . 'plugin/User_Location/pluginMenu.html';
