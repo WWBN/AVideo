3d6e8d477d4d8d9a39210a5c1039a6b1f74495ea
Daniel <danielneto.com@gmail.com>
2021-10-15 10:28:00 -0300

    Improve scheduler

--- plugin/Scheduler/Scheduler.php
+++ plugin/Scheduler/Scheduler.php
@@ -83,7 +83,7 @@ class Scheduler extends PluginAbstract {
             return false;
         }
         if(empty($_executeSchelude[$callBackURL])){
-            $callBackURL = addQueryStringParameter($callBackURL, 'token', getToken(6000));
+            $callBackURL = addQueryStringParameter($callBackURL, 'token', getToken(60));
             _error_log("Scheduler::run getting callback URL {$callBackURL}");
             $_executeSchelude[$callBackURL] = url_get_contents($callBackURL);
             _error_log("Scheduler::run got callback ". json_encode($_executeSchelude[$callBackURL]));
