06818f5af461515aee5c044223c1fc6f094fba5a
Daniel <danielneto.com@gmail.com>
2021-10-29 09:56:09 -0300

    Back to the same page

--- plugin/Gallery/view/Category.php
+++ plugin/Gallery/view/Category.php
@@ -17,9 +17,13 @@ if ((!empty($videos)) || (!empty($obj) && $obj->SubCategorys)) {
             // -1 is a personal workaround only
             if ((empty($parentCat)) && (($currentCat['parentId'] == "0") || ($currentCat['parentId'] == "-1"))) {
                 if (!empty($_GET['catName'])) {
+                    $backURL = getBackURL();
+                    if(!empty($_REQUEST['getBackURL'])){
+                        $backURL = $_REQUEST['getBackURL'];
+                    }
                     ?>
                     <div class="clearfix" style="margin: 10px 0;">
-                        <a class="btn btn-default btn-sm pull-left"  href="<?php echo getBackURL(); ?>">
+                        <a class="btn btn-default btn-sm pull-left"  href="<?php echo $backURL; ?>">
                             <i class="fa fa-backward"></i>
                             <?php echo __("Back"); ?> 
                         </a>
@@ -27,9 +31,13 @@ if ((!empty($videos)) || (!empty($obj) && $obj->SubCategorys)) {
                     <?php
                 }
             } else if (!empty($parentCat)) {
+                $backURL = '';
+                if(!empty($_REQUEST['getBackURL'])){
+                    $backURL = $_REQUEST['getBackURL'];
+                }
                 ?>
                 <div class="clearfix" style="margin: 10px 0;">
-                    <a class="btn btn-default btn-sm pull-left" href="<?php echo $global['webSiteRootURL']; ?>cat/<?php echo $parentCat['clean_name']; ?>">
+                    <a class="btn btn-default btn-sm pull-left" href="<?php echo $global['webSiteRootURL']; ?>cat/<?php echo $parentCat['clean_name']; ?>?backURL=<?php echo urlencode($backURL); ?>">
                         <i class="fa fa-backward"></i>
                         <?php echo __("Back to") . " " . $parentCat['name']; ?> 
                     </a>
