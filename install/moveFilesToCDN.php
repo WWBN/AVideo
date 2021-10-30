719fe6eba9d054ac88ce54a53f2e6c37e928afef
Daniel <danielneto.com@gmail.com>
2021-10-30 16:38:22 -0300

    Add a command line to move all the video to the CDN

--- install/moveFilesToCDN.php
+++ install/moveFilesToCDN.php
@@ -22,23 +22,28 @@ $total = Video::getTotalVideos("", false, true, true, false, false);
 $videos = Video::getAllVideosLight("", false, true, false);
 $count = 0;
 
-
+$countSiteIdNotEmpty = 0;
+$countStatusNotActive = 0;
+$countMoved = 0;
 foreach ($videos as $value) {
     $count++;
     echo "{$count}/{$total} Moving {$global['webSiteRootURL']}v/{$value['id']} {$value['title']}".PHP_EOL;
     if(!empty($value['sites_id'])){
+        $countSiteIdNotEmpty++;
         echo "sites_id is not empty {$value['sites_id']}".PHP_EOL;
         ob_flush();
         continue;
     }
     if($value['status'] !== Video::$statusActive){
+        $countStatusNotActive++;
         echo "The video status is not active {$value['status']}".PHP_EOL;
         ob_flush();
         continue;
     }
-    
+    $countMoved++;
     //CDNStorage::moveLocalToRemote($value['id']);
     echo "{$count}/{$total} Moved done {$global['webSiteRootURL']}v/{$value['id']} {$value['title']}".PHP_EOL;
+    echo "SiteIdNotEmpty = $countSiteIdNotEmpty; StatusNotActive=$countStatusNotActive; Moved=$countMoved;".PHP_EOL;
     ob_flush();
 }
 
