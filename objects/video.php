584c3b960a0811356317e81d3cebb4680b9b889e
Daniel <danielneto.com@gmail.com>
2021-11-01 18:32:29 -0300

    Return YPT Storage to the CDN

--- objects/video.php
+++ objects/video.php
@@ -2956,7 +2956,8 @@ if (!class_exists('Video')) {
                     $site = new Sites($video['sites_id']);
                 }
 
-                if (!empty($cdn_obj->enable_storage) && $isValidType && $fsize < 20 && !empty($site) && ($site->getStatus()=='t' || $site->getUrl()=='url')) {
+                if (!empty($cdn_obj->enable_storage) && $isValidType && $fsize < 20 && !empty($site) && ($site->getStatus()=='t' || $site->getUrl()=='url/')) {
+                    var_dump();
                     if ($type == ".m3u8") {
                         $f = "{$filename}/index{$type}";
                     } else {
