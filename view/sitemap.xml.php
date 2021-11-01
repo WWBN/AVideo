7e909d72b10f4ca96eb14918ff74eb9621f85fca
Daniel <danielneto.com@gmail.com>
2021-11-01 14:54:40 -0300

    do not create sitemap multiple times

--- view/sitemap.xml.php
+++ view/sitemap.xml.php
@@ -9,12 +9,20 @@ $name = "sitemap.xml";
 $lifetime = 43200;
 
 $sitemap = ObjectYPT::getCache($name, $lifetime);
+$videosDir = getVideosDir();
+$lockFile = "{$videosDir}cache/sitemap.lock";
 
 header("Content-type: application/xml");
+if (file_exists($lockFile) && filemtime($filename) > strtotime('-10 minutes')) {
+    echo "<!-- please wait -->";
+    exit;
+}
 if (empty($sitemap)) {
+    file_put_contents($lockFile, time());
     $sitemap = siteMap();
     ObjectYPT::setCache($name, $sitemap);
-}else{
+    unlink($lockFile);
+} else {
     $sitemap .= "<!-- cached -->";
 }
 echo $sitemap;
