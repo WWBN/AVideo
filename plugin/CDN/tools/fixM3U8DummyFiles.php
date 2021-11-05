420c125286b2aed98d1e3b5fc2defb1440666aa5
Daniel <danielneto.com@gmail.com>
2021-11-05 08:04:16 -0300

    Fix

---
+++ plugin/CDN/tools/fixM3U8DummyFiles.php
@@ -0,0 +1,66 @@
+<?php
+
+$config = dirname(__FILE__) . '/../../../videos/configuration.php';
+require_once $config;
+
+if (!isCommandLineInterface()) {
+    return die('Command Line only');
+}
+
+$isCDNEnabled = AVideoPlugin::isEnabledByName('CDN');
+
+if (empty($isCDNEnabled)) {
+    return die('Plugin disabled');
+}
+
+set_time_limit(300);
+ini_set('max_execution_time', 300);
+
+$global['rowCount'] = $global['limitForUnlimitedVideos'] = 999999;
+$path = getVideosDir();
+$total = Video::getTotalVideos("", false, true, true, false, false);
+$videos = Video::getAllVideosLight("", false, true, false);
+$count = 0;
+
+$countSiteIdNotEmpty = 0;
+$countStatusNotActive = 0;
+$countMoved = 0;
+foreach ($videos as $value) {
+    $count++;
+    echo "SiteIdNotEmpty = $countSiteIdNotEmpty; StatusNotActive=$countStatusNotActive; Moved=$countMoved;" . PHP_EOL;
+    echo "{$count}/{$total} Checking {$global['webSiteRootURL']}v/{$value['id']} {$value['title']}" . PHP_EOL;
+    if (empty($value['sites_id'])) {
+        $countSiteIdNotEmpty++;
+        echo "sites_id is empty {$value['id']} {$value['title']}" . PHP_EOL;
+        ob_flush();
+        continue;
+    }
+    if ($value['status'] !== Video::$statusActive) {
+        $countStatusNotActive++;
+        echo "The video status is not active {$value['status']}" . PHP_EOL;
+        ob_flush();
+        continue;
+    }
+    $countMoved++;
+    $videos_id = $value['id'];
+
+    $list = self::getFilesListLocal($videos_id);
+    $totalFiles = count($list);
+    if (empty($totalFiles)) {
+       echo "There is not file to check ";
+        self::setProgress($videos_id, true, true);
+        return false;
+    }
+    
+    foreach ($list as $value) {
+        if(preg_match('/index.m3u8$/', $value['local_path'])){
+            echo "Check {$value['local_path']}" . PHP_EOL;
+        }
+    }
+    
+}
+
+echo "SiteIdNotEmpty = $countSiteIdNotEmpty; StatusNotActive=$countStatusNotActive; Moved=$countMoved;" . PHP_EOL;
+echo PHP_EOL . " Done! " . PHP_EOL;
+die();
+
