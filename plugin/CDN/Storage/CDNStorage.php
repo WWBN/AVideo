bd2e16397ae6d65a2d2b8d06660165ffe558c7eb
Daniel <danielneto.com@gmail.com>
2021-11-10 16:49:43 -0300

    Improve CDN Storage

--- plugin/CDN/Storage/CDNStorage.php
+++ plugin/CDN/Storage/CDNStorage.php
@@ -495,7 +495,7 @@ class CDNStorage {
                     unset($ret[$key]);
                     unset($_uploadInfo[$key]);
 
-                    _error_log("CDNStorage::put:uploadToCDNStorage [$key] [{$fileUploadCount}/{$totalFiles}] FTP_FINISHED in {$seconds} {$humanFilesize} {$mbps}/Mbps ETA: {$ETA}");
+                    _error_log("CDNStorage::put:uploadToCDNStorage [$key] [{$fileUploadCount}/{$totalFiles}] FTP_FINISHED in {$seconds} seconds {$humanFilesize} {$mbps}/Mbps ETA: {$ETA}");
 
                     $file = array_shift($filesToUpload);
                     //echo "File finished... $key" . PHP_EOL;
