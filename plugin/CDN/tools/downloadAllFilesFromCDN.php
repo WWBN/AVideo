1dcd85f217f486bf78b48a716a46a0c665039f2d
Daniel <danielneto.com@gmail.com>
2021-11-11 15:37:51 -0300

    Allow download all files

--- plugin/CDN/tools/downloadAllFilesFromCDN.php
+++ plugin/CDN/tools/downloadAllFilesFromCDN.php
@@ -2,6 +2,7 @@
 
 $config = dirname(__FILE__) . '/../../../videos/configuration.php';
 require_once $config;
+error_reporting(E_ALL);
 
 if (!isCommandLineInterface()) {
     return die('Command Line only');
