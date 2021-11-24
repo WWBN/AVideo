28848a725730cf90371b0f4d3ec1d546b29131c7
Daniel <danielneto.com@gmail.com>
2021-11-24 15:00:17 -0300

    Tool do delete missing tgz

--- install/deleteTGZFiles.php
+++ install/deleteTGZFiles.php
@@ -12,7 +12,7 @@ $files = array_diff(scandir($path), array('.', '..'));
 foreach ($files as $value) {
     $dir = "{$path}{$value}";
     if (is_dir($dir)) {
-        $files2 = array_diff(scandir($path), array('.', '..'));
+        $files2 = array_diff(scandir($dir), array('.', '..'));
         foreach ($files2 as $value2) {
             $ext = pathinfo($value2, PATHINFO_EXTENSION);
             if($ext=='tgz'){
