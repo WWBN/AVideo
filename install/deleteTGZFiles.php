8a6d7b1e76a842cd4008fed30a44284143a67c6f
Daniel <danielneto.com@gmail.com>
2021-11-24 14:59:01 -0300

    Tool do delete missing tgz

---
+++ install/deleteTGZFiles.php
@@ -0,0 +1,24 @@
+<?php
+
+//streamer config
+require_once '../videos/configuration.php';
+ob_end_flish();
+if (!isCommandLineInterface()) {
+    return die('Command Line only');
+}
+
+$path = getVideosDir();
+$files = array_diff(scandir($path), array('.', '..'));
+foreach ($files as $value) {
+    $dir = "{$path}{$value}";
+    if (is_dir($dir)) {
+        $files2 = array_diff(scandir($path), array('.', '..'));
+        foreach ($files2 as $value2) {
+            $ext = pathinfo($value2, PATHINFO_EXTENSION);
+            if($ext=='tgz'){
+                $dir2 = "{$dir2}/{$value2}";
+                echo $dir2.PHP_EOL;
+            }
+        }
+    }
+}
