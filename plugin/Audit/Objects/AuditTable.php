62b80b4e3eaff95945ee38040944f59c2955bdc5
Daniel <danielneto.com@gmail.com>
2021-11-26 09:58:24 -0300

    Do not audit CachesInDB table

--- plugin/Audit/Objects/AuditTable.php
+++ plugin/Audit/Objects/AuditTable.php
@@ -18,6 +18,9 @@ class AuditTable extends ObjectYPT {
         
     
     function audit($method, $class, $statement, $formats, $values, $users_id) {
+        if(strtolower($class) === 'cachesindb'){
+            return false;
+        }
         $this->method = $method;
         $this->class = $class;
         $this->statement = substr(str_replace(array("'","\\","\\x","\x"), array("","","",""), $statement),0,1000)."n";
