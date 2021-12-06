83941213efd78390cd7e883f9d328f66d61992da
Daniel <danielneto.com@gmail.com>
2021-12-06 16:40:37 -0300

    fix category

--- objects/categories.json.php
+++ objects/categories.json.php
@@ -9,7 +9,7 @@ if (!isset($global['systemRootPath'])) {
 require_once $global['systemRootPath'] . 'objects/category.php';
 
 header('Access-Control-Allow-Origin: *');
-header('Content-Type: application/json; charset=latin1');
+header('Content-Type: application/json');
 
 $_REQUEST['rowCount'] = getRowCount(1000);
 $_REQUEST['current'] = getCurrentPage();
