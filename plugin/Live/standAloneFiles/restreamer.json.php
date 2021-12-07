37198168e509004d94d5f78a97d43410d5416e1c
Daniel <danielneto.com@gmail.com>
2021-12-07 17:14:17 -0300

    fix restream

--- plugin/Live/standAloneFiles/restreamer.json.php
+++ plugin/Live/standAloneFiles/restreamer.json.php
@@ -64,7 +64,6 @@ $isCommandLine = php_sapi_name() === 'cli';
 if (!$isCommandLine) { // not command line
     $request = file_get_contents("php://input");
     error_log("Restreamer.json.php php://input {$request}");
-    error_log("Restreamer.json.php POST ". json_encode($_POST));
     $robj = json_decode($request);
 } else {
     $robj = new stdClass();
@@ -231,14 +230,14 @@ function startRestream($m3u8, $restreamsDestinations, $logFile, $tries = 1) {
                 continue;
             }
             $value = clearCommandURL($value);
-            $command .= ' -max_muxing_queue_size 1024 -acodec copy -bsf:a aac_adtstoasc -vcodec copy -preset veryfast -f flv "' . $value . '" ';
+            $command .= ' -max_muxing_queue_size 1024 -acodec copy -bsf:a aac_adtstoasc -ar 96000 -vcodec copy -preset veryfast -f flv "' . $value . '" ';
         }
     } else {
         if (!isOpenSSLEnabled() && preg_match("/rtpms:/i", $restreamsDestinations[0])) {
             error_log("Restreamer.json.php ERROR #2 FFMPEG openssl is not enabled, ignoring {$restreamsDestinations[0]} ");
         } else {
             //$command = "ffmpeg -re -i \"{$m3u8}\" -max_muxing_queue_size 1024 -acodec copy -bsf:a aac_adtstoasc -vcodec copy -f flv \"{$restreamsDestinations[0]}\"";
-            $command = "{$ffmpegBinary} -re -rw_timeout 15000000 -i \"{$m3u8}\" -max_muxing_queue_size 1024 -acodec copy -bsf:a aac_adtstoasc -vcodec copy -preset veryfast -f flv \"{$restreamsDestinations[0]}\"";
+            $command = "{$ffmpegBinary} -re -rw_timeout 15000000 -i \"{$m3u8}\"  -max_muxing_queue_size 1024 -acodec copy -bsf:a aac_adtstoasc -ar 96000 -vcodec copy -preset veryfast -f flv \"{$restreamsDestinations[0]}\"";
         }
     }
     if (empty($command) || !preg_match("/-f flv/i", $command)) {
