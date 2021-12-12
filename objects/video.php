f90401c95b4a28580fa5e8638fbb4b367c9c52c4
Daniel <danielneto.com@gmail.com>
2021-12-12 13:02:48 -0300

    Fix for cache disabled

--- objects/video.php
+++ objects/video.php
@@ -4266,10 +4266,12 @@ if (!class_exists('Video')) {
             ObjectYPT::deleteCache(md5($filename . ".mp4"));
             ObjectYPT::deleteCache(md5($filename . ".m3u81"));
             ObjectYPT::deleteCache(md5($filename . ".mp41"));
-            Cache::deleteCache("getSourceFile($filename)1");
-            Cache::deleteCache("getSourceFile($filename)0");
-            Cache::deleteCache("getVideosPaths_($filename)1");
-            Cache::deleteCache("getVideosPaths_($filename)0");
+            if(class_exists('Cache')){
+                Cache::deleteCache("getSourceFile($filename)1");
+                Cache::deleteCache("getSourceFile($filename)0");
+                Cache::deleteCache("getVideosPaths_($filename)1");
+                Cache::deleteCache("getVideosPaths_($filename)0");
+            }
             Video::clearImageCache($filename);
             Video::clearImageCache($filename, "article");
             Video::clearImageCache($filename, "pdf");
