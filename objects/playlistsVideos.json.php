8d7769b7c63fc1556e2ee2f5ea63bbbe95c0de58
Daniel <danielneto.com@gmail.com>
2021-12-21 16:16:51 -0300

    https://github.com/WWBN/AVideo/issues/6104

--- objects/playlistsVideos.json.php
+++ objects/playlistsVideos.json.php
@@ -41,8 +41,8 @@ foreach ($videos as $key => $value) {
     $videos[$key]['imageClass'] = !empty($objMob->portraitImage)?"portrait":"landscape";
     $videos[$key]['VideoUrl'] = getVideosURL($videos[$key]['filename']);
     $videos[$key]['createdHumanTiming'] = humanTiming(strtotime($videos[$key]['created']));
-    $videos[$key]['pageUrl'] = Video::getLink($videos[$key]['id'], $videos[$key]['clean_title'], false);
-    $videos[$key]['embedUrl'] = Video::getLink($videos[$key]['id'], $videos[$key]['clean_title'], true);
+    $videos[$key]['pageUrl'] = Video::getLink($videos[$key]['videos_id'], $videos[$key]['clean_title'], false);
+    $videos[$key]['embedUrl'] = Video::getLink($videos[$key]['videos_id'], $videos[$key]['clean_title'], true);
     unset($_POST['sort'], $_POST['current'], $_POST['searchPhrase']);
     $_REQUEST['rowCount'] = 10;
     $_POST['sort']['created'] = "desc";
