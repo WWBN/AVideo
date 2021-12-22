646bc29a1f10a7b0d52404bdcf1bd36767e1296c
Daniel <danielneto.com@gmail.com>
2021-12-22 14:58:03 -0300

    Show info on playlists

--- plugin/YouPHPFlix2/view/row_serie.php
+++ plugin/YouPHPFlix2/view/row_serie.php
@@ -7,26 +7,33 @@
      -o-background-size: cover;
      background-size: cover;
      ">
-    <div class="topicRow">
-        <h2 class="infoTitle">
-            <?php
-            $rowLink = $link = PlayLists::getLink($value['serie_playlists_id']);
-            $rowLinkEmbed = $linkEmbed = PlayLists::getLink($value['serie_playlists_id'], true);
-            $canWatchPlayButton = "";
-            if (User::canWatchVideoWithAds($value['id'])) {
-                $canWatchPlayButton = "canWatchPlayButton";
-            } else if ($obj->hidePlayButtonIfCannotWatch) {
-                $canWatchPlayButton = "hidden";
-            }
-            $value['title'] = "<a href='{$link}' embed='{$linkEmbed}' class='{$canWatchPlayButton}'>{$value['title']}</a>";
-            echo $value['title'];
-            ?>
-        </h2>
-        <div class="col-sm-12">
-        <?php
-        include $global['systemRootPath'] . 'plugin/YouPHPFlix2/view/row_info.php';
-        ?>
+    <div class="posterDetails " style="
+         background: -webkit-linear-gradient(left, rgba(<?php echo $obj->backgroundRGB; ?>,1) 40%, rgba(<?php echo $obj->backgroundRGB; ?>,0) 100%);
+         background: -o-linear-gradient(right, rgba(<?php echo $obj->backgroundRGB; ?>,1) 40%, rgba(<?php echo $obj->backgroundRGB; ?>,0) 100%);
+         background: linear-gradient(right, rgba(<?php echo $obj->backgroundRGB; ?>,1) 40%, rgba(<?php echo $obj->backgroundRGB; ?>,0) 100%);
+         background: -moz-linear-gradient(to right, rgba(<?php echo $obj->backgroundRGB; ?>,1) 40%, rgba(<?php echo $obj->backgroundRGB; ?>,0) 100%);">
+        <div class="topicRow">
+            <h2 class="infoTitle">
+                <?php
+                $rowLink = $link = PlayLists::getLink($value['serie_playlists_id']);
+                $rowLinkEmbed = $linkEmbed = PlayLists::getLink($value['serie_playlists_id'], true);
+                $canWatchPlayButton = "";
+                if (User::canWatchVideoWithAds($value['id'])) {
+                    $canWatchPlayButton = "canWatchPlayButton";
+                } else if ($obj->hidePlayButtonIfCannotWatch) {
+                    $canWatchPlayButton = "hidden";
+                }
+                $value['title'] = "<a href='{$link}' embed='{$linkEmbed}' class='{$canWatchPlayButton}'>{$value['title']}</a>";
+                echo $value['title'];
+                ?>
+            </h2>
+            <div class="col-sm-12">
+                <?php
+                include $global['systemRootPath'] . 'plugin/YouPHPFlix2/view/row_info.php';
+                ?>
+            </div>
+            <div id="ajaxLoad-<?php echo $uid; ?>" class="flickity-area col-sm-12"><?php echo __('Loading...'); ?></div>
         </div>
-        <div id="ajaxLoad-<?php echo $uid; ?>" class="flickity-area col-sm-12"><?php echo __('Loading...'); ?></div>
     </div>
+
 </div>
