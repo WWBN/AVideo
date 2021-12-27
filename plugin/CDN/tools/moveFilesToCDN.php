d35402ce441380b1422d92d3d144c7154743139b
Daniel <danielneto.com@gmail.com>
2021-12-27 18:19:25 -0300

    Make dummy

--- plugin/CDN/tools/moveFilesToCDN.php
+++ plugin/CDN/tools/moveFilesToCDN.php
@@ -36,6 +36,7 @@ foreach ($videos as $value) {
     $count++;
     //echo "{$count}/{$total} Checking {$global['webSiteRootURL']}v/{$value['id']} {$value['title']}" . PHP_EOL;
     if (!empty($value['sites_id'])) {
+        self::createDummyFiles($value['id']);
         $countSiteIdNotEmpty++;
         // make sure it is dummy files
         //CDNStorage::createDummyFiles($value['id']);
