350665eced5be2da3f76ad834871327e76f1aa35
Daniel <danielneto.com@gmail.com>
2022-01-03 18:27:26 -0300

    PHPBB improvement

--- plugin/PHPBB3/avideoLogin.txt.php
+++ plugin/PHPBB3/avideoLogin.txt.php
@@ -2,5 +2,5 @@
 require_once '../../videos/configuration.php';
 header("Content-Type: text/plain");
 $content = file_get_contents('avideoLogin.php');
-echo str_replace('{webSiteRootURL}', "{$global['webSiteRootURL']}plugin/PHPBB3/avideoLogin.txt.php", $content);
+echo str_replace('{webSiteRootURL}', "{$global['webSiteRootURL']}", $content);
 ?>
