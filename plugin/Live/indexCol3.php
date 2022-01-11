20e1910ef38106e4e4e1a0d5766db789ab975f39
Daniel <danielneto.com@gmail.com>
2022-01-11 16:07:53 -0300

    Improving key positions

--- plugin/Live/indexCol3.php
+++ plugin/Live/indexCol3.php
@@ -8,18 +8,22 @@ if (!empty($obj->experimentalWebcam)) {
 <div class="tabbable-line <?php echo getCSSAnimationClassAndStyle('animate__fadeInRight', 'live'); ?>"  id="indexTabs">
     <ul class="nav nav-tabs">
         <?php
+        $active = 'active';
         if (empty($obj->disableRestream)) {
             ?>
-            <li class="" data-toggle="tooltip" data-placement="bottom" title="<?php echo __("Live stream to other platforms simultaneously"); ?>"><a data-toggle="tab" href="#tabRestream"><i class="fas fa-sync"></i> <?php echo __("Restream"); ?></a> </li>
+            <li class="<?php echo $active; ?>" data-toggle="tooltip" data-placement="bottom" title="<?php echo __("Live stream to other platforms simultaneously"); ?>"><a data-toggle="tab" href="#tabRestream"><i class="fas fa-sync"></i> <?php echo __("Restream"); ?></a> </li>
             <?php
+            $active = '';
         }
         ?>
-        <li class="active" data-toggle="tooltip" data-placement="bottom" title="<?php echo __("Use streaming software or hardware"); ?>"><a data-toggle="tab" href="#tabStreamKey"><i class="fas fa-key"></i> <?php echo __("Stream Key"); ?></a></li>
+        <li class="<?php echo $active; ?>" data-toggle="tooltip" data-placement="bottom" title="<?php echo __("Use streaming software or hardware"); ?>"><a data-toggle="tab" href="#tabStreamKey"><i class="fas fa-key"></i> <?php echo __("Stream Key"); ?></a></li>
         <?php
+        $active = '';
         if (empty($objLive->hideShare)) {
             ?>
-        <li class="" data-toggle="tooltip" data-placement="bottom" title="<?php echo __("Share information about your live"); ?>"><a data-toggle="tab" href="#tabShare"><i class="fa fa-share"></i> <?php echo __("Share"); ?></a></li>
-        <?php
+            <li class="<?php echo $active; ?>" data-toggle="tooltip" data-placement="bottom" title="<?php echo __("Share information about your live"); ?>"><a data-toggle="tab" href="#tabShare"><i class="fa fa-share"></i> <?php echo __("Share"); ?></a></li>
+            <?php
+            $active = '';
         }
         ?>
     </ul>
@@ -29,8 +33,7 @@ if (!empty($obj->experimentalWebcam)) {
         if (empty($obj->disableRestream)) {
             ?>
             <div id="tabRestream" class="tab-pane fade <?php echo $active; ?>"> 
-                <?php
-                include $global['systemRootPath'] . 'plugin/Live/view/Live_restreams/livePanel.php'; ?>
+                <?php include $global['systemRootPath'] . 'plugin/Live/view/Live_restreams/livePanel.php'; ?>
             </div>
             <?php
             $active = '';
