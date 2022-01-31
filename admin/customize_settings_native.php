4208856a5c666790ea55021a1674b66e8dd81849
Daniel <danielneto.com@gmail.com>
2022-01-31 14:09:27 -0300

    Put max chars limit

--- admin/customize_settings_native.php
+++ admin/customize_settings_native.php
@@ -10,7 +10,7 @@
                     <div class="col-md-8 inputGroupContainer">
                         <div class="input-group">
                             <span class="input-group-addon"><i class="glyphicon glyphicon-globe"></i></span>
-                            <input  id="inputWebSiteTitle" placeholder="<?php echo __("Web site title"); ?>" class="form-control"  type="text"  value="<?php echo $config->getWebSiteTitle(); ?>" >
+                            <input  id="inputWebSiteTitle" placeholder="<?php echo __("Web site title"); ?>" class="form-control"  type="text"  value="<?php echo $config->getWebSiteTitle(); ?>" maxlength="45" >
                         </div>
                     </div>
                 </div>
