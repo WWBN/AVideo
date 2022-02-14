64a0bf757c7b2017bf4e238fae699698851054a9
Daniel <danielneto.com@gmail.com>
2022-02-14 09:49:48 -0300

    Fix flix style on mobile

--- plugin/YouPHPFlix2/view/modeFlixHead.php
+++ plugin/YouPHPFlix2/view/modeFlixHead.php
@@ -1,2 +1,13 @@
-<link href="<?php echo getCDN(); ?>view/js/webui-popover/jquery.webui-popover.min.css" rel="stylesheet" type="text/css" />
-<link href="<?php echo getCDN(); ?>plugin/YouPHPFlix2/view/js/flickity/flickity.min.css" rel="stylesheet" type="text/css" />
+<link href="<?php echo getURL('view/js/webui-popover/jquery.webui-popover.min.css'); ?>" rel="stylesheet" type="text/css" />
+<link href="<?php echo getURL('plugin/YouPHPFlix2/view/js/flickity/flickity.min.css'); ?>" rel="stylesheet" type="text/css" />
+<style>
+<?php
+if (isMobile()) {
+    ?>
+        #carouselRows .posterDetails {
+            height: 100%;
+        }
+    <?php
+}
+?>
+</style>
