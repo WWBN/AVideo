b59bff249e9cda8ed4dab90fb3ddca5a4c403bb9
Daniel <danielneto.com@gmail.com>
2022-02-24 13:59:00 -0300

    Chat fixes

--- plugin/CustomizeUser/View/tabDonation.php
+++ plugin/CustomizeUser/View/tabDonation.php
@@ -65,9 +65,9 @@ foreach ($donation as $value) {
                             <div class="col-md-12 inputGroupContainer">
                                 <div class="input-group">
                                     <span class="input-group-addon"><i class="fas fa-check"></i></span>
-                                    <input id="donationThankyou<?php echo $i; ?>" placeholder="{user} <?php echo __("Thank you for the donation"); ?>" class="form-control donationThankyou"  type="text" value="<?php echo $donationThankyou; ?>" required >
-                                    <small class="text-muted">Use the placeholder {user} and {value} for dinamically replacement</small>
+                                    <input id="donationThankyou<?php echo $i; ?>" placeholder="{user} <?php echo __("Thank you for the donation of"); ?> {value}" class="form-control donationThankyou"  type="text" value="<?php echo $donationThankyou; ?>" required >
                                 </div>
+                                <small class="text-muted">Use the placeholder {user} and {value} for dinamically replacement</small>
                             </div>
                         </div>
                         <div class="form-group">
