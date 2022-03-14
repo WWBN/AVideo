33e7f7384e9d18f96a398bccd8e41ea43b2fd082
Daniel <danielneto.com@gmail.com>
2022-03-14 14:06:03 -0300

    https://github.com/WWBN/AVideo/issues/6345#issuecomment-1067056556

--- plugin/Live/getUploadMenuButton.php
+++ plugin/Live/getUploadMenuButton.php
@@ -1,6 +1,6 @@
 <li>
     <a href="#" onclick="avideoModalIframeFull(webSiteRootURL+'plugin/Live');return false;"  data-toggle="tooltip" title="<?php echo __($buttonTitle); ?>" data-placement="left"  
        class="faa-parent animated-hover">
-        <i class="fas fa-circle faa-flash" style="color: red;" ></i>  <?php echo $buttonTitle; ?>
+        <i class="fas fa-circle faa-flash" style="color: red;" ></i>  <?php echo __($buttonTitle); ?>
     </a>
 </li>
