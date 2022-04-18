02acbd7227cc81fc6fd333c0ff96af559059d731
Daniel <developer@youphptube.com>
2022-04-18 16:20:44 -0300

    Improve comment delete

--- objects/commentDelete.json.php
+++ objects/commentDelete.json.php
@@ -4,11 +4,25 @@ global $global, $config;
 if (!isset($global['systemRootPath'])) {
     require_once '../videos/configuration.php';
 }
-require_once $global['systemRootPath'] . 'objects/user.php';
 require_once $global['systemRootPath'] . 'objects/comment.php';
-if (empty($_POST['id'])) {
-    die('{"error":"'.__("ID can not be empty").'"}');
+
+$obj = new stdClass();
+$obj->error = true;
+$obj->msg = true;
+$obj->id = intval($_POST['id']);
+$obj->status = false;
+
+
+if (empty($obj->id)) {
+    $obj->msg = __("ID can not be empty");
+    die(_json_encode($obj));
 }
-require_once 'category.php';
-$obj = new Comment("", 0, $_POST['id']);
-echo '{"status":"'.$obj->delete().'"}';
+
+$objC = new Comment("", 0, $obj->id);
+$obj->status = $objC->delete();
+
+if(!empty($obj->status)){
+    $obj->error = false;
+}
+
+die(_json_encode($obj));
