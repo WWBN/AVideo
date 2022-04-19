a5a5795707aad35f7494b7e291ea1b52a075147f
DanieL <DanieL@host.docker.internal>
2022-04-19 19:35:21 -0300

    Fix URL

--- objects/functions.php
+++ objects/functions.php
@@ -2973,7 +2973,7 @@ function verify($url) {
     $verifyURL = "https://search.ypt.me/verify.php";
     $verifyURL = addQueryStringParameter($verifyURL, 'url', $url);
     $verifyURL = addQueryStringParameter($verifyURL, 'screenshot', 1);
-    if (!file_exists($cacheFile) || (time() > (filemtime($cacheFile) + $lifetime))) {
+    if (!file_exists($cacheFile) || (time() > (filemtime($cacheFile) + $lifetime)) || empty(file_get_contents($cacheFile))) {
         error_log("Verification Creating the Cache {$url}");
         $result = url_get_contents($verifyURL, '', 5);
         file_put_contents($cacheFile, $result);
