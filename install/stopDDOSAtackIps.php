1996b2461f598be057bb7f687626806c49c8b71b
DanieL <DanieL@host.docker.internal>
2022-04-22 16:32:53 -0300

    Stop DDOS script

--- install/stopDDOSAtackIps.php
+++ install/stopDDOSAtackIps.php
@@ -1,5 +1,7 @@
 <?php
 
+$apacheAccessLogFile = '/var/log/apache2/access.log';
+
 function tailShell($filepath, $lines = 1) {
     ob_start();
     passthru('tail -' . $lines . ' ' . escapeshellarg($filepath));
@@ -8,8 +10,8 @@ function tailShell($filepath, $lines = 1) {
 
 while (1) {
 
-    $lines = tailShell('/var/log/apache2/access.log', 1000);
-
+    $lines = tailShell($apacheAccessLogFile, 1000);
+    file_put_contents($apacheAccessLogFile,'');
     $ips = array();
 
     foreach ($lines as $line) {
