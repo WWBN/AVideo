e886441b9b7ee57d208a7bff250664daee5c4688
DanieL <DanieL@host.docker.internal>
2022-04-22 16:31:33 -0300

    Stop DDOS script

--- install/stopDDOSAtackIps.php
+++ install/stopDDOSAtackIps.php
@@ -2,38 +2,41 @@
 
 function tailShell($filepath, $lines = 1) {
     ob_start();
-    passthru('tail -'  . $lines . ' ' . escapeshellarg($filepath));
+    passthru('tail -' . $lines . ' ' . escapeshellarg($filepath));
     return preg_split("/\r\n|\n|\r/", trim(ob_get_clean()));
 }
 
-$lines = tailShell('/var/log/apache2/access.log', 1000);
+while (1) {
 
-$ips = array();
+    $lines = tailShell('/var/log/apache2/access.log', 1000);
 
-foreach($lines as $line){
-    preg_match('/^([0-9.]+).*X11; Linux/i', $line, $matches);
-    if(!empty($matches[1])){
-        $ip = trim($matches[1]);
-        if(!in_array($ip, $ips)){
-            $ips[] = $ip;
+    $ips = array();
+
+    foreach ($lines as $line) {
+        preg_match('/^([0-9.]+).*X11; Linux/i', $line, $matches);
+        if (!empty($matches[1])) {
+            $ip = trim($matches[1]);
+            if (!in_array($ip, $ips)) {
+                $ips[] = $ip;
+            }
         }
-
     }
-}
 
-$total = count($ips);
-$newRules = array();
+    $total = count($ips);
+    $newRules = array();
 
-foreach($ips as $key => $ip){
-    $cmd = 'sudo ufw insert 1 deny from '.$ip.'  to any'.PHP_EOL;
-    echo "{$key}/{$total} ".$cmd;
-    $output = null;
-    exec($cmd.' 2>&1', $output, $return_var);
-    echo json_encode($output).PHP_EOL;
-    if($output[0]==='Rule inserted'){
-        $newRules[] = $ip;
+    foreach ($ips as $key => $ip) {
+        $cmd = 'sudo ufw insert 1 deny from ' . $ip . '  to any' . PHP_EOL;
+        echo "{$key}/{$total} " . $cmd;
+        $output = null;
+        exec($cmd . ' 2>&1', $output, $return_var);
+        echo json_encode($output) . PHP_EOL;
+        if ($output[0] === 'Rule inserted') {
+            $newRules[] = $ip;
+        }
     }
-}
 
-echo PHP_EOL.'Found '.$total.PHP_EOL;
-echo PHP_EOL.count($newRules).' New IPs added: '. implode(', ', $newRules).PHP_EOL;
+    echo PHP_EOL . 'Found ' . $total . PHP_EOL;
+    echo PHP_EOL . count($newRules) . ' New IPs added: ' . implode(', ', $newRules) . PHP_EOL;
+    sleep(5);
+}
