1f2c3c31aa7f565a711a06aaabfa63b67b089266
DanieL <DanieL@host.docker.internal>
2022-04-23 18:24:27 -0300

    Improve stop DDOS protection

--- install/stopDDOSAtackIps.php
+++ install/stopDDOSAtackIps.php
@@ -30,6 +30,13 @@ while (1) {
                 $ips[] = $ip;
             }
         }
+        preg_match('/^([0-9.]+).*Windows NT 6/i', $line, $matches);
+        if (!empty($matches[1])) {
+            $ip = trim($matches[1]);
+            if (!in_array($ip, $ips)) {
+                $ips[] = $ip;
+            }
+        }
     }
 
     $total = count($ips);
