7164256b5fd93c4987941ec25528f6c3092ff527
DanieL <DanieL@host.docker.internal>
2022-05-15 17:31:24 -0300

    Improve DDOS handle

--- install/stopDDOSAtackIps.php
+++ install/stopDDOSAtackIps.php
@@ -14,7 +14,7 @@ $ipsProcessed = array();
 $mySQLIsStopped = 0;
 while (1) {
 
-    $lines = tailShell($apacheAccessLogFile, 1000);
+    $lines = tailShell($apacheAccessLogFile, 500);
     //file_put_contents($apacheAccessLogFile,'');
 
     foreach ($lines as $line) {
