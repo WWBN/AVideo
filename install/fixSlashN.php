7e9fc5cdf39bb3e9fc503545c2822a3d01121f19
Daniel <developer@youphptube.com>
2022-05-16 12:41:00 -0300

    https://github.com/WWBN/AVideo-Storage/issues/30

--- install/fixSlashN.php
+++ install/fixSlashN.php
@@ -11,10 +11,15 @@ $videos = video::getAllVideosLight("", false, true);
 $count = 0;
 foreach ($videos as $value) {
     $count++;
-    $newDescription = str_replace('\n', PHP_EOL, $value['description']);
+    //$newDescription = str_replace('\n', PHP_EOL, $value['description']);
+    $newDescription =  preg_replace('/(\\\\)+n/', PHP_EOL, $value['description']);
+    $newDescription =  preg_replace('/(\\\\)+/', PHP_EOL, $newDescription);
+    $newTitle =  preg_replace('/(\\\\)+/', '', $value['title']);
+
     if($newDescription !== $value['description']){
         echo "Change ($count) [{$value['id']}]{$value['title']} ******".PHP_EOL;
         $video = new Video('','',$value['id']);
+        $video->setTitle($newTitle);
         $video->setDescription($newDescription);
         $video->save(false, true);
     }else{
