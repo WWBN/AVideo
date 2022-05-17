a04a6c9b89ccf8c1ea47471699713bd3baaed928
DanieL <DanieL@host.docker.internal>
2022-05-17 16:45:44 -0300

    Debug

--- plugin/Live/Objects/LiveTransmitionHistory.php
+++ plugin/Live/Objects/LiveTransmitionHistory.php
@@ -623,6 +623,7 @@ class LiveTransmitionHistory extends ObjectYPT {
                 $total = count($fullData);
                 foreach ($fullData as $row) {
                     if ($total < 10 && strtotime($row['modified']) < strtotime('-1 hour')) {
+                        /*
                         // check if the m3u8 file still exists
                         $m3u8 = Live::getM3U8File($row['key']);
                         $isURL200 = isValidM3U8Link($m3u8);
@@ -630,11 +631,12 @@ class LiveTransmitionHistory extends ObjectYPT {
                             self::finishFromTransmitionHistoryId($row['id']);
                             //var_dump($isURL200, $m3u8, $row);exit;
                             continue;
-                        } else {
-                            // update it to make sure the modified date is updated
-                            $lth = new LiveTransmitionHistory($row['id']);
-                            $lth->save();
-                        }
+                        } 
+                         * 
+                         */
+                        // update it to make sure the modified date is updated
+                        $lth = new LiveTransmitionHistory($row['id']);
+                        $lth->save();
                     }
                     $log = LiveTransmitionHistoryLog::getAllFromHistory($row['id']);
                     $row['totalUsers'] = count($log);
