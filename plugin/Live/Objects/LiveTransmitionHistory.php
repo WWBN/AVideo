f9a685aeaa8b8fdc18cee6abc663bf7ef324ef06
DanieL <DanieL@host.docker.internal>
2022-05-17 16:39:04 -0300

    Debug

--- plugin/Live/Objects/LiveTransmitionHistory.php
+++ plugin/Live/Objects/LiveTransmitionHistory.php
@@ -601,6 +601,7 @@ class LiveTransmitionHistory extends ObjectYPT {
         }
 
         $sql .= " ORDER BY created DESC LIMIT {$count}";
+        //echo $sql;exit;
         $res = sqlDAL::readSql($sql, $formats, $values);
         if ($count == 1) {
             $data = sqlDAL::fetchAssoc($res);
