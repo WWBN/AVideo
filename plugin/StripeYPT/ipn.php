662dc8828c49ac54f0a348250203af4b8847e04b
DanieL <DanieL@host.docker.internal>
2022-05-25 10:23:05 -0300

    Can check multiple webhooks

--- plugin/StripeYPT/ipn.php
+++ plugin/StripeYPT/ipn.php
@@ -16,6 +16,32 @@ $stripeObject = AVideoPlugin::getObjectData("StripeYPT");
 if (empty($stripe)) {
     die("Stripe Plugin Disabled");
 }
+
+function checkWebhook($payload, $sig_header, $endpoint_secret) {
+    
+    $parts = explode(',',$endpoint_secret);
+    
+    foreach ($parts as $secret){
+        $secret = trim($secret);
+        if(empty($secret)){
+            continue;
+        }
+        try {
+            $event = \Stripe\Webhook::constructEvent(
+                            $payload, $sig_header, $endpoint_secret
+            );
+            return $event;
+        } catch (\UnexpectedValueException $e) {
+            // Invalid payload
+            _error_log("Stripe IPN Invalid payload {$secret}");
+        }
+    }
+    _error_log("Stripe IPN END Invalid payload {$endpoint_secret}");
+    http_response_code(400); // PHP 5.4 or greater
+    exit();
+    
+}
+
 $stripe->start();
 
 // You can find your endpoint's secret in your webhook settings
@@ -28,13 +54,12 @@ $event = null;
 $payloadObj = json_decode($payload);
 
 //$payloadObj = object_to_array($payloadObj);
-
 //_error_log("StripeIPN: WEBHOOK: ".json_encode($webhook));
 //_error_log("StripeIPN: payload ".json_encode($payloadObj));
 //_error_log("StripeIPN: sig_header ".json_encode($sig_header));
 
 $whitelist = array('invoice.payment_succeeded', 'charge.succeeded');
-if(!in_array($payloadObj->type, $whitelist)){
+if (!in_array($payloadObj->type, $whitelist)) {
     //_error_log("StripeIPN: type ignored " . $payloadObj->type );
     return '';
 }
@@ -42,32 +67,28 @@ _error_log("StripeIPN Start");
 //_error_log("StripeIPN: ({$sig_header}) ({$endpoint_secret}} payload type: " . $payloadObj->type );
 
 try {
-    $event = \Stripe\Webhook::constructEvent(
-                    $payload, $sig_header, $endpoint_secret
-    );
+    $event = checkWebhook($payload, $sig_header, $endpoint_secret);
     //_error_log("Stripe IPN Valid payload and signature ". json_encode($payloadObj));
     if (StripeYPT::isSubscriptionPayment($payloadObj)) {
-        _error_log("StripeIPN: ** Subscription **" );
+        _error_log("StripeIPN: ** Subscription **");
         // subscription
         $stripe->processSubscriptionIPN($payloadObj);
     } else if (StripeYPT::isSinglePayment($payloadObj)) {
-        _error_log("StripeIPN: ** SinglePayment **" );
+        _error_log("StripeIPN: ** SinglePayment **");
         $stripe->processSinglePaymentIPN($payloadObj);
-    }else{
+    } else {
         //_error_log("StripeIPN: something went wrong: {$payload}" , AVideoLog::$ERROR );
     }
-
-    
 } catch (\UnexpectedValueException $e) {
     // Invalid payload
-    _error_log("Stripe IPN Invalid payload ");
+    _error_log("Stripe IPN Invalid payload: ".$e->getMessage());
     http_response_code(400); // PHP 5.4 or greater
     exit();
 } catch (\Stripe\Error\SignatureVerification $e) {
     // Invalid signature
     _error_log("Stripe IPN sig_header [$sig_header]");
     _error_log("Stripe IPN endpoint_secret [$endpoint_secret]");
-    _error_log("Stripe IPN Invalid signature ");
+    _error_log("Stripe IPN Invalid signature: ".$e->getMessage());
     http_response_code(400); // PHP 5.4 or greater
     exit();
 }
