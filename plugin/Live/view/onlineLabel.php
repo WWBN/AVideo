f2e3ad63e8e846c81cad154e89dace6d06218a80
DanieL <DanieL@host.docker.internal>
2022-05-27 11:12:38 -0300

    Fix

--- plugin/Live/view/onlineLabel.php
+++ plugin/Live/view/onlineLabel.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 if (empty($streamName)) {
     $live = isLive();
     $streamName = $live['key'];
@@ -6,7 +6,7 @@ if (empty($streamName)) {
 } else {
     $live_servers_id = Live::getCurrentLiveServersId();
 }
-if(empty($streamName)){
+if (empty($streamName)) {
     //return false;
 }
 $liveViewStatusID = str_replace('-', '_', "liveViewStatusID_{$streamName}_{$live_servers_id}");
@@ -24,7 +24,160 @@ if (isLiveLink() || Live::isLiveAndIsReadyFromKey($streamName, $live_servers_id,
 ?>
 
 <script>
+    function isInLive(json) {
+        selector1 = '#liveViewStatusID_' + json.key + '_' + json.live_servers_id;
+        selector2 = '.liveViewStatusClass_' + json.key + '_' + json.live_servers_id;
+        selector3 = '#liveViewStatusID_' + json.cleanKey + '_' + json.live_servers_id;
+        selector4 = '.liveViewStatusClass_' + json.cleanKey + '_' + json.live_servers_id;
+        //console.log('isInLive 1', json);
+        //console.log('isInLive 2', selector1, selector2, selector3, selector4);
+        var _isInLive = $(selector1).length || $(selector2).length || $(selector3).length || $(selector4).length;
+        //console.log('isInLive 3', $(selector1).length, $(selector2).length, $(selector3).length, $(selector4).length, _isInLive);
+        return _isInLive;
+    }
 
+    function socketLiveONCallback(json) {
+        //console.log('socketLiveONCallback processLiveStats', json);
+        processLiveStats(json.stats);
+        var selector = '.live_' + json.live_servers_id + "_" + json.key;
+        $(selector).slideDown();
+
+        if (typeof onlineLabelOnline == 'function') {
+            selector = '#liveViewStatusID_' + json.key + '_' + json.live_servers_id;
+            onlineLabelOnline(selector);
+            selector = '.liveViewStatusClass_' + json.key + '_' + json.live_servers_id;
+            onlineLabelOnline(selector);
+            selector = '.liveViewStatusClass_' + json.cleanKey;
+            ////console.log('socketLiveOFFCallback 3', selector);
+            onlineLabelOnline(selector);
+        }
+
+        // update the chat if the history changes
+        var IframeClass = ".yptchat2IframeClass_" + json.key + "_" + json.live_servers_id;
+        if ($(IframeClass).length) {
+            var src = $(IframeClass).attr('src');
+            if (src) {
+                avideoToast('Loading new chat');
+                var newSRC = addGetParam(src, 'live_transmitions_history_id', json.live_transmitions_history_id);
+                $(IframeClass).attr('src', newSRC);
+            }
+        }
+        if (isInLive(json)) {
+            playerPlay();
+            showImage('prerollPoster', json.cleanKey);
+        }
+    }
+    function socketLiveOFFCallback(json) {
+        //console.log('socketLiveOFFCallback', json);
+        var selector = '.live_' + json.live_servers_id + "_" + json.key;
+        selector += ', .liveVideo_live_' + json.live_servers_id + "_" + json.key;
+        selector += ', .live_' + json.key;
+        ////console.log('socketLiveOFFCallback 1', selector);
+        $(selector).slideUp("fast", function () {
+            $(this).remove();
+        });
+        if (typeof onlineLabelOffline == 'function') {
+            selector = '#liveViewStatusID_' + json.key + '_' + json.live_servers_id;
+            ////console.log('socketLiveOFFCallback 2', selector);
+            onlineLabelOffline(selector);
+            selector = '.liveViewStatusClass_' + json.key + '_' + json.live_servers_id;
+            ////console.log('socketLiveOFFCallback 3', selector);
+            onlineLabelOffline(selector);
+            selector = '.liveViewStatusClass_' + json.cleanKey;
+            ////console.log('socketLiveOFFCallback 3', selector);
+            onlineLabelOffline(selector);
+        }
+        setTimeout(function () {
+            //console.log('socketLiveOFFCallback processLiveStats');
+            processLiveStats(json.stats);
+            setTimeout(function () {
+                hideExtraVideosIfEmpty();
+            }, 500);
+        }, 500);
+
+        if (isInLive(json)) {
+            showImage('postrollPoster', json.cleanKey);
+        }
+    }
+    var prerollPosterAlreadyPlayed = false;
+    function showImage(type, key) {
+        if (typeof closeLiveImageRoll == 'function') {
+            closeLiveImageRoll();
+        }
+        $('.' + type).remove();
+        var img = false;
+        console.log('showImage', type, key, player.paused());
+        eval('prerollPoster = prerollPoster_' + key);
+        eval('postrollPoster = postrollPoster_' + key);
+        eval('liveImgCloseTimeInSecondsPreroll = liveImgCloseTimeInSecondsPreroll_' + key);
+        eval('liveImgTimeInSecondsPreroll = liveImgTimeInSecondsPreroll_' + key);
+        eval('liveImgCloseTimeInSecondsPostroll = liveImgCloseTimeInSecondsPostroll_' + key);
+        eval('liveImgTimeInSecondsPostroll = liveImgTimeInSecondsPostroll_' + key);
+        var liveImgTimeInSeconds = 30;
+        var liveImgCloseTimeInSeconds = 30;
+        if (type == 'prerollPoster' && prerollPoster) {
+            if (prerollPosterAlreadyPlayed) {
+                console.log('showImage prerollPosterAlreadyPlayed');
+                return false;
+            }
+            prerollPosterAlreadyPlayed = true;
+            if (player.paused()) {
+                setTimeout(function () {
+                    showImage(type, key);
+                }, 1000);
+                return false;
+            }
+            liveImgTimeInSeconds = liveImgTimeInSecondsPreroll;
+            liveImgCloseTimeInSeconds = liveImgCloseTimeInSecondsPreroll;
+            img = prerollPoster;
+        } else if (type == 'postrollPoster' && postrollPoster) {
+            liveImgTimeInSeconds = liveImgTimeInSecondsPostroll;
+            liveImgCloseTimeInSeconds = liveImgCloseTimeInSecondsPostroll;
+            img = postrollPoster;
+        }
+        console.log('showImage Poster', type, img, key);
+        if (img) {
+
+            var _liveImageBGTemplate = liveImageBGTemplate.replace('{liveImgCloseTimeInSeconds}', liveImgCloseTimeInSeconds);
+            var _liveImageBGTemplate = _liveImageBGTemplate.replace('{liveImgTimeInSeconds}', liveImgTimeInSeconds);
+            var _liveImageBGTemplate = _liveImageBGTemplate.replace('{src}', img);
+            _liveImageBGTemplate = _liveImageBGTemplate.replace(/\{class\}/g, type);
+
+            $(_liveImageBGTemplate).appendTo("#mainVideo");
+        }
+
+        //console.log('prerollPoster', prerollPoster);
+        //console.log('postrollPoster', postrollPoster);
+        //console.log('liveImgTimeInSeconds', liveImgTimeInSeconds);
+        //console.log('liveImgCloseTimeInSeconds', liveImgCloseTimeInSeconds);
+    }
+
+    function hideExtraVideosIfEmpty() {
+        $('#liveScheduleVideos .extraVideos').each(function (index, currentElement) {
+            var somethingIsVisible = false;
+            $(this).children('div').each(function (index2, currentElement2) {
+                if ($(this).is(":visible")) {
+                    somethingIsVisible = true;
+                    return false;
+                }
+            });
+            if (!somethingIsVisible) {
+                $('#liveScheduleVideos').slideUp();
+            }
+        });
+        $('#liveVideos .extraVideos').each(function (index, currentElement) {
+            var somethingIsVisible = false;
+            $(this).children('div').each(function (index2, currentElement2) {
+                if ($(this).is(":visible")) {
+                    somethingIsVisible = true;
+                    return false;
+                }
+            });
+            if (!somethingIsVisible) {
+                $('#liveVideos').slideUp();
+            }
+        });
+    }
     function isOfflineVideo() {
 <?php
 if (isMobile()) {
@@ -32,7 +185,7 @@ if (isMobile()) {
             return !$('#<?php echo $liveViewStatusID; ?>').hasClass('isOnline');
     <?php
 } else {
-        ?>
+    ?>
             if (player.readyState()) {
                 if (typeof player.tech_ !== 'undefined') {
                     var uri = player.tech_.hls.selectPlaylist().uri;
@@ -52,7 +205,7 @@ if (isMobile()) {
                 console.log("isOfflineVideo player.readyState not ready", player.readyState());
             }
     <?php
-    }
+}
 ?>
         return true;
     }
@@ -113,7 +266,7 @@ if (isMobile()) {
                 player.trigger('loadstart');
                 player.posterImage.show();
                 player.bigPlayButton.show();
-                if(!isWebRTC()){
+                if (!isWebRTC()) {
                     player.currentTime(0);
                 }
                 player.on('play', function () {
