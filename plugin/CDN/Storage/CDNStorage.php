65f87c8b4ec63f4ff2eb879a2ee8ad7baba1a688
DanieL <DanieL@host.docker.internal>
2022-06-01 12:01:22 -0300

    Socket new version

--- plugin/CDN/Storage/CDNStorage.php
+++ plugin/CDN/Storage/CDNStorage.php
@@ -848,7 +848,7 @@ class CDNStorage {
         $modified = filemtime($file);
         if (time() - $modified > 3000) {
             // if is laonger than 5 min say it is not moving
-            _error_log('CDNStorage isMoving is taking too long to finish, check your connection speed or FTP errors', AVideoLog::$WARNING);
+            _error_log('CDNStorage isMoving is taking too long to finish, check your connection speed or FTP errors '.$file, AVideoLog::$WARNING);
             return false;
         }
 
