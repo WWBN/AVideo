12b57fb24e47bca3e3bc70c30c8723c9fd9d16d1
DanieL <DanieL@host.docker.internal>
2022-06-03 10:37:03 -0300

    debug

--- plugin/Live/Live.php
+++ plugin/Live/Live.php
@@ -272,7 +272,7 @@ class Live extends PluginAbstract {
         foreach ($rows as $value) {
             unset($_REQUEST['playlists_id_live']);
             // if key is from schedule, skipp it
-            if (!empty($value['key']) && strtotime($value['modified']) > strtotime('+10 minures')) {
+            if (!empty($value['key']) && strtotime($value['modified']) > strtotime('-10 minures')) {
                 $isLiveAndIsReadyFromKey = Live::isLiveAndIsReadyFromKey($value['key'], $value['live_servers_id']);
                 $isStatsAccessible = self::isStatsAccessible($value['live_servers_id']);
                 if (empty($isLiveAndIsReadyFromKey) && $isStatsAccessible) {
