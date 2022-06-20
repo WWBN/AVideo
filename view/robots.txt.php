2b5ed1fa8a3659fea1f62c04d8c41d915dd71adb
DanieL <DanieL@host.docker.internal>
2022-06-20 11:21:27 -0300

    Improve SEO descriptions and title

--- view/robots.txt.php
+++ view/robots.txt.php
@@ -4,7 +4,8 @@ if (!isset($global['systemRootPath'])) {
     require_once '../videos/configuration.php';
 }
 
-header('Content-Type: application/json');
+//header('Content-Type: application/json');
+header("Content-Type: text/plain");
 ?>
 Sitemap: <?php echo $global['webSiteRootURL']; ?>sitemap.xml
 User-Agent: *
