316bf7e10eba59868bd43445558ddf786de969c4
Daniel <developer@youphptube.com>
2022-06-24 20:17:52 -0300

    ddos

--- view/ddosCaptcha.php
+++ view/ddosCaptcha.php
@@ -4,12 +4,12 @@ function captcha($largura,$altura,$quantidade_letras){
     $imagem = imagecreate($largura,$altura); // define a largura e a altura da imagem
     $fonte = "../objects/monof55.ttf"; //voce deve ter essa ou outra fonte de sua preferencia em sua pasta
     $preto  = imagecolorallocate($imagem,0,0,0); // define a cor preta
-    $branco = imagecolorallocate($imagem,255,255,255); // define a cor branca
 
     $palavra = substr(str_shuffle("AaBbCcDdEeFfGgHhIiJjKkLlMmNnPpQqRrSsTtUuVvYyXxWwZz23456789"),0,($quantidade_letras));
     $_SESSION["palavra"] = $palavra; // atribui para a sessao a palavra gerada
     for($i = 1; $i <= $quantidade_letras; $i++){
-        $tamanho_fonte = rand(14,20);
+        $branco = imagecolorallocate($imagem,rand(200,255),rand(200,255),rand(200,255)); // define a cor branca
+        $tamanho_fonte = rand(18,28);
         imagettftext($imagem,$tamanho_fonte,rand(-25,25),($tamanho_fonte*$i),
         ($tamanho_fonte + 10),$branco,$fonte,substr($palavra,($i-1),1));
         // atribui as letras a imagem
