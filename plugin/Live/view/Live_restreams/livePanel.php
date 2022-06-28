6439e0cfe3b545dc927a3006d8fe4b6cc5bb603a
Daniel <developer@youphptube.com>
2022-06-28 17:34:36 -0300

    update

--- plugin/Live/view/Live_restreams/livePanel.php
+++ plugin/Live/view/Live_restreams/livePanel.php
@@ -135,14 +135,14 @@ if (!User::canStream()) {
                     render: function (data, type, row) {
                         if(!empty(data.parameters)){
                             var json = JSON.parse(data.parameters);
-                            if(!empty(json.facebook)){
-                                onclick = "openWindowWithPost('https://restream.ypt.me/get.php', 'theRestreamerGetPopUp', {title: $('#title').val(), description: $('#description').val(), parameters: data.parameters}, 'directories=no,titlebar=no,toolbar=no,location=no,status=no,menubar=no,resizable=no,height=600,width=800');";
-                                return '<a href="#" onclick="'+onclick+'">'+data+'</a>';
-                            }else if(!empty(json.youtube)){
-                                
-                            }else if(!empty(json.twitch)){
-                                
-                            }
+if(!empty(json['restream.ypt.me'])){
+    var obj = json['restream.ypt.me'];
+
+    
+    onclick = "openWindowWithPost('https://restream.ypt.me/get.php', 'theRestreamerGetPopUp', {title: $('#title').val(), description: $('#description').val(), parameters: "+JSON.stringify(obj)+"}, 'directories=no,titlebar=no,toolbar=no,location=no,status=no,menubar=no,resizable=no,height=600,width=800');";
+    return '<a href="#" onclick="'+onclick+'">'+data+'</a>';
+}
+
                         }
                         return data;
                     } 
