fd81dec82ee0ca527f77f51e101ba51e0a28e027
Daniel <developer@youphptube.com>
2022-06-29 13:39:10 -0300

    improve restream

---
+++ plugin/Live/view/Live_restreams/getLiveKey.json.php
@@ -0,0 +1,47 @@
+<?php
+require_once '../../../../videos/configuration.php';
+if(!AVideoPlugin::isEnabledByName('Live')){
+    forbiddenPage('Live plugin is disabled');
+}
+require_once $global['systemRootPath'] . 'plugin/Live/Objects/Live_restreams.php';
+header('Content-Type: application/json');
+
+if(empty($_REQUEST['live_restreams_id'])){
+    forbiddenPage('live_restreams_id cannot be empty');
+}
+
+$Live_restreams = new Live_restreams($_REQUEST['live_restreams_id']);
+
+if(empty($Live_restreams->getName())){
+    forbiddenPage('Name not found for live_restreams_id='.$_REQUEST['live_restreams_id']);
+}
+
+if($Live_restreams->getUsers_id() !==User::getId() && !User::isAdmin() && !isCommandLineInterface()){
+    forbiddenPage('You have no access to this restream');
+}
+
+$lt = LiveTransmition::getFromDbByUser($Live_restreams->getUsers_id());
+
+$url = 'https://restream.ypt.me/get.php';
+$array = array(
+    'title'=> $lt['title'],
+    'description'=> $lt['description'],
+    'parameters64'=> base64_encode($Live_restreams->getParameters()),
+);
+
+if(!empty($_REQUEST['live_schedule_id'])){
+    $ls = new live_schedule($_REQUEST['live_schedule_id']);
+
+    if(!empty($ls->getTitle())){
+        $array['title'] = $ls->getTitle();
+    }
+    if(!empty($ls->getDescription())){
+        $array['description'] = $ls->getDescription();
+    }
+}
+
+$response = postVariables($url, $array);
+
+echo $response;
+
+?>
