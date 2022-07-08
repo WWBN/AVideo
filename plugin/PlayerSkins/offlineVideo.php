3f02a0ef2a2f9b401ae8067b0c3d8ab867b1b57d
DanieL <DanieL@host.docker.internal>
2022-07-08 14:30:50 -0300

    Improve offline video

--- plugin/PlayerSkins/offlineVideo.php
+++ plugin/PlayerSkins/offlineVideo.php
@@ -70,7 +70,7 @@ $sources = getVideosURL_V2($video['filename']);
                                     <button class="btn btn-danger"  onclick='_deleteOfflineVideo(<?php echo json_encode($matches[1]); ?>);'>
                                         <i class="fas fa-trash"></i> <?php echo __('Delete'); ?>
                                     </button>
-                                    <button class="btn btn-warning" onclick='_downloadOfflineVideo(<?php echo json_encode($value['url']); ?>, <?php echo json_encode($matches[1]); ?>, "#downloadProgressBar<?php echo json_encode($videos_id); ?>");'>
+                                    <button class="btn btn-warning" onclick='_downloadOfflineVideo(<?php echo json_encode($value['url']); ?>, <?php echo json_encode($matches[1]); ?>, "#downloadProgressBar<?php echo $videos_id; ?>_<?php echo $matches[1]; ?>");'>
                                         <i class="fas fa-download"></i> <?php echo __('Download'); ?>
                                     </button>
                                     <button class="btn btn-success" onclick='_updateVideo(<?php echo json_encode($videos_id); ?>);'>
@@ -82,7 +82,7 @@ $sources = getVideosURL_V2($video['filename']);
                                 </div>
                                 <div class="panel-footer">
                                     <?php echo humanFileSize(filesize($value['path'])); ?>
-                                    <div class="progress" id="downloadProgressBar<?php echo json_encode($videos_id); ?>">
+                                    <div class="progress" id="downloadProgressBar<?php echo json_encode($videos_id); ?>_<?php echo $matches[1]; ?>">
                                         <div class="progress-bar progress-bar-striped active" role="progressbar"
                                              aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width:0;">
                                             0%
