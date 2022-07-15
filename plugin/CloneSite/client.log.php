c994c58c548c7d158c879735091f5cf46ee8b1e1
DanieL <DanieL@host.docker.internal>
2022-07-15 17:06:20 -0300

    Fix for the new version

--- plugin/CloneSite/client.log.php
+++ plugin/CloneSite/client.log.php
@@ -4,6 +4,8 @@ $doNotConnectDatabaseIncludeConfig = 1;
 $config = '../../videos/configuration.php';
 require_once $config;
 session_write_close();
+_mysql_close();
+();
 header('Content-Type: plain/text');
 if(User::isAdmin()){
     include $global['systemRootPath'] . 'videos/cache/clones/client.log';
