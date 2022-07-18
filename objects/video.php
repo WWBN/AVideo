98764a57e4a1b313bfc20a168e8f1717624961fc
DanieL <DanieL@host.docker.internal>
2022-07-18 20:48:29 -0300

    https://github.com/WWBN/AVideo/issues/6897

--- objects/video.php
+++ objects/video.php
@@ -5190,7 +5190,7 @@ if (!class_exists('Video')) {
                 $video = Video::getVideoLight($videos_id);
             }
             $images = Video::getImageFromFilename($video['filename'], $video['type']);
-            //var_dump($videos_id, $images->poster, $images);
+            //var_dump($videos_id, $video, $images->poster, $images);
             $img = getVideoImagewithHoverAnimation($images->poster, $images->thumbsGif, $video['title']);
             $program = AVideoPlugin::loadPluginIfEnabled('PlayLists');
             $isserie = Video::isSerie($videos_id);
