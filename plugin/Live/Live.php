e258d0778a6e34b1da0f01f56273154e2315c741
DanieL <DanieL@host.docker.internal>
2022-07-21 20:08:31 -0300

    fix

--- plugin/Live/Live.php
+++ plugin/Live/Live.php
@@ -1875,12 +1875,12 @@ Click <a href=\"{link}\">here</a> to join our live.";
         //$force_recreate = true;
         if (empty($force_recreate)) {
             if (!empty($_getStats[$live_servers_id][$_REQUEST['name']]) && is_object($_getStats[$live_servers_id][$_REQUEST['name']])) {
-                _error_log("Live::_getStats cached result 1 {$_REQUEST['name']} ");
+                //_error_log("Live::_getStats cached result 1 {$_REQUEST['name']} ");
                 return $_getStats[$live_servers_id][$_REQUEST['name']];
             }
             $result = ObjectYPT::getCache($cacheName, maxLifetime() + 60, true);
             if (!empty($result)) {
-                _error_log("Live::_getStats cached result 2 {$_REQUEST['name']} {$cacheName}");
+                //_error_log("Live::_getStats cached result 2 {$_REQUEST['name']} {$cacheName}");
                 return _json_decode($result);
             }
         }
