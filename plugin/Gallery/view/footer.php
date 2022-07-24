becf8745057d8bdfd14431a7458086ed4fb6a07a
DanieL <DanieL@host.docker.internal>
2022-07-23 22:01:01 -0300

    Fix

--- plugin/Gallery/view/footer.php
+++ plugin/Gallery/view/footer.php
@@ -3,12 +3,14 @@
     function showTrailer(iframe) {
         avideoModalIframe(iframe);
     }
-    $('#TrailerModal').on('hidden.bs.modal', function () {
-        $('#TrailerModal iframe').attr('src', '');
-    });
-    $('#bigVideoCarousel').bind('slide.bs.carousel', function (e) {
-        setTimeout(function(){
-            lazyImage();
-        },500);
+    $(document).ready(function () {
+        $('#TrailerModal').on('hidden.bs.modal', function () {
+            $('#TrailerModal iframe').attr('src', '');
+        });
+        $('#bigVideoCarousel').bind('slide.bs.carousel', function (e) {
+            setTimeout(function(){
+                lazyImage();
+            },500);
+        });
     });
 </script>
