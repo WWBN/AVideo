9acaa3f42178171578d4865533900fed761f3c36
DanieL <DanieL@host.docker.internal>
2022-07-26 11:53:39 -0300

    Fix

--- plugin/Live/on_record_done.php
+++ plugin/Live/on_record_done.php
@@ -1,12 +1,15 @@
 <?php
 
 require_once '../../videos/configuration.php';
+_error_log("on_record_done SendRecordedToEncoder {$global['webSiteRootURL']} line=" . __LINE__);
 require_once '../AVideoPlugin.php';
+_error_log("on_record_done SendRecordedToEncoder {$global['webSiteRootURL']} line=" . __LINE__);
 $global['bypassSameDomainCheck'] = 1;
 _error_log("on_record_done start");
 $plugin = AVideoPlugin::loadPluginIfEnabled('SendRecordedToEncoder');
+_error_log("on_record_done SendRecordedToEncoder {$global['webSiteRootURL']} line=" . __LINE__);
 if ($plugin) {
-    _error_log("on_record_done SendRecordedToEncoder");
+    _error_log("on_record_done SendRecordedToEncoder {$global['webSiteRootURL']} line=" . __LINE__);
     $plugin->on_record_done();
 }
 _error_log("on_record_done end");
