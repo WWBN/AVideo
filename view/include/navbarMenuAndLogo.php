d40c27e94ea13f50acd48f46e69d728ae15103bb
DanieL <DanieL@host.docker.internal>
2022-07-26 14:05:10 -0300

    Fix

--- view/include/navbarMenuAndLogo.php
+++ view/include/navbarMenuAndLogo.php
@@ -19,7 +19,13 @@
         <li style="width: 100%; text-align: center;">
             <a class="navbar-brand" id="mainNavbarLogo" href="<?php echo empty($advancedCustom->logoMenuBarURL) ? $global['webSiteRootURL'] : $advancedCustom->logoMenuBarURL; ?>" >
                 <img src="<?php echo getURL($config->getLogo()); ?>" alt="<?php echo $config->getWebSiteTitle(); ?>" class="img-responsive "  width="250" height="70">
+                <?php
+                if(isFirstPage()){
+                ?>
                 <h1 class="hidden"><?php echo __('Site:'), ' ',$config->getWebSiteTitle(); ?></h1>
+                <?php 
+                }
+                ?>
             </a>
         </li>
         <?php
