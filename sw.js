052d1b9503838993d56c53e67e02f6c66ceb2f5a
Daniel <developer@youphptube.com>
2022-07-30 11:12:35 -0300

    remove browser cache

--- sw.js
+++ sw.js
@@ -201,7 +201,7 @@ async function getStrategy(args) {
 
 }
 
-workbox.routing.registerRoute(/.*/, getStrategy);
+//workbox.routing.registerRoute(/.*/, getStrategy);
 
 self.addEventListener('install', event => {
     //console.log('sw.js 1', event);
