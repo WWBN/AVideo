4b3c6365e45c789c2efc7f23507253778ce70daf
DanieL <DanieL@host.docker.internal>
2022-08-08 14:56:20 -0300

    Fix save usergroup

--- objects/userGroupSave.json.php
+++ objects/userGroupSave.json.php
@@ -5,30 +5,28 @@ global $global, $config;
 if (!isset($global['systemRootPath'])) {
     require_once '../videos/configuration.php';
 }
+
+$obj = new stdClass();
+$obj->error = true;
+$obj->msg = '';
+
 require_once $global['systemRootPath'] . 'objects/userGroups.php';
-if (!User::canUpload() || empty($_POST['id'])) {
-    die('{"error":"' . __("Permission denied") . '"}');
-}
 if (!is_array($_POST['id'])) {
     $_POST['id'] = [$_POST['id']];
 }
+$obj->videos_ids = $_POST['id'];
+$obj->users_groups_id = $_POST['users_groups_id'];
+$obj->add = $_POST['add'];
 
-require_once 'video.php';
-$id = 0;
-foreach ($_POST['id'] as $videos_id) {
-    $obj = new Video("", "", $videos_id);
-    if (empty($obj)) {
-        die("Object not found");
+foreach ($obj->videos_ids as $videos_id) {
+    if (Video::canEdit($videos_id)) {
+        forbiddenPage('You can not Manage This Video');
     }
-    if (!$obj->userCanManageVideo()) {
-        $obj->msg = __("You can not Manage This Video");
-        die(json_encode($obj));
-    }
-    if (!empty($_POST['add'])) {
-        UserGroups::addVideoGroups($videos_id, $_POST['users_groups_id']);
+    if (!empty($obj->add)) {
+        UserGroups::addVideoGroups($videos_id, $obj->users_groups_id);
     } else {
-        UserGroups::deleteVideoGroups($videos_id, $_POST['users_groups_id']);
+        UserGroups::deleteVideoGroups($videos_id, $obj->users_groups_id);
     }
     $resp = true;
 }
-echo '{"status":"' . !empty($resp) . '"}';
+die(json_encode($obj));
