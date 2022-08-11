7607d3109f853fe76aa5794bb40a4c4d8538be8d
DanieL <DanieL@host.docker.internal>
2022-08-11 17:55:09 -0300

    Improve

--- plugin/Live/view/Live_restreams_logs/add.json.php
+++ plugin/Live/view/Live_restreams_logs/add.json.php
@@ -40,7 +40,7 @@ if(!User::isAdmin()){
 $o = new Live_restreams_logs(@$_POST['id']);
 $o->setRestreamer($_POST['restreamerURL']);
 $o->setM3u8($_POST['m3u8']);
-$o->setDestinations(json_encode($_POST['restreamsDestinations'])0;
+$o->setDestinations(json_encode($_POST['restreamsDestinations']));
 $o->setLogFile($_POST['logFile']);
 $o->setUsers_id($token->users_id);
 $o->setJson(json_encode($_POST));
