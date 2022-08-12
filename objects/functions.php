5ed17e923cd906081a64afa7a9e23a76b87fc869
DanieL <DanieL@host.docker.internal>
2022-08-12 17:09:09 -0300

    Timezone fix

--- objects/functions.php
+++ objects/functions.php
@@ -8478,7 +8478,7 @@ function convertFromDefaultTimezoneTimeToMyTimezone($date) {
 }
 
 function convertDateFromToTimezone($date, $fromTimezone, $toTimezone) {
-    if (!preg_match('/[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}/', $date)) {
+    if (!preg_match('/[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}(:[0-9]{2})?/', $date)) {
         _error_log("convertDateFromToTimezone ERROR ($date, $fromTimezone, $toTimezone)");
         return $date;
     }
