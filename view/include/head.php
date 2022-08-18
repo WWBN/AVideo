626f81e9a40acd188947553724b4f81faf1cea70
DanieL <DanieL@host.docker.internal>
2022-08-18 12:54:29 -0300

    System timezone added

--- view/include/head.php
+++ view/include/head.php
@@ -192,8 +192,8 @@ if (isRTL()) {
     var _serverDBTime = "<?php echo getDatabaseTime(); ?>";
     var _serverTimeString = "<?php echo date('Y-m-d H:i:s'); ?>";
     var _serverDBTimeString = "<?php echo date('Y-m-d H:i:s', getDatabaseTime()); ?>";
-    var _serverTimezone = "<?php echo date_default_timezone_get(); ?>";
-    var _serverSystemTimezone = "<?php echo getSystemTimezone(); ?>";
+    var _serverTimezone = <?php echo json_encode(date_default_timezone_get()); ?>;
+    var _serverSystemTimezone = <?php json_encode(getSystemTimezone()); ?>;
     var avideoModalIframeFullScreenCloseButton = <?php echo json_encode(getHamburgerButton('avideoModalIframeFullScreenCloseButton', 2, 'class="btn btn-default pull-left hamburger " onclick="avideoModalIframeFullScreenClose();"', true)); ?>;
     var avideoModalIframeFullScreenCloseButtonSmall = <?php echo json_encode(getHamburgerButton('avideoModalIframeFullScreenCloseButton', 4, 'class="btn btn-default btn-sm pull-left hamburger " onclick="avideoModalIframeFullScreenClose();"', true)); ?>;
     var PHPSESSID = "<?php echo session_id(); ?>";
