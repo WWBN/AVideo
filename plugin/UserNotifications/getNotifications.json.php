9e9793606912c266ebeec1251d32f913014df70b
DanieL <DanieL@host.docker.internal>
2022-09-01 12:31:19 -0300

    Avoid errors

--- plugin/UserNotifications/getNotifications.json.php
+++ plugin/UserNotifications/getNotifications.json.php
@@ -6,7 +6,13 @@ $obj->msg = '';
 $obj->error = false;
 $obj->users_id = User::getId();
 $obj->notifications = User_notifications::getAllForUsers_id($obj->users_id);
-usort($obj->notifications, 'cmpNotifications');
+
+if(empty($obj->notifications) || !is_array($obj->notifications)){
+    $obj->notifications = array();
+}else{
+    usort($obj->notifications, 'cmpNotifications');
+}
+
 echo json_encode($obj);
 
 function cmpNotifications($a, $b) {
