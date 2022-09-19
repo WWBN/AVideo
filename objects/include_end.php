712d05e652fc5f167acc02e6f2e767114a29e23f
DanieL <DanieL@host.docker.internal>
2022-09-19 14:14:03 -0300

    Getting ready for iframe

--- objects/include_end.php
+++ objects/include_end.php
@@ -2,5 +2,7 @@
 global $global;
 if (class_exists("Plugin") && empty($global['avideoEndIncluded'])) {
     AVideoPlugin::getEnd();
+}else{
+    _error_log('avideoEndIncluded');
 }
 $global['avideoEndIncluded'] = 1;
