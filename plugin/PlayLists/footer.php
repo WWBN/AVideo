149f5f8c56aefc23aa7c2828a96c5dff78700b63
DanieL <DanieL@host.docker.internal>
2022-09-21 21:30:10 -0300

    https://github.com/WWBN/AVideo/issues/7161

--- plugin/PlayLists/footer.php
+++ plugin/PlayLists/footer.php
@@ -107,7 +107,7 @@
             
 
         }
-        $('.searchlist' + videos_id + crc).btsListFilter('#searchinput' + videos_id + crc, {itemChild: 'span'});
+        $('.searchlist' + videos_id + crc).btsListFilter('#searchinput' + videos_id + crc, {itemChild: '.nameSearch'});
         $('.playListsVideosIds' + videos_id).change(function () {
             if (playListsAdding) {
                 return false;
