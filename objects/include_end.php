54174d451fd171fccff1a71a649bfff6e361cc1f
DanieL <DanieL@host.docker.internal>
2022-09-27 11:31:13 -0300

    Update

--- objects/include_end.php
+++ objects/include_end.php
@@ -2,7 +2,8 @@
 global $global;
 if (class_exists("Plugin") && empty($global['avideoEndIncluded'])) {
     AVideoPlugin::getEnd();
-}else{
     _error_log('avideoEndIncluded');
+}else{
+    _error_log('avideoEndIncluded ERROR');
 }
 $global['avideoEndIncluded'] = 1;
