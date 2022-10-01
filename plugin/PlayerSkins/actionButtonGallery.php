5ac8883fb53db5e9ee2dd5b0c8fbc433ffaa8bc2
DanieL <DanieL@host.docker.internal>
2022-10-01 10:27:49 -0300

    update

--- plugin/PlayerSkins/actionButtonGallery.php
+++ plugin/PlayerSkins/actionButtonGallery.php
@@ -1,11 +1,13 @@
 <?php
-$link = Video::getEPGLink($videos_id);
-if (!empty($link)) {
-    ?>
-    <button type="button" class="btn label label-default" onclick="avideoModalIframeFullTransparent('<?php echo $link; ?>');" data-toggle="tooltip" title="<?php echo __("TV Guide"); ?>">
-        <i class="fas fa-list-alt"></i> 
-        <?php echo __("TV Guide"); ?>
-    </button>    
-    <?php
+if(!empty($videos_id)){
+    $link = Video::getEPGLink($videos_id);
+    if (!empty($link)) {
+        ?>
+        <button type="button" class="btn label label-default" onclick="avideoModalIframeFullTransparent('<?php echo $link; ?>');" data-toggle="tooltip" title="<?php echo __("TV Guide"); ?>">
+            <i class="fas fa-list-alt"></i> 
+            <?php echo __("TV Guide"); ?>
+        </button>    
+        <?php
+    }
 }
 ?>
