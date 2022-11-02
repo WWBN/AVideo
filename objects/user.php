a41c727fd37d4161f6364444301d25f53b4eeef4
DanieL <DanieL@host.docker.internal>
2022-11-02 16:22:28 -0300

    Fix

--- objects/user.php
+++ objects/user.php
@@ -917,8 +917,8 @@ if (typeof gtag !== \"function\") {
         if (!empty($this->id)) {
             
             $arrayTables = array(
-                'live_transmitions',
                 'live_transmition_history_log',
+                'live_transmitions',
                 'users_login_history',
                 'audit',
                 'ppvlive_purchases',
