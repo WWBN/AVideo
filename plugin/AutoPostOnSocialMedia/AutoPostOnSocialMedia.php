ce5480c1ac6c8f4871690ee9a797ca69324a9235
DanieL <DanieL@host.docker.internal>
2022-11-24 08:57:40 -0300

    https://github.com/WWBN/AVideo/issues/7400

--- plugin/AutoPostOnSocialMedia/AutoPostOnSocialMedia.php
+++ plugin/AutoPostOnSocialMedia/AutoPostOnSocialMedia.php
@@ -94,6 +94,8 @@ In the Reset keys tab, press the Reset button, update the consumer key and secre
             365 => __("12 Months"));
         $o->value = 365;
         $obj->postARandomVideoFromLastDays = $o;
+        
+        $obj->debugMode = true;
 
         return $obj;
     }
@@ -122,6 +124,9 @@ In the Reset keys tab, press the Reset button, update the consumer key and secre
         if($obj->TwitterEnable){
             $connection = new TwitterOAuth($obj->TwitterAPIKey, $obj->TwitterAPIKeySecret, $obj->TwitterAccessToken, $obj->TwitterAccessTokenSecret);
             $post_tweets = $connection->post("statuses/update", ["status" => $msg]);
+            if($obj->debugMode){
+                _error_log(json_encode($post_tweets), AVideoLog::$DEBUG);
+            }
         }
 
         return $post_tweets;
