642c06a28bccddeef3e58e9b3aefd9618097c73a
DanieL <DanieL@host.docker.internal>
2022-11-29 19:08:02 -0300

    fix

--- objects/mysql_dal.php
+++ objects/mysql_dal.php
@@ -114,8 +114,6 @@ class sqlDAL {
 
         if ($stmt->errno !== 0) {
             log_error('Error in writeSql : (' . $stmt->errno . ') ' . $stmt->error . ", SQL-CMD:" . $preparedStatement);
-            $stmt->close();
-            
             if(empty($global['mysqli_charset']) && preg_match('/collation utf8/', $stmt->error)){
                 $global['mysqli_charset'] = 'latin1';
                 _mysql_close();
@@ -123,7 +121,7 @@ class sqlDAL {
                 return self::writeSql($preparedStatement, $formats, $values);
             }
             
-            
+            $stmt->close();            
             return false;
         }
         $iid = @$global['mysqli']->insert_id;
