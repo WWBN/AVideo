997524e59edca25cfb0763d4792c9000ab0a035f
DanieL <DanieL@host.docker.internal>
2022-12-22 13:15:03 -0300

    Update

--- view/js/script.js
+++ view/js/script.js
@@ -2097,7 +2097,7 @@ function checkMoment() {
 }
 
 function addGetParam(_url, _key, _value) {
-    if (typeof _url !== 'string') {
+    if (typeof _url !== 'string' || _url == 'undefined') {
         return false;
     }
     var param = _key + '=' + escape(_value);
