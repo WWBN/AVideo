c1dbc3b2ebf98583672380d4310c919175a085bf
DanieL <DanieL@host.docker.internal>
2023-01-06 11:49:23 -0300

    improive search and ignore accents

--- objects/bootGrid.php
+++ objects/bootGrid.php
@@ -62,6 +62,7 @@ class BootGrid
             $search = str_replace('&quot;', '"', $search);
             $like = [];
             foreach ($searchFieldsNames as $value) {
+                $like[] = " {$value} LIKE '%{$search}%' ";
                 $like[] = " {$value} LIKE _utf8 '%{$search}%' collate utf8_general_ci ";
                 if (preg_match('/description/', $value)) {
                     $like[] = " CONVERT(CAST({$value} as BINARY) USING utf8) regexp '\\b{$search}\\b' ";
