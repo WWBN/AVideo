7845be181921ca290a04e64bef799f6518ae75fd
DanieL <DanieL@host.docker.internal>
2023-02-13 09:34:37 -0300

    Update

--- plugin/CustomizeAdvanced/actionButton.php
+++ plugin/CustomizeAdvanced/actionButton.php
@@ -3,12 +3,13 @@ $videoAB = new Video('', '', $videos_id);
 $trailer = $videoAB->getTrailer1();
 //var_dump($_REQUEST);exit;
 if (!isValidURL($trailer)) {
-    if (empty($_REQUEST['playlist_id'])) {
-        echo '<!-- invalid trailer URL -->';
-        return false;
-    } else {
+    if (!empty($_REQUEST['playlist_id'])) {
         $trailer = PlayLists::getTrailerIfIsSerie($_REQUEST['playlist_id']);
     }
+}
+if (!isValidURL($trailer)) {
+    echo '<!-- invalid trailer URL -->';
+    return false;
 }
 ?>
 <button type="button" class="btn btn-default no-outline" onclick="avideoModalIframe('<?php echo parseVideos($trailer, 1); ?>');" data-toggle="tooltip" title="<?php echo __("Trailer"); ?>">
