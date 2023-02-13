64c36d9f4e3ceb08ea16e060d8b9b2cdfaa70e29
DanieL <DanieL@host.docker.internal>
2023-02-13 10:40:13 -0300

    Update

--- objects/functions.php
+++ objects/functions.php
@@ -4685,7 +4685,7 @@ function _mysql_connect($persistent = false, $try = 0)
         }
     } catch (Exception $exc) {
         if(empty($try)){
-            _error_log('Error on connect, trying again '.mysqli_connect_error());
+            _error_log('Error on connect, trying again ['.mysqli_connect_error().']');
             _mysql_close();
             sleep(5);
             return _mysql_connect($persistent, $try+1);
