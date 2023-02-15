c1b6a080dff39a34c87d07c1dd333ab1025f46d2
DanieL <DanieL@host.docker.internal>
2023-02-14 22:13:52 -0300

    https://github.com/WWBN/AVideo/issues/7664

---
+++ install/deleteSystemdPrivate.php
@@ -0,0 +1,20 @@
+<?php
+
+//streamer config
+require_once '../videos/configuration.php';
+
+if (!isCommandLineInterface()) {
+    return die('Command Line only');
+}
+
+set_time_limit(300);
+ini_set('max_execution_time', 300);
+$glob = glob("/tmp/systemd-private-*-apache2.service-*/tmp/");
+$totalItems = count($glob);
+echo "Found total of {$totalItems} items " . PHP_EOL;
+$countItems = 0;
+foreach ($glob as $file) {
+    
+    $countItems++;
+    echo "$file".PHP_EOL;
+}
