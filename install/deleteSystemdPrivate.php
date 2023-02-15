feed28d2a980512bc74c63f1db87da039b46e2c8
DanieL <DanieL@host.docker.internal>
2023-02-14 22:19:29 -0300

    https://github.com/WWBN/AVideo/issues/7664

--- install/deleteSystemdPrivate.php
+++ install/deleteSystemdPrivate.php
@@ -11,10 +11,21 @@ set_time_limit(300);
 ini_set('max_execution_time', 300);
 $glob = glob("/tmp/systemd-private-*-apache2.service-*/tmp/*");
 $totalItems = count($glob);
+$one_day_ago = time() - (24 * 60 * 60); // timestamp of 1 day ago
 echo "Found total of {$totalItems} items " . PHP_EOL;
 $countItems = 0;
+$totalFilesize = 0;
 foreach ($glob as $file) {
-    
     $countItems++;
-    echo "$file".PHP_EOL;
+    if (filemtime($file) < $one_day_ago) {
+        $size = filesize($file);
+        $humanFSize = humanFileSize($size);
+        echo "delete {$humanFSize} $file" . PHP_EOL;
+        $totalFilesize += $size;
+        unlink($file);
+    }
 }
+
+$humanFSize = humanFileSize($totalFilesize);
+echo " ----- " . PHP_EOL;
+echo "Total deleted {$humanFSize}" . PHP_EOL;
