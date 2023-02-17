aaccd6bb02fc132576a3e23a516bdc93b44c5199
DanieL <DanieL@host.docker.internal>
2023-02-17 14:48:31 -0300

    Update

--- install/fixDurationHLS.php
+++ install/fixDurationHLS.php
@@ -10,6 +10,8 @@ if(!AVideoPlugin::isEnabledByName('VideoHLS')){
     return die('VideoHLS disabled');
 }
 
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
 ob_end_flush();
 $global['limitForUnlimitedVideos'] = -1;
 $videos = video::getAllVideosLight("", false, true);
