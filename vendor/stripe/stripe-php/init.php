48acfac28cc10e30e02570e3e514d33d8b0801e3
DanieL <DanieL@host.docker.internal>
2023-02-17 12:44:40 -0300

    Updates

---
+++ vendor/stripe/stripe-php/init.php
@@ -0,0 +1,7 @@
+<?php
+
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
+ */
+
