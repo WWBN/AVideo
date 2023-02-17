21d3f82c0d4450f9b1d1bc887f50e32551f296ec
DanieL <DanieL@host.docker.internal>
2023-02-17 14:46:34 -0300

    Update

---
+++ install/fixDurationHLS.php
@@ -0,0 +1,29 @@
+<?php
+//streamer config
+require_once '../videos/configuration.php';
+
+if (!isCommandLineInterface()) {
+    return die('Command Line only');
+}
+
+if(!AVideoPlugin::isEnabledByName('VideoHLS')){
+    return die('VideoHLS disabled');
+}
+
+ob_end_flush();
+$global['limitForUnlimitedVideos'] = -1;
+$videos = video::getAllVideosLight("", false, true);
+$count = 0;
+$total = count();
+foreach ($videos as $value) {
+    $count++;
+    
+    if(!Video::isValidDuration($value['duration'])){
+        $v = new Video('', '', $value['id'], true);
+        if(VideoHLS::updateHLSDurationIfNeed($v)){
+            echo "[{$count}/{$total}] Success updated [{$value['id']}] ".$v->getDuration().PHP_EOL;
+        }else{
+            echo "[{$count}/{$total}] ERROR updated [{$value['id']}] ".$v->getDuration().PHP_EOL;
+        }
+    }
+}
