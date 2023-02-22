aa7b090dccba1f2ee2f09b35dd9e18c4ea7d52d9
Daniel Neto <me@danielneto.com>
2023-02-22 15:05:26 -0300

    Update

--- plugin/LoginControl/pgp/functions.php
+++ plugin/LoginControl/pgp/functions.php
@@ -6,7 +6,7 @@ use phpseclib3\Crypt\RSA\Formats\Keys\PKCS1;
 //require_once dirname(__FILE__).'/../../../objects/singpolyma/openpgp-php/lib/openpgp.php';
 //require_once dirname(__FILE__).'/../../../objects/singpolyma/openpgp-php/lib/openpgp_crypt_rsa.php';
 error_reporting(0);
-//error_reporting(E_ALL);
+error_reporting(E_ALL);
 
 function createKeys($UserIDPacket = 'Test <test@example.com>', $password = ''){
     /*
