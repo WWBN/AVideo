e73acacc24289ce5361d4bd20b974177ffede1c9
Daniel Neto <me@danielneto.com>
2023-03-03 09:42:30 -0300

    https://github.com/WWBN/AVideo/issues/7743

--- view/hls.php
+++ view/hls.php
@@ -84,7 +84,8 @@ if (isAVideoEncoderOnSameDomain() || $tokenIsValid || !empty($advancedCustom->vi
         if (!preg_match('/index.m3u8$/', $filename)) {
             $filename .= '/index.m3u8';
         }
-        $content = file_get_contents($filename);
+        $context = stream_context_create(array('http' => array('timeout' => 30)));
+        $content = file_get_contents($filename, false, $context);
         $newContent = str_replace('{$pathToVideo}', "{$global['webSiteRootURL']}videos/{$_GET['videoDirectory']}/../", $content);
         if (!empty($_GET['token'])) {
             $newContent = str_replace('/index.m3u8', "/index.m3u8?token={$_GET['token']}", $newContent);
