f761437873f66fd29831984fd83db3088b8799a0
Daniel Neto <me@danielneto.com>
2023-03-07 07:32:10 -0300

    https://github.com/WWBN/AVideo/issues/7745#event-8680536437

--- plugin/Live/view/Live_restreams/testRestreamer.json.php
+++ plugin/Live/view/Live_restreams/testRestreamer.json.php
@@ -16,6 +16,7 @@ if(!User::canStream()){
     forbiddenPage('You cannot stream');
 }
 
+_error_log('Testing reestream users_id=['.User::getId().'] '.json_error(debug_backtrace()));
 
 $lth = new LiveTransmitionHistory();
 $lth->setTitle('Restream test '.date('Y-m-d H:i:s'));
