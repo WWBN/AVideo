7ecd4066dbf86ef6b66fc9bdf49fa2b1a5cd6c32
Daniel Neto <me@danielneto.com>
2023-03-11 19:39:32 -0300

    Update

--- objects/functions.php
+++ objects/functions.php
@@ -10742,6 +10742,7 @@ function getDockerStatsURL()
 }
 
 function set_error_reporting() {
+    global $global;
     if (!empty($global['debug'])) {
         error_reporting(E_ALL);
         ini_set('display_errors', 1);
