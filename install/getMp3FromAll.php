7faebdc7274092bb87840c1c3671dad6ed16ea52
Daniel Neto <me@danielneto.com>
2023-03-14 16:01:17 -0300

    Update

---
+++ install/getMp3FromAll.php
@@ -0,0 +1,19 @@
+<?php
+//streamer config
+require_once '../videos/configuration.php';
+
+if (!isCommandLineInterface()) {
+    return die('Command Line only');
+}
+$global['rowCount'] = 99999;
+$total = Video::getTotalVideos("", false, true, true, false, false);
+$videos = Video::getAllVideosLight("", false, true, false);
+$count = 0;
+foreach ($videos as $value) {
+    $count++;
+    $updated = convertVideoToMP3FileIfNotExists($value['id']);
+    echo "{$count}/{$total} (".($updated ? "success" : "fail").") [{$value['id']}] {$value['title']}".PHP_EOL;
+    ob_flush();
+}
+
+die();
