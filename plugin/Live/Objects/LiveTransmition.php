ef9ca3dbc5149ad5a4f90872b635b0106fa05a14
Daniel Neto <me@danielneto.com>
2023-03-16 07:11:27 -0300

    Update

--- plugin/Live/Objects/LiveTransmition.php
+++ plugin/Live/Objects/LiveTransmition.php
@@ -4,8 +4,8 @@ require_once dirname(__FILE__) . '/../../../videos/configuration.php';
 require_once dirname(__FILE__) . '/../../../objects/bootGrid.php';
 require_once dirname(__FILE__) . '/../../../objects/user.php';
 
-class LiveTransmition extends ObjectYPT
-{
+class LiveTransmition extends ObjectYPT {
+
     protected $properties = [];
     protected $id;
     protected $title;
@@ -18,43 +18,35 @@ class LiveTransmition extends ObjectYPT
     protected $showOnTV;
     protected $password;
 
-    public static function getSearchFieldsNames()
-    {
+    public static function getSearchFieldsNames() {
         return ['title'];
     }
 
-    public static function getTableName()
-    {
+    public static function getTableName() {
         return 'live_transmitions';
     }
 
-    public function getId()
-    {
+    public function getId() {
         return $this->id;
     }
 
-    public function getTitle()
-    {
+    public function getTitle() {
         return $this->title;
     }
 
-    public function getPublic()
-    {
+    public function getPublic() {
         return $this->public;
     }
 
-    public function getSaveTransmition()
-    {
+    public function getSaveTransmition() {
         return $this->saveTransmition;
     }
 
-    public function getUsers_id()
-    {
+    public function getUsers_id() {
         return $this->users_id;
     }
 
-    public function getCategories_id()
-    {
+    public function getCategories_id() {
         return $this->categories_id;
     }
 
@@ -62,62 +54,51 @@ class LiveTransmition extends ObjectYPT
      * 
      * @return string
      */
-    public function getKey()
-    {
+    public function getKey() {
         return $this->key;
     }
 
-    public function getDescription()
-    {
+    public function getDescription() {
         return $this->description;
     }
 
-    public function setId($id)
-    {
+    public function setId($id) {
         $this->id = $id;
     }
 
-    public function setTitle($title)
-    {
+    public function setTitle($title) {
         global $global;
         //$title = ($title);
         $this->title = xss_esc($title);
     }
 
-    public function setPublic($public)
-    {
+    public function setPublic($public) {
         $this->public = intval($public);
     }
 
-    public function setSaveTransmition($saveTransmition)
-    {
+    public function setSaveTransmition($saveTransmition) {
         $this->saveTransmition = $saveTransmition;
     }
 
-    public function setUsers_id($users_id)
-    {
+    public function setUsers_id($users_id) {
         $this->users_id = $users_id;
     }
 
-    public function setCategories_id($categories_id)
-    {
+    public function setCategories_id($categories_id) {
         $this->categories_id = $categories_id;
     }
 
-    public function setKey($key)
-    {
+    public function setKey($key) {
         $this->key = $key;
     }
 
-    public function setDescription($description)
-    {
+    public function setDescription($description) {
         global $global;
         //$description = ($description);
         $this->description = xss_esc($description);
     }
 
-    public function loadByUser($user_id)
-    {
+    public function loadByUser($user_id) {
         $user = self::getFromDbByUser($user_id);
         if (empty($user)) {
             return false;
@@ -129,8 +110,7 @@ class LiveTransmition extends ObjectYPT
         return true;
     }
 
-    public function loadByKey($uuid)
-    {
+    public function loadByKey($uuid) {
         $row = self::getFromKey($uuid);
         if (empty($row)) {
             return false;
@@ -142,8 +122,7 @@ class LiveTransmition extends ObjectYPT
         return true;
     }
 
-    public static function getFromDbByUser($user_id, $refreshCache = false)
-    {
+    public static function getFromDbByUser($user_id, $refreshCache = false) {
         global $global;
         if (!self::isTableInstalled(static::getTableName())) {
             _error_log("Save error, table " . static::getTableName() . " does not exists", AVideoLog::$ERROR);
@@ -159,7 +138,7 @@ class LiveTransmition extends ObjectYPT
             $liveStreamObject = new LiveStreamObject($data['key'], $data['live_servers_id']);
             $data['key_with_index'] = $liveStreamObject->getKeyWithIndex(true);
             $data['live_index'] = $liveStreamObject->getIndex();
-            
+
             $user = $data;
         } else {
             $user = false;
@@ -167,15 +146,14 @@ class LiveTransmition extends ObjectYPT
         return $user;
     }
 
-    public static function createTransmitionIfNeed($user_id)
-    {
+    public static function createTransmitionIfNeed($user_id) {
         if (empty($user_id)) {
             return false;
         }
         $row = static::getFromDbByUser($user_id);
         if ($row && !empty($row['key'])) {
             $row['just_created'] = false;
-        }else{
+        } else {
             $l = new LiveTransmition(0);
             $l->setTitle("I am Live");
             $l->setDescription("");
@@ -189,8 +167,7 @@ class LiveTransmition extends ObjectYPT
         return $row;
     }
 
-    public static function resetTransmitionKey($user_id)
-    {
+    public static function resetTransmitionKey($user_id) {
         $row = static::getFromDbByUser($user_id);
 
         $l = new LiveTransmition($row['id']);
@@ -203,8 +180,7 @@ class LiveTransmition extends ObjectYPT
         }
     }
 
-    public static function getFromRequest()
-    {
+    public static function getFromRequest() {
         if (!empty($_REQUEST['live_transmitions_id'])) {
             return LiveTransmition::getFromDb($_REQUEST['live_transmitions_id']);
         } else if (!empty($_REQUEST['live_schedule'])) {
@@ -217,8 +193,7 @@ class LiveTransmition extends ObjectYPT
         return false;
     }
 
-    public static function getFromDbByUserName($userName)
-    {
+    public static function getFromDbByUserName($userName) {
         global $global;
         _mysql_connect();
         $sql = "SELECT * FROM users WHERE user = ? LIMIT 1";
@@ -236,8 +211,7 @@ class LiveTransmition extends ObjectYPT
         }
     }
 
-    public static function getFromDbByChannelName($channelName)
-    {
+    public static function getFromDbByChannelName($channelName) {
         global $global;
         _mysql_connect();
         $sql = "SELECT * FROM users WHERE channelName = ? LIMIT 1";
@@ -255,8 +229,7 @@ class LiveTransmition extends ObjectYPT
         }
     }
 
-    public static function getFromDbBySchedule($live_schedule_id)
-    {
+    public static function getFromDbBySchedule($live_schedule_id) {
         global $global;
         $live_schedule_id = intval($live_schedule_id);
         $sql = "SELECT lt.*, ls.* FROM live_schedule ls "
@@ -274,8 +247,7 @@ class LiveTransmition extends ObjectYPT
         return $user;
     }
 
-    public static function keyExists($key, $checkSchedule = true)
-    {
+    public static function keyExists($key, $checkSchedule = true) {
         global $global;
         if (!is_string($key)) {
             return false;
@@ -292,23 +264,21 @@ class LiveTransmition extends ObjectYPT
         sqlDAL::close($res);
         if ($res) {
             $row = $data;
-            if (!empty($row)) {
+            if(!empty($row)) {
+
                 $row['live_schedule_id'] = 0;
                 $row['scheduled'] = 0;
-            }
-            if (!empty($row)) {
                 $p = $row['live_password'];
                 $row = cleanUpRowFromDatabase($row);
                 $row['live_password'] = $p;
+
+                if (empty($row['users_id'])) {
+                    $row['users_id'] = 0;
+                }
+                if (!isset($row['live_servers_id'])) {
+                    $row['live_servers_id'] = Live::getLiveServersIdRequest();
+                }
             }
-            
-            if (empty($row['users_id'])) {
-                $row['users_id'] = 0;
-            }
-            if (!isset($row['live_servers_id'])) {
-                $row['live_servers_id'] = Live::getLiveServersIdRequest();
-            }
-            
         } else {
             $row = false;
         }
@@ -324,12 +294,12 @@ class LiveTransmition extends ObjectYPT
         return $row;
     }
 
-    public function save(){
-        if(empty($this->users_id)){
+    public function save() {
+        if (empty($this->users_id)) {
             return false;
         }
         $row = self::getFromDbByUser($this->users_id, true);
-        if(!empty($row)){
+        if (!empty($row)) {
             $this->id = $row['id'];
         }
 
@@ -343,13 +313,12 @@ class LiveTransmition extends ObjectYPT
         Category::clearCacheCount();
         Live::deleteStatsCache(true);
 
-        $socketObj = sendSocketMessageToAll(['stats'=>getStatsNotifications(false, false)], "socketLiveONCallback");
+        $socketObj = sendSocketMessageToAll(['stats' => getStatsNotifications(false, false)], "socketLiveONCallback");
 
         return $id;
     }
 
-    public function deleteGroupsTrasmition()
-    {
+    public function deleteGroupsTrasmition() {
         if (empty($this->id)) {
             return false;
         }
@@ -358,20 +327,17 @@ class LiveTransmition extends ObjectYPT
         return sqlDAL::writeSql($sql, "i", [$this->id]);
     }
 
-    public function insertGroup($users_groups_id)
-    {
+    public function insertGroup($users_groups_id) {
         global $global;
         $sql = "INSERT INTO live_transmitions_has_users_groups (live_transmitions_id, users_groups_id) VALUES (?,?)";
         return sqlDAL::writeSql($sql, "ii", [$this->id, $users_groups_id]);
     }
 
-    public function isAPrivateLive()
-    {
+    public function isAPrivateLive() {
         return !empty($this->getGroups());
     }
 
-    public function getGroups()
-    {
+    public function getGroups() {
         $rows = [];
         if (empty($this->id)) {
             return $rows;
@@ -385,12 +351,11 @@ class LiveTransmition extends ObjectYPT
             foreach ($fullData as $row) {
                 $rows[] = $row["users_groups_id"];
             }
-        } 
+        }
         return $rows;
     }
 
-    public function userCanSeeTransmition()
-    {
+    public function userCanSeeTransmition() {
         global $global;
         require_once $global['systemRootPath'] . 'objects/userGroups.php';
         require_once $global['systemRootPath'] . 'objects/user.php';
@@ -398,10 +363,10 @@ class LiveTransmition extends ObjectYPT
             return true;
         }
         /*
-        $password = $this->getPassword();
-        if(!empty($password) && !Live::passwordIsGood($this->getKey())){
-            return false;
-        }
+          $password = $this->getPassword();
+          if(!empty($password) && !Live::passwordIsGood($this->getKey())){
+          return false;
+          }
          *
          */
 
@@ -430,15 +395,14 @@ class LiveTransmition extends ObjectYPT
         }
     }
 
-    public static function getFromKey($key, $checkSchedule = true){
+    public static function getFromKey($key, $checkSchedule = true) {
         return self::keyExists($key, $checkSchedule);
     }
 
-    public static function keyNameFix($key)
-    {
+    public static function keyNameFix($key) {
         $key = str_replace('/', '', $key);
         if (!empty($_REQUEST['live_index']) && !preg_match("/.*-([0-9a-zA-Z]+)/", $key)) {
-            if (!empty($_REQUEST['live_index']) && $_REQUEST['live_index']!=='false') {
+            if (!empty($_REQUEST['live_index']) && $_REQUEST['live_index'] !== 'false') {
                 $key .= "-{$_REQUEST['live_index']}";
             }
         }
@@ -448,40 +412,35 @@ class LiveTransmition extends ObjectYPT
         return $key;
     }
 
-    public function getShowOnTV()
-    {
+    public function getShowOnTV() {
         return $this->showOnTV;
     }
 
-    public function setShowOnTV($showOnTV)
-    {
+    public function setShowOnTV($showOnTV) {
         $this->showOnTV = $showOnTV;
     }
 
-    public function getPassword()
-    {
+    public function getPassword() {
         return $this->password;
     }
 
-    public function setPassword($password): void
-    {
+    public function setPassword($password): void {
         $this->password = trim($password);
     }
 
-    public static function canSaveTransmition($users_id)
-    {
+    public static function canSaveTransmition($users_id) {
         $lt = self::getFromDbByUser($users_id);
         return !empty($lt['saveTransmition']);
     }
-    
-    
+
     static function getUsers_idOrCompanyFromKey($key) {
-        
+
         $row = self::getFromKey($key);
-        if(!empty($row['users_id_company'])){
+        if (!empty($row['users_id_company'])) {
             return $row['users_id_company'];
         }
-        
+
         return $row['users_id'];
     }
+
 }
