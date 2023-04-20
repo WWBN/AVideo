5a00bc6ce8f5f63a338918a547f7cf21b6304b7f
Daniel Neto <me@danielneto.com>
2023-04-20 11:41:25 -0300

    update

--- plugin/Layout/searchOptions.php
+++ plugin/Layout/searchOptions.php
@@ -1,28 +1,87 @@
-<div class="container" style=" position: relative;
-    height: 50px;
+<style>
+#searchOptionsMenu {
+  position: relative;
+  height: 50px;
   display: flex;
-  align-items: center;    z-index: 1029;" id="searchOptionsMenu">
-    <div style="
-    position: absolute;
-    left: 50%;
-    transform: translateX(-50%);">
+  align-items: center;
+  z-index: 1029;
+}
+
+#searchOptionsMenu > div {
+  position: absolute;
+  left: 50%;
+  transform: translateX(-50%);
+}
+
+#searchFieldsNamesBelowNavbar-dropdown , #catNameBelowNavbar-dropdown {
+  min-width: 150px;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+}
+
+#searchFieldsNamesBelowNavbar-dropdown {
+  border-right-width: 0;
+  border-top-right-radius: 0;
+  border-bottom-right-radius: 0;
+}
+#catNameBelowNavbar-dropdown {
+  border-left-width: 0;
+  border-top-left-radius: 0;
+  border-bottom-left-radius: 0;
+  border-right-width: 0;
+  border-top-right-radius: 0;
+  border-bottom-right-radius: 0;
+}
+#searchOptionsButton {
+  border-left-width: 0;
+  border-top-left-radius: 0;
+  border-bottom-left-radius: 0;
+}
+
+#searchOptionsMenu .panel {
+  margin: 0;
+}
+
+#searchOptionsMenu .panel-body {
+  max-height: 60vh;
+  overflow: auto;
+}
+
+#searchOptionsMenu .form-check {
+  white-space: nowrap;
+}
+
+#searchOptionsMenu .form-check-input {
+  margin-right: 5px;
+}
+
+#searchOptionsMenu .form-check-label {
+  display: inline-block;
+  margin-bottom: 0;
+  vertical-align: middle;
+}
+
+#searchOptionsMenu .dropdown-menu-right {
+  right: 0;
+  left: auto;
+}
+
+</style>
+<div class="container" id="searchOptionsMenu">
+    <div>
         <div class="btn-group-justified">
             <div class="dropdown">
-                <button class="btn btn-default dropdown-toggle" type="button" data-toggle="dropdown" id="searchFieldsNamesBelowNavbar-dropdown" style="width: 150px;
-                        white-space: nowrap;
-                        overflow: hidden;
-                        text-overflow: ellipsis;
-                        border-right-width: 0;
-                        border-top-right-radius: 0;
-                        border-bottom-right-radius: 0;"><?php echo __('Search in'); ?>: <span class="badge">0</span>
+                <button class="btn btn-default dropdown-toggle" type="button" data-toggle="dropdown" 
+                id="searchFieldsNamesBelowNavbar-dropdown" ><?php echo __('Search in'); ?>: <span class="badge">0</span>
                     <span class="caret"></span></button>
-                <div class="panel panel-default dropdown-menu dropdown-menu-right" style="margin: 0;">
+                <div class="panel panel-default dropdown-menu dropdown-menu-right">
                     <div class="panel-body">
                         <?php
                         AVideoPlugin::loadPlugin('Layout');
                         foreach (Layout::$searchOptions as $key => $value) {
                         ?>
-                            <div class="form-check" style="white-space: nowrap;">
+                            <div class="form-check">
                                 <input class="form-check-input" type="checkbox" value="<?php echo $value['value']; ?>" id="filterCheck<?php echo $key; ?>" name="searchFieldsNamesBelowNavbar[]">
                                 <label class="form-check-label" for="filterCheck<?php echo $key; ?>">
                                     <?php echo $value['text']; ?>
@@ -35,22 +94,13 @@
                 </div>
             </div>
             <div class="dropdown">
-                <button class="btn btn-default dropdown-toggle" type="button" data-toggle="dropdown" id="catNameBelowNavbar-dropdown" style="width: 150px;
-                        white-space: nowrap;
-                        overflow: hidden;
-                        text-overflow: ellipsis;
-                        border-right-width: 0;
-                        border-top-right-radius: 0;
-                        border-bottom-right-radius: 0;
-                        border-left-width: 0;
-                        border-top-left-radius: 0;
-                        border-bottom-left-radius: 0;">
+                <button class="btn btn-default dropdown-toggle" type="button" data-toggle="dropdown" id="catNameBelowNavbar-dropdown">
                         <i class="fas fa-list"></i> <?php echo __('All Categories'); ?>
                     <span class="caret"></span>
                 </button>
-                <div class="panel panel-default dropdown-menu dropdown-menu-right" style="margin: 0;">
-                    <div class="panel-body" style="max-height: 60vh; overflow: auto;">
-                        <div class="form-check" style="white-space: nowrap;">
+                <div class="panel panel-default dropdown-menu dropdown-menu-right">
+                    <div class="panel-body">
+                        <div class="form-check">
                             <input class="form-check-input" type="radio" value="" id="catNameBelowNavbar" name="catNameBelowNavbar">
                             <label class="form-check-label" for="catNameBelowNavbar">
                                 <span class="content"><i class="fas fa-list"></i> <?php echo __('All Categories'); ?></span>
@@ -62,7 +112,7 @@
                         $global['doNotSearch'] = 0;
                         foreach ($categories_edit as $key => $value) {
                         ?>
-                            <div class="form-check" style="white-space: nowrap;">
+                            <div class="form-check">
                                 <input class="form-check-input" type="radio" value="<?php echo $value['clean_name']; ?>" id="catNameBelowNavbar<?php echo $value['id']; ?>" name="catNameBelowNavbar">
                                 <label class="form-check-label" for="catNameBelowNavbar<?php echo $value['id']; ?>">
                                     <span class="content"><i class="<?php echo $value['iconClass']; ?>"></i> <?php echo __($value['hierarchyAndName']); ?></span>
@@ -74,10 +124,7 @@
                     </div>
                 </div>
             </div>
-            <button class="btn btn-default btn-outline-secondary border-right-0 faa-parent animated-hover" type="button" id="searchOptionsButton" onclick="searchOptionsButton();" style="
-                    border-left-width: 0;
-                    border-top-left-radius: 0;
-                    border-bottom-left-radius: 0;">
+            <button class="btn btn-default btn-outline-secondary border-right-0 faa-parent animated-hover" type="button" id="searchOptionsButton" onclick="searchOptionsButton();" >
                 <i class="fas fa-search faa-shake"></i>
             </button>
         </div>
@@ -98,6 +145,7 @@
             $otherCheckbox.prop('checked', $(this).prop('checked'));
             $('#searchFieldsNamesBelowNavbar-dropdown .badge').text($(searchFieldsNamesBelowNavbarChecked).length);
             saveSearchFiltersToCookie();
+            setSearchFilterIcon();
         });
 
         $(catNameBelowNavbarCatName).on('change', function() {
@@ -107,6 +155,7 @@
             var selectedText = $(catNameBelowNavbarChecked).parent().find('span.content').html();
             $('#catNameBelowNavbar-dropdown').html(selectedText+' <span class="caret"></span>');
             saveSearchCategoryToCookie();
+            setSearchFilterIcon();
         });
 
         $(searchFieldsNamesBelowNavbarSearchFieldsNames).on('click', function() {
