5a6487bc4ac62e6aa71f789deb0a584d97d52a38
Daniel Neto <me@danielneto.com>
2023-05-06 18:33:03 -0300

    update

--- objects/category.php
+++ objects/category.php
@@ -464,10 +464,10 @@ class Category {
                 //_error_log('getAllCategories setCache');
                 //$category = $res->fetch_all(MYSQLI_ASSOC);
                 $cache = ObjectYPT::setCache($cacheName, $category);
-                //$cachefile = ObjectYPT::getCacheFileName($cacheName, false);
                 //$cacheObj = ObjectYPT::getCacheGlobal($cacheName, 36000);
                 //$category = object_to_array($cacheObj);
-                //_error_log('getAllCategories respond '.json_encode(array($cachefile, $cacheName, $cache)));
+                $cachefile = ObjectYPT::getCacheFileName($cacheName, false);
+                _error_log('getAllCategories respond '.json_encode(array($cachefile, $cacheName, $cache)));
                 //var_dump(array($cache, $cacheObj, $category));exit;
             
             } else {
