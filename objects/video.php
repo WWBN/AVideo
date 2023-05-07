4cad2ca82ba9970a3589f6ed12c3f808509d67a3
Daniel Neto <me@danielneto.com>
2023-05-07 10:57:40 -0300

    https://github.com/WWBN/AVideo/issues/7990

--- objects/video.php
+++ objects/video.php
@@ -1084,7 +1084,8 @@ if (!class_exists('Video')) {
                     $sql .= " AND v.isSuggested = 1 AND v.status = '" . self::$statusActive . "' ";
                 }
                 if (empty($random) && !empty($_GET['videoName'])) {
-                    $sql .= " AND v.clean_title = '{$_GET['videoName']}' ";
+                    $videoName = addslashes($_GET['videoName']);
+                    $sql .= " AND v.clean_title = '{$videoName}' ";
                 } elseif (!empty($random)) {
                     $sql .= " AND v.id != {$random} ";
                     //getTotalVideos($status = "viewable", $showOnlyLoggedUserVideos = false, $ignoreGroup = false, $showUnlisted = false, $activeUsersOnly = true, $suggestedOnly = false, $type = '') {
