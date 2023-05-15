4c3d8b985908585f90179592aaf93b3ebb305df4
Daniel Neto <me@danielneto.com>
2023-05-15 11:08:16 -0300

    update

--- objects/functions.php
+++ objects/functions.php
@@ -6240,10 +6240,10 @@ function getCurrentPage()
         _error_log("getCurrentPage current>1000 ERROR [{$current}] ".json_encode(debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS)));
         _error_log("getCurrentPage current>1000 ERROR [{$current}] ".getSelfURI().' '.json_encode($_SERVER));
         
-        if($current>1000){
-            _error_log("getCurrentPage current>1000 ERROR die [{$current}] ".getSelfURI().' '.json_encode($_SERVER));
-            exit;
-        }
+    }
+    if($current>1000){
+        _error_log("getCurrentPage current>1000 ERROR die [{$current}] ".getSelfURI().' '.json_encode($_SERVER));
+        exit;
     }
     $lastCurrent = $current;
     return $current;
