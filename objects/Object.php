a2d649d23149c7925dac4248fcdab89a89b5d5b0
Daniel Neto <me@danielneto.com>
2023-05-15 11:54:24 -0300

    update

--- objects/Object.php
+++ objects/Object.php
@@ -284,11 +284,15 @@ abstract class ObjectYPT implements ObjectInterface
 
     public function save()
     {
+        
+        if (!empty($_GET['search'])) {
+            _error_log("Do not save if it is searching", AVideoLog::$WARNING);
+            return false;
+        }
         if (!$this->tableExists()) {
             _error_log("Save error, table " . static::getTableName() . " does not exists", AVideoLog::$ERROR);
             return false;
         }
-
         if (!self::ignoreTableSecurityCheck() && isUntrustedRequest("SAVE " . static::getTableName())) {
             return false;
         }
