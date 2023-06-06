1eaf6301522d4a9e12563d738cd71f27759e6386
Daniel Neto <me@danielneto.com>
2023-06-06 13:15:40 -0300

    Update

--- plugin/CDN/tools/moveMissingFiles.php
+++ plugin/CDN/tools/moveMissingFiles.php
@@ -6,6 +6,9 @@ if (!isCommandLineInterface()) {
     return die('Command Line only');
 }
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 $isCDNEnabled = AVideoPlugin::isEnabledByName('CDN');
 
 if (empty($isCDNEnabled)) {
