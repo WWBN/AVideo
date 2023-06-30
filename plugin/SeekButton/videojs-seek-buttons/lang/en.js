2b7ac37bc32c59173986e02b1c45ee1b820facce
Daniel Neto <me@danielneto.com>
2023-06-30 14:36:00 -0300

    Update seek buttons

--- plugin/SeekButton/videojs-seek-buttons/lang/en.js
+++
@@ -1,4 +0,0 @@
-videojs.addLanguage('en', {
-  "Seek forward {{seconds}} seconds": "Seek forward {{seconds}} seconds",
-  "Seek back {{seconds}} seconds": "Seek back {{seconds}} seconds"
-});
--- plugin/SeekButton/videojs-seek-buttons/lang/it.js
+++
@@ -1,4 +0,0 @@
-videojs.addLanguage('it', {
-  "Seek forward {{seconds}} seconds": "Avanti di {{seconds}} secondi",
-  "Seek back {{seconds}} seconds": "Indietro di {{seconds}} secondi"
-});
