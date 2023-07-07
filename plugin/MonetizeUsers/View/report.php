9a84d306338e1803952a06614be8ccc5bb308d2c
Daniel Neto <me@danielneto.com>
2023-07-07 15:09:03 -0300

    Update

--- plugin/MonetizeUsers/View/report.php
+++ plugin/MonetizeUsers/View/report.php
@@ -2,6 +2,11 @@
 
 $YPTWallet = AVideoPlugin::isEnabledByName('YPTWallet');
 
+$users_id = User::getId();
+if(User::isAdmin() && !empty($_REQUEST['users_id'])){
+  $users_id = $_REQUEST['users_id'];
+}
+
 $data = MonetizeUsers::getRewards(User::getId(), date('Y-m-d H:i:s', strtotime('-7 days')), date('Y-m-d H:i:s', strtotime('+1 day')), MonetizeUsers::$GetRewardModeGrouped);
 //var_dump($data);exit;
 $rows = [];
