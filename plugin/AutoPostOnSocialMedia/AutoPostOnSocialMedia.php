ea8e6347bc63e8834fb1244435d694cf67ab4c61
Daniel Neto <me@danielneto.com>
2023-07-27 11:21:33 -0300

    update

--- plugin/AutoPostOnSocialMedia/AutoPostOnSocialMedia.php
+++ plugin/AutoPostOnSocialMedia/AutoPostOnSocialMedia.php
@@ -127,6 +127,9 @@ In the Reset keys tab, press the Reset button, update the consumer key and secre
         if($obj->TwitterEnable){
             $connection = new TwitterOAuth($obj->TwitterAPIKey, $obj->TwitterAPIKeySecret, $obj->TwitterAccessToken, $obj->TwitterAccessTokenSecret);
             $connection->setApiVersion('2');
+            if($obj->debugMode){
+                _error_log($msg);
+            }
             $post_tweets = $connection->post("statuses/update", ["status" => $msg]);
             if($obj->debugMode){
                 _error_log(json_encode($post_tweets), AVideoLog::$DEBUG);
