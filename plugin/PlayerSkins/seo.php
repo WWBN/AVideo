e1e255191f4f21fe5f16dee8ec9fc7679ba6c02d
Daniel Neto <me@danielneto.com>
2023-08-17 18:22:25 -0300

    update

--- plugin/PlayerSkins/seo.php
+++ plugin/PlayerSkins/seo.php
@@ -7,9 +7,24 @@ $videos_id = getVideos_id();
 if (empty($videos_id)) {
     return '';
 }
+function extractKeywords($description) {
+    // List of common stopwords
+    $stopWords = ["a", "about", "above", "after", "again", "against", "all", "am", "an", "and", "any", "are", "as", "at", "be", "because", "been", "before", "being", "below", "between", "both", "but", "by", "could", "did", "do", "does", "doing", "down", "during", "each", "few", "for", "from", "further", "had", "has", "have", "having", "he", "he'd", "he'll", "he's", "her", "here", "here's", "hers", "herself", "him", "himself", "his", "how", "how's", "i", "i'd", "i'll", "i'm", "i've", "if", "in", "into", "is", "it", "it's", "its", "itself", "let's", "me", "more", "most", "my", "myself", "nor", "of", "on", "once", "only", "or", "other", "ought", "our", "ours", "ourselves", "out", "over", "own", "same", "she", "she'd", "she'll", "she's", "should", "so", "some", "such", "than", "that", "that's", "the", "their", "theirs", "them", "themselves", "then", "there", "there's", "these", "they", "they'd", "they'll", "they're", "they've", "this", "those", "through", "to", "too", "under", "until", "up", "very", "was", "we", "we'd", "we'll", "we're", "we've", "were", "what", "what's", "when", "when's", "where", "where's", "which", "while", "who", "who's", "whom", "why", "why's", "with", "would", "you", "you'd", "you'll", "you're", "you've", "your", "yours", "yourself", "yourselves"];
+
+    // Convert description to lowercase and tokenize into words
+    $words = str_word_count(strtolower($description), 1);
+
+    // Filter out stopwords and get unique words
+    $keywords = array_diff($words, $stopWords);
+
+    // Return the unique keywords
+    return array_unique($keywords);
+}
 $video = new Video('', '', $videos_id);
 $keywords = strip_tags($advancedCustom->keywords);
 $relatedVideos = Video::getRelatedMovies($videos_id);
+$keywords2 = extractKeywords($video->getDescription());
+$keywords3 = implode(', ', $keywords2);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -19,14 +34,14 @@ $relatedVideos = Video::getRelatedMovies($videos_id);
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="device_id" content="<?php echo getDeviceID(); ?>">
-    <meta name="keywords" content=<?php printJSString($keywords); ?>>
+    <meta name="keywords" content=<?php printJSString($keywords.$keywords3); ?>>
     <link rel="manifest" href="<?php echo $global['webSiteRootURL']; ?>manifest.json">
     <link rel="apple-touch-icon" sizes="180x180" href="<?php echo $config->getFavicon(true); ?>">
     <link rel="icon" type="image/png" href="<?php echo $config->getFavicon(true); ?>">
     <link rel="shortcut icon" href="<?php echo $config->getFavicon(); ?>" sizes="16x16,24x24,32x32,48x48,144x144">
     <meta name="msapplication-TileImage" content="<?php echo $config->getFavicon(true); ?>">
     <meta name="robots" content="index, follow" />
-    <meta name="description" content="<?php echo getSEODescription($video->getTitle()); ?>">
+    <meta name="description" content="<?php echo getSEODescription($video->getDescription()); ?>">
     <?php
     getOpenGraph($videos_id);
     ?>
