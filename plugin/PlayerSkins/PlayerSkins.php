26c66746d3ee858ec781ef1597ca3b11cae4394d
Daniel Neto <me@danielneto.com>
2023-09-04 12:47:16 -0300

    Update

--- plugin/PlayerSkins/PlayerSkins.php
+++ plugin/PlayerSkins/PlayerSkins.php
@@ -365,7 +365,7 @@ class PlayerSkins extends PluginAbstract {
             }
             $videos_id = getVideos_id();
 
-            $event = "updateMediaSessionMetadata();";
+            $event = "if(typeof updateMediaSessionMetadata === \"function\"){updateMediaSessionMetadata();}";
             PlayerSkins::getStartPlayerJS($event);
             if (!empty($videos_id) && Video::getEPG($videos_id)) {
                 PlayerSkins::getStartPlayerJS(file_get_contents("{$global['systemRootPath']}plugin/PlayerSkins/epgButton.js"));
