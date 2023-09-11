d118c8db5ad0d0e9fc67c3e2a930139e0cab3f2c
Daniel Neto <me@danielneto.com>
2023-09-11 17:14:52 -0300

    Update

--- plugin/Chromecast/footer.php
+++ plugin/Chromecast/footer.php
@@ -1,10 +1,10 @@
-<script type="text/javascript" src="https://www.gstatic.com/cv/js/sender/v1/cast_sender.js?loadCastFramework=1"></script>
 <script>
     window.SILVERMINE_VIDEOJS_CHROMECAST_CONFIG = {
         preloadWebComponents: true,
     };
 </script>
 <script src="<?php echo getURL('node_modules/@silvermine/videojs-chromecast/dist/silvermine-videojs-chromecast.min.js'); ?>" type="text/javascript"></script>
+<script type="text/javascript" src="https://www.gstatic.com/cv/js/sender/v1/cast_sender.js?loadCastFramework=1"></script>
 <script>
     <?php
     echo PlayerSkins::getStartPlayerJS('player.chromecast();',",controls: true,techOrder: ['chromecast', 'html5'], plugins: {chromecast: {}}");
