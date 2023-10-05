3ecc58e7e38adbee6f61f94a60641c4c33feac08
Daniel Neto <me@danielneto.com>
2023-10-05 15:56:50 -0300

    Update

--- view/report1.json.php
+++ view/report1.json.php
@@ -10,27 +10,34 @@ require_once $global['systemRootPath'] . 'objects/video_statistic.php';
 session_write_close();
 $from = date("Y-m-d 00:00:00", strtotime($_POST['dateFrom']));
 $to = date('Y-m-d 23:59:59', strtotime($_POST['dateTo']));
+_error_log("report1: from=$from to=$to");
 if ($config->getAuthCanViewChart() == 0) {
     // list all channels
     if (User::isAdmin()) {
+        _error_log("report1: line=".__LINE__);
         $users = Channel::getChannels();
     } elseif (User::isLogged()) {
+        _error_log("report1: line=".__LINE__);
         $users = [['id'=> User::getId()]];
     } else {
+        _error_log("report1: line=".__LINE__);
         $users = [];
     }
 } elseif ($config->getAuthCanViewChart() == 1) {
     if ((!empty($_SESSION['user']['canViewChart']))||(User::isAdmin())) {
+        _error_log("report1: line=".__LINE__);
         $users = Channel::getChannels();
     }
 }
 
+_error_log("report1: users=".count($users));
 $rows = [];
 foreach ($users as $key => $value) {
     // list all videos on that channel
     $videos = Video::getAllVideosLight("a", $value['id']);
     $identification = User::getNameIdentificationById($value['id']);
     $views = 0;
+    _error_log("report1: users_id=".$value['id'].' total videos='.count($videos));
     foreach ($videos as $key2 => $value2) {
         $views+=VideoStatistic::getStatisticTotalViews($value2['id'], false, $from, $to);
     }
