38a768f5939ee6bb024b17e46c417bfeaaf1d90d
Daniel Neto <me@danielneto.com>
2023-10-05 16:18:02 -0300

    Update

--- view/reportMyVideos.php
+++ view/reportMyVideos.php
@@ -13,47 +13,47 @@
     </div>
     <?php
     if (Permissions::canAdminUsers()) {
-        ?>
+    ?>
         <div class="form-group col-sm-3">
             <input id="inputUserOwner" placeholder="<?php echo __("User"); ?>" class="form-control">
             <input type="hidden" id="inputUserOwner_id">
             <script>
-                $(document).ready(function () {
+                $(document).ready(function() {
                     $("#inputUserOwner").autocomplete({
                         minLength: 0,
-                        source: function (req, res) {
+                        source: function(req, res) {
                             $.ajax({
                                 url: '<?php echo $global['webSiteRootURL']; ?>objects/users.json.php',
                                 type: "POST",
                                 data: {
                                     searchPhrase: req.term
                                 },
-                                success: function (data) {
+                                success: function(data) {
                                     res(data.rows);
                                 }
                             });
                         },
-                        focus: function (event, ui) {
+                        focus: function(event, ui) {
                             $("#inputUserOwner").val(ui.item.user);
                             return false;
                         },
-                        select: function (event, ui) {
+                        select: function(event, ui) {
                             $("#inputUserOwner").val(ui.item.user);
                             $("#inputUserOwner_id").val(ui.item.id);
                             return false;
                         }
-                    }).autocomplete("instance")._renderItem = function (ul, item) {
+                    }).autocomplete("instance")._renderItem = function(ul, item) {
                         return $("<li>").append("<div>" + item.creator + item.email + "</div>").appendTo(ul);
                     };
                 });
             </script>
         </div>
-        <?php
+    <?php
     }
     ?>
     <div class="form-group col-sm-3">
         <button class="btn btn-primary" id="refreshMyVideosRep"><i class="fa fa-refresh"></i> <?php echo __('Refresh'); ?></button>
-        <button onclick="getVideoViewsAndWatchingTimeCSV();" class="btn btn-primary" >
+        <button onclick="getVideoViewsAndWatchingTimeCSV();" class="btn btn-primary">
             <i class="fas fa-file-csv"></i> <?php echo __('CSV File'); ?>
         </button>
     </div>
@@ -93,15 +93,25 @@
 
         document.location = url;
     }
-    $(document).ready(function () {
+    $(document).ready(function() {
         $("#datefromVideosRep").datepicker();
         $("#datefromVideosRep").datepicker("setDate", "<?php echo date("m/d/Y", strtotime("-30 days")); ?>");
         $("#datetoVideosRep").datepicker();
         $("#datetoVideosRep").datepicker("setDate", "<?php echo date("m/d/Y"); ?>");
 
-        $('#refreshMyVideosRep').click(function () {
+        $('#refreshMyVideosRep').click(function() {
             $('#dtMyVideosRep').DataTable().ajax.reload();
         });
+
+    });
+
+    function loadReportMyVideos() {
+        if (!$('#dtMyVideosRep').is(':visible')) {
+            setTimeout(function() {
+                loadReport1();
+            }, 3000);
+            return false;
+        }
 
         $('#dtMyVideosRep').DataTable({
             "language": {
@@ -133,8 +143,9 @@
                 'url': "<?php echo $global['webSiteRootURL']; ?>view/reportMyVideos.json.php",
                 'data': getDataFromVideoRep,
             },
-            "footerCallback": function (row, data, start, end, display) {
-                var api = this.api(), data;
+            "footerCallback": function(row, data, start, end, display) {
+                var api = this.api(),
+                    data;
 
                 // Update footer by showing the total with the reference of the column index
                 var totalViewsAllVideos = 0;
@@ -148,13 +159,19 @@
                 $(api.column(1).footer()).html(totalViewsAllVideos);
                 $(api.column(2).footer()).html(totalWatchingTimeAllVideosHuman);
             },
-            "columns": [
-                {"data": "title", render: function (data, type, row) {
+            "columns": [{
+                    "data": "title",
+                    render: function(data, type, row) {
                         return '<button class="btn btn-default btn-block" onclick="avideoModalIframe(webSiteRootURL +\'view/videoViewsInfo.php?videos_id=' + row.videos_id + '\');return false;">' + row.title + '</button>';
-                    }},
-                {"data": "total_views"},
-                {"data": "seconds_watching_video_human"},
+                    }
+                },
+                {
+                    "data": "total_views"
+                },
+                {
+                    "data": "seconds_watching_video_human"
+                },
             ]
         });
-    });
+    }
 </script>
