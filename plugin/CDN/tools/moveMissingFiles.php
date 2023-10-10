56eba82c4d9308bb4baa65e4e52dafe69a746424
Daniel Neto <me@danielneto.com>
2023-10-10 10:19:14 -0300

    Update

--- plugin/CDN/tools/moveMissingFiles.php
+++ plugin/CDN/tools/moveMissingFiles.php
@@ -62,7 +62,7 @@ if ($res != false) {
                 continue;
             }
             if ($last['acumulativeFilesize']<10000) {
-                echo "{$info} videos_id = {$row['id']} too small size status={$row['status']} {$last['acumulativeFilesize']} {$localList['local_path']} ". humanFileSize($last['acumulativeFilesize']). PHP_EOL;
+                echo "{$info} videos_id = {$row['id']} too small size status={$row['status']} {$last['acumulativeFilesize']} ". humanFileSize($last['acumulativeFilesize']). PHP_EOL;
                 if($last['acumulativeFilesize']<50){
                    CDNStorage::deleteLog($row['id']); 
                 }
