8d2815d377a63b00d507f9e3aaeb2530611d2d16
Daniel Neto <me@danielneto.com>
2023-11-13 14:27:30 -0300

    Update

--- objects/videoDelete.json.php
+++ objects/videoDelete.json.php
@@ -16,12 +16,10 @@ $id = 0;
 $obj = new stdClass();
 $obj->error = true;
 $obj->msg = '';
-$obj->videos_id = '';
 $obj->users_id = User::getId();
 foreach ($_POST['id'] as $value) {
     $video = new Video("", "", $value);
-    $obj->videos_id = $video->getId();
-    if(empty($obj->videos_id) || $obj->videos_id != User::getId()){
+    if(empty($video->getUsers_id()) || $video->getUsers_id() != User::getId()){
         if (!$video->userCanManageVideo()) {
             $obj->msg = __('You can not Manage This Video');
             die(_json_encode($obj));
