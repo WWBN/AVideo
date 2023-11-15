28ebadb5e56c8e0a8ff2e8226185df1bb282abac
Daniel Neto <me@danielneto.com>
2023-11-15 10:11:03 -0300

    Update

--- objects/functionsOpenGraph.php
+++ objects/functionsOpenGraph.php
@@ -348,16 +348,22 @@ function getOpenGraphLive()
     global $global, $config, $advancedCustom;
     $isLive = isLive();
     $liveT = new LiveTransmition($isLive['liveLink']);
-    $poster = Live::getPosterImage($liveT->getUsers_id(), $isLive['live_servers_id'], $isLive['live_schedule']);
+    $users_id = $liveT->getUsers_id();
+    $poster = Live::getPosterImage($users_id, $isLive['live_servers_id'], $isLive['live_schedule']);
     $liveStreamObject = new LiveStreamObject($isLive['key'], $isLive['live_servers_id'], $isLive['live_index'], 0);
-    echo PHP_EOL . "<!-- OpenGraph Live -->" . PHP_EOL;
+    echo PHP_EOL . "<!-- OpenGraph Live users_id={$users_id} -->" . PHP_EOL;
     $videoType = '';
     $modifiedDate = '';
     $createdDate = '';
     $title = $liveT->getTitle();
     if(empty($title)){
-        Live::getTitleFromUsers_Id($liveT->getUsers_id());
+        Live::getTitleFromUsers_Id($users_id);
     }
+
+    if(empty($title)){
+        echo PHP_EOL . "<!-- OpenGraph title is empty -->" . PHP_EOL;
+    }
+
     $description = $liveT->getDescription();
     $duration_in_seconds = 0;
     $sourceFileURL = $liveStreamObject->getM3U8(true);
