38c20d8c16ded46c83c44b98e2d2b26d8c62677f
Daniel Neto <me@danielneto.com>
2023-11-17 19:29:07 -0300

    Update

--- install/liveUnfinishAllFromStats.php
+++ install/liveUnfinishAllFromStats.php
@@ -6,6 +6,27 @@ if (!isCommandLineInterface()) {
     return die('Command Line only');
 }
 AVideoPlugin::loadPlugin('Live');
+
+
+
+
+$obj = AVideoPlugin::getObjectData("Live");
+var_dump(__LINE__, $obj->server_type->value);
+$rows = LiveTransmitionHistory::getActiveLiveFromUser(0, '', '', 50);
+var_dump(__LINE__, $rows);
+foreach ($rows as $value) {
+    var_dump(__LINE__, is_array($value));
+}
+$getStatsLive = Live::_getStats(0, true);
+var_dump(__LINE__, $getStatsLive);
+
+
+
+exit;
+
+
+
+
 //$p = AVideoPlugin::loadPlugin("Live");
 //$xml = $p->getStatsObject($live_servers_id, true);
 //var_dump(url_get_contents('https://veestreamz.com:8443/stat', '', 4));exit;
