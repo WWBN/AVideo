d68427992fab4e98b85d7b70ec8e5b36f9ba602e
Daniel Neto <me@danielneto.com>
2023-11-27 09:37:33 -0300

    Update

--- install/test.php
+++ install/test.php
@@ -3,7 +3,7 @@
 require_once '../videos/configuration.php';
 AVideoPlugin::loadPlugin('YPTStorage');
 if (!isCommandLineInterface()) {
-    return die('Command Line only');
+    //return die('Command Line only');
 }
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
