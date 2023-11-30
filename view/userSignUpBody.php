0371606ddb1bbc32826f508b4ae3ae8207155ee8
Daniel Neto <me@danielneto.com>
2023-11-30 10:19:20 -0300

    Update invalid user message

--- view/userSignUpBody.php
+++ view/userSignUpBody.php
@@ -25,9 +25,9 @@ if (!empty($advancedCustomUser->afterSignUpGoToURL) && isValidURL($advancedCusto
 $siteRedirectUri = addQueryStringParameter($siteRedirectUri, 'success', _($advancedCustomUser->signupWelcomeAlert));
 ?>
 <div class="panel panel-default <?php
-echo getCSSAnimationClassAndStyle();
-getCSSAnimationClassAndStyleAddWait(0.5);
-?>">
+                                echo getCSSAnimationClassAndStyle();
+                                getCSSAnimationClassAndStyleAddWait(0.5);
+                                ?>">
     <div class="panel-heading tabbable-line">
         <ul class="nav nav-tabs" id="signupNavTabs">
             <li class="nav-item active" id="sinupBasic">
@@ -38,14 +38,14 @@ getCSSAnimationClassAndStyleAddWait(0.5);
             </li>
             <?php
             if (empty($advancedCustomUser->disableCompanySignUp)) {
-                ?>
+            ?>
                 <li class="nav-item" id="sinupCompany">
                     <a class="nav-link " href="#" data-toggle="tab" onclick="showCompanyFields(true)">
                         <i class="fas fa-building"></i>
                         <?php echo __("Company Sign Up"); ?>
                     </a>
                 </li>
-                <?php
+            <?php
             }
             ?>
         </ul>
@@ -53,7 +53,7 @@ getCSSAnimationClassAndStyleAddWait(0.5);
     <div class="panel-body">
 
         <div class="tab-content" id="signupTabContent">
-            <div class="tab-pane active" id="signupRegular" >
+            <div class="tab-pane active" id="signupRegular">
 
                 <form id="updateUserForm" onsubmit="">
                     <div class="form-group">
@@ -72,28 +72,28 @@ getCSSAnimationClassAndStyleAddWait(0.5);
                         <div class="col-sm-8 inputGroupContainer">
                             <div class="input-group">
                                 <span class="input-group-addon"><i class="glyphicon glyphicon-pencil"></i></span>
-                                <input  id="inputName" placeholder="<?php echo __("Name"); ?>" name="name" class="form-control"  type="text" value="" required >
+                                <input id="inputName" placeholder="<?php echo __("Name"); ?>" name="name" class="form-control" type="text" value="" required>
                             </div>
                         </div>
                     </div>
                     <div class="row form-group <?php echo getCSSAnimationClassAndStyle(); ?>">
-                        <label class="col-sm-4 control-label hidden-xs" for="inputUser"><?php echo!empty($advancedCustomUser->forceLoginToBeTheEmail) ? __("E-mail") : __("User"); ?></label>
+                        <label class="col-sm-4 control-label hidden-xs" for="inputUser"><?php echo !empty($advancedCustomUser->forceLoginToBeTheEmail) ? __("E-mail") : __("User"); ?></label>
                         <div class="col-sm-8 inputGroupContainer">
                             <div class="input-group">
                                 <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
-                                <input  id="inputUser" placeholder="<?php echo!empty($advancedCustomUser->forceLoginToBeTheEmail) ? "me@example.com" : __("User"); ?>" class="form-control"  type="<?php echo empty($advancedCustomUser->forceLoginToBeTheEmail) ? "text" : "email"; ?>" value="" name="user" required >
+                                <input id="inputUser" placeholder="<?php echo !empty($advancedCustomUser->forceLoginToBeTheEmail) ? "me@example.com" : __("User"); ?>" class="form-control" type="<?php echo empty($advancedCustomUser->forceLoginToBeTheEmail) ? "text" : "email"; ?>" value="" name="user" required>
                             </div>
                         </div>
                     </div>
                     <?php
                     if (empty($advancedCustomUser->forceLoginToBeTheEmail)) {
-                        ?>
+                    ?>
                         <div class="row form-group <?php echo getCSSAnimationClassAndStyle(); ?>">
                             <label class="col-sm-4 control-label hidden-xs" for="inputEmail"><?php echo __("E-mail"); ?></label>
                             <div class="col-sm-8 inputGroupContainer">
                                 <div class="input-group">
                                     <span class="input-group-addon"><i class="glyphicon glyphicon-envelope"></i></span>
-                                    <input  id="inputEmail" placeholder="<?php echo __("E-mail"); ?>" name="email" class="form-control"  type="email" value="" required >
+                                    <input id="inputEmail" placeholder="<?php echo __("E-mail"); ?>" name="email" class="form-control" type="email" value="" required>
                                 </div>
                             </div>
                         </div>
@@ -101,13 +101,13 @@ getCSSAnimationClassAndStyleAddWait(0.5);
                     ?>
                     <?php
                     if (empty($advancedCustomUser->doNotShowPhoneOnSignup)) {
-                        ?>
+                    ?>
                         <div class="row form-group <?php echo getCSSAnimationClassAndStyle(); ?>">
                             <label class="col-sm-4 control-label hidden-xs" for="phone"><?php echo __("Phone"); ?></label>
                             <div class="col-sm-8 inputGroupContainer">
                                 <div class="input-group">
                                     <span class="input-group-addon"><i class="fas fa-phone"></i></span>
-                                    <input  id="phone" placeholder="<?php echo __("Phone"); ?>" name="phone" class="form-control"  type="text" value="" >
+                                    <input id="phone" placeholder="<?php echo __("Phone"); ?>" name="phone" class="form-control" type="text" value="">
                                 </div>
                             </div>
                         </div>
@@ -145,21 +145,21 @@ getCSSAnimationClassAndStyleAddWait(0.5);
                         if (Users_extra_info::isCompanyOnlyField($value['status'])) {
                             $class = 'companyField';
                             $style = 'display: none;';
-                            ?>
+                    ?>
                             <div class="row form-group <?php echo $class; ?>" style="<?php echo $style; ?>">
                                 <?php echo Users_extra_info::typeToHTML($value, 'col-sm-4 control-label hidden-xs', 'col-sm-8 inputGroupContainer'); ?>
                             </div>
-                            <?php
+                        <?php
                         } else {
-                            ?>
-                            <div class="row form-group <?php echo getCSSAnimationClassAndStyle(); ?>" >
+                        ?>
+                            <div class="row form-group <?php echo getCSSAnimationClassAndStyle(); ?>">
                                 <?php echo Users_extra_info::typeToHTML($value, 'col-sm-4 control-label hidden-xs', 'col-sm-8 inputGroupContainer'); ?>
                             </div>
-                            <?php
+                        <?php
                         }
                         ?>
                         <div class="clearfix"></div>
-                        <?php
+                    <?php
                     }
                     ?>
                     <div class="clearfix"></div>
@@ -187,17 +187,17 @@ getCSSAnimationClassAndStyleAddWait(0.5);
 
     </div>
     <div class="panel-footer">
-        <button class="btn btn-primary btn-block btn-lg <?php echo getCSSAnimationClassAndStyle(); ?>" onclick="$('#updateUserForm').submit();" ><i class="fas fa-user-plus"></i> <?php echo __("Sign Up"); ?></button>
+        <button class="btn btn-primary btn-block btn-lg <?php echo getCSSAnimationClassAndStyle(); ?>" onclick="$('#updateUserForm').submit();"><i class="fas fa-user-plus"></i> <?php echo __("Sign Up"); ?></button>
         <?php
         if (!empty($redirectUri)) {
             if (isAVideoMobileApp()) {
-                ?>
-                <button onclick="sendAVideoMobileMessage('pop', {title:'', description:''});" class="btn btn-default btn-block btn-xs <?php echo getCSSAnimationClassAndStyle(); ?>" ><i class="fas fa-times"></i> <?php echo __("Cancel"); ?></button>
-                <?php
+        ?>
+                <button onclick="sendAVideoMobileMessage('pop', {title:'', description:''});" class="btn btn-default btn-block btn-xs <?php echo getCSSAnimationClassAndStyle(); ?>"><i class="fas fa-times"></i> <?php echo __("Cancel"); ?></button>
+            <?php
             } else {
-                ?>
-                <a href="<?php echo $redirectUri; ?>" class="btn btn-default btn-block btn-xs <?php echo getCSSAnimationClassAndStyle(); ?>" ><i class="fas fa-times"></i> <?php echo __("Cancel"); ?></a>
-                <?php
+            ?>
+                <a href="<?php echo $redirectUri; ?>" class="btn btn-default btn-block btn-xs <?php echo getCSSAnimationClassAndStyle(); ?>"><i class="fas fa-times"></i> <?php echo __("Cancel"); ?></a>
+        <?php
             }
         }
         ?>
@@ -221,7 +221,7 @@ getCSSAnimationClassAndStyleAddWait(0.5);
         var errorFound = false;
         var errorClass = 'has-error';
         $('#updateUserForm .input-group').removeClass(errorClass);
-        $('#updateUserForm input').each(function () {
+        $('#updateUserForm input').each(function() {
             console.log('found', $(this).attr('name'));
             if ($(this).prop('required') && $(this).is(":visible")) {
                 if ($(this).attr('type') === 'checkbox') {
@@ -269,10 +269,9 @@ getCSSAnimationClassAndStyleAddWait(0.5);
         }
         if (!$('#inputUser').val().match(/^[0-9a-z@._-]{3,}$/i) && !isEmailValid($('#inputUser').val())) {
             $('#inputUser').closest('.input-group').addClass(errorClass);
-            avideoAlertError(__("Invalid user"));
+            avideoAlertError(__("Invalid username. Your username must be at least 3 characters long and can only contain letters, numbers, and symbols (@ . _ -). If you're trying to use an email, please ensure it's a valid email address."));
             return false;
         }
-
         return true;
     }
 
@@ -291,13 +290,13 @@ getCSSAnimationClassAndStyleAddWait(0.5);
         return true;
     }
 
-    $(document).ready(function () {
-        $('#inputPasswordConfirm').keyup(function () {
+    $(document).ready(function() {
+        $('#inputPasswordConfirm').keyup(function() {
             checkIfPasswordsMatch();
         });
         passStrengthCheckInputKeyUp('#inputPassword');
 
-        $('#updateUserForm').submit(function (evt) {
+        $('#updateUserForm').submit(function(evt) {
             evt.preventDefault();
             if (validateSignupForm()) {
                 modal.showPleaseWait();
@@ -305,16 +304,16 @@ getCSSAnimationClassAndStyleAddWait(0.5);
                     url: webSiteRootURL + 'objects/userCreate.json.php?PHPSESSID=<?php echo session_id(); ?>',
                     data: $('#updateUserForm').serialize(),
                     type: 'post',
-                    success: function (response) {
+                    success: function(response) {
                         avideoResponse(response);
                         if (!response.error) {
-<?php
-if (isAVideoMobileApp()) {
-    echo "sendAVideoMobileMessage('pop',  {title:'Success', description:''});";
-} else {
-    echo "window.location.href = '{$siteRedirectUri}';";
-}
-?>
+                            <?php
+                            if (isAVideoMobileApp()) {
+                                echo "sendAVideoMobileMessage('pop',  {title:'Success', description:''});";
+                            } else {
+                                echo "window.location.href = '{$siteRedirectUri}';";
+                            }
+                            ?>
                         } else {
                             modal.hidePleaseWait();
                         }
