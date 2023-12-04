711cebe32f2dc0ce602026ce6cee03117189ff34
Daniel Neto <me@danielneto.com>
2023-12-04 11:25:57 -0300

    Update

--- plugin/Gallery/view/modeGallery.php
+++ plugin/Gallery/view/modeGallery.php
@@ -1,12 +1,9 @@
 <?php
 $isFirstPage = 1;
-echo '<!-- page='. (@$_GET['page']) .' line='.__LINE__.' file='.basename(__FILE__).' -->';
 include $global['systemRootPath'] . 'plugin/Gallery/view/topLogic.php';
-echo '<!-- page='. (@$_GET['page']) .' line='.__LINE__.' file='.basename(__FILE__).' -->';
 if (empty($_REQUEST['catName'])) {
     $leaderBoardTop = getAdsLeaderBoardTop();
 }
-echo '<!-- page='. (@$_GET['page']) .' line='.__LINE__.' file='.basename(__FILE__).' -->';
 $_page = new Page(array($siteTitle), 'mainPage');
 ?>
 <div class="<?php echo Gallery::getContaierClass('avideoLoadPage'); ?>">
@@ -16,16 +13,12 @@ $_page = new Page(array($siteTitle), 'mainPage');
     } else {
         echo '<!-- getAdsLeaderBoardTop is empty -->';
     }
-    echo '<!-- page='. (@$_GET['page']) .' line='.__LINE__.' file='.basename(__FILE__).' -->';
     ?>
     <div class="panel panel-default">
         <div class="panel-body" style="overflow: hidden;">
             <?php
-        echo '<!-- page='. (@$_GET['page']) .' line='.__LINE__.' file='.basename(__FILE__).' -->';
             include_once $global['systemRootPath'] . 'view/include/categoryTop.php';
-            echo '<!-- page='. (@$_GET['page']) .' line='.__LINE__.' file='.basename(__FILE__).' -->';
             include $global['systemRootPath'] . 'plugin/Gallery/view/mainArea.php';
-            echo '<!-- page='. (@$_GET['page']) .' line='.__LINE__.' file='.basename(__FILE__).' -->';
             ?>
         </div>
     </div>
