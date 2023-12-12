13b1bae32a0c627f9ce59f05195ab6627a06712d
Daniel Neto <me@danielneto.com>
2023-12-12 15:51:02 -0300

    https://github.com/WWBN/AVideo/issues/8720#issuecomment-1852070788

--- plugin/PlayLists/actionButton.php
+++ plugin/PlayLists/actionButton.php
@@ -2,6 +2,7 @@
 global $advancedCustom;
 $crc = uniqid();
 doNOTOrganizeHTMLIfIsPagination();
+$global['laodPlaylistScript'] = 1;
 ?>
     <?php if ((empty($_POST['disableAddTo'])) && (( ($advancedCustom != false) && ($advancedCustom->disableShareAndPlaylist == false)) || ($advancedCustom == false))) { ?>
        <a href="#" class="<?php echo $btnClass; ?>" id="addBtn<?php echo $videos_id . $crc; ?>" onclick="loadPlayLists('<?php echo $videos_id; ?>', '<?php echo $crc; ?>');return false;" data-toggle="tooltip" title="<?php echo __("Add to"); ?>">
