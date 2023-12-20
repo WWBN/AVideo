4beef95ecd320ec75ec919655d6d5c3de8b04d5a
Daniel Neto <me@danielneto.com>
2023-12-20 11:17:10 -0300

    Update

--- plugin/PlayLists/install/updateV3.0.sql
+++ plugin/PlayLists/install/updateV3.0.sql
@@ -1,3 +1,3 @@
 ALTER TABLE `playlists_schedules`
-CHANGE `start_datetime` `start_datetime` INT NULL,
-CHANGE `finish_datetime` `finish_datetime` INT NULL;
+CHANGE `start_datetime` `start_datetime` BIGINT NULL,
+CHANGE `finish_datetime` `finish_datetime` BIGINT NULL;
