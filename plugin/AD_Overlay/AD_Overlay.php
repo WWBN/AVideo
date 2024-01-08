3db58629437855194c16fec7a75831a9a6bd268e
Daniel Neto <me@danielneto.com>
2024-01-08 11:05:38 -0300

    Update

--- plugin/AD_Overlay/AD_Overlay.php
+++ plugin/AD_Overlay/AD_Overlay.php
@@ -3,9 +3,11 @@
 require_once $global['systemRootPath'] . 'plugin/Plugin.abstract.php';
 require_once $global['systemRootPath'] . 'plugin/AD_Overlay/Objects/AD_Overlay_Code.php';
 
-class AD_Overlay extends PluginAbstract {
+class AD_Overlay extends PluginAbstract
+{
 
-    public function getTags() {
+    public function getTags()
+    {
         return array(
             PluginTags::$MONETIZATION,
             PluginTags::$ADS,
@@ -14,26 +16,31 @@ class AD_Overlay extends PluginAbstract {
         );
     }
 
-    public function getDescription() {
+    public function getDescription()
+    {
         $txt = "Display simple overlays - similar to YouTube's \"Annotations\" feature in appearance - during video playback.";
         $help = "<br><small><a href='https://github.com/WWBN/AVideo/wiki/AD_Overlay-Plugin' target='_blank'><i class='fas fa-question-circle'></i> Help</a></small>";
 
         return $txt . $help;
     }
 
-    public function getName() {
+    public function getName()
+    {
         return "AD_Overlay";
     }
 
-    public function getUUID() {
+    public function getUUID()
+    {
         return "ADO73225-3807-4167-ba81-0509dd280e06";
     }
 
-    public function getPluginVersion() {
+    public function getPluginVersion()
+    {
         return "2.1";
     }
 
-    public function getEmptyDataObject() {
+    public function getEmptyDataObject()
+    {
         global $global;
         $obj = new stdClass();
 
@@ -91,8 +98,9 @@ class AD_Overlay extends PluginAbstract {
         return $obj;
     }
 
-    public function getHeadCode() {
-        if(self::showAdsOverlay() ){
+    public function getHeadCode()
+    {
+        if (!self::showAdsOverlay()) {
             return '';
         }
         $videos_id = getVideos_id();
@@ -109,7 +117,7 @@ class AD_Overlay extends PluginAbstract {
         if (!empty($obj->adWidth) && !empty($obj->adHeight)) {
             $style = "width: $obj->adWidth; height: width: $obj->adHeight;";
         }
-        $css = '<link href="' .getURL('node_modules/videojs-overlay/dist/videojs-overlay.css') . '" rel="stylesheet" type="text/css"/>';
+        $css = '<link href="' . getURL('node_modules/videojs-overlay/dist/videojs-overlay.css') . '" rel="stylesheet" type="text/css"/>';
 
         $css .= '<style>#adOverlay{min-width: 640px;}.video-js .vjs-overlay-background, .video-js .vjs-overlay-no-background {
     max-height: 50%;
@@ -122,13 +130,15 @@ class AD_Overlay extends PluginAbstract {
         return $css;
     }
 
-    static function showAdsOverlay() {
+    static function showAdsOverlay()
+    {
         global $global;
         return empty($global['ignoreADOverlay']);
     }
 
-    public function getFooterCode() {
-        if(self::showAdsOverlay() ){
+    public function getFooterCode()
+    {
+        if (!self::showAdsOverlay()) {
             return '';
         }
         global $global, $video;
@@ -178,10 +188,10 @@ class AD_Overlay extends PluginAbstract {
         $ad = AVideoPlugin::getObjectData('ADs');
 
         $js = '<div id="adOverlay" style="display:none;"><button class="pull-right btn" onclick="$(\'.vjs-overlay\').fadeOut();"><i class="fa fa-times"></i></button>'
-                . '<center>' . ADs::giveGoogleATimeout($adText) . '</center>'
-                . '</div>';
+            . '<center>' . ADs::giveGoogleATimeout($adText) . '</center>'
+            . '</div>';
 
-        $js .= '<script src="' .getURL('node_modules/videojs-overlay/dist/videojs-overlay.min.js') . '" type="text/javascript"></script>';
+        $js .= '<script src="' . getURL('node_modules/videojs-overlay/dist/videojs-overlay.min.js') . '" type="text/javascript"></script>';
 
         $onPlayerReady = "setTimeout(function(){
                         \$('#cbb').click(function() {
@@ -210,7 +220,8 @@ class AD_Overlay extends PluginAbstract {
         return $js;
     }
 
-    public static function profileTabName($users_id) {
+    public static function profileTabName($users_id)
+    {
         global $global;
         if (!User::canUpload()) {
             return '';
@@ -218,7 +229,8 @@ class AD_Overlay extends PluginAbstract {
         include $global['systemRootPath'] . 'plugin/AD_Overlay/profileTabName.php';
     }
 
-    public static function profileTabContent($users_id) {
+    public static function profileTabContent($users_id)
+    {
         global $global;
         if (!User::canUpload()) {
             return '';
@@ -226,7 +238,8 @@ class AD_Overlay extends PluginAbstract {
         include $global['systemRootPath'] . 'plugin/AD_Overlay/profileTabContent.php';
     }
 
-    public function getUsersManagerListButton() {
+    public function getUsersManagerListButton()
+    {
         $btn = "";
         $obj = $this->getDataObject();
         if (!empty($obj->allowUserAds)) {
@@ -235,7 +248,8 @@ class AD_Overlay extends PluginAbstract {
         return $btn;
     }
 
-    private function getAdsFromUserIfActive($users_id) {
+    private function getAdsFromUserIfActive($users_id)
+    {
         $ad = new AD_Overlay_Code(0);
         $ad->loadFromUser($users_id);
         if (!empty($ad->getStatus()) && $ad->getStatus() == 'a') {
@@ -243,5 +257,4 @@ class AD_Overlay extends PluginAbstract {
         }
         return false;
     }
-
 }
