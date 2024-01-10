b6420562f396913c8cc0c70afd5cdf6b6f374618
Daniel Neto <me@danielneto.com>
2024-01-10 14:01:05 -0300

    https://github.com/WWBN/AVideo/issues/8771

--- plugin/Live/latestOrLive.php
+++ plugin/Live/latestOrLive.php
@@ -147,8 +147,8 @@ $objectToReturnToParentIframe->posterURL = $poster;
 ?>
 <!DOCTYPE html>
 <html lang="<?php echo getLanguage(); ?>">
-
 <head>
+    <!-- version 2024-10-01 -->
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
