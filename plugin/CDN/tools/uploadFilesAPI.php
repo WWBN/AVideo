e261160085f683dbb60b73e4c4702c249681fa4b
Daniel Neto <me@danielneto.com>
2024-01-12 14:21:57 -0300

    Update

--- plugin/CDN/tools/uploadFilesAPI.php
+++ plugin/CDN/tools/uploadFilesAPI.php
@@ -22,32 +22,26 @@ set_time_limit($_2hours);
 ini_set('max_execution_time', $_2hours);
 $parts = explode('.', $cdnObj->storage_hostname);
 $apiAccessKey = $cdnObj->storage_password;
-$storageZoneName = $cdnObj->storage_username; // Replace with your storage zone name
-$storageZoneRegion = trim(strtoupper($parts[0])); // Replace with your storage zone region code
+$storageZoneName = $cdnObj->storage_username;
+$storageZoneRegion = trim(strtoupper($parts[0]));
 
 echo ("CDNStorage::APIput line $apiAccessKey, $storageZoneName, $storageZoneRegion [startFromIndex=$startFromIndex]") . PHP_EOL;
 $client = new \Bunny\Storage\Client($apiAccessKey, $storageZoneName, $storageZoneRegion);
 echo ("CDNStorage::APIput line " . __LINE__) . PHP_EOL;
 
-$sql = "SELECT * FROM  videos WHERE 1=1 ORDER BY id ";
+$sql = "SELECT * FROM videos ORDER BY id";
 $res = sqlDAL::readSql($sql, "", [], true);
-echo ("CDNStorage::APIput line " . __LINE__) . PHP_EOL;
 $fullData = sqlDAL::fetchAllAssoc($res);
-echo ("CDNStorage::APIput line " . __LINE__) . PHP_EOL;
 sqlDAL::close($res);
-echo ("CDNStorage::APIput line " . __LINE__) . PHP_EOL;
 
 $secondsInAMinute = 60;
 $secondsInAnHour = 60 * $secondsInAMinute;
 $secondsInADay = 24 * $secondsInAnHour;
 $secondsInAWeek = 7 * $secondsInADay;
-$secondsInAMonth = 30 * $secondsInADay; // Approximate, varies by month
+$secondsInAMonth = 30 * $secondsInADay;
 
-$totalProcessedSize = 0; // Total size of files processed
-$totalProcessedTime = 0; // Total time taken to process the files
-
-$totalProcessedTime = 0; // Total time taken to process videos
-$processedVideosCount = 0; // Number of videos processed
+$totalProcessedTime = 0;
+$processedVideosCount = 0;
 
 if ($res != false) {
     $total = count($fullData);
@@ -57,14 +51,10 @@ if ($res != false) {
             continue;
         }
         $videos_id = $row['id'];
-        $info1 = "videos_id = $videos_id [{$total}, {$key}] ";
+        $info1 = "videos_id = $videos_id [{$totalVideos}, {$key}] ";
         $list = CDNStorage::getFilesListBoth($videos_id);
         $totalFiles = count($list);
         echo ("{$info1} CDNStorage::APIput found {$totalFiles} files for videos_id = $videos_id ") . PHP_EOL;
-        $count = 0;
-        $totalSizeRemaining = array_sum(array_map(function ($value) {
-            return $value['isLocal'] ? filesize($value['local']['local_path']) : 0;
-        }, $list));
 
         foreach ($list as $value) {
             $count++;
@@ -84,29 +74,15 @@ if ($res != false) {
                         echo "$info2 CDNStorage::APIput Upload ERROR " . $th->getMessage() . PHP_EOL;
                     }
                     $endTime = microtime(true);
-                    $timeTaken = $endTime - $startTime; // Time taken in seconds
-                    $totalSizeRemaining -= $filesize; // Update remaining size
-                    $timeTakenFormated = number_format($timeTaken, 1);
-                    $speed = $filesize / $timeTaken; // Bytes per second
-                    $etaForCurrentFile = $totalSizeRemaining / $speed; // ETA in seconds
 
-                    $totalProcessedSize += $filesize; // Update the total processed size
-                    $totalProcessedTime += $timeTaken; // Update the total processed time
+                    $timeTaken = $endTime - $startTime;
+                    $totalProcessedTime += $timeTaken;
                     $processedVideosCount++;
-                    // Calculate the average time per video
+
+                    // Average time per video
                     $averageTimePerVideo = $totalProcessedTime / $processedVideosCount;
-
-                    // Calculate the average speed so far (bytes per second)
-                    $averageSpeed = $totalProcessedSize / $totalProcessedTime;
-
-                    // Estimate the time remaining for the current file
-                    $etaForCurrentFile = ($totalSizeRemaining - $totalProcessedSize) / $averageSpeed;
-                    
-                    $averageETAForThisVideo = ($averageTimePerVideo * $totalFiles);
-                    // Estimate the time remaining for the rest of the videos
-                    $remainingVideos = $total - $key;
-                    $etaForAllVideos = $averageETAForThisVideo * $remainingVideos;
-                    //echo "averageTimePerVideo($averageTimePerVideo)[$totalProcessedTime / $processedVideosCount]: " . @gmdate("H:i:s", $averageTimePerVideo) . " remainingVideos: " . $remainingVideos . PHP_EOL;
+                    $remainingVideos = $totalVideos - $processedVideosCount;
+                    $etaForAllVideos = $averageTimePerVideo * $remainingVideos;
 
                     // Convert the estimated time into a readable format
                     $months = floor($etaForAllVideos / $secondsInAMonth);
@@ -121,10 +97,7 @@ if ($res != false) {
                     $remainingSeconds = (int)$minuteSeconds % $secondsInAMinute;
 
                     $ETA = "{$months}m {$weeks}w {$days}d {$hours}:{$minutes}:{$remainingSeconds}";
-
-                    echo "$info2 {$timeTakenFormated}s, " . humanFileSize($speed) . "/s ".@gmdate("H:i:s", $averageETAForThisVideo)."/".@gmdate("H:i:s", $etaForCurrentFile);
-                    echo  ", Average: " . humanFileSize($averageSpeed) . '/s ';
-                    echo "Final ETA: " . $ETA . PHP_EOL;
+                    echo "Processing video $videos_id completed. ETA for all videos: $ETA" . PHP_EOL;
                 } else {
                     echo ("$info2 CDNStorage::APIput same size {$value['remote']['remote_filesize']} {$value['remote']['relative']}") . PHP_EOL;
                 }
