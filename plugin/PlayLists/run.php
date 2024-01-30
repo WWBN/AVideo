7832cee0fdad3f90d865dcdede4102e386adcec3
Daniel Neto <me@danielneto.com>
2024-01-30 13:02:51 -0300

    Update

--- plugin/PlayLists/run.php
+++ plugin/PlayLists/run.php
@@ -72,29 +72,29 @@ foreach ($rows as $value) {
         continue;
     }
     $processed[] = $value['id'];
-    $ps = Playlists_schedules::getPlaying($value['id']);
     if ($value['finish_datetime'] < time()) {
         _error_log("Playlist rebroadcast line ".__LINE__);
         PlayLists::setScheduleStatus($key, Playlists_schedules::STATUS_COMPLETE);
         continue;
     }
     $forceIndex = Playlists_schedules::getPlayListScheduledIndex($value['id']);
-    $key = Rebroadcaster::getRebroadcastkey($ps->current_videos_id, $forceIndex);
 
     $stats = getStatsNotifications();
     $found = false;
     foreach ($stats["applications"] as $value) {
-       if($value['key'] === $key){
+       if(preg_match("/.*{$forceIndex}$/", $value['key'])){
             $found = true;
             break;
        }
     }
+    //var_dump($value['key'], $found);
     if(!$found){
+        $ps = Playlists_schedules::getPlaying($value['id']);
         $pl = new PlayList($ps->playlists_id);
         $title = $pl->getName() . ' [' . $ps->msg . ']';
         $title = '';
-        _error_log("Playlist rebroadcast executed {$value['id']}");
-        //$response = Rebroadcaster::rebroadcastVideo($ps->current_videos_id, $pl->getUsers_id(), $forceIndex, $title);
+        _error_log("Playlist rebroadcast executing but not found {$value['id']}");
+        $response = Rebroadcaster::rebroadcastVideo($ps->current_videos_id, $pl->getUsers_id(), $forceIndex, $title);
     }
     //var_dump($response, $ps);
 }
