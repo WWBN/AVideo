069650c37553c31d06ed93d6bef0f60d6cb92f3b
Daniel Neto <me@danielneto.com>
2024-02-15 11:19:10 -0300

    Update

--- objects/functiongetCaptcha.php
+++ objects/functiongetCaptcha.php
@@ -8,13 +8,19 @@ if ($forceCaptcha) {
 }
 ?>
 <div class="input-group">
-    <span class="input-group-addon"><img src="<?php echo $url; ?>captcha?cache=<?php echo time(); ?>" id="<?php echo $uid; ?>" style="border-radius: 8px;"></span>
-    <span class="input-group-addon"><span class="btn btn-xs btn-success" id="btnReload<?php echo $uid; ?>"><span class="glyphicon glyphicon-refresh"></span></span></span>
+    <span class="input-group-addon">
+        <img src="<?php echo $url; ?>" id="<?php echo $uid; ?>" style="border-radius: 8px;">
+    </span>
+    <span class="input-group-addon">
+        <span class="btn btn-xs btn-success" id="btnReload<?php echo $uid; ?>">
+            <span class="glyphicon glyphicon-refresh"></span>
+        </span>
+    </span>
     <input name="captcha" placeholder="<?php echo __("Type the code"); ?>" class="form-control" type="text" style="height: 60px;" maxlength="5" id="<?php echo $uid; ?>Text">
 </div>
 <script>
-    $(document).ready(function () {
-        $('#btnReload<?php echo $uid; ?>').click(function () {
+    $(document).ready(function() {
+        $('#btnReload<?php echo $uid; ?>').click(function() {
             var url = '<?php echo $url; ?>';
             url = addQueryStringParameter(url, "cache", Math.random());
             $('#<?php echo $uid; ?>').attr('src', url);
