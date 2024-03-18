a5ab2e853de9502d14969fc8561995b4e894224c
Daniel Neto <me@danielneto.com>
2024-03-18 10:04:26 -0300

    Update

--- view/managerVideos_body.php
+++ view/managerVideos_body.php
@@ -2240,7 +2240,7 @@ if (empty($advancedCustom->disableHTMLDescription)) {
                 data: {
                     "users_id": <?php echo User::getId(); ?>,
                     "videos_id": videos_ids_list,
-                    "rowCount": 20
+                    "rowCount": videos_ids_list.length * 2
                 },
                 type: 'post',
                 success: function(response) {
