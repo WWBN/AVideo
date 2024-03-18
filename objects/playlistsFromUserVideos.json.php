00ffc97b220a642bedeedc69a155892c9e3634e0
Daniel Neto <me@danielneto.com>
2024-03-18 10:16:32 -0300

    Update

--- objects/playlistsFromUserVideos.json.php
+++ objects/playlistsFromUserVideos.json.php
@@ -11,13 +11,14 @@ if (empty($_REQUEST['users_id'])) {
     forbiddenPage('You need a user');
 }
 
-setRowCount(100);
+//setRowCount(100);
 require_once $global['systemRootPath'] . 'videos/configuration.php';
 require_once './playlist.php';
 header('Content-Type: application/json');
 //setDefaultSort('created', 'DESC');
 //mysqlBeginTransaction();
 if (is_array($_REQUEST['videos_id'])) {
+    setRowCount(500/count($_REQUEST['videos_id']));
     $rows = [];
     foreach ($_REQUEST['videos_id'] as $value) {
         $rows[] = ['videos_id' => $value, 'playlists' => PlayList::getAllFromUserVideo($_REQUEST['users_id'], $value, false)];
