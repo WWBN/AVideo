550a060aae278114428163bfce7dcaf7f5fcccff
Daniel Neto <me@danielneto.com>
2024-03-21 17:32:16 -0300

    Update

--- install/unfinishLives.php
+++ install/unfinishLives.php
@@ -7,19 +7,4 @@ if (!isCommandLineInterface()) {
 ob_end_flush();
 AVideoPlugin::loadPlugin('Live');
 
-$stats = Live::getStatsApplications(true);
-
-foreach ($stats as $key => $live) {
-    if (!empty($live['key'])) {
-        echo $live['key'].PHP_EOL;
-        $row = LiveTransmitionHistory::getLatest($live['key'], @$live['live_servers_id']);
-        echo "{$row['id']} [{$row['finished']}] {$row['title']}".PHP_EOL;
-        if (!empty($row['finished'])) {
-            echo "{$row['id']} [{$row['finished']}] Finishing".PHP_EOL;
-            //LiveTransmitionHistory::unfinishFromTransmitionHistoryId($row['id']);
-        }
-    }else{
-        echo "Error {$key2}".PHP_EOL;
-        var_dump($live);
-    }
-}
+Live::checkAllFromStats(true);
