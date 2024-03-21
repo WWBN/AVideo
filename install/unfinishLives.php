aeefbb274d296c9733b7e18ffdb981a900c90fa3
Daniel Neto <me@danielneto.com>
2024-03-21 15:51:55 -0300

    Update

--- install/unfinishLives.php
+++ install/unfinishLives.php
@@ -9,21 +9,17 @@ AVideoPlugin::loadPlugin('Live');
 
 $stats = Live::getStatsApplications(true);
 
-foreach ($stats as $key => $server) {
-    if (is_array($server) || is_object($server)) {
-        foreach ($server as $key2 => $live) {
-            if (!empty($live->key)) {
-                echo $live->key.PHP_EOL;
-                $row = LiveTransmitionHistory::getLatest($live->key, @$live->live_servers_id);
-                echo "{$row['id']} [{$row['finished']}] {$row['title']}".PHP_EOL;
-                if (!empty($row['finished'])) {
-                    echo "{$row['id']} [{$row['finished']}] Finishing".PHP_EOL;
-                    //LiveTransmitionHistory::unfinishFromTransmitionHistoryId($row['id']);
-                }
-            }else{
-                echo "Error {$key2}".PHP_EOL;
-                var_dump($live);
-            }
+foreach ($stats as $key => $live) {
+    if (!empty($live['key'])) {
+        echo $live['key'].PHP_EOL;
+        $row = LiveTransmitionHistory::getLatest($live['key'], @$live['live_servers_id']);
+        echo "{$row['id']} [{$row['finished']}] {$row['title']}".PHP_EOL;
+        if (!empty($row['finished'])) {
+            echo "{$row['id']} [{$row['finished']}] Finishing".PHP_EOL;
+            //LiveTransmitionHistory::unfinishFromTransmitionHistoryId($row['id']);
         }
+    }else{
+        echo "Error {$key2}".PHP_EOL;
+        var_dump($live);
     }
 }
