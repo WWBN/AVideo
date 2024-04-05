3c7bb9e57d99384789e734a7a0a3db0a5e4fc542
Daniel Neto <me@danielneto.com>
2024-04-05 09:58:01 -0300

    Update

--- objects/mysql_dal.php
+++ objects/mysql_dal.php
@@ -203,7 +203,7 @@ class sqlDAL
              *
              */
 
-             _error_log("writeSql [{$stmt->errno}] {$stmt->error} ".' '.json_encode(debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS)));
+             _error_log("writeSql [{$stmt->errno}] {$stmt->error} ".' '.json_encode(debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS))) . ' '.$_SERVER['HTTP_USER_AGENT'];
              if($stmt->errno == 1205 && preg_match('/CachesInDB/', $preparedStatement)){//Lock wait timeout exceeded; try restarting transaction
                 _error_log("writeSql Recreate CachesInDB ");
                 $sql = 'DROP TABLE IF EXISTS `CachesInDB`';
