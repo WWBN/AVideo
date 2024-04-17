7695fd0fee42be1de6b6967dad2a8179cbd7530f
Daniel Neto <me@danielneto.com>
2024-04-17 13:48:23 -0300

    Update

--- plugin/Cache/Objects/CachesInDB.php
+++ plugin/Cache/Objects/CachesInDB.php
@@ -349,6 +349,7 @@ class CachesInDB extends ObjectYPT
         //$sql = "DELETE FROM " . static::getTableName() . " WHERE name LIKE '{$name}%'";
         $sql = "DELETE FROM " . static::getTableName() . " WHERE MATCH(name) AGAINST('{$name}*' IN BOOLEAN MODE);";
         
+        _error_log("CachesInDB::_deleteCacheStartingWith($name)($name) $sql");
         $global['lastQuery'] = $sql;
         //_error_log("Delete Query: ".$sql);
         self::readUncomited();
