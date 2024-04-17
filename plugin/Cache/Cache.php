9f738287b7ee390cd81dc532522a62e1d5ac0953
Daniel Neto <me@danielneto.com>
2024-04-17 12:02:12 -0300

    Update

--- plugin/Cache/Cache.php
+++ plugin/Cache/Cache.php
@@ -390,7 +390,8 @@ class Cache extends PluginAbstract {
             if (!empty($row)) {
                 //$time = getTimeInTimezone(strtotime($row['modified']), $row['timezone']);
                 $time = $row['created_php_time'];
-                if (!empty($lifetime) && ($time + $lifetime) < time() && !empty($row['id'])) {                    
+                if (!empty($lifetime) && ($time + $lifetime) < time() && !empty($row['id'])) {   
+                    _error_log("getCache($name, $lifetime, $ignoreMetadata) line=".__LINE__);             
                     $cacheNotFound++;
                 } else if(!empty($row['content'])) {
                     $_getCacheDB[$index] = _json_decode($row['content']);
@@ -398,6 +399,7 @@ class Cache extends PluginAbstract {
                         $cacheFound++;
                         $_getCacheDB[$index] = $row['content'];
                     }else{
+                        _error_log("getCache($name, $lifetime, $ignoreMetadata) line=".__LINE__);      
                         $cacheNotFound++;
                     }
                 }
