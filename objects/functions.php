1dca1178b613210519423f6b5d461522c4428615
Daniel Neto <me@danielneto.com>
2024-04-17 11:01:29 -0300

    Update

--- objects/functions.php
+++ objects/functions.php
@@ -5330,7 +5330,8 @@ function getStatsNotifications($force_recreate = false, $listItIfIsAdminOrOwner 
         if (!empty($__getStatsNotifications__)) {
             return $__getStatsNotifications__;
         }
-        $json = $cacheHandler->getCache(LiveCacheHandler::$cacheTypeNotificationSuffix, 0);
+        $pobj = AVideoPlugin::getDataObject("Live");
+        $json = $cacheHandler->getCache(LiveCacheHandler::$cacheTypeNotificationSuffix, $pobj->cacheStatsTimout);
     }
     TimeLogEnd($timeName, __LINE__);
     if ($isLiveEnabled && (empty($json) || !empty($json->error) || !isset($json->error))) {
