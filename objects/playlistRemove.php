62852ab8fc8cd3a55bedca69bb98d21c65655ad1
Daniel Neto <me@danielneto.com>
2024-05-20 13:45:04 -0300

    Now the user can delete someone else playlist if he can manageAllPlaylists

--- objects/playlistRemove.php
+++ objects/playlistRemove.php
@@ -10,8 +10,10 @@ if (!User::isLogged()) {
     die('{"error":"'.__("Permission denied").'"}');
 }
 $obj = new PlayList($_POST['playlist_id']);
-if (User::getId() !== $obj->getUsers_id()) {
-    die('{"error":"'.__("Permission denied").'"}');
+if (!PlayLists::canManageAllPlaylists()) {
+    if (User::getId() !== $obj->getUsers_id()) {
+        die('{"error":"'.__("Permission denied").'"}');
+    }
 }
 
 echo '{"status":"'.$obj->delete().'"}';
