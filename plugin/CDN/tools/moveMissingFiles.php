1217f23f7e353d822d9b278a88b3050e2b57096e
Daniel Neto <me@danielneto.com>
2024-06-10 10:07:17 -0300

    Update

--- plugin/CDN/tools/moveMissingFiles.php
+++ plugin/CDN/tools/moveMissingFiles.php
@@ -28,6 +28,8 @@ ini_set('max_execution_time', $_2hours);
 $sort = @$argv[1];
 if(empty($sort) || strtolower($sort) !== 'asc'){
     $sort = 'DESC';
+}else if(strtolower($sort) == 'asc'){
+    $alsoMoveUnlisted = 1;
 }
 
 $sql = "SELECT * FROM  videos WHERE 1=1 ORDER BY id $sort ";
