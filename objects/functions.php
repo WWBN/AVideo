e0b2f54df4ae5a77d1fc5a9cef9fafc21517e131
Daniel Neto <me@danielneto.com>
2024-06-14 18:02:52 -0300

    Update

--- objects/functions.php
+++ objects/functions.php
@@ -3649,7 +3649,7 @@ function getPlayListCurrentVideosId($setVideos_id = true)
 {
     $playListData = getPlayListData();
     $playlist_index = getPlayListIndex();
-    if(empty($playListData) && !empty($_REQUEST['playlist_index'])){
+    if(empty($playListData) && !empty($_REQUEST['playlist_index']) && class_exists('PlayList')){
         $videosArrayId = PlayList::getVideosIdFromPlaylist($_REQUEST['playlist_index']);
         $videos_id = $videosArrayId[$playlist_index];
     }else {
