003a67db8fa83cbc144e5f7cfaef743a4abdcd30
Daniel Neto <me@danielneto.com>
2024-06-14 20:16:35 -0300

    Update

--- plugin/Cache/install/install.sql
+++ plugin/Cache/install/install.sql
@@ -20,25 +20,10 @@ CREATE TABLE IF NOT EXISTS `CachesInDB` (
   INDEX `caches2` (`ishttps` ASC),
   INDEX `caches3` (`loggedType` ASC),
   INDEX `caches4` (`user_location` ASC),
-  INDEX `caches9` (`name` ASC))
-ENGINE = InnoDB;
-
-ALTER TABLE CachesInDB ADD FULLTEXT(name);
-
--- Identify and remove duplicates
-DELETE t1 FROM `CachesInDB` t1
-INNER JOIN `CachesInDB` t2 
-WHERE 
-    t1.id < t2.id AND
-    t1.`name` = t2.`name` AND
-    t1.`domain` = t2.`domain` AND
-    t1.`ishttps` = t2.`ishttps` AND
-    t1.`user_location` = t2.`user_location` AND
-    t1.`loggedType` = t2.`loggedType`;
-
--- Add the unique index
-ALTER TABLE CachesInDB 
-ADD UNIQUE `unique_cache_index`(`name`(250), `domain`(50), `ishttps`, `user_location`(50), `loggedType`);
+  INDEX `caches9` (`name` ASC),
+  UNIQUE INDEX `unique_cache_index`(`name`(250), `domain`(50), `ishttps`, `user_location`(50), `loggedType`),
+  FULLTEXT INDEX `name_fulltext` (`name`)
+) ENGINE = InnoDB;
 
 -- Drop the cache_schedule_delete table if it exists
 DROP TABLE IF EXISTS `cache_schedule_delete`;
@@ -48,5 +33,5 @@ CREATE TABLE IF NOT EXISTS `cache_schedule_delete` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(500) NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE INDEX `name_UNIQUE` (`name` ASC))
-ENGINE = InnoDB;
+  UNIQUE INDEX `name_UNIQUE` (`name` ASC)
+) ENGINE = InnoDB;
