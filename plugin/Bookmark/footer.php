4e3e6dcdc9f1c12e7d2e9648a9aca3a74b849f8c
Daniel Neto <developer@youphptube.com>
2024-07-03 18:33:34 -0300

    Update

--- plugin/Bookmark/footer.php
+++ plugin/Bookmark/footer.php
@@ -2,4 +2,5 @@
 PlayerSkins::createMarker($rows);
 PlayerSkins::getStartPlayerJS('setTimeout(function(){adjustMarkerWidths();},1000);');
 ?>
+<link  href="<?php echo getURL('plugin/Bookmark/style.css'); ?>" rel="stylesheet" type="text/css"/>
 <script src="<?php echo getURL('plugin/Bookmark/script.js'); ?>" type="text/javascript"></script>
