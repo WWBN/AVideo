d179df24b74f05132c13219bb4108d20f909e7b0
Daniel Neto <developer@youphptube.com>
2024-07-15 10:58:03 -0300

    Update

--- plugin/CustomizeAdvanced/actionButton.php
+++ plugin/CustomizeAdvanced/actionButton.php
@@ -23,6 +23,7 @@ if($obj->allowDownloadMP3){
     $mp3File = "{$paths['path']}{$video['filename']}.mp3";
     if (file_exists($mp3File)) {
         $mp3URL = getVideosURLAudio($mp3File, true);
+        var_dump($mp3URL);
         $mp3URL['url'] = addQueryStringParameter($mp3URL['url'], 'download', 1);
         ?>
         <a href="<?php echo $mp3URL['url']; ?>" class="btn btn-default no-outline" data-toggle="tooltip" title="<?php echo __("MP3"); ?>" target="_blank">
