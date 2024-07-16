938ee4cd3b9472084a9eb19b05ed45af2b8b8bbb
Caleb Mazalevskis <maikuolan@gmail.com>
2024-07-16 13:12:09 +0800

    Add Canadian English support + update Australian English.

---
+++ locale/en_CA.php
@@ -0,0 +1,11 @@
+<?php
+// Because we're including the US English file here as a default to fall back
+// on, we should only include translations in this file here where they differ
+// from their US counterparts.
+include __DIR__ . '/en_US.php';
+
+$t['Canceled'] = 'Cancelled';
+$t['Color Legend'] = 'Colour Legend';
+$t['Colors'] = 'Colours';
+$t['Customize Your site colors'] = 'Customize Your site colours';
+$t['In authorized credentials allow the following URIs redirection'] = 'In authorized credentials allow the following URIs redirection';
