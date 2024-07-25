14abf16e4b6e92a14ecf48b8509d736279cd9c27
Daniel Neto <me@danielneto.com>
2024-07-25 17:07:36 -0300

    Update

--- install/mysqlRestore.php
+++ install/mysqlRestore.php
@@ -6,6 +6,7 @@ $doNotIncludeConfig = 1;
 require_once __DIR__.'/../videos/configuration.php';
 
 $global['mysqli'] = new mysqli($mysqlHost, $mysqlUser, $mysqlPass, '', @$mysqlPort);
+$createSQL = "DROP DATABASE {$mysqlDatabase};";
 $createSQL = "CREATE DATABASE IF NOT EXISTS {$mysqlDatabase};";
 $global['mysqli']->query($createSQL);
 $global['mysqli']->select_db($mysqlDatabase);
