a4def2d45c60b987ae369b0b53a34ee16ece08c9
Daniel Neto <me@danielneto.com>
2024-08-05 11:44:36 -0300

    Update

--- updatedb/updateDb.v14.4.sql
+++ updatedb/updateDb.v14.4.sql
@@ -3,7 +3,7 @@ ALTER TABLE `videos`
 ADD COLUMN `isChannelSuggested` INT(1) UNSIGNED NOT NULL DEFAULT 0;
 
 -- Add new index for isChannelSuggested if it does not exist
-CREATE INDEX IF NOT EXISTS `is_channel_suggested` ON `videos` (`isChannelSuggested`);
+CREATE INDEX `is_channel_suggested` ON `videos` (`isChannelSuggested`);
 
 -- Update the version in configurations table
 UPDATE configurations SET version = '14.4', modified = now() WHERE id = 1;
