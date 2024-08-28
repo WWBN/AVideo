a7e82f4b9a7e546db3b56a69c128bd4dbacd2a13
Daniel Neto <me@danielneto.com>
2024-08-28 17:15:38 -0300

    Update

--- plugin/Live/Objects/LiveTransmition.php
+++ plugin/Live/Objects/LiveTransmition.php
@@ -361,7 +361,7 @@ class LiveTransmition extends ObjectYPT {
         if (empty($this->password)) {
             $this->password = '';
         }
-        _error_log("LiveTransmition::save users_id=".User::getId().' '.json_encode(debug_backtrace()));
+        _error_log("LiveTransmition::save users_id=".User::getId().' IP='.getRealIpAddr().' '.json_encode(debug_backtrace()));
         $id = parent::save();
         //Category::clearCacheCount();
         deleteStatsNotifications(true);
