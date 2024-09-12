2a5777ca803610e1970954c1cd0c2f3a876a95d9
Daniel Neto <me@danielneto.com>
2024-09-12 10:52:55 -0300

    Update

--- plugin/VideosStatistics/history.php
+++ plugin/VideosStatistics/history.php
@@ -1,6 +1,11 @@
 <?php
 require_once '../../videos/configuration.php';
 $_page = new Page(array('History'));
+$_page->setExtraStyles(
+    array(
+        'plugin/Gallery/style.css',
+    )
+);
 include $global['systemRootPath'] . 'plugin/VideosStatistics/historyContent.php';
 $_page->print();
 ?>
