47cd021b08edf61743a4c938e01ce76629015d73
Daniel Neto <me@danielneto.com>
2024-09-13 23:38:31 -0300

    Update

--- plugin/PlayLists/actionButton.php
+++ plugin/PlayLists/actionButton.php
@@ -6,7 +6,7 @@ $global['laodPlaylistScript'] = 1;
 ?>
 <!-- <?php echo __FILE__; ?> -->
     <?php if ((empty($_POST['disableAddTo'])) && (( ($advancedCustom != false) && ($advancedCustom->disableShareAndPlaylist == false)) || ($advancedCustom == false))) { ?>
-       <a href="#" class="<?php echo $btnClass; ?>" id="addBtn<?php echo $videos_id . $crc; ?>" onclick="$(this).webuiPopover();callFunctionOrLoadScript('loadPlayLists', '<?php echo getURL('plugin/PlayLists/script.js'); ?>', '<?php echo $videos_id; ?>', '<?php echo $crc; ?>'); return false;" data-toggle="tooltip" title="<?php echo __("Add to"); ?>">
+       <a href="#" class="<?php echo $btnClass; ?>" id="addBtn<?php echo $videos_id . $crc; ?>" onclick="actionButtonClick(this, '<?php echo $videos_id; ?>', '<?php echo $crc; ?>'); return false;" data-toggle="tooltip" title="<?php echo __("Add to"); ?>">
             <span class="fa fa-plus"></span> 
             <?php echo __("Add to"); ?>
         </a>
--- plugin/PlayLists/script.js
+++ plugin/PlayLists/script.js
@@ -274,6 +274,11 @@ function addVideoToPlayList(videos_id, isChecked, playlists_id) {
     });
 }
 
+function actionButtonClick(t, videos_id, crc){
+    $(t).webuiPopover();
+    callFunctionOrLoadScript('loadPlayLists', webSiteRootURL+'plugin/PlayLists/script.js', videos_id, crc); 
+}
+
 $(function () {
     if (empty(mediaId)) {
         reloadPlayLists();
