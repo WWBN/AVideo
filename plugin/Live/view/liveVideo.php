9966c8ff2c56e0fca4fafcdd940f7d78b58b0503
Daniel Neto <me@danielneto.com>
2024-11-20 19:37:13 -0300

    Update

--- plugin/Live/view/liveVideo.php
+++ plugin/Live/view/liveVideo.php
@@ -14,7 +14,7 @@ $htmlMediaTag = '<video poster="' . $posterURL . '" controls '.PlayerSkins::getP
                 </video>';
 if(!empty($_REQUEST['debugLive'])){
     $uuid = LiveTransmition::keyNameFix($livet['key']);
-    var_dump(__LINE__, $uuid, $livet['key']);
+    var_dump(__LINE__, $uuid, $livet['key'], $_REQUEST['live_index']);
     exit;
 }
 
