52d6acc342d8799b31a1981f09e49a121ece6969
Daniel Neto <me@danielneto.com>
2025-01-02 14:03:16 -0300

    https://github.com/WWBN/AVideo/issues/9764

--- plugin/TheaterButton/actionButton.php
+++ plugin/TheaterButton/actionButton.php
@@ -1,7 +1,13 @@
-<button type="button" class="btn btn-default no-outline show-when-is-expanded" onclick="compress();" data-toggle="tooltip" data-placement="bottom"  title="<?php echo __("Compress"); ?>">
+<?php
+if(isMobile()){
+    return false;
+}
+?>
+
+<button type="button" class="btn btn-default no-outline show-when-is-expanded hidden-xs" onclick="compress();" data-toggle="tooltip" data-placement="bottom"  title="<?php echo __("Compress"); ?>">
     <i class="fas fa-compress-arrows-alt"></i> <small class="hidden-sm hidden-xs"><?php echo __("Compress"); ?></small>
 </button>
 
-<button type="button" class="btn btn-default no-outline show-when-is-compressed" onclick="expand();" data-toggle="tooltip" data-placement="bottom"  title="<?php echo __("Expand"); ?>">
+<button type="button" class="btn btn-default no-outline show-when-is-compressed hidden-xs" onclick="expand();" data-toggle="tooltip" data-placement="bottom"  title="<?php echo __("Expand"); ?>">
     <i class="fas fa-expand-arrows-alt"></i> <small class="hidden-sm hidden-xs"><?php echo __("Expand"); ?></small>
 </button>
