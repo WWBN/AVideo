6a786b58dcdefd4108cf2eb52a1beea033cc8426
Daniel Neto <me@danielneto.com>
2025-02-05 13:26:02 -0300

    Update

--- plugin/Live/authorizeKeyAccess.php
+++ plugin/Live/authorizeKeyAccess.php
@@ -97,14 +97,15 @@ if ($isCached) {
     require_once dirname(__FILE__) . '/../../videos/configuration.php';
     $obj = AVideoPlugin::getDataObjectIfEnabled('VideoHLS');
     if (class_exists('VideoHLS')) {
+        global $verifyTokenReturnFalseReason;
+        $verifyTokenReturnFalseReason = '';
         if (isAVideoUserAgent() && (empty($_SERVER['HTTP_REFERER']) || !isSameDomain($_SERVER['HTTP_REFERER'], $global['webSiteRootURL']))) {
-            error_log("HTTP_REFERER {$_SERVER['HTTP_REFERER']}, {$global['webSiteRootURL']} ".getRealIpAddr().' '.$_SERVER['HTTP_USER_AGENT']);
+            $verifyTokenReturnFalseReason = "HTTP_REFERER={$_SERVER['HTTP_REFERER']}, webSiteRootURL={$global['webSiteRootURL']} IP=".getRealIpAddr().' HTTP_USER_AGENT='.$_SERVER['HTTP_USER_AGENT'];
             $authorized = false;
         }else if (isAVideoUserAgent() || empty($obj->downloadProtection) || VideoHLS::verifyToken($token)) {
             $authorized = true;
         }
         if (!$authorized) {
-            global $verifyTokenReturnFalseReason;
             http_response_code(403);
             $msg = 'authorizeKeyAccess: Access denied ['.$verifyTokenReturnFalseReason.'] '.getRealIpAddr();
             error_log($msg.' '.@$_SERVER['HTTP_REFERER']);
