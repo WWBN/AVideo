62fa0bc2d692500f55edf2a2fc6c891141448a4e
Daniel Neto <me@danielneto.com>
2025-02-11 13:07:54 -0300

    Update

--- plugin/PlayerSkins/events/playerReady.js
+++ plugin/PlayerSkins/events/playerReady.js
@@ -1,6 +1,14 @@
 player.ready(function () {
     console.log('player.ready');
     player.on('error', () => { AvideoJSError(player.error().code); });
+
+    player.on('timeupdate', function() {
+        if (player.liveTracker && player.liveTracker.atLiveEdge()) {
+            if (player.playbackRate() !== 1) {
+                player.playbackRate(1);
+            }
+        }
+    });
     try {
         var err = this.error();
         if (err && err.code) {
@@ -14,7 +22,7 @@ player.ready(function () {
         $('.vjs-menu-item').on('click', function() {
             // Remove vjs-selected class from all chapter menu items
            $(this).parent().find('.vjs-menu-item').removeClass('vjs-selected');
-        
+
             // Add vjs-selected class back to the clicked chapter
             $(this).addClass('vjs-selected');
         });
