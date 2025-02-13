3481f6ca8d6e464cd5172b68cf92501487e3b44a
Daniel Neto <me@danielneto.com>
2025-02-13 16:50:19 -0300

    Update

---
+++ install/createAdminUser.php
@@ -0,0 +1,27 @@
+<?php
+//streamer config
+require_once '../videos/configuration.php';
+
+if (!isCommandLineInterface()) {
+    return die('Command Line only');
+}
+echo "Enter the username or press enter to skip:";
+echo "\n";
+ob_flush();
+$userName = trim(readline(""));
+echo "Enter the password:";
+echo "\n";
+ob_flush();
+$userPass = trim(readline(""));
+
+if (!empty($userName) && !empty($userPass)) {
+    $user = new User(0, $userName, $userPass);
+    $user->setIsAdmin(1);
+    $user->setEmail("{$userName}@{$userName}.com");
+    $user->setName($userName);
+    $user->setEmailVerified(1);
+    $userId = $user->save();
+}
+echo "Bye";
+echo "\n";
+die();
