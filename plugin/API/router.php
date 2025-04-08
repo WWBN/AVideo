af005fe2ede695f040ccb8a9981f9d4b801f031c
Daniel Neto <me@danielneto.com>
2025-04-08 13:14:46 -0300

    Update

--- plugin/API/router.php
+++ plugin/API/router.php
@@ -1,4 +1,6 @@
 <?php
+error_reporting(E_ALL);           // Report all types of errors
+ini_set('display_errors', '1');
 
 if (empty($_REQUEST['APISecret'])) {
     $_REQUEST['APISecret'] = getBearerToken();
