21246af3abebb86538950e90ab3940f8949b8f9c
Daniel Neto <me@danielneto.com>
2025-06-17 13:14:01 -0300

    Refactor user socket connection handling to use Object.values for improved readability and added safety checks for undefined users.

--- plugin/CustomizeUser/onUserSocketConnect.js
+++ plugin/CustomizeUser/onUserSocketConnect.js
@@ -1,8 +1,6 @@
-for (const key in response.users_id_online) {
-    if (Object.hasOwnProperty.call(response.users_id_online, key)) {
-        const element = response.users_id_online[key];
-        $('#caller'+element.users_id).show();
-        $('.users_id_'+element.users_id).addClass('online');
-    }
-}
+Object.values(response.users_id_online).forEach(element => {
+    if (!element || typeof element.users_id === 'undefined') return;
 
+    $('#caller' + element.users_id).show();
+    $('.users_id_' + element.users_id).addClass('online');
+});
