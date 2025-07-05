be5efab9af40d1ae16b9257fe546ba56e2dcb8f2
Daniel Neto <me@danielneto.com>
2025-07-05 16:20:21 -0300

    Add detailed logging for URL conversion in convertCDNHLSVideoToDownload

--- plugin/CDN/Storage/CDNStorage.php
+++ plugin/CDN/Storage/CDNStorage.php
@@ -1403,10 +1403,10 @@ class CDNStorage
             }
         }
         if ($file_exists && isURL200($url, true)) {
-            if(!preg_match('/cdnstorage.cdn.ypt.me/', $localFile)){
+            if(!preg_match('/cdnstorage.cdn.ypt.me/', $url)){
                 // if the local file is a dummy file, then use the storage url
                 if(isDummyFile($localFile)){
-                    _error_log("convertCDNHLSVideoToDownload: before url ($url)");
+                    _error_log("convertCDNHLSVideoToDownload: before url ($url) - {$localFile}");
                     $url = str_replace('.cdn.ypt.me', 'cdnstorage.cdn.ypt.me', $url);
                     $url = str_replace('/videos/', '/', $url);
                 }
