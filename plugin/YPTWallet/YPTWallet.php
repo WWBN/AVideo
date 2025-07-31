be3b379d6053a98d85898b2cfdd1252e76b1bbcd
Daniel Neto <me@danielneto.com>
2025-07-31 19:41:37 -0300

    Add missing use statement and fix variable assignment in afterDonation method

--- plugin/YPTWallet/YPTWallet.php
+++ plugin/YPTWallet/YPTWallet.php
@@ -1,5 +1,7 @@
 <?php
 
+use ___PHPSTORM_HELPERS\object;
+
 global $global;
 if (empty($global)) {
     $global = [];
@@ -1005,6 +1007,7 @@ class YPTWallet extends PluginAbstract
 
     public function afterDonation($from_users_id, $how_much, $videos_id, $users_id, $extraParameters)
     {
+        $$extraParameters = object_to_array($extraParameters);
         $donation_notification_url = self::getDonationNotificationURL($users_id);
         $webhookSecret = self::getDonationNotificationSecret($users_id); // Get user's secret
 
