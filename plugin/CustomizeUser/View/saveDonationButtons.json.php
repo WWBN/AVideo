65e15b1455347765154e950f591cae45c5ad86cc
Daniel Neto <danielneto.com@gmail.com>
2026-01-12 15:34:30 -0300

    Sanitize donation buttons input to prevent XSS vulnerabilities

--- plugin/CustomizeUser/View/saveDonationButtons.json.php
+++ plugin/CustomizeUser/View/saveDonationButtons.json.php
@@ -16,7 +16,23 @@ if (!User::isLogged()) {
     forbiddenPage('You need to be logged in');
 }
 
-$obj->saved = User::setDonationButtons($obj->users_id, @$_POST['donationButtonsList']);
+// Sanitize donation buttons to prevent XSS
+$donationButtonsList = @$_POST['donationButtonsList'];
+if (!empty($donationButtonsList) && is_array($donationButtonsList)) {
+    foreach ($donationButtonsList as $key => $button) {
+        if (isset($button['label'])) {
+            $donationButtonsList[$key]['label'] = strip_tags($button['label']);
+        }
+        if (isset($button['thankyou'])) {
+            $donationButtonsList[$key]['thankyou'] = strip_tags($button['thankyou']);
+        }
+        if (isset($button['donationFlyIcon'])) {
+            $donationButtonsList[$key]['donationFlyIcon'] = strip_tags($button['donationFlyIcon']);
+        }
+    }
+}
+
+$obj->saved = User::setDonationButtons($obj->users_id, $donationButtonsList);
 if($obj->saved){
     $obj->error = false;
 }
