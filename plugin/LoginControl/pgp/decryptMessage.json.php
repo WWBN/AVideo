6de2f2f2d75109d07f75cca7332755100d848882
Daniel Neto <me@danielneto.com>
2026-03-18 14:22:32 -0300

    fix: require authentication for decryption requests in decryptMessage.json.php

--- plugin/LoginControl/pgp/decryptMessage.json.php
+++ plugin/LoginControl/pgp/decryptMessage.json.php
@@ -1,11 +1,19 @@
 <?php
-require_once  '../../../plugin/LoginControl/pgp/functions.php';
+require_once '../../../videos/configuration.php';
+require_once '../../../plugin/LoginControl/pgp/functions.php';
 header('Content-Type: application/json');
 
 $obj = new stdClass();
 $obj->error = true;
 $obj->msg = '';
 $obj->textDecrypted = '';
+
+// Security: Require authentication before processing decryption requests
+if (!User::isLogged()) {
+    $obj->msg = 'Authentication required';
+    die(json_encode($obj));
+}
+
 $obj->textToDecrypt = @$_REQUEST['textToDecrypt'];
 $obj->privateKeyToDecryptMsg = @$_REQUEST['privateKeyToDecryptMsg'];
 $obj->keyPassword = @$_REQUEST['keyPassword'];
