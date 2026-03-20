b583acdc9a9d1eab461543caa363e1a104fb4516
Daniel Neto <me@danielneto.com>
2026-03-20 15:08:00 -0300

    fix: Add admin check to restrict access to user groups permissions data
    https://github.com/WWBN/AVideo/security/advisories/GHSA-96qp-8cmq-jvq8

--- plugin/Permissions/View/Users_groups_permissions/list.json.php
+++ plugin/Permissions/View/Users_groups_permissions/list.json.php
@@ -3,6 +3,11 @@ require_once '../../../../videos/configuration.php';
 require_once $global['systemRootPath'] . 'plugin/Permissions/Objects/Users_groups_permissions.php';
 header('Content-Type: application/json');
 
+$plugin = AVideoPlugin::loadPluginIfEnabled('Permissions');
+if (!User::isAdmin()) {
+    die(json_encode(['error' => true, 'msg' => 'You cant do this']));
+}
+
 $rows = Users_groups_permissions::getAll();
 ?>
 {"data": <?php echo json_encode($rows); ?>}
