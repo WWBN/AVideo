33ea2beec7c21bb20699a376098d62cedda25151
Daniel Neto <me@danielneto.com>
2026-03-24 10:20:12 -0300

    fix: Remove unnecessary whitespace in Comment class save method

--- objects/comment.php
+++ objects/comment.php
@@ -138,7 +138,7 @@ class Comment {
         } else {
             AVideoPlugin::afterNewResponse($this->id);
         }
-        
+
         $cacheHandler = new VideoCacheHandler($this->videos_id);
         $cacheHandler->deleteCache();
         return $id;
