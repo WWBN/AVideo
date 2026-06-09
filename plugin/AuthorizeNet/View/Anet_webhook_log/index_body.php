3b89ec61de60ad6af3f787698f14e95c188b18f5
Daniel Neto <me@danielneto.com>
2026-06-09 14:37:51 -0300

    Refactor Anet webhook log: Update table structure and enhance data rendering

--- plugin/AuthorizeNet/View/Anet_webhook_log/index_body.php
+++ plugin/AuthorizeNet/View/Anet_webhook_log/index_body.php
@@ -8,177 +8,77 @@ if (!User::isAdmin()) {
 }
 ?>
 <div class="container-fluid">
-<div class="panel panel-default">
-    <div class="panel-heading">
-        <i class="fas fa-cog"></i> <?php echo __("Configurations"); ?>
-    </div>
-    <div class="panel-body">
-        <div class="row">
-            <div class="col-sm-4">
-                <div class="panel panel-default ">
-                    <div class="panel-heading"><i class="far fa-plus-square"></i> <?php echo __("Create"); ?></div>
-                    <div class="panel-body">
-                        <form id="panelAnet_webhook_logForm">
-                            <div class="row">
-                                
-                                <div class="form-group col-sm-12">
-                                    <div class="btn-group pull-right">
-                                        <span class="btn btn-success" id="newAnet_webhook_logLink" onclick="clearAnet_webhook_logForm()"><i class="fas fa-plus"></i> <?php echo __("New"); ?></span>
-                                        <button class="btn btn-primary" type="submit"><i class="fas fa-save"></i> <?php echo __("Save"); ?></button>
-                                    </div>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-            <div class="col-sm-8">
-                <div class="panel panel-default ">
-                    <div class="panel-heading"><i class="fas fa-edit"></i> <?php echo __("Edit"); ?></div>
-                    <div class="panel-body">
-                        <table id="Anet_webhook_logTable" class="display table table-bordered table-responsive table-striped table-hover table-condensed" width="100%" cellspacing="0">
-                            <thead>
-                                <tr>
-                                    <th><?php echo __('Event Type'); ?></th>
-                                    <th><?php echo __('Trans ID'); ?></th>
-                                    <th><?php echo __('Status'); ?></th>
-                                    <th><?php echo __('Processed'); ?></th>
-                                    <th><?php echo __('Error'); ?></th>
-                                    <th><?php echo __('Created'); ?></th>
-                                    <th></th>
-                                </tr>
-                            </thead>
-                            <tfoot>
-                                <tr>
-                                    <th><?php echo __('Event Type'); ?></th>
-                                    <th><?php echo __('Trans ID'); ?></th>
-                                    <th><?php echo __('Status'); ?></th>
-                                    <th><?php echo __('Processed'); ?></th>
-                                    <th><?php echo __('Error'); ?></th>
-                                    <th><?php echo __('Created'); ?></th>
-                                    <th></th>
-                                </tr>
-                            </tfoot>
-                        </table>
-                    </div>
-                </div>
-            </div>
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            <i class="fas fa-list"></i> <?php echo __("Webhook Log"); ?>
+        </div>
+        <div class="panel-body">
+            <table id="Anet_webhook_logTable" class="display table table-bordered table-responsive table-striped table-hover table-condensed" width="100%" cellspacing="0">
+                <thead>
+                    <tr>
+                        <th><?php echo __('Event Type'); ?></th>
+                        <th><?php echo __('Trans ID'); ?></th>
+                        <th><?php echo __('Amount'); ?></th>
+                        <th><?php echo __('Auth Code'); ?></th>
+                        <th><?php echo __('Processed'); ?></th>
+                        <th><?php echo __('Error'); ?></th>
+                        <th><?php echo __('Created'); ?></th>
+                    </tr>
+                </thead>
+                <tfoot>
+                    <tr>
+                        <th><?php echo __('Event Type'); ?></th>
+                        <th><?php echo __('Trans ID'); ?></th>
+                        <th><?php echo __('Amount'); ?></th>
+                        <th><?php echo __('Auth Code'); ?></th>
+                        <th><?php echo __('Processed'); ?></th>
+                        <th><?php echo __('Error'); ?></th>
+                        <th><?php echo __('Created'); ?></th>
+                    </tr>
+                </tfoot>
+            </table>
         </div>
     </div>
 </div>
-<div id="Anet_webhook_logbtnModelLinks" style="display: none;">
-    <div class="btn-group pull-right">
-        <button href="" class="edit_Anet_webhook_log btn btn-default btn-xs">
-            <i class="fa fa-edit"></i>
-        </button>
-        <button href="" class="delete_Anet_webhook_log btn btn-danger btn-xs">
-            <i class="fa fa-trash"></i>
-        </button>
-    </div>
-</div>
-</div>
 <script type="text/javascript">
-    function clearAnet_webhook_logForm() {
-    
-    }
     $(document).ready(function () {
-    $('#addAnet_webhook_logBtn').click(function () {
-        $.ajax({
-            url: webSiteRootURL+'plugin/AuthorizeNet/View/addAnet_webhook_logVideo.php',
-            data: $('#panelAnet_webhook_logForm').serialize(),
-            type: 'post',
-            success: function (response) {
-                if (response.error) {
-                    avideoAlertError(response.msg);
-                } else {
-                    avideoToast("<?php echo __("Your register has been saved!"); ?>");
-                    $("#panelAnet_webhook_logForm").trigger("reset");
+        $('#Anet_webhook_logTable').DataTable({
+            serverSide: true,
+            ajax: webSiteRootURL + "plugin/AuthorizeNet/View/Anet_webhook_log/list.json.php",
+            order: [[6, 'desc']],
+            columns: [
+                { data: 'event_type' },
+                {
+                    data: 'payload_json',
+                    render: function (data) {
+                        try { var p = JSON.parse(data); return p.id || ''; } catch(e) { return ''; }
+                    }
+                },
+                {
+                    data: 'payload_json',
+                    render: function (data) {
+                        try { var p = JSON.parse(data); return p.authAmount != null ? '$' + p.authAmount : ''; } catch(e) { return ''; }
+                    }
+                },
+                {
+                    data: 'payload_json',
+                    render: function (data) {
+                        try { var p = JSON.parse(data); return p.authCode || ''; } catch(e) { return ''; }
+                    }
+                },
+                {
+                    data: 'processed',
+                    render: function (data) {
+                        return data ? '<span class="label label-success"><?php echo __("Yes"); ?></span>'
+                                    : '<span class="label label-default"><?php echo __("No"); ?></span>';
+                    }
+                },
+                { data: 'error_text', defaultContent: '' },
+                {
+                    data: 'created_php_time',
+                    render: function (data) { return data ? new Date(data * 1000).toLocaleString() : ''; }
                 }
-                clearAnet_webhook_logForm();
-                tableVideos.ajax.reload();
-                modal.hidePleaseWait();
-            }
+            ]
         });
-    });
-    var Anet_webhook_logtableVar = $('#Anet_webhook_logTable').DataTable({
-        serverSide: true,
-        "ajax": webSiteRootURL+"plugin/AuthorizeNet/View/Anet_webhook_log/list.json.php",
-        "columns": [
-        { data: 'event_type' },
-        { data: 'trans_id' },
-        { data: 'status' },
-        { data: 'processed' },
-        { data: 'error_text' },
-        { data: 'created_php_time', render: function(data) { return data ? new Date(data * 1000).toLocaleString() : ''; } },
-        {
-            sortable: false,
-            data: null,
-            defaultContent: $('#Anet_webhook_logbtnModelLinks').html()
-        }
-        ],
-        select: true,
-    });
-    $('#newAnet_webhook_log').on('click', function (e) {
-    e.preventDefault();
-    $('#panelAnet_webhook_logForm').trigger("reset");
-    $('#Anet_webhook_logid').val('');
-    });
-    $('#panelAnet_webhook_logForm').on('submit', function (e) {
-        e.preventDefault();
-        modal.showPleaseWait();
-        $.ajax({
-            url: webSiteRootURL+'plugin/AuthorizeNet/View/Anet_webhook_log/add.json.php',
-            data: $('#panelAnet_webhook_logForm').serialize(),
-            type: 'post',
-            success: function (response) {
-                if (response.error) {
-                    avideoAlertError(response.msg);
-                } else {
-                    avideoToast(__("Your register has been saved!"));
-                    $("#panelAnet_webhook_logForm").trigger("reset");
-                }
-                Anet_webhook_logtableVar.ajax.reload();
-                $('#Anet_webhook_logid').val('');
-                modal.hidePleaseWait();
-            }
-        });
-    });
-    $('#Anet_webhook_logTable').on('click', 'button.delete_Anet_webhook_log', function (e) {
-    e.preventDefault();
-    var tr = $(this).closest('tr')[0];
-    var data = Anet_webhook_logtableVar.row(tr).data();
-    swal({
-    title: __("Are you sure?"),
-            text: __("You will not be able to recover this action!"),
-            icon: "warning",
-            buttons: true,
-            dangerMode: true,
-    })
-            .then(function (willDelete) {
-            if (willDelete) {
-            modal.showPleaseWait();
-            $.ajax({
-            type: "POST",
-                    url: webSiteRootURL+"plugin/AuthorizeNet/View/Anet_webhook_log/delete.json.php",
-                    data: data
-
-            }).done(function (resposta) {
-            if (resposta.error) {
-                avideoAlertError(resposta.msg);
-            }
-            Anet_webhook_logtableVar.ajax.reload();
-            modal.hidePleaseWait();
-            });
-            } else {
-
-            }
-            });
-    });
-    $('#Anet_webhook_logTable').on('click', 'button.edit_Anet_webhook_log', function (e) {
-    e.preventDefault();
-    var tr = $(this).closest('tr')[0];
-    var data = Anet_webhook_logtableVar.row(tr).data();
-    
-    });
     });
 </script>
