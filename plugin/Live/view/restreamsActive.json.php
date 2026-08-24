1fa99878baca26d7e6547ea73c79b9295518f37e
Daniel Neto <me@danielneto.com>
2026-08-24 12:29:41 -0300

    Fix: Refactor user ID handling in restreamsActive.json.php for improved access control
    https://github.com/WWBN/AVideo/security/advisories/GHSA-qh45-c3p8-jh4g

--- plugin/Live/view/restreamsActive.json.php
+++ plugin/Live/view/restreamsActive.json.php
@@ -16,27 +16,35 @@ if (!User::canStream() && !User::isAdmin()) {
     $obj->msg = __('Cannot stream');
     die(json_encode($obj));
 }
+$users_id = User::getId();
 
 $obj = getRestreamsRuning();
 
 $totalConnections = array();
 foreach ($obj->process as $key => $value) {
-    $users_id = LiveTransmition::getUsers_idOrCompanyFromKey($value['key']);
+    $processUsersId = LiveTransmition::getUsers_idOrCompanyFromKey($value['key']);
+
+    // Non-admins may only see their own restreams, not every streamer's source key/identity.
+    if (!User::isAdmin() && intval($processUsersId) !== intval($users_id)) {
+        unset($obj->process[$key]);
+        continue;
+    }
 
     $lt = new Live_restreams($value['live_restreams_id']);
     $lth = new LiveTransmitionHistory($value['liveTransmitionHistory_id']);
 
     $obj->process[$key]['restream_name'] = $lt->getName();
-    $obj->process[$key]['live_title'] = $lth->getTitle();  
-    $obj->process[$key]['identification'] = User::getNameIdentificationById($users_id);    
+    $obj->process[$key]['live_title'] = $lth->getTitle();
+    $obj->process[$key]['identification'] = User::getNameIdentificationById($processUsersId);
 
-    $obj->process[$key]['users_id'] = $users_id;
+    $obj->process[$key]['users_id'] = $processUsersId;
     // Count total connections per users_id
-    if (!isset($totalConnections[$users_id])) {
-        $totalConnections[$users_id] = 0;
+    if (!isset($totalConnections[$processUsersId])) {
+        $totalConnections[$processUsersId] = 0;
     }
-    $totalConnections[$users_id]++;
+    $totalConnections[$processUsersId]++;
 }
+$obj->process = array_values($obj->process);
 $obj->totalConnections = $totalConnections;
 //$obj->isRestreamRuning = isRestreamRuning(2, 2);
 
