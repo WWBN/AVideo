0b0844ef2feb47488f9e65bf327545eba6469e86
Daniel Neto <me@danielneto.com>
2026-09-18 11:28:01 -0300

    Update trailer button

--- plugin/YouPHPFlix2/view/row_info.php
+++ plugin/YouPHPFlix2/view/row_info.php
@@ -160,7 +160,7 @@ TimeLogStart($timeLog6);
     }
     if (!empty($value['trailer1'])) {
         ?>
-        <a href="#" class="btn btn-warning" onclick="flixFullScreen('<?php echo parseVideos($value['trailer1'], 1, 0, 0, 0, 1); ?>', '');return false;">
+        <a href="#" class="btn btn-default" onclick="flixFullScreen('<?php echo parseVideos($value['trailer1'], 1, 0, 0, 0, 1); ?>', '');return false;">
             <span class="fa fa-film"></span>
             <span><?php echo __("Trailer"); ?></span>
         </a>
